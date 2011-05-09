860e6b3b589de34d16d7f6d8df6fdfb147cd6e65
Seth Tisue <seth@tisue.net>
2011-05-09 09:49:05 -0400

    further refactor gl.render.Builtins internally
    using Scala features to enforce block structure

--- src/main/org/nlogo/gl/render/Builtins.scala
+++ src/main/org/nlogo/gl/render/Builtins.scala
@@ -61,37 +61,37 @@ private class Builtins(gl: GL, glu: GLU, quadric: GLUquadric) {
   }
 
   private def default() {
-    glBegin(GL_TRIANGLES)
-    glNormal3f(0f, 0f, -1f)
-    glVertex3f(-0.10f, -0.125f, 0f)
-    glVertex3f(0f, 0.125f, 0f)
-    glVertex3f(0f, -0.075f, 0f)
+    begin(GL_TRIANGLES) {
+      glNormal3f(0f, 0f, -1f)
+      glVertex3f(-0.10f, -0.125f, 0f)
+      glVertex3f(0f, 0.125f, 0f)
+      glVertex3f(0f, -0.075f, 0f)
 
-    glNormal3f(0f, 0f, -1f)
-    glVertex3f(0f, -0.075f, 0f)
-    glVertex3f(0f, 0.125f, 0f)
-    glVertex3f(0.10f, -0.125f, 0f)
+      glNormal3f(0f, 0f, -1f)
+      glVertex3f(0f, -0.075f, 0f)
+      glVertex3f(0f, 0.125f, 0f)
+      glVertex3f(0.10f, -0.125f, 0f)
 
-    glNormal3f(-0.707f, 0f, 0.707f)
-    glVertex3f(0f, 0.125f, 0f)
-    glVertex3f(-0.10f, -0.125f, 0f)
-    glVertex3f(0f, -0.075f, 0.08f)
+      glNormal3f(-0.707f, 0f, 0.707f)
+      glVertex3f(0f, 0.125f, 0f)
+      glVertex3f(-0.10f, -0.125f, 0f)
+      glVertex3f(0f, -0.075f, 0.08f)
 
-    glNormal3f(0.707f, 0f, 0.707f)
-    glVertex3f(0f, 0.125f, 0f)
-    glVertex3f(0f, -0.075f, 0.08f)
-    glVertex3f(0.10f, -0.125f, 0f)
+      glNormal3f(0.707f, 0f, 0.707f)
+      glVertex3f(0f, 0.125f, 0f)
+      glVertex3f(0f, -0.075f, 0.08f)
+      glVertex3f(0.10f, -0.125f, 0f)
 
-    glNormal3f(-0.6f, -0.8f, 0f)
-    glVertex3f(0f, -0.075f, 0.08f)
-    glVertex3f(-0.10f, -0.125f, 0f)
-    glVertex3f(0f, -0.075f, 0f)
+      glNormal3f(-0.6f, -0.8f, 0f)
+      glVertex3f(0f, -0.075f, 0.08f)
+      glVertex3f(-0.10f, -0.125f, 0f)
+      glVertex3f(0f, -0.075f, 0f)
 
-    glNormal3f(-0.6f, 0.8f, 0f)
-    glVertex3f(0f, -0.075f, 0.08f)
-    glVertex3f(0f, -0.075f, 0f)
-    glVertex3f(0.10f, -0.125f, 0f)
-    glEnd()
+      glNormal3f(-0.6f, 0.8f, 0f)
+      glVertex3f(0f, -0.075f, 0.08f)
+      glVertex3f(0f, -0.075f, 0f)
+      glVertex3f(0.10f, -0.125f, 0f)
+    }
   }
 
   private def car() {
@@ -99,205 +99,196 @@ private class Builtins(gl: GL, glu: GLU, quadric: GLUquadric) {
     glRotatef(90f, 1f, 0f, 0f)
     glRotatef(90f, 0f, 1f, 0f)
     glScalef(0.5f, 0.5f, 0.5f)
-    glBegin(GL_QUADS)
+    begin(GL_QUADS) {
 
-    // Left (or is it right?) Face
-    glNormal3f(0f, 0f, 1f)
-    glVertex3f(.14f * -2f, .14f * -0.5f, .14f * 1f)
-    glVertex3f(.14f * 2f, .14f * -0.5f, .14f * 1f)
-    glVertex3f(.14f * 2f, .14f * 0.5f, .14f * 1f)
-    glVertex3f(.14f * -2f, .14f * 0.5f, .14f * 1f)
-    // Right (or is it left?) Face
-    glNormal3f(0f, 0f, -1f)
-    glVertex3f(.14f * -2f, .14f * -0.5f, .14f * -1f)
-    glVertex3f(.14f * -2f, .14f * 0.5f, .14f * -1f)
-    glVertex3f(.14f * 2f, .14f * 0.5f, .14f * -1f)
-    glVertex3f(.14f * 2f, .14f * -0.5f, .14f * -1f)
-    // Top Face
-    glNormal3f(0f, 1f, 0f)
-    glVertex3f(.14f * -2f, .14f * 0.5f, .14f * -1f)
-    glVertex3f(.14f * -2f, .14f * 0.5f, .14f * 1f)
-    glVertex3f(.14f * 2f, .14f * 0.5f, .14f * 1f)
-    glVertex3f(.14f * 2f, .14f * 0.5f, .14f * -1f)
-    // Bottom Face
-    glNormal3f(0f, -1f, 0f)
-    glVertex3f(.14f * -2f, .14f * -0.5f, .14f * -1f)
-    glVertex3f(.14f * 2f, .14f * -0.5f, .14f * -1f)
-    glVertex3f(.14f * 2f, .14f * -0.5f, .14f * 1f)
-    glVertex3f(.14f * -2f, .14f * -0.5f, .14f * 1f)
-    // From face
-    glNormal3f(1f, 0f, 0f)
-    glVertex3f(.14f * 2f, .14f * -0.5f, .14f * -1f)
-    glVertex3f(.14f * 2f, .14f * 0.5f, .14f * -1f)
-    glVertex3f(.14f * 2f, .14f * 0.5f, .14f * 1f)
-    glVertex3f(.14f * 2f, .14f * -0.5f, .14f * 1f)
-    // Back Face
-    glNormal3f(-1f, 0f, 0f)
-    glVertex3f(.14f * -2f, .14f * -0.5f, .14f * -1f)
-    glVertex3f(.14f * -2f, .14f * -0.5f, .14f * 1f)
-    glVertex3f(.14f * -2f, .14f * 1f, .14f * 1f)
-    glVertex3f(.14f * -2f, .14f * 1f, .14f * -1f)
+      // Left (or is it right?) Face
+      glNormal3f(0f, 0f, 1f)
+      glVertex3f(.14f * -2f, .14f * -0.5f, .14f * 1f)
+      glVertex3f(.14f * 2f, .14f * -0.5f, .14f * 1f)
+      glVertex3f(.14f * 2f, .14f * 0.5f, .14f * 1f)
+      glVertex3f(.14f * -2f, .14f * 0.5f, .14f * 1f)
+      // Right (or is it left?) Face
+      glNormal3f(0f, 0f, -1f)
+      glVertex3f(.14f * -2f, .14f * -0.5f, .14f * -1f)
+      glVertex3f(.14f * -2f, .14f * 0.5f, .14f * -1f)
+      glVertex3f(.14f * 2f, .14f * 0.5f, .14f * -1f)
+      glVertex3f(.14f * 2f, .14f * -0.5f, .14f * -1f)
+      // Top Face
+      glNormal3f(0f, 1f, 0f)
+      glVertex3f(.14f * -2f, .14f * 0.5f, .14f * -1f)
+      glVertex3f(.14f * -2f, .14f * 0.5f, .14f * 1f)
+      glVertex3f(.14f * 2f, .14f * 0.5f, .14f * 1f)
+      glVertex3f(.14f * 2f, .14f * 0.5f, .14f * -1f)
+      // Bottom Face
+      glNormal3f(0f, -1f, 0f)
+      glVertex3f(.14f * -2f, .14f * -0.5f, .14f * -1f)
+      glVertex3f(.14f * 2f, .14f * -0.5f, .14f * -1f)
+      glVertex3f(.14f * 2f, .14f * -0.5f, .14f * 1f)
+      glVertex3f(.14f * -2f, .14f * -0.5f, .14f * 1f)
+      // From face
+      glNormal3f(1f, 0f, 0f)
+      glVertex3f(.14f * 2f, .14f * -0.5f, .14f * -1f)
+      glVertex3f(.14f * 2f, .14f * 0.5f, .14f * -1f)
+      glVertex3f(.14f * 2f, .14f * 0.5f, .14f * 1f)
+      glVertex3f(.14f * 2f, .14f * -0.5f, .14f * 1f)
+      // Back Face
+      glNormal3f(-1f, 0f, 0f)
+      glVertex3f(.14f * -2f, .14f * -0.5f, .14f * -1f)
+      glVertex3f(.14f * -2f, .14f * -0.5f, .14f * 1f)
+      glVertex3f(.14f * -2f, .14f * 1f, .14f * 1f)
+      glVertex3f(.14f * -2f, .14f * 1f, .14f * -1f)
 
-    // Cabin
-    // Left Face
-    glNormal3f(0f, 0f, 1f)
-    glVertex3f(.14f * -2f, .14f * 0.5f, .14f * 1f)
-    glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * 1f)
-    glVertex3f(.14f * 0.5f, .14f * 1f, .14f * 1f)
-    glVertex3f(.14f * -2f, .14f * 1f, .14f * 1f)
+      // Cabin
+      // Left Face
+      glNormal3f(0f, 0f, 1f)
+      glVertex3f(.14f * -2f, .14f * 0.5f, .14f * 1f)
+      glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * 1f)
+      glVertex3f(.14f * 0.5f, .14f * 1f, .14f * 1f)
+      glVertex3f(.14f * -2f, .14f * 1f, .14f * 1f)
 
-    // Right Face
-    glNormal3f(0f, 0f, -1f)
-    glVertex3f(.14f * -2f, .14f * 0.5f, .14f * -1f)
-    glVertex3f(.14f * -2f, .14f * 1f, .14f * -1f)
-    glVertex3f(.14f * 0.5f, .14f * 1f, .14f * -1f)
-    glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * -1f)
+      // Right Face
+      glNormal3f(0f, 0f, -1f)
+      glVertex3f(.14f * -2f, .14f * 0.5f, .14f * -1f)
+      glVertex3f(.14f * -2f, .14f * 1f, .14f * -1f)
+      glVertex3f(.14f * 0.5f, .14f * 1f, .14f * -1f)
+      glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * -1f)
 
-    // Top Face
-    glNormal3f(0f, 1f, 0f)
-    glVertex3f(.14f * -2f, .14f * 1f, .14f * -1f)
-    glVertex3f(.14f * -2f, .14f * 1f, .14f * 1f)
-    glVertex3f(.14f * 0.5f, .14f * 1f, .14f * 1f)
-    glVertex3f(.14f * 0.5f, .14f * 1f, .14f * -1f)
+      // Top Face
+      glNormal3f(0f, 1f, 0f)
+      glVertex3f(.14f * -2f, .14f * 1f, .14f * -1f)
+      glVertex3f(.14f * -2f, .14f * 1f, .14f * 1f)
+      glVertex3f(.14f * 0.5f, .14f * 1f, .14f * 1f)
+      glVertex3f(.14f * 0.5f, .14f * 1f, .14f * -1f)
 
-    // Front face
-    glNormal3f(1f, 0f, 0f)
-    glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * -1f)
-    glVertex3f(.14f * 0.5f, .14f * 1f, .14f * -1f)
-    glVertex3f(.14f * 0.5f, .14f * 1f, .14f * 1f)
-    glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * 1f)
-
-    glEnd() // quads
+      // Front face
+      glNormal3f(1f, 0f, 0f)
+      glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * -1f)
+      glVertex3f(.14f * 0.5f, .14f * 1f, .14f * -1f)
+      glVertex3f(.14f * 0.5f, .14f * 1f, .14f * 1f)
+      glVertex3f(.14f * 0.5f, .14f * 0.5f, .14f * 1f)
+    }
 
     // wheels
     val p = gluNewQuadric()
-    glPushMatrix()
-    glTranslatef(0.14f * 1.25f,
-      0.5f * -0.14f, // up and down
-      0.14f * -1.01f)
-    //glColor3fv(car_axle)
-    glColor3f(0.1f, 0.1f, 0.1f)
-    glRotatef(180f, 1f, 0f, 0f)
+    matrix {
+      glTranslatef(0.14f * 1.25f,
+        0.5f * -0.14f, // up and down
+        0.14f * -1.01f)
+      //glColor3fv(car_axle)
+      glColor3f(0.1f, 0.1f, 0.1f)
+      glRotatef(180f, 1f, 0f, 0f)
 
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
 
-    glRotatef(180f, 1f, 0f, 0f)
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
-    glRotatef(-180f, 1f, 0f, 0f)
+      glRotatef(180f, 1f, 0f, 0f)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      glRotatef(-180f, 1f, 0f, 0f)
 
-    glTranslatef(0f, 0f, 0.14f * 2f * -1.01f)
-    glRotatef(180f, 1f, 0f, 0f)
-    glColor3f(0.1f, 0.1f, 0.1f)
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      glTranslatef(0f, 0f, 0.14f * 2f * -1.01f)
+      glRotatef(180f, 1f, 0f, 0f)
+      glColor3f(0.1f, 0.1f, 0.1f)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
 
-    glRotatef(180f, 1f, 0f, 0f)
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
-    glRotatef(-180f, 1f, 0f, 0f)
-    glPopMatrix()
+      glRotatef(180f, 1f, 0f, 0f)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      glRotatef(-180f, 1f, 0f, 0f)
+    }
 
-    glPushMatrix()
-    glTranslatef(
-      -0.14f * 1.25f,
-      0.5f * -0.14f, // up and down
-      0.14f * -1.01f)
-    glColor3f(0.1f, 0.1f, 0.1f)
-    glRotatef(180f, 1f, 0f, 0f)
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
-    glRotatef(180f, 1f, 0f, 0f)
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
-    glRotatef(-180f, 1f, 0f, 0f)
+    matrix {
+      glTranslatef(
+        -0.14f * 1.25f,
+        0.5f * -0.14f, // up and down
+        0.14f * -1.01f)
+      glColor3f(0.1f, 0.1f, 0.1f)
+      glRotatef(180f, 1f, 0f, 0f)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      glRotatef(180f, 1f, 0f, 0f)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      glRotatef(-180f, 1f, 0f, 0f)
 
-    glTranslatef(0f, 0f, 0.14f * 2f * -1.01f)
-    glRotatef(180f, 1f, 0f, 0f)
-    glColor3f(0.1f, 0.1f, 0.1f)
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
-    glRotatef(180f, 1f, 0f, 0f)
-    gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
-    glRotatef(-180f, 1f, 0f, 0f)
-
-    glPopMatrix()
+      glTranslatef(0f, 0f, 0.14f * 2f * -1.01f)
+      glRotatef(180f, 1f, 0f, 0f)
+      glColor3f(0.1f, 0.1f, 0.1f)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      glRotatef(180f, 1f, 0f, 0f)
+      gluDisk(p, 0.0, 0.14 * 0.4, 16, 1)
+      glRotatef(-180f, 1f, 0f, 0f)
+    }
   }
 
   private def plane() {
-    glBegin(GL_QUADS)
-    glNormal3f(0f, 0f, 1f)
-    glVertex3f(.14f, .14f, -.14f)
-    glVertex3f(-.14f, .14f, -.14f)
-    glVertex3f(-.14f, -.14f, -.14f)
-    glVertex3f(.14f, -.14f, -.14f)
+    begin(GL_QUADS) {
+      glNormal3f(0f, 0f, 1f)
+      glVertex3f(.14f, .14f, -.14f)
+      glVertex3f(-.14f, .14f, -.14f)
+      glVertex3f(-.14f, -.14f, -.14f)
+      glVertex3f(.14f, -.14f, -.14f)
 
-    glNormal3f(0f, 0f, -1f)
-    glVertex3f(.14f, -.14f, -.14f)
-    glVertex3f(-.14f, -.14f, -.14f)
-    glVertex3f(-.14f, .14f, -.14f)
-    glVertex3f(.14f, .14f, -.14f)
-    glEnd()
+      glNormal3f(0f, 0f, -1f)
+      glVertex3f(.14f, -.14f, -.14f)
+      glVertex3f(-.14f, -.14f, -.14f)
+      glVertex3f(-.14f, .14f, -.14f)
+      glVertex3f(.14f, .14f, -.14f)
+    }
   }
 
   private def crosshairs() {
-    glPushAttrib(GL_ENABLE_BIT)
-    glPushAttrib(GL_CURRENT_BIT)
-    glDisable(GL_LIGHTING)
-    glDisable(GL_CULL_FACE)
+    attrib(GL_ENABLE_BIT, GL_CURRENT_BIT) {
+      glDisable(GL_LIGHTING)
+      glDisable(GL_CULL_FACE)
 
-    glColor3f(0f, 0f, 1f)
+      glColor3f(0f, 0f, 1f)
+      gluDisk(quadric, 0.120f, 0.135f, SMOOTHNESS, 1)
 
-    gluDisk(quadric, 0.120f, 0.135f, SMOOTHNESS, 1)
+      begin(GL_QUADS) {
+        glVertex3f(-0.03f, 0.12f, 0f)
+        glVertex3f(-0.03f, -0.12f, 0f)
+        glVertex3f(0.03f, -0.12f, 0f)
+        glVertex3f(0.03f, 0.12f, 0f)
 
-    glBegin(GL_QUADS)
-    glVertex3f(-0.03f, 0.12f, 0f)
-    glVertex3f(-0.03f, -0.12f, 0f)
-    glVertex3f(0.03f, -0.12f, 0f)
-    glVertex3f(0.03f, 0.12f, 0f)
+        glVertex3f(-0.12f, 0.03f, 0f)
+        glVertex3f(-0.12f, -0.03f, 0f)
+        glVertex3f(0.12f, -0.03f, 0f)
+        glVertex3f(0.12f, 0.03f, 0f)
+      }
 
-    glVertex3f(-0.12f, 0.03f, 0f)
-    glVertex3f(-0.12f, -0.03f, 0f)
-    glVertex3f(0.12f, -0.03f, 0f)
-    glVertex3f(0.12f, 0.03f, 0f)
-    glEnd()
-
-    glBegin(GL_TRIANGLES)
-    glVertex3f(-0.03f, 0.140f, 0f)
-    glVertex3f(0.03f, 0.140f, 0f)
-    glVertex3f(0.00f, 0.185f, 0f)
-    glEnd()
-
-    glPopAttrib()
-    glPopAttrib()
+      begin(GL_TRIANGLES) {
+        glVertex3f(-0.03f, 0.140f, 0f)
+        glVertex3f(0.03f, 0.140f, 0f)
+        glVertex3f(0.00f, 0.185f, 0f)
+      }
+    }
   }
 
   private def halo() {
-    glPushAttrib(GL_ENABLE_BIT)
-    glPushAttrib(GL_CURRENT_BIT)
+    attrib(GL_ENABLE_BIT, GL_CURRENT_BIT) {
 
-    glDisable(GL_LIGHTING)
-    glDisable(GL_CULL_FACE)
+      glDisable(GL_LIGHTING)
+      glDisable(GL_CULL_FACE)
 
-    // inner-circle (used by stencil buffer)
-    glColorMask(false, false, false, false)
-    // use SMOOTHNESS * 2 for halo polygons because it looks better 
-    // and since there's only 1 halo, doesn't cost much - AZS 7/7/05
-    gluDisk(quadric, 0f, 0.255f, SMOOTHNESS * 2, 1)
-    glColorMask(true, true, true, true)
+      // inner-circle (used by stencil buffer)
+      glColorMask(false, false, false, false)
+      // use SMOOTHNESS * 2 for halo polygons because it looks better 
+      // and since there's only 1 halo, doesn't cost much - AZS 7/7/05
+      gluDisk(quadric, 0f, 0.255f, SMOOTHNESS * 2, 1)
+      glColorMask(true, true, true, true)
 
-    glEnable(GL_BLEND)
+      glEnable(GL_BLEND)
 
-    // inner-ring
-    glColor4f(0.784f, 1f, 1f, 0.392f)
-    gluDisk(quadric, 0.255f, 0.275f, SMOOTHNESS * 2, 1)
+      // inner-ring
+      glColor4f(0.784f, 1f, 1f, 0.392f)
+      gluDisk(quadric, 0.255f, 0.275f, SMOOTHNESS * 2, 1)
 
-    // middle-ring
-    glColor4f(0.784f, 1f, 1f, 0.196f)
-    gluDisk(quadric, 0.275f, 0.285f, SMOOTHNESS * 2, 1)
+      // middle-ring
+      glColor4f(0.784f, 1f, 1f, 0.196f)
+      gluDisk(quadric, 0.275f, 0.285f, SMOOTHNESS * 2, 1)
 
-    // dark outermost-ring
-    glColor4f(0f, 0f, 0f, 0.196f)
-    gluDisk(quadric, 0.285f, 0.295f, SMOOTHNESS * 2, 1)
+      // dark outermost-ring
+      glColor4f(0f, 0f, 0f, 0.196f)
+      gluDisk(quadric, 0.285f, 0.295f, SMOOTHNESS * 2, 1)
 
-    glDisable(GL_BLEND)
-
-    glPopAttrib()
-    glPopAttrib()
+      glDisable(GL_BLEND)
+    }
   }
 
   private def stoplight() {
@@ -328,6 +319,23 @@ private class Builtins(gl: GL, glu: GLU, quadric: GLUquadric) {
     glRotatef(180f, 1f, 0f, 0f)
     gluDisk(quadric, 0f, Renderer.WORLD_SCALE / 2,
       SMOOTHNESS, 1)
+  }
+
+  // OpenGL helpers
+  private def begin(n: Int)(body: => Unit) {
+    glBegin(n)
+    body
+    glEnd()
+  }
+  private def attrib(ns: Int*)(body: => Unit) {
+    ns.foreach(glPushAttrib)
+    body
+    ns.foreach(_ => glPopAttrib())
+  }
+  private def matrix(body: => Unit) {
+    glPushMatrix()
+    body
+    glPopMatrix()
   }
 
 }
