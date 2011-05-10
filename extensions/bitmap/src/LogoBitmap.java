6d7a41c1269847bafa91f6f36ee1e4b6f8104c39
Josh Cough <joshcough@gmail.com>
2011-05-10 11:53:06 -0500

    Reformatted Java code using IntelliJ.

--- extensions/bitmap/src/LogoBitmap.java
+++ extensions/bitmap/src/LogoBitmap.java
@@ -1,37 +1,31 @@
 package org.nlogo.extensions.bitmap;
 
-import java.awt.image.* ;
+import java.awt.image.*;
 
 public class LogoBitmap
-	extends BufferedImage
-	implements org.nlogo.api.ExtensionObject
-{
-	public LogoBitmap( BufferedImage image )
-	{
-		super( image.getColorModel() , image.getRaster() , image.isAlphaPremultiplied() , null ) ;
-	}
+    extends BufferedImage
+    implements org.nlogo.api.ExtensionObject {
+  public LogoBitmap(BufferedImage image) {
+    super(image.getColorModel(), image.getRaster(), image.isAlphaPremultiplied(), null);
+  }
 
-	public String dump( boolean readable , boolean exporting , boolean reference )
-	{
-		// we need to do something more complicated for exporting, obviously
-		return getWidth() + "x" + getHeight() ;
-	} 
+  public String dump(boolean readable, boolean exporting, boolean reference) {
+    // we need to do something more complicated for exporting, obviously
+    return getWidth() + "x" + getHeight();
+  }
 
-	public String getExtensionName()
-	{
-		return "bitmap" ;
-	}
+  public String getExtensionName() {
+    return "bitmap";
+  }
 
-	public String getNLTypeName()
-	{
-		return "" ;
-	}
+  public String getNLTypeName() {
+    return "";
+  }
 
-	public boolean recursivelyEqual( Object o )
-	{
-		// I guess we could do something more complicated like
-		// comparing the data, but why?
-		return o == this ;
-	}
+  public boolean recursivelyEqual(Object o) {
+    // I guess we could do something more complicated like
+    // comparing the data, but why?
+    return o == this;
+  }
 }
 
