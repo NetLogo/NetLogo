153e6db1178524e92fc8ea57f040c662eca2cc01
Seth Tisue <seth@tisue.net>
2011-05-10 10:57:34 -0400

    convert more prim.gui stuff to Scala

--- src/main/org/nlogo/prim/gui/_deletelogfiles.scala
+++ src/main/org/nlogo/prim/gui/_deletelogfiles.scala
@@ -1,25 +1,12 @@
-package org.nlogo.prim.gui ;
+package org.nlogo.prim.gui
 
-import org.nlogo.nvm.Command;
-import org.nlogo.nvm.Context;
-import org.nlogo.nvm.Syntax;
+import org.nlogo.nvm.{ Command, Context, Syntax }
 
-public final strictfp class _deletelogfiles
-	extends Command
-{
-	@Override
-	public Syntax syntax()
-	{
-		return Syntax.commandSyntax( "O---", true ) ;
-	}
-	@Override
-	public void perform( final Context context )
-	{
-		perform_1( context ) ; 
-	}
-	public void perform_1( final Context context )
-	{
-		workspace.deleteLogFiles() ;
-		context.ip = next ;
-	}
+class _deletelogfiles extends Command {
+  override def syntax =
+    Syntax.commandSyntax("O---", true)
+  override def perform(context: Context) {
+    workspace.deleteLogFiles()
+    context.ip = next
+  }
 }
