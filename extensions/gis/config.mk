62371f70c791e8c96cd447053c4c0773e9aa8560
Seth Tisue <seth@tisue.net>
2011-05-11 16:59:01 -0400

    gis extension build: use better flags when calling curl
    -s to avoid annoying progress, -o for better error handling than >

--- extensions/gis/config.mk
+++ extensions/gis/config.mk
@@ -3,16 +3,16 @@ JARS=jai_codec-1.1.3.jar jai_core-1.1.3.jar jscience-4.2.jar jts-1.9.jar commons
 JARSPATH=jai_codec-1.1.3.jar:jai_core-1.1.3.jar:jscience-4.2.jar:jts-1.9.jar:commons-codec-1.3.jar:commons-logging-1.1.jar:commons-httpclient-3.0.1.jar
 
 jai_codec-1.1.3.jar:
-	curl 'http://ccl.northwestern.edu/devel/jai_codec-1.1.3.jar' > jai_codec-1.1.3.jar
+	curl -s 'http://ccl.northwestern.edu/devel/jai_codec-1.1.3.jar' -o jai_codec-1.1.3.jar
 jai_core-1.1.3.jar:
-	curl 'http://ccl.northwestern.edu/devel/jai_core-1.1.3.jar' > jai_core-1.1.3.jar
+	curl -s 'http://ccl.northwestern.edu/devel/jai_core-1.1.3.jar' -o jai_core-1.1.3.jar
 jscience-4.2.jar:
-	curl 'http://ccl.northwestern.edu/devel/jscience-4.2.jar' > jscience-4.2.jar
+	curl -s 'http://ccl.northwestern.edu/devel/jscience-4.2.jar' -o jscience-4.2.jar
 jts-1.9.jar:
-	curl 'http://ccl.northwestern.edu/devel/jts-1.9.jar' > jts-1.9.jar
+	curl -s 'http://ccl.northwestern.edu/devel/jts-1.9.jar' -o jts-1.9.jar
 commons-codec-1.3.jar:
-	curl 'http://ccl.northwestern.edu/devel/commons-codec-1.3.jar' > commons-codec-1.3.jar
+	curl -s 'http://ccl.northwestern.edu/devel/commons-codec-1.3.jar' -o commons-codec-1.3.jar
 commons-logging-1.1.jar:
-	curl 'http://ccl.northwestern.edu/devel/commons-logging-1.1.jar' > commons-logging-1.1.jar
+	curl -s 'http://ccl.northwestern.edu/devel/commons-logging-1.1.jar' -o commons-logging-1.1.jar
 commons-httpclient-3.0.1.jar:
-	curl 'http://ccl.northwestern.edu/devel/commons-httpclient-3.0.1.jar' > commons-httpclient-3.0.1.jar
+	curl -s 'http://ccl.northwestern.edu/devel/commons-httpclient-3.0.1.jar' -o commons-httpclient-3.0.1.jar
