da5d92da09faa0b8a8619937ceadebe9c713760d
Seth Tisue <seth@tisue.net>
2011-05-15 22:20:45 -0400

    convert more prim.gui stuff to Scala

---
+++ src/main/org/nlogo/prim/gui/_inspect.scala
@@ -0,0 +1,28 @@
+package org.nlogo.prim.gui
+
+import org.nlogo.agent.Agent
+import org.nlogo.api.I18N
+import org.nlogo.nvm.{ Command, Context, EngineException, Syntax }
+
+class _inspect extends Command {
+  override def syntax =
+    Syntax.commandSyntax(Array(Syntax.TYPE_AGENT))
+  override def perform(context: Context) {
+    val agent = argEvalAgent(context, 0)
+    if (agent.id == -1)
+      throw new EngineException(context, this,
+        I18N.errors.getN("org.nlogo.$common.thatAgentIsDead", agent.classDisplayName))
+    org.nlogo.awt.Utils.invokeLater(
+      new Runnable {
+        override def run() {
+          // we usually use a default radius of 3, but that doesnt work when the world has a radius
+          // of less than 3. so simply take the miniumum. - JC 7/1/10
+            val minWidthOrHeight =
+              (workspace.world.worldWidth  / 2) min
+              (workspace.world.worldHeight / 2)
+            val radius = 3 min (minWidthOrHeight / 2)
+            workspace.inspectAgent(agent.getAgentClass, agent, radius)
+        }})
+    context.ip = next
+  }
+}
