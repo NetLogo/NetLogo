9381bd4ab2feb671d0770c42ba21c73363c8d63f
Seth Tisue <seth@tisue.net>
2011-05-20 11:20:13 -0400

    remove unused code

--- src/main/org/nlogo/gl/render/LinkRenderer3D.scala
+++ src/main/org/nlogo/gl/render/LinkRenderer3D.scala
@@ -5,14 +5,6 @@ import org.nlogo.api.{ Agent, Link, Link3D, World }
 private class LinkRenderer3D(world: World, shapeRenderer: ShapeRenderer)
 extends LinkRenderer(world, shapeRenderer) {
 
-  def getXYandZComponents(agent: Agent, dist: Double): Array[Double] = {
-    val link = agent.asInstanceOf[Link3D]
-    val size = link.size
-    Array((link.x2 - link.x1) / size * dist,
-          (link.y2 - link.y1) / size * dist, 
-          (link.z2 - link.z1) / size * dist)
-  }
-  
   override def getOrientation(agent: Agent): Array[Double] = {
     val link = agent.asInstanceOf[Link3D]
     Array(link.heading, link.pitch, 0d)
