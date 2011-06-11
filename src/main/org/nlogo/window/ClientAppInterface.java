f88a1fa8c41b7702c7a92124933b37d311b944cc
joshcough <joshcough@gmail.com>
2011-06-10 22:50:24 -0500

    converted to scala

--- src/main/org/nlogo/window/ClientAppInterface.java
+++
@@ -1,9 +0,0 @@
-package org.nlogo.window;
-
-import org.nlogo.api.CompilerServices;
-
-public interface ClientAppInterface {
-  void startup(final EditorFactory editorFactory, final String userid, final String hostip,
-               final int port, final boolean isLocal, final boolean isRobo, final long waitTime,
-               final CompilerServices workspace);
-}
