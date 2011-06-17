a4e2d8b6885a1bd36692c074d04269ba6b189c77
joshcough <joshcough@gmail.com>
2011-06-17 15:12:44 -0500

    fixed tests

--- src/test/org/nlogo/api/WidgetIOTest.scala
+++ src/test/org/nlogo/api/WidgetIOTest.scala
@@ -127,112 +127,112 @@ class WidgetIOTest extends FunSuite {
 
 
   test("parse button"){
-    assert(parse1(button) === "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,None,false)")
-    assert(parseN(Buffer(button, button)) === List(
+    verboseAssert(parse1(button), "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,None,false)")
+    verboseAssert(parseN(Buffer(button, button)), List(
       "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,None,false)",
       "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,None,false)"))
   }
   test("parse button display name"){
     val b = button
     b(5) = "press me" // set display name
-    assert(parse1(b) === "ButtonSpec(Loc(131,10,209,43),Some(press me),go,true,OBSERVER,None,false)")
+    verboseAssert(parse1(b), "ButtonSpec(Loc(131,10,209,43),Some(press me),go,true,OBSERVER,None,false)")
   }
   test("parse button forever"){
     val b = button
     b(7) = "NIL" // set forever to NIL
-    assert(parse1(b) === "ButtonSpec(Loc(131,10,209,43),None,go,false,OBSERVER,None,false)")
+    verboseAssert(parse1(b), "ButtonSpec(Loc(131,10,209,43),None,go,false,OBSERVER,None,false)")
   }
   test("parse button agent type"){
     val b = button
     b(10) = "TURTLE" // set agent type
-    assert(parse1(b) === "ButtonSpec(Loc(131,10,209,43),None,go,true,TURTLE,None,false)")
+    verboseAssert(parse1(b), "ButtonSpec(Loc(131,10,209,43),None,go,true,TURTLE,None,false)")
     b(10) = "PATCH" // set agent type
-    assert(parse1(b) === "ButtonSpec(Loc(131,10,209,43),None,go,true,PATCH,None,false)")
+    verboseAssert(parse1(b), "ButtonSpec(Loc(131,10,209,43),None,go,true,PATCH,None,false)")
     b(10) = "LINK" // set agent type
-    assert(parse1(b) === "ButtonSpec(Loc(131,10,209,43),None,go,true,LINK,None,false)")
+    verboseAssert(parse1(b), "ButtonSpec(Loc(131,10,209,43),None,go,true,LINK,None,false)")
   }
   test("parse button action key"){
     val b = button
     b(12) = "X" // set forever to NIL
-    assert(parse1(b) === "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,Some(X),false)")
+    verboseAssert(parse1(b), "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,Some(X),false)")
   }
   test("parse button go time"){
     val b = button
     b(15) = "0" // set go time (0 = true)
-    assert(parse1(b) === "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,None,true)")
+    verboseAssert(parse1(b), "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,None,true)")
   }
   
   test("parse slider"){
-    assert(parse1(slider) === "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,%,false)")
-    assert(parseN(Buffer(slider, slider)) === List(
-      "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,%,false)",
-      "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,%,false)"))
+    verboseAssert(parse1(slider), "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,Some(%),false)")
+    verboseAssert(parseN(Buffer(slider, slider)), List(
+      "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,Some(%),false)",
+      "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,Some(%),false)"))
   }
   test("parse slider code"){
     val s = slider
     s(7) = "count turtles" // set min
     s(8) = "count turtles + 100" // set max
-    assert(parse1(s) === "SliderSpec(Loc(31,181,226,214),infection-chance,count turtles,count turtles + 100,100.0,1,%,false)")
+    verboseAssert(parse1(s), "SliderSpec(Loc(31,181,226,214),infection-chance,count turtles,count turtles + 100,100.0,1,Some(%),false)")
   }
   test("parse slider direction"){
     val s = slider
     s(13) = "VERTICAL" // set direction
-    assert(parse1(s) === "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,%,true)")
+    verboseAssert(parse1(s), "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,Some(%),true)")
   }
 
   test("parse switch"){
-    assert(parse1(switch) === "SwitchSpec(Loc(30,62,133,95),a-switch,false)")
-    assert(parseN(Buffer(switch, switch)) === List(
+    verboseAssert(parse1(switch), "SwitchSpec(Loc(30,62,133,95),a-switch,false)")
+    verboseAssert(parseN(Buffer(switch, switch)), List(
       "SwitchSpec(Loc(30,62,133,95),a-switch,false)",
       "SwitchSpec(Loc(30,62,133,95),a-switch,false)"))
     val s = switch
     s(7) = "0" // set isOn to true...really.
-    assert(parse1(s) === "SwitchSpec(Loc(30,62,133,95),a-switch,true)")
+    verboseAssert(parse1(s), "SwitchSpec(Loc(30,62,133,95),a-switch,true)")
   }
 
   test("parse chooser"){
-    assert(parse1(chooser) === """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],2)""")
-    assert(parseN(Buffer(chooser, chooser)) === List(
+    verboseAssert(parse1(chooser), """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],2)""")
+    verboseAssert(parseN(Buffer(chooser, chooser)), List(
       """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],2)""",
       """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],2)"""))
     val c = chooser
     c(8) = "0" // set selected index to 0
-    assert(parse1(c) === """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],0)""")
+    verboseAssert(parse1(c), """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],0)""")
     c(8) = "1" // set selected index to 1
-    assert(parse1(c) === """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],1)""")
+    verboseAssert(parse1(c), """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],1)""")
   }
 
   test("parse input"){
-    assert(parse1(input) === "InputBoxSpec(Loc(62,138,297,198),my-input,null,false,Some(String))")
+    verboseAssert(parse1(input), "InputBoxSpec(Loc(62,138,297,198),my-input,null,false,String)")
   }
 
   test("parse output"){
-    assert(parse1(output) === "OutputSpec(Loc(76,323,316,377),Some(12))")
+    verboseAssert(parse1(output), "OutputSpec(Loc(76,323,316,377),Some(12))")
   }
 
   test("parse monitor"){
-    assert(parse1(monitor) === "MonitorSpec(Loc(69,247,157,292),,my-monitor,Some(17),Some(11))")
+    verboseAssert(parse1(monitor), "MonitorSpec(Loc(69,247,157,292),None,Some(my-monitor),Some(17),Some(11))")
   }
 
   test("parse note"){
-    assert(parse1(note) === "NoteSpec(Loc(81,414,231,432),random random.\nrandom random.,Some(11),Some(15.0),true)")
+    verboseAssert(parse1(note), "NoteSpec(Loc(81,414,231,432),random random.\\nrandom random.,Some(11),Some(15.0),true)")
   }
 
   test("parse plot"){
-    assert(parse1(plot) === "PlotSpec(Loc(83,455,283,605),plot 1,NIL,NIL,0.0,10.0,0.0,10.0,true,false,Some(crt 1 ; setup command),Some(crt 2 ; update command),List(PlotPenSpec(default,1.0,0,-16777216,true,None,Some(plot count turtles)), PlotPenSpec(pen-1,1.0,0,-7500403,true,None,Some(plotxy 6 7))))")
+    verboseAssert(parse1(plot), "PlotSpec(Loc(83,455,283,605),plot 1,None,None,0.0,10.0,0.0,10.0,true,false,Some(crt 1 ; setup command),Some(crt 2 ; update command),List(PlotPenSpec(default,1.0,0,-16777216,true,None,Some(plot count turtles)), PlotPenSpec(pen-1,1.0,0,-7500403,true,None,Some(plotxy 6 7))))")
   }
 
   test("parse everyyhing") {
-    assert(parseN(Buffer(button, slider, switch, chooser, input, output, monitor, note, plot)) === List(
+    verboseAssert(parseN(Buffer(button, slider, switch, chooser, input, output, monitor, note, plot)), List(
       "ButtonSpec(Loc(131,10,209,43),None,go,true,OBSERVER,None,false)",
-      "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,%,false)",
+      "SliderSpec(Loc(31,181,226,214),infection-chance,0,100,100.0,1,Some(%),false)",
       "SwitchSpec(Loc(30,62,133,95),a-switch,false)",
       """ChooserSpec(Loc(36,72,174,117),my-choices,42 "zoo" [1 2 3],2)""",
-      "InputBoxSpec(Loc(62,138,297,198),my-input,null,false,Some(String))",
+      "InputBoxSpec(Loc(62,138,297,198),my-input,null,false,String)",
       "OutputSpec(Loc(76,323,316,377),Some(12))",
-      "MonitorSpec(Loc(69,247,157,292),,my-monitor,Some(17),Some(11))",
-      "NoteSpec(Loc(81,414,231,432),random random.\nrandom random.,Some(11),Some(15.0),true)",
-      "PlotSpec(Loc(83,455,283,605),plot 1,NIL,NIL,0.0,10.0,0.0,10.0,true,false,Some(crt 1 ; setup command),Some(crt 2 ; update command),List(PlotPenSpec(default,1.0,0,-16777216,true,None,Some(plot count turtles)), PlotPenSpec(pen-1,1.0,0,-7500403,true,None,Some(plotxy 6 7))))"
+      "MonitorSpec(Loc(69,247,157,292),None,Some(my-monitor),Some(17),Some(11))",
+      "NoteSpec(Loc(81,414,231,432),random random.\\nrandom random.,Some(11),Some(15.0),true)",
+      "PlotSpec(Loc(83,455,283,605),plot 1,None,None,0.0,10.0,0.0,10.0,true,false,Some(crt 1 ; setup command),Some(crt 2 ; update command),List(PlotPenSpec(default,1.0,0,-16777216,true,None,Some(plot count turtles)), PlotPenSpec(pen-1,1.0,0,-7500403,true,None,Some(plotxy 6 7))))"
     ))
   }
 
@@ -240,6 +240,17 @@ class WidgetIOTest extends FunSuite {
   def parse1(widget: Buffer[String]): String = parseN(Buffer(widget))(0)
   def parseN(widgets: Buffer[Buffer[String]]): List[String] = {
     WidgetIO.parseWidgets(widgets.map(_ :+ "").flatten).map(_.toString).toList
+  }
+
+  def verboseAssert(actual:Any, expected:Any){
+    if( actual != expected ){
+      println("assertion error. expected followed by actual:")
+      println("================")
+      println(expected)
+      println(actual)
+      println("================")
+    }
+    assert(actual === expected)
   }
 }
 
