8772123910a18f46565cfc4f7a41656cb10ef892
Seth Tisue <seth@tisue.net>
2011-07-05 19:23:43 -0400

    fix small bug where a model file with an extra blank line at the end could result in an exception at load time

--- src/main/org/nlogo/app/EditMenu.scala
+++ src/main/org/nlogo/app/EditMenu.scala
@@ -49,7 +49,8 @@ with org.nlogo.window.Events.LoadSectionEvent.Handler
   def handle(e: org.nlogo.window.Events.LoadSectionEvent) {
     if(e.section == org.nlogo.api.ModelSection.MODEL_SETTINGS) {
       app.workspace.snapOn(e.lines != null &&
-                           e.lines.size > 0 &&
+                           e.lines.nonEmpty &&
+                           e.lines.head.trim.nonEmpty &&
                            e.lines.head.toInt != 0)
       snapper.setState(app.workspace.snapOn)
     }
