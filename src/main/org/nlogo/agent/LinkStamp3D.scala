40683834a7438c5d789aa558512a19b543852b69
Seth Tisue <seth@tisue.net>
2011-08-07 10:55:08 -0400

    convert agent.LinkStamp3D to Scala

---
+++ src/main/org/nlogo/agent/LinkStamp3D.scala
@@ -0,0 +1,28 @@
+package org.nlogo.agent
+
+case class LinkStamp3D(
+  shape: String, x1: Double, y1: Double, z1: Double, x2: Double, y2: Double, z2: Double,
+  color: AnyRef, lineThickness: Double, isDirectedLink: Boolean, linkDestinationSize: Double,
+  heading: Double, pitch: Double)
+extends org.nlogo.api.LinkStamp3D {
+  def this(l: Link3D) =
+    this(l.shape, l.x1, l.y1, l.z1, l.x2, l.y2, l.z2, l.color, l.lineThickness,
+         l.isDirectedLink, l.linkDestinationSize, l.heading, l.pitch)
+  override def midpointX = (x1 + x2) / 2
+  override def midpointY = (y1 + y2) / 2
+  override def getBreedIndex = 0
+  override def labelColor = 0: java.lang.Double
+  override def labelString = ""
+  override def hasLabel = false
+  override def hidden = false
+  override def getBreed = null
+  override def end1 = null
+  override def end2 = null
+  override def size = 0
+  override def id = 0
+  override def world = null
+  override def classDisplayName = ""
+  override def getVariable(vn: Int) = unsupported
+  override def setVariable(vn: Int, value: AnyRef) = unsupported
+  private def unsupported = throw new UnsupportedOperationException
+}
