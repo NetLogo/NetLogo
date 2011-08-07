a4c10926c3f96046906f4b0836d703768646aa93
Seth Tisue <seth@tisue.net>
2011-08-07 09:15:56 -0400

    convert agent package stuff to Scala

---
+++ src/main/org/nlogo/agent/Agent3D.scala
@@ -0,0 +1,8 @@
+package org.nlogo.agent
+
+import org.nlogo.api.AgentException
+
+trait Agent3D {
+  @throws(classOf[AgentException])
+  def getPatchAtOffsets(dx: Double, dy: Double, dz: Double): Patch3D 
+}
