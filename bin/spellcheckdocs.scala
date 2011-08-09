511322e305a60c304445b27daebf062bb80906ca
Seth Tisue <seth@tisue.net>
2011-08-09 09:40:41 -0400

    make spell checking scripts use aspell not ispell
    since homebrew doesn't have ispell and gnu seems to intend that aspell
    is the replacement for ispell anyway

--- bin/spellcheckdocs.scala
+++ bin/spellcheckdocs.scala
@@ -8,11 +8,13 @@ exec bin/scala -classpath bin -deprecation -nocompdaemon "$0" "$@"
 // finds spelling mistakes in the User Manual (because yea, verily,
 // spelling mistakes are an abomination unto the Lord)
 
+// installing aspell: brew install aspell --lang=en
+
 import Scripting.{shell,read}
 
 for{path <- shell("find docs -name \\*.html")
     if !path.startsWith("docs/scaladoc/")
-    lines = shell("ispell -H -l -p dist/docwords.txt < " + path)
+    lines = shell("aspell -H -p ./dist/docwords.txt list < " + path)
     if lines.hasNext}
 {
   println(path)
