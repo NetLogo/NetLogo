f1c4f7d871f3252524591bb8d6d4bdf2ff1ed934
Seth Tisue <seth@tisue.net>
2011-08-26 12:53:07 -0400

    minor cleanup

--- src/test/org/nlogo/awt/LineBreakerTests.scala
+++ src/test/org/nlogo/awt/LineBreakerTests.scala
@@ -1,19 +1,21 @@
 package org.nlogo.awt
 
 import org.scalatest.FunSuite
+import java.awt.image.BufferedImage
+import java.awt.Font
 
 class LineBreakerTests extends FunSuite {
-  test("break lines 1") {
-    val image = new java.awt.image.BufferedImage(1,1,java.awt.image.BufferedImage.TYPE_INT_RGB)  // arbitrary type
-    val metrics = image.getGraphics.getFontMetrics(new java.awt.Font("courier",java.awt.Font.PLAIN,400))
+  test("don't blow up") {
+    val image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)  // arbitrary type
+    val metrics = image.getGraphics.getFontMetrics(new Font("courier", Font.PLAIN, 400))
     // We're not testing that we get reasonable answers, only that it doesn't blow up or hang
     LineBreaker.breakLines(
-      "this is a test",metrics,10)
+      "this is a test", metrics, 10)
     LineBreaker.breakLines(
       "thisisatestthisisatestthisisatestthisisatestthis" +
       "isatestthisisatestthisisatestthisisatestthisisat" +
       "estthisisatestthisisatestthisisatestthisisatestt" +
       "hisisatestthisisatestthisisatest",
-      metrics,10)
+      metrics, 10)
   }
 }
