f4bbbf85a0bf277e92d7e630a05f682b3464e412
Seth Tisue <seth@tisue.net>
2011-08-27 19:36:51 -0400

    convert awt.Colors to Scala

---
+++ src/main/org/nlogo/awt/Colors.scala
@@ -0,0 +1,32 @@
+package org.nlogo.awt
+
+import java.awt.Color
+
+object Colors {
+
+  /**
+   * Mixes the rgb components of two colors.
+   *
+   * @param mix the proportion, from 0 to 1, of the first color in the mix.
+   * @return a new color with <code>red = mix*(c1.red) + (1-mix)*c2.red</code>, etc.
+   */
+  def mixColors(c1: Color, c2: Color, mix: Double): Color = {
+    require(mix >= 0.0 && mix <= 1.0)
+    new Color(((c1.getRed * mix) + (c2.getRed * (1 - mix))).toInt,
+              ((c1.getGreen * mix) + (c2.getGreen * (1 - mix))).toInt,
+              ((c1.getBlue * mix) + (c2.getBlue * (1 - mix))).toInt)
+  }
+
+  /**
+   * Converts a java.awt.Color to a 6-digit hex string suitible for HTML/CSS tags. *
+   */
+  def AWTColorToHex(c: Color) =
+    java.lang.Integer.toHexString(c.getRGB).takeRight(6)
+
+  /**
+   * Wraps a string with HTML font tag for color. *
+   */
+  def colorize(s: String, c: Color) =
+    "<font color=\"#" + AWTColorToHex(c) + "\">" + s + "</font>"
+
+}
