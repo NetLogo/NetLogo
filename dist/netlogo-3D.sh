c888933d81fab91882ced87d6ec18d88aa33774a
Seth Tisue <seth@tisue.net>
2011-08-28 11:04:54 -0400

    permit deeper recursion using -Xss everywhere (fixes #1382)

--- dist/netlogo-3D.sh
+++ dist/netlogo-3D.sh
@@ -4,9 +4,10 @@ cd "`dirname "$0"`"             # the copious quoting is for handling paths with
 # -Djava.library.path=./lib     ensure JOGL can find native libraries 
 # -Djava.ext.dir=               ignore any existing JOGL installation
 # -XX:MaxPermSize=128m          avoid OutOfMemory errors for large models
+# -Xss16m                       increase stack size to allow deeper recursion
 # -Xmx1024m                     use up to 1GB RAM (edit to increase)
 # -Dfile.encoding=UTF-8         ensure Unicode characters in model files are compatible cross-platform
 # -Dorg.nlogo.is3d=true         run 3D NetLogo
 # -jar NetLogo.jar              specify main jar
 # "$@"                          pass along any command line arguments
-java -server -Djava.library.path=./lib -Djava.ext.dir= -XX:MaxPermSize=128m -Xmx1024m -Dfile.encoding=UTF-8 -Dorg.nlogo.is3d=true -jar NetLogo.jar "$@"
+java -server -Djava.library.path=./lib -Djava.ext.dir= -XX:MaxPermSize=128m -Xss16m -Xmx1024m -Dfile.encoding=UTF-8 -Dorg.nlogo.is3d=true -jar NetLogo.jar "$@"
