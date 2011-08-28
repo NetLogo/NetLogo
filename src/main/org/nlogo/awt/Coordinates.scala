9c68a7858d2e917c64e703a67d25fea1668e840f
Seth Tisue <seth@tisue.net>
2011-08-27 20:34:20 -0400

    remove unused code

--- src/main/org/nlogo/awt/Coordinates.scala
+++ src/main/org/nlogo/awt/Coordinates.scala
@@ -26,26 +26,6 @@ object Coordinates {
   }
 
   /**
-   * Converts point to a component's coordinate system from screen coordinates.
-   */
-  def convertPointFromScreen(p: Point, c: Component) {
-    if(c != null) {
-      val done = c.isInstanceOf[Applet] || c.isInstanceOf[Window]
-      val (x, y) =
-        if (done) {
-          val pp = c.getLocationOnScreen
-          (pp.x, pp.y)
-        }
-        else
-          (c.getLocation.x, c.getLocation.y)
-      p.x -= x
-      p.y -= y
-      if (!done)
-        convertPointToScreen(p, c.getParent)
-    }
-  }
-
-  /**
    * Returns the location of a component on the screen.
    */
   def getLocationOnScreen(c: Component): Point = {
@@ -53,11 +33,5 @@ object Coordinates {
     convertPointToScreen(result, c)
     result
   }
-
-  /**
-   * Returns the difference between two points.
-   */
-  def subtractPoints(p1: Point, p2: Point): Point =
-    new Point(p1.x - p2.x, p1.y - p2.y)
 
 }
