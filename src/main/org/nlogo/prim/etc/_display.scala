25cd12a2f9678f54d6f15f0fba9e4437455dce86
Seth Tisue <seth@tisue.net>
2011-08-31 18:21:25 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_display.scala
@@ -0,0 +1,14 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context }
+
+class _display extends Command {
+  override def syntax =
+    Syntax.commandSyntax(true)
+  override def perform(context: Context) {
+    world.displayOn(true)
+    workspace.requestDisplayUpdate(true)
+    context.ip = next
+  }
+}
---
+++ src/main/org/nlogo/prim/etc/_nodisplay.scala
@@ -0,0 +1,13 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context }
+
+class _nodisplay extends Command {
+  override def syntax =
+    Syntax.commandSyntax
+  override def perform(context: Context) {
+    world.displayOn(false)
+    context.ip = next
+  }
+}
