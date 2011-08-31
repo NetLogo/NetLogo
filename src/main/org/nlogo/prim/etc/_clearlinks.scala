55f98abaefbe883eec001aff1f4f298c35ac1edc
Seth Tisue <seth@tisue.net>
2011-08-31 18:14:43 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_clearlinks.scala
@@ -0,0 +1,13 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context }
+
+class _clearlinks extends Command {
+  override def syntax =
+    Syntax.commandSyntax("O---", true)
+  override def perform(context: Context) {
+    world.clearLinks()
+    context.ip = next
+  }
+}
---
+++ src/main/org/nlogo/prim/etc/_stamperase.scala
@@ -0,0 +1,13 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context }
+
+class _stamperase extends Command {
+  override def syntax =
+    Syntax.commandSyntax("-T-L", true)
+  override def perform(context: Context) {
+    world.stamp(context.agent, true)
+    context.ip = next
+  }
+}
