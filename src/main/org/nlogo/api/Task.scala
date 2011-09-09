f13aa9b4778ef1aab2071385292e3f1f8054afaa
Seth Tisue <seth@tisue.net>
2011-09-09 19:06:23 -0400

    add report/perform methods to api.Task

--- src/main/org/nlogo/api/Task.scala
+++ src/main/org/nlogo/api/Task.scala
@@ -1,5 +1,10 @@
 package org.nlogo.api
 
 sealed trait Task
-trait ReporterTask extends Task
-trait CommandTask extends Task
+trait ReporterTask extends Task {
+  def report(c: Context, args: Array[AnyRef]): AnyRef
+}
+trait CommandTask extends Task {
+  def perform(c: Context, args: Array[AnyRef])
+}
+
