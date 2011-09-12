c797b76c485f0c1310072f7adb9d581b2a9a0ab2
Seth Tisue <seth@tisue.net>
2011-09-12 18:42:21 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_penup.scala
@@ -0,0 +1,14 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.agent.Turtle
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context }
+
+class _penup extends Command {
+  override def syntax =
+    Syntax.commandSyntax("-T--", true)
+  override def perform(context: Context) {
+    context.agent.asInstanceOf[Turtle].penMode(Turtle.PEN_UP)
+    context.ip = next
+  }
+}
---
+++ src/main/org/nlogo/prim/etc/_self.scala
@@ -0,0 +1,14 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.agent.Agent
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Context, Reporter }
+
+class _self extends Reporter {
+  override def syntax =
+    Syntax.reporterSyntax(Syntax.AgentType, "-TPL")
+  override def report(context: Context) =
+    report_1(context)
+  def report_1(context: Context) =
+    context.agent
+}
