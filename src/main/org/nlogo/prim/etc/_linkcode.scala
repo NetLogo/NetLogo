65248a26630d9d916dc84a51cb1a6ee2a2a25f51
Seth Tisue <seth@tisue.net>
2011-09-13 09:07:42 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_linkcode.scala
@@ -0,0 +1,15 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context, CustomAssembled, AssemblerAssistant }
+
+class _linkcode extends Command with CustomAssembled {
+  override def syntax =
+    Syntax.commandSyntax("---L", false)
+  override def perform(context: Context) {
+    throw new UnsupportedOperationException
+  }
+  override def assemble(a: AssemblerAssistant) {
+    // by doing nothing here, drop out of existence
+  }
+}
