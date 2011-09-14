6884c7b9279679897f012e4dc6f69ac0781ad952
Seth Tisue <seth@tisue.net>
2011-09-14 10:09:40 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_worldheight.scala
@@ -0,0 +1,15 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Context, Reporter }
+
+class _worldheight extends Reporter {
+  override def syntax =
+    Syntax.reporterSyntax(Syntax.NumberType)
+  override def report(context: Context) =
+    report_1(context)
+  def report_1(context: Context): java.lang.Double =
+    world.worldHeightBoxed
+  def report_2(context: Context): Double =
+    world.worldHeight
+}
