8acc4b83005f7415c2af78ecfa171201e72eb621
Seth Tisue <seth@tisue.net>
2011-09-15 11:04:28 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_randomycor.scala
@@ -0,0 +1,16 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Context, Reporter }
+
+class _randomycor extends Reporter {
+  override def syntax =
+    Syntax.reporterSyntax(Syntax.NumberType)
+  override def report(context: Context) =
+    Double.box(report_1(context))
+  def report_1(context: Context): Double = {
+    val min = world.minPycor - 0.5
+    val max = world.maxPycor + 0.5
+    min + context.job.random.nextDouble * (max - min)
+  }
+}
