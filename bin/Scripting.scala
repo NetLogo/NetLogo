830189430a04ef4c63cf9eee413d3b76755092b5
Seth Tisue <seth@tisue.net>
2011-09-14 21:22:18 -0400

    bin/Scripting.scala: use sys.process, not our own low level code

--- bin/Scripting.scala
+++ bin/Scripting.scala
@@ -6,40 +6,32 @@ object Scripting {
 
   // run shell command (discarding output)
   def shellDo(cmd: String, requireZeroExitStatus: Boolean = true) {
-    val result = Seq("/bin/sh", "-c", cmd).!
-    require(!requireZeroExitStatus || result == 0,
-            "exit status " + result + ": " + cmd)
+    val status = exec(cmd).!
+    require(!requireZeroExitStatus || status == 0,
+            "exit status " + status + ": " + cmd)
   }
   
   // pipe a string through a shell command
   def pipe(input: String, cmd: String): Iterator[String] = {
-    val process = exec(cmd)
-    val out = new java.io.PrintWriter(process.getOutputStream)
-    out.print(input)
-    out.close(); process.getOutputStream.close() // both necessary? not sure
-    io.Source.fromInputStream(process.getInputStream).getLines
+    val inputStream =
+      new java.io.ByteArrayInputStream(input.getBytes("UTF-8"))
+    exec(cmd).#<(inputStream).lines.iterator
   }
 
   // run shell command, get iterator over lines of output
   def shell(cmd: String, requireZeroExitStatus: Boolean = true): Iterator[String] =
-    new Iterator[String] {
-      val process = exec(cmd)
-      val source = io.Source.fromInputStream(process.getInputStream)
-      val iter = source.getLines
-      def next() = iter.next()
-      def hasNext = iter.hasNext || {
-        process.waitFor()
-        require(!requireZeroExitStatus || process.exitValue == 0,
-          "exit status " + process.exitValue + ": " + cmd)
-        false
-      }
-    }
+    if(requireZeroExitStatus)
+      exec(cmd).lines.iterator
+    else
+      exec(cmd).lines_!.iterator
 
   // run a shell command
-  private def exec(cmd: String): java.lang.Process = {
+  private def exec(cmd: String): ProcessBuilder = {
+    // We drop down to j.l.P here in order to merge the output and error streams.  (Why?
+    // I forget.  Maybe it was useful in benches.scala or something? - ST 9/14/11)
     val builder = new java.lang.ProcessBuilder("/bin/sh", "-c", cmd)
     builder.redirectErrorStream(true)
-    builder.start()
+    builder
   }
 
   // read a whole file into a string
