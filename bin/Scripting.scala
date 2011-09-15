aa6d5747e75300502cb2155fdb61cd94b2f73884
Seth Tisue <seth@tisue.net>
2011-09-14 20:37:50 -0400

    bin/Scripting.scala: clean up

--- bin/Scripting.scala
+++ bin/Scripting.scala
@@ -2,6 +2,8 @@ import sys.process._
 
 object Scripting {
 
+  implicit val codec: io.Codec = io.Codec.UTF8
+
   // run shell command (discarding output)
   def shellDo(cmd: String, requireZeroExitStatus: Boolean = true) {
     val result = Seq("/bin/sh", "-c", cmd).!
@@ -15,20 +17,20 @@ object Scripting {
     val out = new java.io.PrintWriter(process.getOutputStream)
     out.print(input)
     out.close(); process.getOutputStream.close() // both necessary? not sure
-    io.Source.fromInputStream(process.getInputStream)("UTF-8").getLines
+    io.Source.fromInputStream(process.getInputStream).getLines
   }
 
   // run shell command, get iterator over lines of output
   def shell(cmd: String, requireZeroExitStatus: Boolean = true): Iterator[String] =
     new Iterator[String] {
       val process = exec(cmd)
-      val source = io.Source.fromInputStream(process.getInputStream)("UTF-8")
+      val source = io.Source.fromInputStream(process.getInputStream)
       val iter = source.getLines
       def next() = iter.next()
       def hasNext = iter.hasNext || {
         process.waitFor()
-        if (requireZeroExitStatus)
-          require(process.exitValue == 0, "exit status " + process.exitValue + ": " + cmd)
+        require(!requireZeroExitStatus || process.exitValue == 0,
+          "exit status " + process.exitValue + ": " + cmd)
         false
       }
     }
@@ -42,12 +44,12 @@ object Scripting {
 
   // read a whole file into a string
   def slurp(name: String): String =
-    io.Source.fromFile(name)("UTF-8").mkString
+    io.Source.fromFile(name).mkString
 
   // get iterator over lines in a text file
   def read: Iterator[String] =
-    io.Source.fromInputStream(System.in)("UTF-8").getLines
+    io.Source.fromInputStream(System.in).getLines
   def read(name: String): Iterator[String] =
-    io.Source.fromFile(name)("UTF-8").getLines
+    io.Source.fromFile(name).getLines
 
 }
