36265d98e5e999601dc901aae5baf501f159bff5
Seth Tisue <seth@tisue.net>
2011-09-20 18:19:35 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_type.scala
@@ -0,0 +1,15 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context, Workspace }
+
+class _type extends Command {
+  override def syntax =
+    Syntax.commandSyntax(Array(Syntax.WildcardType))
+  override def perform(context: Context) {
+    workspace.outputObject(
+      args(0).report(context), null, false, false,
+      Workspace.OutputDestination.NORMAL)
+    context.ip = next
+  }
+}
