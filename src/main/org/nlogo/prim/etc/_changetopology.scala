5ef8d8f16ff81768fb8498116b1e5c3672dc0bb7
Seth Tisue <seth@tisue.net>
2011-09-24 15:29:45 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_changetopology.scala
@@ -0,0 +1,16 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context }
+
+class _changetopology extends Command {
+  override def syntax =
+    Syntax.commandSyntax(
+      Array(Syntax.BooleanType, Syntax.BooleanType))
+  override def perform(context: Context) {
+    workspace.changeTopology(
+      argEvalBooleanValue(context, 0),
+      argEvalBooleanValue(context, 1))
+    context.ip = next
+  }
+}
