d6771da24ad80a21e241ebd1a359d8b2d05d5111
Seth Tisue <seth@tisue.net>
2011-09-25 13:50:34 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_precision.scala
@@ -0,0 +1,16 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Context, Pure, Reporter }
+
+class _precision extends Reporter with Pure {
+  override def syntax =
+    Syntax.reporterSyntax(
+      Array(Syntax.NumberType, Syntax.NumberType),
+      Syntax.NumberType)
+  override def report(context: Context) =
+    newValidDouble(
+      org.nlogo.api.Approximate.approximate(
+        argEvalDoubleValue(context, 0),
+        argEvalIntValue(context, 1)))
+}
