2e464a3cda49845879e2d8c7eae095bc671f8220
Seth Tisue <seth@tisue.net>
2011-09-29 21:33:49 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_pwd.scala
@@ -0,0 +1,17 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Command, Context, Workspace }
+
+class _pwd extends Command {
+  override def syntax =
+    Syntax.commandSyntax("O---", false)
+  override def perform(context: Context) {
+    val path =
+      Option(workspace.getModelPath)
+        .getOrElse("no model loaded!")
+    workspace.outputObject(path, null, true, true,
+      Workspace.OutputDestination.NORMAL)
+    context.ip = next
+  }
+}
