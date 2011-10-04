670165641e31212d3bf937a2a377ff60d979b3dc
Seth Tisue <seth@tisue.net>
2011-10-04 13:53:59 -0400

    convert more prim.etc stuff to Scala

---
+++ src/main/org/nlogo/prim/etc/_readfromstring.scala
@@ -0,0 +1,17 @@
+package org.nlogo.prim.etc
+
+import org.nlogo.api.{ CompilerException, Syntax }
+import org.nlogo.nvm.{ Context, EngineException, Reporter }
+
+class _readfromstring extends Reporter {
+  override def syntax =
+    Syntax.reporterSyntax(
+      Array(Syntax.StringType),
+      Syntax.ReadableType)
+  override def report(context: Context) =
+    try workspace.readFromString(argEvalString(context, 0))
+    catch {
+      case e: CompilerException =>
+        throw new EngineException(context, this, e.getMessage)
+    }
+}
