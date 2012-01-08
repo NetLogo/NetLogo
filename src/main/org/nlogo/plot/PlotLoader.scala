b547c39d955f0ec24cfd41cf7ccfbca26e1d21e5
Seth Tisue <seth@tisue.net>
2012-01-08 14:42:03 -0500

    refactor PlotLoader to be shorter, clearer
    avoid StringTokenizer; avoid duplicate logic

--- src/main/org/nlogo/plot/PlotLoader.scala
+++ src/main/org/nlogo/plot/PlotLoader.scala
@@ -24,8 +24,8 @@ object PlotLoader {
         case Nil => // old style model, no new plot code. this is ok.
         case setup :: update :: Nil =>
           // the correct amount of plot code.
-          plot.setupCode = autoConvert(setup)
-          plot.updateCode = autoConvert(update)
+          plot.setupCode = autoConvert(unEscapeString(setup))
+          plot.updateCode = autoConvert(unEscapeString(update))
         case _ =>
           // 1, or 3 or more bits of code...error.
           sys.error("Plot '" + plot.name + "' contains invalid setup and/or update code: " + plotLines(14))
@@ -64,56 +64,46 @@ object PlotLoader {
                                    setupCode: String, updateCode: String)
 
   private[plot] def parsePen(s: String): PenSpec = {
-    // the drop(1) skips the opening quote
-    val tokens = tokenize(s.drop(1)).toList
-
-    // span would put the last part of the name in the wrong part of the result,
-    // so we need spanPlusOne
-    val (nameTokens, moreTokens) =
-      spanPlusOne(tokens)(tok => !tok.endsWith("\"") || tok.endsWith("\\\""))
-
-    // dropRight drops the closing quote
-    val name = unEscapeString(nameTokens.mkString.dropRight(1))
-
-    // the rest of the line is easy to handle
-    val (interval, mode, color, inLegend) =
-      moreTokens.filter(_.trim.nonEmpty) match {
-        case List(interval, mode, color, inLegend, _*) =>
-          if (!PlotPen.isValidPlotPenMode(mode.toInt))
-            sys.error(mode + " is not a valid plot pen mode")
-          (interval.toDouble, mode.toInt, color.toInt, inLegend.toBoolean)
+    require(s.head == '"')
+    val (name, rest) = parseOne(s.tail)
+    val (rest1, rest2) = rest.span(_ != '"')
+    val List(interval, mode, color, inLegend) =
+      rest1.trim.split("\\s+").toList
+    require(PlotPen.isValidPlotPenMode(mode.toInt))
+    // optional; pre-5.0 models don't have them
+    val (setup, update) =
+      parseStringLiterals(rest2) match {
+        case List(setup, update) =>
+          (setup, update)
         case _ =>
-          sys.error("bad line: \"" + s + "\"")
+          ("", "")
       }
-
-    val codeString = moreTokens.dropWhile(!_.startsWith("\"")).mkString.trim
-    parseStringLiterals(codeString) match {
-      case List(setup, update) =>
-        PenSpec(name, interval, mode, color, inLegend, setup, update)
-      case _ =>
-        PenSpec(name, interval, mode, color, inLegend, "", "")
-    }
+    PenSpec(unEscapeString(name), interval.toDouble, mode.toInt, color.toInt, inLegend.toBoolean,
+            unEscapeString(setup), unEscapeString(update))
   }
 
-  // Used to parse a line that may contain multiple string literals, surrounded by double quotes
-  // and separated by spaces.  This is tricky because the string literals may contain escaped
-  // double quotes, so it's nontrivial to figure out where one literal ends and the next starts.
-  private[plot] def parseStringLiterals(s: String): List[String] = {
-    def parseOne(s: String): (String, String) =
-      if(s.isEmpty)
-        ("", "")
-      else if (s.head == '"')
-        ("", s.tail.trim)
-      else if(s.take(2) == "\\\"")
-        parseOne(s.drop(2)) match {
-          case (more1, more2) =>
-            ('"' +: more1, more2)
-        }
-      else
-        parseOne(s.tail) match {
-          case (more1, more2) =>
-            (s.head +: more1, more2)
-        }
+  // This is tricky because the string literals may contain escaped double quotes, so it's
+  // nontrivial to figure out where one literal ends and the next starts.  Assumes the
+  // opening double quote has already been detected and discarded.
+  private def parseOne(s: String): (String, String) =
+    if(s.isEmpty)
+      ("", "")
+    else if (s.head == '"')
+      ("", s.tail.trim)
+    else if(s.take(2) == "\\\"")
+      parseOne(s.drop(2)) match {
+        case (more1, more2) =>
+          ('"' + more1, more2)
+      }
+    else
+      parseOne(s.tail) match {
+        case (more1, more2) =>
+          (s.head + more1, more2)
+      }
+
+  // Used to parse a line that may contain multiple string literals, surrounded by double quotes and
+  // separated by spaces.
+  private[plot] def parseStringLiterals(s: String): List[String] =
     s.headOption match {
       case Some('"') =>
         val (result, more) = parseOne(s.tail)
@@ -121,22 +111,5 @@ object PlotLoader {
       case _ =>
         Nil
     }
-  }
 
-  // encapsulate ugly StringTokenizer
-  private def tokenize(s: String): Iterator[String] = {
-    import java.util.StringTokenizer
-    val tokenizer = new StringTokenizer(s, " ", true) // returnDelims = true
-    new Iterator[String]() {
-      def hasNext = tokenizer.hasMoreTokens
-      def next() = tokenizer.nextToken()
-    }
-  }
-
-  // like span, but keeps the first failing item
-  private def spanPlusOne[T](list: List[T])(pred: T => Boolean) =
-    list.span(pred) match {
-      case (xs, y :: ys) => (xs :+ y, ys)
-      case (xs, Nil) => (xs, Nil)
-    }
 }
