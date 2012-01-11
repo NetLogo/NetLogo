0ac9c17a1a22974658e2b6c6bedf451feed76b21
Seth Tisue <seth@tisue.net>
2012-01-10 20:32:48 -0500

    remove commented-out code

--- src/main/org/nlogo/workspace/AbstractWorkspace.java
+++ src/main/org/nlogo/workspace/AbstractWorkspace.java
@@ -2,30 +2,6 @@
 
 package org.nlogo.workspace;
 
-//import java.util.HashMap;
-//import java.util.Iterator;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.WeakHashMap;
-//import org.nlogo.agent.Agent;
-//import org.nlogo.agent.Importer;
-//import org.nlogo.api.CompilerException;
-//import org.nlogo.api.Dump;
-//import org.nlogo.api.HubNetInterface;
-//import org.nlogo.api.LogoException;
-//import org.nlogo.api.ModelType;
-//import org.nlogo.api.ModelTypeJ;
-//import org.nlogo.api.Token;
-//import org.nlogo.nvm.Activation;
-//import org.nlogo.nvm.Command;
-//import org.nlogo.nvm.FileManager;
-//import org.nlogo.nvm.Job;
-//import org.nlogo.nvm.JobManagerInterface;
-//import org.nlogo.nvm.MutableLong;
-//import org.nlogo.nvm.Procedure;
-//import org.nlogo.nvm.Workspace;
-//import org.nlogo.util.Femto;
-
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
