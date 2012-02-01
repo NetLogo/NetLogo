f9db789d0ec923b1ff0468b8e7305b29e3e3727f
Seth Tisue <seth@tisue.net>
2012-02-01 07:26:28 -0600

    simplify mirroring.ClientWorld

--- src/main/org/nlogo/hubnet/mirroring/ClientWorldS.scala
+++ src/main/org/nlogo/hubnet/mirroring/ClientWorldS.scala
@@ -5,33 +5,19 @@ import java.util.Comparator
 private object ClientWorldS {
 
   case class TurtleKey(who: Long, breedIndex: Int)
-
+  private val turtleOrdering = implicitly[Ordering[(Int, Long)]]
   class TurtleKeyComparator extends Comparator[TurtleKey] {
-    override def compare(tk1: TurtleKey, tk2: TurtleKey) =
-      if (tk1.breedIndex == tk2.breedIndex)
-        (tk1.who - tk2.who).toInt
-      else
-        tk1.breedIndex - tk2.breedIndex
+    override def compare(key1: TurtleKey, key2: TurtleKey) =
+      turtleOrdering.compare((key1.breedIndex, key1.who),
+                             (key2.breedIndex, key2.who))
   }
 
   case class LinkKey(id: Long, end1: Long, end2: Long, breedIndex: Int)
-
+  private val linkOrdering = implicitly[Ordering[(Long, Long, Int, Long)]]
   class LinkKeyComparator extends Comparator[LinkKey] {
-    override def compare(key1: LinkKey, key2: LinkKey): Int = {
-      if (key1.end1 == key2.end1) {
-        if (key1.end2 == key2.end2) {
-          if (key1.breedIndex == key2.breedIndex) {
-            return (key1.id - key2.id).toInt
-          } else {
-            return key1.breedIndex - key2.breedIndex
-          }
-        } else {
-          return (key1.end2 - key2.end2).toInt
-        }
-      } else {
-        return (key1.end1 - key2.end1).toInt
-      }
-    }
+    override def compare(key1: LinkKey, key2: LinkKey): Int =
+      linkOrdering.compare((key1.end1, key1.end2, key1.breedIndex, key1.id),
+                           (key2.end1, key2.end2, key2.breedIndex, key2.id))
   }
 
 }
