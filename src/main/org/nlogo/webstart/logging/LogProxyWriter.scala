db5793b9907825fec053fb623f6c8cdfa6d3a591
TheBizzle <jason.bertsche@gmail.com>
2012-04-19 12:11:22 -0500

    Fixing LogProxyWriter (flush() no longer throws a silent exception)

--- src/main/org/nlogo/webstart/logging/LogProxyWriter.scala
+++ src/main/org/nlogo/webstart/logging/LogProxyWriter.scala
@@ -7,7 +7,7 @@ import message.DirectorMessage.{ToDirectorWrite, ToDirectorAbandon, ToDirectorFi
 class LogProxyWriter(mode: LogSendingMode, destinations: URL*) extends Writer {
   private val director = new LogDirector(mode, destinations: _*).start()
   override def write(cbuf: Array[Char], off: Int, len: Int) { director ! ToDirectorWrite(cbuf.subSequence(off, off + len).toString) }
-  override def flush() { throw new UnsupportedOperationException("Cannot flush a LogProxyWriter; the director manages its own flushing.") }
+  override def flush()  { /* While the Director manages its own flushing, making this throw an exception breaks everything; ignore operation */ }
   /*none*/ def delete() { director ! ToDirectorAbandon }
-  override def close() { director ! ToDirectorFinalize }
+  override def close()  { director ! ToDirectorFinalize }
 }
