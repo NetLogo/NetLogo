d16a7d92b25c6d9897bb4b558ce202085d302f94
TheBizzle <jason.bertsche@gmail.com>
2012-05-02 15:48:07 -0500

    Altering LogDirector for substantially better speed/memory performance

--- src/main/org/nlogo/log/webstart/LogDirector.scala
+++ src/main/org/nlogo/log/webstart/LogDirector.scala
@@ -5,6 +5,7 @@ import actors.{TIMEOUT, Actor}
 import message.{DirectorMessage, LogManagementMessage, LoggingServerMessage}
 import LogManagementMessage.{Write, Abandon, Flush, Read, Finalize}
 import LoggingServerMessage.{ToServerWrite, ToServerPulse, ToServerAbandon, ToServerFinalize}
+import collection.mutable.ListBuffer
 
 // An actor controller; receives logging data and figures out what to do with it
 class LogDirector(val mode: LogSendingMode, destinations: URL*) extends Actor {
@@ -21,7 +22,7 @@ class LogDirector(val mode: LogSendingMode, destinations: URL*) extends Actor {
     loop {
       react {
         case Flush =>
-          transmitFormatted((LogBufferManager !? Read).asInstanceOf[List[String]])
+          transmitFormatted((LogBufferManager !? Read).asInstanceOf[Seq[String]])
 
         case ToDirectorWrite(x) =>
           LogBufferManager ! Write(x)
@@ -34,7 +35,7 @@ class LogDirector(val mode: LogSendingMode, destinations: URL*) extends Actor {
 
         case ToDirectorFinalize =>
           actorConditionTuple map (_._1) filterNot (_ == LogBufferManager) foreach (_ ! Finalize)
-          transmitFormatted((LogBufferManager !? Finalize).asInstanceOf[List[String]])
+          transmitFormatted((LogBufferManager !? Finalize).asInstanceOf[Seq[String]])
           finalizeLog()
           replyClosing()
           exit("That's a cut!")
@@ -47,7 +48,7 @@ class LogDirector(val mode: LogSendingMode, destinations: URL*) extends Actor {
   private def abandonLog()  { transmit(ToServerAbandon.toString) }
   private def finalizeLog() { transmit(ToServerFinalize.toString) }
 
-  private def transmitFormatted(messages: List[String]) {
+  private def transmitFormatted(messages: Seq[String]) {
     val msgListOpt = if (messages filterNot (_.isEmpty) isEmpty) None else Some(messages)
     msgListOpt map (_ map (ToServerWrite(_).toString)) getOrElse (List(ToServerPulse.toString)) foreach transmit
   }
@@ -74,9 +75,9 @@ class LogDirector(val mode: LogSendingMode, destinations: URL*) extends Actor {
   private object LogBufferManager extends Actor {
 
     // Only change this constant if you have a veeeeery good reason; it has been specially tuned to this value on purpose --JAB (4/26/12)
-    private val MessageCharLimit = 30000
+    private val MessageCharLimit = 12000
 
-    private val dataBuffer = new collection.mutable.ListBuffer[String]()
+    private val dataBuffer = new ListBuffer[String]()
 
     def act() {
       loop {
@@ -93,31 +94,49 @@ class LogDirector(val mode: LogSendingMode, destinations: URL*) extends Actor {
       dataBuffer += data
     }
 
-    private def flushBuffer() : List[String] = {
+    private def flushBuffer() : Seq[String] = {
+
       // Sequentially accumulates the passed-in strings into strings that are as large as possible
       // while still adhering to the restriction that `accumulatedStr.size < MessageCharLimit`
-      // Note: Could be made more efficient by retaining length info across calls
-      // Note: Potential infinite recursion bug when passing in any string that has a size of > `MessageCharLimit`
-      def condenseToLimitedStrs(inContents: List[String]) : List[String] = {
-        def condensationHelper(remainingContents: List[String]) : List[String] = {
-          remainingContents match {
-            case Nil => Nil
-            case _   =>
-              val totalChars = remainingContents map (_.size) sum
-              val accumSizeToElemList = remainingContents.foldLeft(List[(Int, String)]()){
-                case (Nil, x) => (totalChars, x) :: Nil
-                case (acc, x) => (acc.head._1 - acc.head._2.size, x) :: acc
-              }.reverse
-              val (canGoSizeStrPairs, mustWaitSizeStrPairs) = accumSizeToElemList partition (_._1 < MessageCharLimit)
-              canGoSizeStrPairs.map(_._2).mkString :: condensationHelper(mustWaitSizeStrPairs map (_._2))
+      // Note: Before anyone gets clever and tries to convert this into functional-style code... please don't!
+      //       When functional, this code suffered from performance issues (in both speed and memory).
+      //       I understand that I could have improved the code to be better in both regards, but I think it's
+      //       preferable to be as fast as possible here--which means using an imperative style  --JAB (4/30/12)
+      def condenseToLimitedStrs(inContents: Array[String]): Seq[String] = {
+
+        val bigBuffer = new ListBuffer[String]()
+        val littleBuffer = new ListBuffer[String]()
+
+        var i = 0
+
+        while (i < inContents.size) {
+
+          var size = 0
+          var curr = inContents(i)
+
+          // Potential massive breakage of code if `curr` is, itself, more than `MessageCharLimit` in size
+          while ((size + curr.size) <= MessageCharLimit && (i < inContents.size)) {
+            littleBuffer += curr
+            size += curr.size
+            i += 1
+            if (i < inContents.size) curr = inContents(i)
           }
+
+          bigBuffer += littleBuffer.mkString
+          littleBuffer.clear()
+
         }
-        condensationHelper(inContents).reverse
+
+        bigBuffer.toSeq
+
       }
-      val bufferContents = dataBuffer.toList
+
+      val bufferContents = dataBuffer.toArray
       dataBuffer.clear()
       condenseToLimitedStrs(bufferContents)
+
     }
+
   }
 
   // Essentially, a timer that reminds the Director to request buffer flushes from the LogBufferManager
