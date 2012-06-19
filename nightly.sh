5b6b111c8bc249c9b176e70865769337f83281ba
Seth Tisue <seth@tisue.net>
2012-06-19 14:01:05 -0400

    hopefully get nightly.sh passing again on java5 branch

--- nightly.sh
+++ nightly.sh
@@ -14,30 +14,33 @@ make
 rm -rf tmp/nightly
 mkdir -p tmp/nightly
 
+# it's absurd, but on java5 branch, we need to clean every time
+# because of the usual sbt issue with dist/java5/classes.jar - ST 6/19/12
+
 # here we're using pipes so "-e" isn't enough to stop when something fails.
 # maybe there's an easier way, than I've done it below, I don't know. 
 # I suck at shell scripting - ST 2/15/11
-bin/sbt fast:test 2>&1 | tee tmp/nightly/0-fast-test.txt
+bin/sbt clean fast:test 2>&1 | tee tmp/nightly/0-fast-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: fast:test"; exit 1; fi
 echo "*** done: fast:test"
-bin/sbt nogen fast:test 2>&1 | tee tmp/nightly/1-nogen-fast-test.txt
+bin/sbt clean nogen fast:test 2>&1 | tee tmp/nightly/1-nogen-fast-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen fast:test"; exit 1; fi
 echo "*** done: nogen fast:test"
-bin/sbt threed fast:test 2>&1 | tee tmp/nightly/2-threed-fast-test.txt
+bin/sbt clean threed fast:test 2>&1 | tee tmp/nightly/2-threed-fast-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: threed fast:test"; exit 1; fi
 echo "*** done: threed fast:test"
 
-bin/sbt slow:test 2>&1 | tee tmp/nightly/3-slow-test.txt
+bin/sbt clean slow:test 2>&1 | tee tmp/nightly/3-slow-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: slow:test"; exit 1; fi
 echo "*** done: slow:test"
-bin/sbt nogen slow:test 2>&1 | tee tmp/nightly/4-nogen-slow-test.txt
+bin/sbt clean nogen slow:test 2>&1 | tee tmp/nightly/4-nogen-slow-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen slow:test"; exit 1; fi
 echo "*** done: nogen slow:test"
-bin/sbt threed slow:test 2>&1 | tee tmp/nightly/5-threed-slow-test.txt
+bin/sbt clean threed slow:test 2>&1 | tee tmp/nightly/5-threed-slow-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: threed slow:test"; exit 1; fi
 echo "*** done: threed slow:test"
 
-bin/sbt depend 2>&1 | tee tmp/nightly/6-depend.txt
+bin/sbt clean depend 2>&1 | tee tmp/nightly/6-depend.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: depend"; exit 1; fi
 echo "*** done: depend"
 
