4ec4d5c0a6424e6fa52605f9787d12f6acd85807
Seth Tisue <seth@tisue.net>
2012-07-06 23:02:17 -0400

    sbt build: 'extensions' task fails if a sub-build fails

--- project/Extensions.scala
+++ project/Extensions.scala
@@ -20,12 +20,14 @@ object Extensions {
   private def buildExtension(dir: File, scalaLibrary: File, log: Logger): File = {
     log.info("building extension: " + dir.getName)
     val jar = dir / (dir.getName + ".jar")
-    if((dir / "build.sbt").exists)
-      Process(Seq("bin/sbt", "package"), dir,
-              "SCALA_JAR" -> scalaLibrary.getPath) ! log
-    else
-      Process(List("make", "-s", jar.getName), dir,
-              "SCALA_JAR" -> scalaLibrary.getPath) ! log
+    val exitCode =
+      if((dir / "build.sbt").exists)
+        Process(Seq("bin/sbt", "package"), dir,
+                "SCALA_JAR" -> scalaLibrary.getPath) ! log
+      else
+        Process(Seq("make", "-s", jar.getName), dir,
+                "SCALA_JAR" -> scalaLibrary.getPath) ! log
+    assert(exitCode == 0, "extension build failed, exitCode = " + exitCode)
     jar
   }
 
