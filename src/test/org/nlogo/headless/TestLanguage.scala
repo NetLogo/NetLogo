305211a9f6d916e544c07dd270e2a8156084edc8
Seth Tisue <seth@tisue.net>
2012-07-18 14:10:14 -0400

    temporarily disable some tests failing on Scala 2.10.0-M5
    SI-5974 again

--- src/test/org/nlogo/headless/TestLanguage.scala
+++ src/test/org/nlogo/headless/TestLanguage.scala
@@ -23,7 +23,7 @@ case class TxtsInDir(dir:String) extends TestFinder {
   override def iterator =
     new File(dir).listFiles
       .filter(_.getName.endsWith(".txt"))
-      .filterNot(_.getName == "HubNet.txt")  // disabled until issues.scala-lang.org/browse/SI-5974 fix released
+      .filterNot(_.getName.containsSlice("HubNet"))  // disabled until issues.scala-lang.org/browse/SI-5974 fix released
       .iterator
 }
 case object ExtensionTestsDotTxt extends TestFinder {
