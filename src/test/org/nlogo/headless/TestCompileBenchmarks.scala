e03d8165cbad49a5ec09fac59f37601c6148a27a
Seth Tisue <seth@tisue.net>
2012-07-20 08:21:24 -0400

    whoops fix branch name in TestCompileBenchmarks

--- src/test/org/nlogo/headless/TestCompileBenchmarks.scala
+++ src/test/org/nlogo/headless/TestCompileBenchmarks.scala
@@ -42,5 +42,5 @@ object BranchName {
   // directory with the dumps for the master branch here in the main repo, and for any branch here
   // which needs different dumps, there's a corresponding directory in the models repo.
   // A bit kludgy I guess, but it's OK. - ST 11/17/11
-  val branch = "api-program-refactoring"
+  val branch = "master"
 }
