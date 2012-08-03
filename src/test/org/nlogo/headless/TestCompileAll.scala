180ca90b4abbcff71d215d32f23c1463ca13b596
Seth Tisue <seth@tisue.net>
2012-08-03 12:56:04 -0400

    clean up TestCompileAll for models reorg
    under development models are kept elsewhere now so don't need to be
    accounted for. and bundled extensions no longer have their own .nlogo
    files in them, those are in Code Examples now

--- src/test/org/nlogo/headless/TestCompileAll.scala
+++ src/test/org/nlogo/headless/TestCompileAll.scala
@@ -7,39 +7,32 @@ import org.nlogo.workspace.ModelsLibrary
 import org.scalatest.FunSuite
 import org.nlogo.util.SlowTest
 
-class TestCompileAll extends FunSuite with SlowTest{
+class TestCompileAll extends FunSuite with SlowTest {
 
-  for (path <- ModelsLibrary.getModelPaths ++ ModelsLibrary.getModelPathsAtRoot("extensions")) {
+  for (path <- ModelsLibrary.getModelPaths)
     test("compile: " + path) {
       compile(path)
     }
-  }
 
   def compile(path: String) {
     import java.io.File.separatorChar
     def pathMatches(bad: String) =
       path.toUpperCase.containsSlice(separatorChar + bad + separatorChar)
-    if (pathMatches("DOESN'T COMPILE") ||
-        // letting the textbook team deal with these should help ensure
-        // the book gets updated too - ST 4/22/10
-        pathMatches("TEXTBOOK MODELS") ||
-        !Version.is3D && path.endsWith(".nlogo3d") ||
+    if (!Version.is3D && path.endsWith(".nlogo3d") ||
         // in 3D skip models that aren't in the 3D directory.
         Version.is3D && !pathMatches("3D"))
       return
     val workspace = HeadlessWorkspace.newInstance
-    // this keeps patches from being created, which we don't need,
-    // and which was slowing things down - ST 1/13/05
-    // and has some other effects - ST 12/6/07
+    // compilerTestingMode keeps patches from being created, which we don't need (and which was
+    // slowing things down), and has some other effects too - ST 1/13/05, 12/6/07
     workspace.compilerTestingMode = true
     try {
       workspace.open(path)
-      // compile BehaviorSpace experiments
       val lab = HeadlessWorkspace.newLab
       lab.load(HeadlessModelOpener.protocolSection(path))
       lab.names.foreach(lab.newWorker(_).compile(workspace))
     }
-    finally {workspace.dispose()}
+    finally workspace.dispose()
   }
 
 }
