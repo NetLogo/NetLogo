09d9481cec7b0bdb33790ab284c915c6c9ecbfeb
Seth Tisue <seth@tisue.net>
2012-08-07 17:03:54 -0400

    model runs: split off (and re-enable) TestMirroringModels

--- headless/src/test/org/nlogo/headless/TestMirroring.scala
+++ headless/src/test/org/nlogo/headless/TestMirroring.scala
@@ -13,16 +13,20 @@ import mirror._
 import Mirroring._
 import Mirrorables._
 
-class TestMirroring extends FunSuite {
-
-  def sizes(u: Update) =
-    (u.births.size, u.deaths.size, u.changes.size)
-
+object TestMirroring {
   def withWorkspace[T](body: (HeadlessWorkspace, () => Iterable[Mirrorable]) => T): T = {
     val ws = HeadlessWorkspace.newInstance
     try body(ws, () => allMirrorables(ws.world, ws.plotManager.plots))
     finally ws.dispose()
   }
+}
+
+class TestMirroring extends FunSuite {
+
+  import TestMirroring.withWorkspace
+
+  def sizes(u: Update) =
+    (u.births.size, u.deaths.size, u.changes.size)
 
   def checkAllAgents(ws: HeadlessWorkspace, state: State) {
     def check[A <: api.Agent](agentSet: api.AgentSet, kind: Kind, toMirrorable: A => MirrorableAgent[A]) {
@@ -178,84 +182,6 @@ class TestMirroring extends FunSuite {
 //
 //      expect(realChecksum) { mirrorChecksum }
 //
-//    }
-//  }
-
-  private val testSerializer = true
-
-  def modelRenderingTest(path: String) {
-    withWorkspace { (ws, mirrorables) =>
-      ws.open(path)
-      ws.command("random-seed 0")
-      ws.command(ws.previewCommands)
-      val (m0, u0) = diffs(Map(), mirrorables())
-      var state = Mirroring.merge(
-        Map(),
-        if (testSerializer)
-          Serializer.fromBytes(Serializer.toBytes(u0))
-        else
-          u0)
-      // should I test that m0 and state are identical? maybe have a separate test for that
-      val dummy = new FakeWorld(state)
-      val pico = new Pico
-      pico.add("org.nlogo.render.Renderer")
-      pico.addComponent(dummy)
-      val renderer = pico.getComponent(classOf[api.RendererInterface])
-      renderer.resetCache(ws.patchSize)
-
-      val realChecksum =
-        Checksummer.calculateGraphicsChecksum(ws.renderer, ws)
-      val mirrorChecksum =
-        Checksummer.calculateGraphicsChecksum(renderer, ws)
-
-      def exportPNG(r: api.RendererInterface, suffix: String) = {
-        new java.io.File("tmp").mkdir()
-        val outputFile = new java.io.File(path).getName + "." + suffix + ".png"
-        val outputPath = new java.io.File("tmp/" + outputFile)
-        javax.imageio.ImageIO.write(r.exportView(ws), "png", outputPath)
-      }
-
-      if (mirrorChecksum != realChecksum) {
-        exportPNG(ws.renderer, "original")
-        exportPNG(renderer, "mirror")
-      }
-
-      expect(realChecksum) { mirrorChecksum }
-    }
-  }
-
-  // should eventually be updated to handle 2D/3D distinction - ST 8/6/12
-  def allTestableModels = {
-    def hasExclusion(fileName: String) = {
-      val exclusions = Seq(
-        "SYSTEM-DYNAMICS-SETUP",
-        "MOUSE-DOWN?",
-        "USER-MESSAGE",
-        "MOUSE-INSIDE?",
-        "NEED-TO-MANUALLY-MAKE-PREVIEW-FOR-THIS-MODEL",
-        "USER-YES-OR-NO?",
-        "USER-INPUT",
-        "GIS:")
-      io.Source.fromFile(fileName)
-        .getLines.exists { line =>
-          exclusions.exists { exclusion =>
-            line.toUpperCase contains exclusion
-          }
-        }
-    }
-    io.Source.fromFile("models/test/checksums.txt")
-      .getLines.map(_.split(" - ")(0)) // get model paths
-      .filterNot(hasExclusion)
-  }
-
-  def modelsToTest = Seq(
-    "models/Sample Models/Networks/Diffusion on a Directed Network.nlogo")
-
-    // Only "models/Sample Models/Networks/Diffusion on a Directed Network.nlogo"
-    // is failing, but still disabling for now (NP 2012-07-26)
-//  allTestableModels.foreach { modelPath =>
-//    test("Mirroring: " + modelPath) {
-//      modelRenderingTest(modelPath)
 //    }
 //  }
 
---
+++ src/test/org/nlogo/headless/TestMirroringModels.scala
@@ -0,0 +1,93 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.headless
+
+import org.scalatest.FunSuite
+import org.nlogo.{ api, mirror }
+import org.nlogo.util.{ Pico, SlowTest }
+import mirror._
+import Mirroring._
+import Mirrorables._
+import TestMirroring.withWorkspace
+
+class TestMirroringModels extends FunSuite with SlowTest {
+
+  private val testSerializer = true
+
+  def modelRenderingTest(path: String) {
+    withWorkspace { (ws, mirrorables) =>
+      ws.open(path)
+      ws.command("random-seed 0")
+      ws.command(ws.previewCommands)
+      val (m0, u0) = diffs(Map(), mirrorables())
+      var state = Mirroring.merge(
+        Map(),
+        if (testSerializer)
+          Serializer.fromBytes(Serializer.toBytes(u0))
+        else
+          u0)
+      // should I test that m0 and state are identical? maybe have a separate test for that
+      val dummy = new FakeWorld(state)
+      val pico = new Pico
+      pico.add("org.nlogo.render.Renderer")
+      pico.addComponent(dummy)
+      val renderer = pico.getComponent(classOf[api.RendererInterface])
+      renderer.resetCache(ws.patchSize)
+
+      val realChecksum =
+        Checksummer.calculateGraphicsChecksum(ws.renderer, ws)
+      val mirrorChecksum =
+        Checksummer.calculateGraphicsChecksum(renderer, ws)
+
+      def exportPNG(r: api.RendererInterface, suffix: String) = {
+        new java.io.File("tmp").mkdir()
+        val outputFile = new java.io.File(path).getName + "." + suffix + ".png"
+        val outputPath = new java.io.File("tmp/" + outputFile)
+        javax.imageio.ImageIO.write(r.exportView(ws), "png", outputPath)
+      }
+
+      if (mirrorChecksum != realChecksum) {
+        exportPNG(ws.renderer, "original")
+        exportPNG(renderer, "mirror")
+      }
+
+      expect(realChecksum) { mirrorChecksum }
+    }
+  }
+
+  // should eventually be updated to handle 2D/3D distinction - ST 8/6/12
+  def allTestableModels = {
+    def hasExclusion(fileName: String) = {
+      val exclusions = Seq(
+        "SYSTEM-DYNAMICS-SETUP",
+        "MOUSE-DOWN?",
+        "USER-MESSAGE",
+        "MOUSE-INSIDE?",
+        "NEED-TO-MANUALLY-MAKE-PREVIEW-FOR-THIS-MODEL",
+        "USER-YES-OR-NO?",
+        "USER-INPUT",
+        "GIS:")
+      io.Source.fromFile(fileName)
+        .getLines.exists { line =>
+          exclusions.exists { exclusion =>
+            line.toUpperCase contains exclusion
+          }
+        }
+    }
+    io.Source.fromFile("models/test/checksums.txt")
+      .getLines.map(_.split(" - ")(0)) // get model paths
+      .filterNot(hasExclusion)
+  }
+
+  def modelsToTest = Seq(
+    "models/Sample Models/Networks/Diffusion on a Directed Network.nlogo")
+
+   allTestableModels.foreach { modelPath =>
+     // exclude 1 model for now, failing & we don't know why yet
+     if(!modelPath.endsWith("Diffusion on a Directed Network.nlogo"))
+       test("Mirroring: " + modelPath) {
+         modelRenderingTest(modelPath)
+       }
+   }
+
+}
