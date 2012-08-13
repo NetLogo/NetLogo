b6345a19b5a6f68c51fc76379ab1574b57bcc0f5
Seth Tisue <seth@tisue.net>
2012-08-13 09:59:53 -0400

    convert some _patch* prims to Scala

---
+++ headless/src/main/org/nlogo/prim/_patchat.scala
@@ -0,0 +1,26 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.prim
+
+import org.nlogo.agent.Patch
+import org.nlogo.api.{ Syntax, AgentException, Nobody }
+import org.nlogo.nvm.{ Reporter, Context }
+
+class _patchat extends Reporter {
+
+  override def syntax =
+    Syntax.reporterSyntax(
+      Array(Syntax.NumberType, Syntax.NumberType),
+      Syntax.PatchType | Syntax.NobodyType,
+      "-TP-")
+
+  // I've tried to rejigger this and the result gets past TryCatchSafeChecker but then
+  // doesn't work at runtime ("Inconsistent stack height") - ST 2/10/09
+  override def report(context: Context) = {
+    val dx = argEvalDoubleValue(context, 0)
+    val dy = argEvalDoubleValue(context, 1)
+    try context.agent.getPatchAtOffsets(dx, dy)
+    catch { case e: AgentException => Nobody }
+  }
+
+}
---
+++ headless/src/main/org/nlogo/prim/_patchhereinternal.scala
@@ -0,0 +1,26 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.prim
+
+import org.nlogo.agent.{ Turtle, Patch }
+import org.nlogo.api.Syntax
+import org.nlogo.nvm.{ Reporter, Context }
+
+// needed by _patchat.optimize() because regular _patchhere is turtle-only
+
+class _patchhereinternal extends Reporter {
+
+  override def syntax =
+    Syntax.reporterSyntax(
+      Syntax.PatchType, "-TP-")
+
+  override def report(context: Context) =
+    report_1(context)
+
+  def report_1(context: Context): Patch =
+    context.agent match {
+      case p: Patch => p
+      case t: Turtle => t.getPatchHere
+    }
+
+}
