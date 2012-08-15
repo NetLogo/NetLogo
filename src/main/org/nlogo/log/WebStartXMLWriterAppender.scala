64b8d50cefd0588acfabc3667fc4e967c986dc6d
TheBizzle <jason.bertsche@gmail.com>
2012-08-15 16:37:25 -0500

    Fixing IntelliJ's automated massacre of my code style

--- src/main/org/nlogo/log/WebStartXMLWriterAppender.scala
+++ src/main/org/nlogo/log/WebStartXMLWriterAppender.scala
@@ -12,54 +12,26 @@ import org.apache.log4j.{Appender, WriterAppender}
  */
 class WebStartXMLWriterAppender(mode: LogSendingMode, destinations: URL*)
                                (writer: RemoteLogWriter = new RemoteLogWriter(mode, destinations: _*))
-        extends WriterAppender(new XMLLayout, writer) with XMLAppender with WebStartAppender {
-  /*none*/
-  def deleteLog() {
-    writer.delete()
-  }
-
-  /*none*/
-  def initialize() {
-    initializeTransformer(destinations.mkString("|"), writer)
-  }
-
-  override def close() {
-    closeDocument(); super.close()
-  }
+                               extends WriterAppender(new XMLLayout, writer) with XMLAppender with WebStartAppender {
+  /*none*/ def deleteLog() { writer.delete() }
+  /*none*/ def initialize() { initializeTransformer(destinations.mkString("|"), writer) }
+  override def close() { closeDocument(); super.close() }
 }
 
 
 // An interface for log-handling `Appender`s that operate through Java WebStart
 trait WebStartAppender extends Appender {
-  /*none*/
-  def deleteLog()
-
-  /*none*/
-  def initialize()
+  /*none*/ def deleteLog()
+  /*none*/ def initialize()
 }
 
 
 // A `Writer` subclass for writing log entries to remote locations
 class RemoteLogWriter(mode: LogSendingMode, destinations: URL*) extends Writer {
-
   import DirectorMessage.{ToDirectorWrite, ToDirectorAbandon, ToDirectorFinalize}
-
   private val director = new LogDirector(mode, destinations: _*).start()
-
-  override def write(cbuf: Array[Char], off: Int, len: Int) {
-    director ! ToDirectorWrite(cbuf.subSequence(off, off + len).toString)
-  }
-
-  override def flush() {
-    /* While the Director manages its own flushing, making this throw an exception breaks everything; ignore operation */
-  }
-
-  /*none*/
-  def delete() {
-    director !? ToDirectorAbandon
-  }
-
-  override def close() {
-    director !? ToDirectorFinalize
-  } // It's important that this blocks while waiting for operation to complete
+  override def write(cbuf: Array[Char], off: Int, len: Int) { director ! ToDirectorWrite(cbuf.subSequence(off, off + len).toString) }
+  override def flush()  { /* While the Director manages its own flushing, making this throw an exception breaks everything; ignore operation */ }
+  /*none*/ def delete() { director !? ToDirectorAbandon }
+  override def close()  { director !? ToDirectorFinalize } // It's important that this blocks while waiting for operation to complete
 }
