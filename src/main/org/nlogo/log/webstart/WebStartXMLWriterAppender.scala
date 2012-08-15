02ab7b7d4d003d8a07035a2624e5ddd260fc327f
TheBizzle <jason.bertsche@gmail.com>
2012-08-15 15:38:44 -0500

    Refactoring RemoteLogWriter into WebStartXMLWriterAppender.scala

--- src/main/org/nlogo/log/webstart/WebStartXMLWriterAppender.scala
+++ src/main/org/nlogo/log/webstart/WebStartXMLWriterAppender.scala
@@ -1,5 +1,6 @@
 package org.nlogo.log.webstart
 
+import java.io.Writer
 import java.net.URL
 import org.nlogo.log.{XMLAppender, XMLLayout}
 import org.apache.log4j.{Appender, WriterAppender}
@@ -18,7 +19,20 @@ class WebStartXMLWriterAppender(mode: LogSendingMode, destinations: URL*)
   override def close()      { closeDocument(); super.close() }
 }
 
+
+// An interface for log-handling `Appender`s that operate through Java WebStart
 trait WebStartAppender extends Appender {
   /*none*/ def deleteLog()
   /*none*/ def initialize()
+}
+
+
+// A `Writer` subclass for writing log entries to remote locations
+class RemoteLogWriter(mode: LogSendingMode, destinations: URL*) extends Writer {
+  import DirectorMessage.{ToDirectorWrite, ToDirectorAbandon, ToDirectorFinalize}
+  private val director = new LogDirector(mode, destinations: _*).start()
+  override def write(cbuf: Array[Char], off: Int, len: Int) { director ! ToDirectorWrite(cbuf.subSequence(off, off + len).toString) }
+  override def flush()  { /* While the Director manages its own flushing, making this throw an exception breaks everything; ignore operation */ }
+  /*none*/ def delete() { director !? ToDirectorAbandon }
+  override def close()  { director !? ToDirectorFinalize } // It's important that this blocks while waiting for operation to complete
 }
