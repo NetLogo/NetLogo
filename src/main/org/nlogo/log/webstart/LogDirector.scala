5d25cb53b311782d05c70e4714c3c3102b0ee6e0
TheBizzle <jason.bertsche@gmail.com>
2012-08-15 15:15:58 -0500

    Code cleanup of LogDirector

--- src/main/org/nlogo/log/webstart/LogDirector.scala
+++ src/main/org/nlogo/log/webstart/LogDirector.scala
@@ -204,24 +204,10 @@ object DirectorMessage {
 private[webstart] sealed trait LoggingServerMessage
 
 private[webstart] object LoggingServerMessage {
-
   private val Sep = "|"
-
-  case class ToServerWrite(data: String) {
-    override def toString: String = "write%s%s".format(Sep, data)
-  }
-
-  case object ToServerPulse {
-    override def toString: String = "pulse"
-  }
-
-  case object ToServerAbandon {
-    override def toString: String = "abandon"
-  }
-
-  case object ToServerFinalize {
-    override def toString: String = "finalize"
-  }
-
+  case class ToServerWrite(data: String) { override def toString = "write%s%s".format(Sep, data) }
+  case object ToServerPulse              { override def toString = "pulse" }
+  case object ToServerAbandon            { override def toString = "abandon" }
+  case object ToServerFinalize           { override def toString = "finalize" }
 }
 
