7ce71ce35459a124e7c87bc2299b1f50c6bbb6e5
TheBizzle <jason.bertsche@gmail.com>
2012-08-15 15:05:53 -0500

    Renaming LogProxyWriter => RemoteLogWriter

--- src/main/org/nlogo/log/webstart/RemoteLogWriter.scala
+++ src/main/org/nlogo/log/webstart/RemoteLogWriter.scala
@@ -5,7 +5,7 @@ import java.net.URL
 import message.DirectorMessage.{ToDirectorWrite, ToDirectorAbandon, ToDirectorFinalize}
 
 // A `Writer` subclass for writing log entries to remote locations
-class LogProxyWriter(mode: LogSendingMode, destinations: URL*) extends Writer {
+class RemoteLogWriter(mode: LogSendingMode, destinations: URL*) extends Writer {
   private val director = new LogDirector(mode, destinations: _*).start()
   override def write(cbuf: Array[Char], off: Int, len: Int) { director ! ToDirectorWrite(cbuf.subSequence(off, off + len).toString) }
   override def flush()  { /* While the Director manages its own flushing, making this throw an exception breaks everything; ignore operation */ }
--- src/main/org/nlogo/log/webstart/WebStartXMLWriterAppender.scala
+++ src/main/org/nlogo/log/webstart/WebStartXMLWriterAppender.scala
@@ -5,13 +5,13 @@ import org.nlogo.log.{XMLAppender, XMLLayout}
 import org.apache.log4j.WriterAppender
 
 /*
- The `LogProxyWriter` pretty much needs to be a default param in a sneaky second parameter list here, because:
-   a) I don't want the user of this class to worry about constructing `LogProxyWriter`s
+ The `RemoteLogWriter` pretty much needs to be a default param in a sneaky second parameter list here, because:
+   a) I don't want the user of this class to worry about constructing `RemoteLogWriter`s
    b) We have to extend `WriterAppender` here
    c) A `Writer` needs to be passed to the constructor of `WriterAppender` in order for us to extend it
  */
 class WebStartXMLWriterAppender(mode: LogSendingMode, destinations: URL*)
-                               (writer: LogProxyWriter = new LogProxyWriter(mode, destinations: _*))
+                               (writer: RemoteLogWriter = new RemoteLogWriter(mode, destinations: _*))
                                 extends WriterAppender(new XMLLayout, writer) with XMLAppender with WebStartAppender {
   /*none*/ def deleteLog()  { writer.delete() }
   /*none*/ def initialize() { initializeTransformer(destinations.mkString("|"), writer) }
