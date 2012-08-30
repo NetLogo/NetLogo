6c2a79c347bd6f147ef7694e30fe79f6c1581a82
TheBizzle <jason.bertsche@gmail.com>
2012-08-30 13:43:04 -0500

    Reinserting 'implicit' keyword that I shouldn't have removed

--- src/main/org/nlogo/log/WebStartXMLWriterAppender.scala
+++ src/main/org/nlogo/log/WebStartXMLWriterAppender.scala
@@ -11,7 +11,7 @@ import org.apache.log4j.{Appender, WriterAppender}
    c) A `Writer` needs to be passed to the constructor of `WriterAppender` in order for us to extend it
  */
 class WebStartXMLWriterAppender(mode: LogSendingMode, destinations: URL*)
-                               (writer: RemoteLogWriter = new RemoteLogWriter(mode, destinations: _*))
+                               (implicit writer: RemoteLogWriter = new RemoteLogWriter(mode, destinations: _*))
                                extends WriterAppender(new XMLLayout, writer) with XMLAppender with WebStartAppender {
   /*none*/ def deleteLog() { writer.delete() }
   /*none*/ def initialize() { initializeTransformer(destinations.mkString("|"), writer) }
