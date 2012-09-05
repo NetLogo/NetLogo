3b8728d250af16f6bdccb8b139ff61bbe8687229
Seth Tisue <seth@tisue.net>
2012-09-05 12:23:20 -0400

    fix theoretical performance issue in ModelReader.restoreLines
    by using Vector to accumulate instead of String, since Vector supports
    efficient incremental building
    
    my old code and Nicolas's new code were both O(n^2).  I measured it in
    the REPL and processing 10,000 input chars took 0.1 seconds and
    processing 100,000 input chars took 10 seconds.  we're not expecting
    more than about 1,000 input chars in practice, but it was bugging me
    anyway. the new code can handle a million characters of input in half
    a second.

--- headless/src/main/org/nlogo/api/ModelReader.scala
+++ headless/src/main/org/nlogo/api/ModelReader.scala
@@ -70,19 +70,19 @@ object ModelReader {
 
   def restoreLines(s: String): String = {
     @scala.annotation.tailrec
-    def loop(acc: String, rest: String): String = {
+    def loop(acc: Vector[Char], rest: String): Vector[Char] = {
       if (rest.size < 2)
-        acc + rest
+        acc ++ rest
       else if (rest.head == '\\')
         rest.tail.head match {
-          case 'n'  => loop(acc + '\n', rest.tail.tail)
-          case '\\' => loop(acc + '\\', rest.tail.tail)
-          case '"'  => loop(acc + '"', rest.tail.tail)
+          case 'n'  => loop(acc :+ '\n', rest.tail.tail)
+          case '\\' => loop(acc :+ '\\', rest.tail.tail)
+          case '"'  => loop(acc :+ '"', rest.tail.tail)
           case _    => sys.error("invalid escape sequence in \"" + s + "\"")
         }
-      else loop(acc + rest.head, rest.tail)
+      else loop(acc :+ rest.head, rest.tail)
     }
-    loop("", s)
+    loop(Vector(), s).mkString
   }
 
 }
