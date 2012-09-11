cd3e6816e38c47e16dbbec2e3caf33ee9d5cc9e8
Seth Tisue <seth@tisue.net>
2012-09-11 13:22:10 -0400

    convert _extern/report to Scala

---
+++ headless/src/main/org/nlogo/prim/_externreport.scala
@@ -0,0 +1,41 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.prim
+
+import org.nlogo.{ api, nvm }
+import api.Syntax
+
+class _externreport(reporter: api.Reporter) extends nvm.Reporter {
+
+  override def syntax = {
+    val s = reporter.getSyntax
+    val acs = reporter.getAgentClassString.split(":")
+    if (acs(0).size < 4)
+      acs(0) = Syntax.convertOldStyleAgentClassString(acs(0))
+    if (acs.length >= 2) {
+      if (acs(1).size < 4)
+        acs(1) = Syntax.convertOldStyleAgentClassString(acs(1))
+      Syntax.reporterSyntax(
+        s.left, s.right, s.ret, s.precedence, s.dfault, false, acs(0), acs(1))
+    }
+    else
+      Syntax.reporterSyntax(
+        s.left, s.right, s.ret, s.precedence, s.dfault, false, acs(0), null)
+  }
+
+  override def report(context: nvm.Context) = {
+    val arguments = Array.tabulate[api.Argument](args.length)(i =>
+      new nvm.Argument(context, args(i)))
+    try reporter.report(
+      arguments, new nvm.ExtensionContext(workspace, context))
+    catch {
+      case ex: api.ExtensionException =>
+        val ee = new nvm.EngineException(
+          context, this, "Extension exception: " + ex.getMessage)
+        // it might be better to use setCause(), for the long term... but then i think the handler
+        // would have to be changed, too.
+        ee.setStackTrace(ex.getStackTrace)
+        throw ee
+    }
+  }
+}
