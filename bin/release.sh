a11a7be6288e6f439cde78edf769f859c761184f
Seth Tisue <seth@tisue.net>
2012-10-24 22:08:30 -0400

    get install4j working again on current Mac Java
    5bb1b04c1e86f05dd12bb56c691275a1d4607985 failed to get install4jc
    running on Java 7. so use an environment variable it helpfully
    provides to force the compiler to run with Java 6

--- bin/release.sh
+++ bin/release.sh
@@ -131,12 +131,12 @@ for FOO in *
 do
   echo "cleaning extension" $FOO
   cd $FOO
-  rm -f $FOO.jar $FOO.jar.pack.gz
+  # rm -f $FOO.jar $FOO.jar.pack.gz
   cd ..
 done
 cd ..
-rm -f *.jar
-./sbt clean all
+# rm -f *.jar
+./sbt all
 
 # remember version number
 export VERSION=`$JAVA -cp NetLogo.jar:$SCALA_JAR org.nlogo.headless.Main --version | $SED -e "s/NetLogo //"`
@@ -413,7 +413,7 @@ $CHMOD -R go+rX .
 if [ $WINDOWS -eq 1 ]
 then
   $PERL -pi -e "s/\@\@\@VM\@\@\@/$VM/g" NetLogo.install4j
-  "$IJDIR/$IJ" --quiet -r "$COMPRESSEDVERSION" -d "." NetLogo.install4j
+  INSTALL4J_JAVA_HOME_OVERRIDE=`/usr/libexec/java_home -F -v1.6*` "$IJDIR/$IJ" --quiet -r "$COMPRESSEDVERSION" -d "." NetLogo.install4j
   $CHMOD -R a+x *.exe
   $DU -h *.exe
   $MV *.exe ../$COMPRESSEDVERSION
