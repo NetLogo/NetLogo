c1323b2ea515274d20eae3400256ac02e80bb529
Seth Tisue <seth@tisue.net>
2012-12-07 11:26:54 -0500

    fix TestCompileAll so skipped model names aren't printed

--- headless/src/test/org/nlogo/headless/TestCompileAll.scala
+++ headless/src/test/org/nlogo/headless/TestCompileAll.scala
@@ -9,24 +9,26 @@ import org.nlogo.util.SlowTest
 
 class TestCompileAll extends FunSuite with SlowTest {
 
-  for (path <- ModelsLibrary.getModelPaths)
+  for (path <- ModelsLibrary.getModelPaths.filterNot(badPath))
     test("compile: " + path) {
       compile(path)
     }
 
-  def compile(path: String) {
+  // core branch doesn't have these features - ST 1/11/12
+  def badPath(path: String): Boolean = {
     import java.io.File.separatorChar
     def pathMatches(bad: String) =
       path.toUpperCase.containsSlice(separatorChar + bad + separatorChar)
-    if (// core branch doesn't have these features - ST 1/11/12
-        pathMatches("SYSTEM DYNAMICS") ||
-        pathMatches("GIS") ||
-        pathMatches("QUICKTIME EXTENSION") ||
-        pathMatches("HUBNET ACTIVITIES") ||
-        path.containsSlice("GoGoMonitor") ||
-        path.containsSlice("Movie Example") ||
-        path.endsWith(".nlogo3d"))
-      return
+    pathMatches("SYSTEM DYNAMICS") ||
+      pathMatches("GIS") ||
+      pathMatches("QUICKTIME EXTENSION") ||
+      pathMatches("HUBNET ACTIVITIES") ||
+      path.containsSlice("GoGoMonitor") ||
+      path.containsSlice("Movie Example") ||
+      path.endsWith(".nlogo3d")
+  }
+
+  def compile(path: String) {
     val workspace = HeadlessWorkspace.newInstance
     // compilerTestingMode keeps patches from being created, which we don't need (and which was
     // slowing things down), and has some other effects too - ST 1/13/05, 12/6/07
