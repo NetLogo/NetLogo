46d997d1a395fe2faba36186077c31d59f88be5a
Aditi Wagh <aditiwagh@u.northwestern.edu>
2013-01-22 20:18:29 -0600

    Code revisions from Aug & Jan

---
+++ src/main/org/nlogo/deltatick/SpeciesInspector.java
@@ -0,0 +1,144 @@
+package org.nlogo.deltatick;
+
+import org.nlogo.api.Color;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: aditiwagh
+ * Date: 1/20/13
+ * Time: 11:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SpeciesInspector extends JPanel {
+    //JPanel contentPanel = new JPanel();
+    JPanel topPanel = new JPanel();
+    JPanel bottomPanel = new JPanel();
+    BreedBlock myParent;
+    JLabel breedNameLabel = new JLabel("Species");
+    JLabel breedName = new JLabel();
+    JLabel lifeSpanLabel = new JLabel("Age");
+    JLabel startLifeSpan = new JLabel("From");
+    JLabel endLifeSpan = new JLabel("to");
+    JTextField startLifeSpanBlank = new JTextField();
+    JTextField endLifeSpanBlank = new JTextField();
+    JLabel energyLabel = new JLabel("Energy");
+    JLabel lowestEnergy = new JLabel("lowest");
+    JLabel highestEnergy = new JLabel("highest");
+    JLabel lowestEnergyValue = new JLabel("0");
+    JTextField highestEnergyBlank = new JTextField();
+    JButton okayButton = new JButton("Okay"); // placeholder button to test sending values - A. (Jan 22, 2013)
+
+    JFrame myFrame;
+
+    JTabbedPane traitsTabbedPane;
+    int countTabs = 1;
+
+    public SpeciesInspector (BreedBlock myParent, JFrame myFrame) {
+        this.myParent = myParent;
+        this.myFrame = myFrame;
+        breedName.setText(myParent.plural());
+        highestEnergyBlank.setText(myParent.breed.getOwnVarMaxReporter("energy"));
+        endLifeSpanBlank.setText(myParent.breed.getOwnVarMaxReporter("age"));
+        activateOkayButton();
+    }
+
+    public void addPanels(Container pane) {
+        topPanel.setBackground(ColorSchemer.getColor(3));
+        bottomPanel.setBackground(ColorSchemer.getColor(2));
+        TitledBorder titleTopPanel;
+        titleTopPanel = BorderFactory.createTitledBorder("Set up");
+        topPanel.setBorder(titleTopPanel);
+        setupTopPanel();
+
+        TitledBorder titleBottomPanel;
+        titleBottomPanel = BorderFactory.createTitledBorder("Traits");
+        bottomPanel.setBorder(titleBottomPanel);
+        //setupBottomPanel();
+
+
+        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        pane.add(topPanel);
+        pane.add(bottomPanel);
+
+    }
+
+    public void setupTopPanel() {
+        GroupLayout layout = new GroupLayout(topPanel);
+        topPanel.setLayout(layout);
+
+        //startLifeSpanBlank.setMaximumSize();
+        //layout.setAutoCreateGaps(true);
+        //layout.setAutoCreateContainerGaps(true);
+
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(breedNameLabel)
+                    .addComponent(breedName))
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(lifeSpanLabel)
+                    .addComponent(startLifeSpan)
+                    .addComponent(startLifeSpanBlank)
+                    .addComponent(endLifeSpan)
+                    .addComponent(endLifeSpanBlank))
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(energyLabel)
+                    .addComponent(lowestEnergy)
+                    .addComponent(lowestEnergyValue)
+                    .addComponent(highestEnergy)
+                    .addComponent(highestEnergyBlank))
+                .addComponent(okayButton)
+        );
+
+        layout.setHorizontalGroup(
+                layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(breedNameLabel)
+                        .addComponent(lifeSpanLabel)
+                        .addComponent(energyLabel))
+                .addGroup(layout.createParallelGroup()
+                .addComponent(breedName)
+                .addGroup(layout.createSequentialGroup()
+                .addComponent(startLifeSpan)
+                .addComponent(startLifeSpanBlank)
+                .addComponent(endLifeSpan)
+                .addComponent(endLifeSpanBlank))
+                .addGroup(layout.createSequentialGroup()
+                .addComponent(lowestEnergy)
+                .addComponent(lowestEnergyValue)
+                .addComponent(highestEnergy)
+                .addComponent(highestEnergyBlank)))
+                .addComponent(okayButton)
+        );
+        validate();
+    }
+
+    public void setupBottomPanel() {
+        traitsTabbedPane = new JTabbedPane();
+        bottomPanel.add(traitsTabbedPane);
+        if (countTabs == 1) {
+            JPanel panel1 = new JPanel();
+            traitsTabbedPane.addTab("add trait", panel1);
+
+        }
+
+
+    }
+
+    public void activateOkayButton() {
+        okayButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                System.out.println(endLifeSpanBlank.getText());
+                myParent.breed.setOwnVarMaxReporter("age", endLifeSpanBlank.getText());
+                myParent.breed.setOwnVarMaxReporter("energy", highestEnergyBlank.getText());
+                myFrame.setVisible(false);   //TODO how to close a JFrame - A (Jan 23, 2013)
+            }
+        });
+    }
+
+
+}
