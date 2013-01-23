1857220f321d7326dce547ba82bb62a774ca9b46
Aditi Wagh <aditiwagh@u.northwestern.edu>
2013-01-22 21:44:52 -0600

    Adding TraitEditor.java

---
+++ src/main/org/nlogo/deltatick/TraitEditor.java
@@ -0,0 +1,11 @@
+package org.nlogo.deltatick;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: aditiwagh
+ * Date: 1/22/13
+ * Time: 9:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TraitEditor {
+}
