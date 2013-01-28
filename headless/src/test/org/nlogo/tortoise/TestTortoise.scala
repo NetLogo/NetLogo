c688f59193ef2ff2fad9cc1cfd0f6e7fabd9eb77
Seth Tisue <seth@tisue.net>
2013-01-27 23:13:02 -0600

    Tortoise: add Termites model (commented out)

--- headless/src/test/org/nlogo/tortoise/TestTortoise.scala
+++ headless/src/test/org/nlogo/tortoise/TestTortoise.scala
@@ -374,4 +374,89 @@ class TestTortoise extends FunSuite {
     compareCommands("""__ask-sorted patches [output-print (word self " -> " living?) ]""")
   }
 
+  tester("turtle motion") {
+    defineProcedures("", -5, 5, -5, 5)
+    compareCommands("random-seed 0 crt 100")
+    compareCommands("__ask-sorted turtles [ setxy 0.6 * random-xcor 0.6 * random-ycor ]")
+    compareCommands("__ask-sorted turtles [ fd 1 ]")
+  }
+
+/*
+ currently fails because the topology stuff doesn't wrap turtles
+ around the world edges right - ST 1/27/13
+  tester("termites, TODO: WORK IN PROGRESS") {
+    val code =
+      """
+       |turtles-own [next steps]
+       |
+       |to setup
+       |  clear-all
+       |  __ask-sorted patches [
+       |    if random 100 < 20
+       |      [ set pcolor yellow ] ]
+       |  crt 50
+       |  __ask-sorted turtles [
+       |    set color white
+       |    setxy random-xcor random-ycor
+       |    set size 3
+       |    set next 1
+       |  ]
+       |end
+       |
+       |to go
+       |  __ask-sorted turtles
+       |    [ ifelse steps > 0
+       |        [ set steps steps - 1 ]
+       |        [ action
+       |          wiggle ]
+       |      fd 1 ]
+       |end
+       |
+       |to wiggle
+       |  rt random 50
+       |  lt random 50
+       |end
+       |
+       |to action
+       |  ifelse next = 1
+       |    [ searchforchip ]
+       |    [ ifelse next = 2
+       |      [ findnewpile ]
+       |      [ ifelse next = 3
+       |        [ putdownchip ]
+       |        [ getaway ] ] ]
+       |end
+       |
+       |to searchforchip
+       |  if pcolor = yellow
+       |    [ set pcolor black
+       |      set color orange
+       |      set steps 20
+       |      set next 2 ]
+       |end
+       |
+       |to findnewpile
+       |  if pcolor = yellow
+       |    [ set next 3 ]
+       |end
+       |
+       |to putdownchip
+       |  if pcolor = black
+       |   [ set pcolor yellow
+       |     set color white
+       |     set steps 20
+       |     set next 4 ]
+       |end
+       |
+       |to getaway
+       |  if pcolor = black
+       |    [ set next 1 ]
+       |end
+      """.stripMargin
+    defineProcedures(code, -20, 20, -20, 20)
+    compareCommands("random-seed 0 setup")
+    for (_ <- 1 to 20)
+      compareCommands("go")
+  }
+
 }
