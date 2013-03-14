5491834a17a81834822fd0175146a5e0e2f1b1f6
Seth Tisue <seth@tisue.net>
2013-03-14 08:57:14 -0400

    remove unused code

--- src/main/org/nlogo/swing/InputDialog.scala
+++ src/main/org/nlogo/swing/InputDialog.scala
@@ -6,13 +6,6 @@ import java.awt.{ Component, Frame }
 import org.nlogo.awt.Hierarchy.getFrame
 import javax.swing.JOptionPane
 
-object InputDialog {
-  def show(owner: Component, title: String, message: String, defaultInput: String, options: Array[String]): String =
-    javax.swing.JOptionPane.showInputDialog(
-      getFrame(owner), message, title, JOptionPane.QUESTION_MESSAGE,
-      null, null, defaultInput)
-}
-
 class InputDialog(owner: Frame, title: String, message: String, i18n: String => String)
 extends UserDialog(owner, title, i18n) {
   private val field = new javax.swing.JTextField
