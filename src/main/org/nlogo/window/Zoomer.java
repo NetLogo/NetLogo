NetLogo/NetLogo

commit b55c765c0450607df21ff4e377544e42b427896b

make tabs zoom independently, not together

src/main/org/nlogo/window/Zoomer.java
@@ -1,24 +1,22 @@
 // (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
 
-package org.nlogo.app;
+package org.nlogo.window;
 
 // There's a lot of carelessness here about Component vs.
 // Widget that should be cleaned up at some point
 //   - ST 8/9/03, 10/14/03
 
-import org.nlogo.window.Widget;
-
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-strictfp class Zoomer {
+public strictfp class Zoomer {
 
   private final java.awt.Container container;
 
   private double zoomFactor = 1.0;
 
-  double zoomFactor() {
+  public double zoomFactor() {
     return zoomFactor;
   }
 
@@ -41,22 +39,22 @@ strictfp class Zoomer {
 
   ///
 
-  void zoomWidgets(double newZoom) {
+  public void zoomWidgets(double newZoom) {
     container.setVisible(false);
     double oldZoom = zoomFactor;
     zoomFactor = newZoom;
     java.awt.Component[] comps = container.getComponents();
     for (int i = 0; i < comps.length; i++) {
-      if (comps[i] instanceof WidgetWrapper) {
-        WidgetWrapper wrapper = (WidgetWrapper) comps[i];
+      if (comps[i] instanceof WidgetWrapperInterface) {
+        WidgetWrapperInterface wrapper = (WidgetWrapperInterface) comps[i];
         zoomWidget(wrapper, false, false, oldZoom, zoomFactor);
       }
     }
     container.setVisible(true);
   }
 
-  void zoomWidget(WidgetWrapper wrapper, boolean newWidget, boolean loadingWidget,
-                  double oldZoom, double newZoom) {
+  public void zoomWidget(WidgetWrapperInterface wrapper, boolean newWidget, boolean loadingWidget,
+                         double oldZoom, double newZoom) {
     if (oldZoom == newZoom) {
       return;
     }
@@ -65,7 +63,7 @@ strictfp class Zoomer {
     zoomWidgetFont(wrapper, wrapper.widget(), newWidget, loadingWidget, oldZoom, newZoom);
   }
 
-  void zoomWidgetSize(WidgetWrapper wrapper, boolean newWidget, boolean loadingWidget,
+  public void zoomWidgetSize(WidgetWrapperInterface wrapper, boolean newWidget, boolean loadingWidget,
                       double oldZoom, double newZoom) {
     java.awt.Component component = wrapper.widget();
     java.awt.Dimension originalSize = sizes.get(component);
@@ -83,18 +81,18 @@ strictfp class Zoomer {
     }
   }
 
-  java.awt.Dimension zoomSize(java.awt.Dimension originalSize, double oldZoom, double newZoom) {
+  public java.awt.Dimension zoomSize(java.awt.Dimension originalSize, double oldZoom, double newZoom) {
     return new java.awt.Dimension
         ((int) StrictMath.ceil(originalSize.width * newZoom / oldZoom),
             (int) StrictMath.ceil(originalSize.height * newZoom / oldZoom));
   }
 
-  java.awt.Dimension zoomSize(java.awt.Dimension originalSize) {
+  public java.awt.Dimension zoomSize(java.awt.Dimension originalSize) {
     return zoomSize(originalSize, 1.0, zoomFactor);
   }
 
-  void zoomWidgetLocation(WidgetWrapper wrapper, boolean newWidget, boolean loadingWidget,
-                          double oldZoom, double newZoom) {
+  public void zoomWidgetLocation(WidgetWrapperInterface wrapper, boolean newWidget, boolean loadingWidget,
+                                 double oldZoom, double newZoom) {
     java.awt.Component component = wrapper.widget();
     java.awt.Point originalLocation = locations.get(component);
     Double originalZoom = locationZooms.get(component);
@@ -124,7 +122,7 @@ strictfp class Zoomer {
    * it may seems redundant to take both wrapper and widget as arguments,
    * but when we are used by CommandCenter, there is no wrapper - ST 7/13/04
    */
-  public void zoomWidgetFont(WidgetWrapper wrapper, Widget widget,
+  public void zoomWidgetFont(WidgetWrapperInterface wrapper, Widget widget,
                              boolean newWidget, boolean loadingWidget,
                              double oldZoom, double newZoom) {
     boolean recursive = widget.zoomSubcomponents();
@@ -143,7 +141,7 @@ strictfp class Zoomer {
     // want to remember what the font size at the 100% zoom level is, because
     // font sizes are so small that rounding error will mess us up unless we
     // always scale from the normal, unzoomed size
-    if (!(component instanceof org.nlogo.window.ViewWidgetInterface) ||
+    if (!(component instanceof ViewWidgetInterface) ||
         !newWidget ||
         loadingWidget) {
       fonts.put(component, component.getFont());
@@ -158,8 +156,8 @@ strictfp class Zoomer {
     }
   }
 
-  void scaleComponentFont(java.awt.Component component, double newZoom, double oldZoom,
-                          boolean recursive) {
+  public void scaleComponentFont(java.awt.Component component, double newZoom, double oldZoom,
+                                 boolean recursive) {
     if (fonts.get(component) == null) {
       storeComponentFont(component, recursive, false, false, oldZoom);
     }
@@ -179,7 +177,7 @@ strictfp class Zoomer {
     }
   }
 
-  void forgetAllZoomInfo() {
+  public void forgetAllZoomInfo() {
     sizes.clear();
     sizeZooms.clear();
     locations.clear();
@@ -189,17 +187,17 @@ strictfp class Zoomer {
     for (Iterator<java.awt.Component> comps = fonts.keySet().iterator();
          comps.hasNext();) {
       java.awt.Component comp = comps.next();
-      if (!(comp instanceof org.nlogo.window.View)) {
+      if (!(comp instanceof View)) {
         comps.remove();
         fontZooms.remove(comp);
       }
     }
   }
 
-  void updateZoomInfo(java.awt.Component component) {
+  public void updateZoomInfo(java.awt.Component component) {
     java.awt.Container parent = component.getParent();
-    if (parent instanceof WidgetWrapper) {
-      WidgetWrapper wrapper = (WidgetWrapper) parent;
+    if (parent instanceof WidgetWrapperInterface) {
+      WidgetWrapperInterface wrapper = (WidgetWrapperInterface) parent;
 
       java.awt.Dimension storedSize = sizes.get(component);
       if (storedSize != null &&
@@ -223,18 +221,18 @@ strictfp class Zoomer {
       // except for View, always go from original font
       // size, since at the moment the user can't the change
       // individual font sizes of other widget types
-      if (component instanceof org.nlogo.window.ViewWidget) {
+      if (component instanceof ViewWidget) {
         fonts.remove
-            (((org.nlogo.window.ViewWidget) component).view);
+            (((ViewWidget) component).view);
         fontZooms.remove
-            (((org.nlogo.window.ViewWidget) component).view);
+            (((ViewWidget) component).view);
       }
     }
   }
 
   /// called indirectly from the save() methods of the individual Widget classes
 
-  java.awt.Rectangle getUnzoomedBounds(java.awt.Component comp) {
+  public java.awt.Rectangle getUnzoomedBounds(java.awt.Component comp) {
     java.awt.Rectangle r = comp.getBounds();
     java.awt.Component parent = comp.getParent();
     if (parent != null) {

src/main/org/nlogo/app/App.scala
@@ -261,6 +261,7 @@ class App extends
     ModelSavedEvent.Handler with
     Events.SwitchedTabsEvent.Handler with
     AboutToQuitEvent.Handler with
+    ZoomedEvent.Handler with
     Controllable {
 
   import App.{pico, logger, commandLineMagic, commandLineModel, commandLineURL, commandLineModelIsLaunch, loggingName}
@@ -419,7 +420,7 @@ class App extends
       add(fileMenu)
       add(new EditMenu(App.this))
       add(pico.getComponent(classOf[ToolsMenu]))
-      add(new ZoomMenu(App.this))
+      add(new ZoomMenu)
       add(tabs.tabsMenu)
     }
     // a little ugly we have to typecast here, but oh well - ST 10/11/05
@@ -476,7 +477,7 @@ class App extends
     def createFileMenu:  JMenu = pico.getComponent(classOf[FileMenu])
     def createEditMenu:  JMenu = new EditMenu(App.this)
     def createToolsMenu: JMenu = new ToolsMenu(App.this)
-    def createZoomMenu:  JMenu = new ZoomMenu(App.this)
+    def createZoomMenu:  JMenu = new ZoomMenu
     override def addHelpMenu(menuBar:JMenuBar) = {
       val newMenu = new HelpMenu (App.this, new EditorColorizer(workspace))
       menuBar.add(newMenu)
@@ -560,18 +561,14 @@ class App extends
     else fileMenu.newModel()
   }
 
-  /// zooming stuff
-  private var zoomSteps = 0
-  def zoomLarger(){ zoomSteps+=1; finishZoom() }
-  def resetZoom() { zoomSteps=0; finishZoom() }
-  def zoomSmaller() {
-    zoomSteps-=1
-    zoomSteps = StrictMath.max(-5, zoomSteps)
-    finishZoom()
-  }
-  private def finishZoom() {
-    new ZoomedEvent(1.0 + zoomSteps * 0.1).raise(this)
+  /// zooming
+
+  def handle(e: ZoomedEvent) {
     smartPack(frame.getPreferredSize)
+  }
+
+  def resetZoom() {
+    new ZoomedEvent(0).raise(this)
   }
 
   // AppEvent stuff (kludgy)

src/main/org/nlogo/app/CommandCenter.scala
@@ -11,6 +11,7 @@ import org.nlogo.api.I18N
 
 class CommandCenter(workspace: org.nlogo.workspace.AbstractWorkspace,
                     locationToggleAction: Action) extends JPanel
+  with org.nlogo.window.Zoomable
   with org.nlogo.window.CommandCenterInterface
   with org.nlogo.window.Events.LoadBeginEvent.Handler
   with org.nlogo.window.Events.ZoomedEvent.Handler {
@@ -142,14 +143,5 @@ class CommandCenter(workspace: org.nlogo.workspace.AbstractWorkspace,
     }
     repaintPrompt()
     commandLine.requestFocus()
-  }
-  /// zooming
-  private lazy val zoomer: Zoomer = new Zoomer(this)
-  private var zoomFactor = 1.0
-  def zoomSubcomponents = true
-  def handle(e: org.nlogo.window.Events.ZoomedEvent) {
-    zoomer.scaleComponentFont(this, e.zoomFactor, zoomFactor, true)
-    zoomFactor = e.zoomFactor
-    revalidate()
   }
 }

src/main/org/nlogo/app/InfoTab.scala
@@ -21,7 +21,8 @@ import java.io.File
 class InfoTab(attachModelDir: String => String) extends JPanel with
         DocumentListener with Printable with HyperlinkListener with
         org.nlogo.window.Events.LoadSectionEvent.Handler with
-        org.nlogo.window.Events.ZoomedEvent.Handler {
+        org.nlogo.window.Events.ZoomedEvent.Handler with
+        org.nlogo.window.Zoomable {
 
   val baseDocUrl = "docs/infotab.html"
 
@@ -72,6 +73,8 @@ class InfoTab(attachModelDir: String => String) extends JPanel with
   private val scrollPane = new JScrollPane(view,
                                            ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
                                            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED)
+
+  override def zoomTarget = scrollPane
 
   locally {
     resetBorders()
@@ -145,15 +148,13 @@ class InfoTab(attachModelDir: String => String) extends JPanel with
 
   private var editorPaneFontSize = InfoFormatter.defaultFontSize
   private var originalFontSize = -1
-  private var zoomFactor = 1.0
-  def handle(e: org.nlogo.window.Events.ZoomedEvent) {
-    if(zoomFactor != e.zoomFactor) {
-      zoomFactor = e.zoomFactor
-      if(originalFontSize == -1) { originalFontSize = textArea.getFont.getSize }
-      textArea.setFont(textArea.getFont.deriveFont(StrictMath.ceil(originalFontSize * zoomFactor).toFloat))
-      editorPaneFontSize = StrictMath.ceil(InfoFormatter.defaultFontSize * zoomFactor).toInt
-      updateEditorPane()
-    }
+  override def handle(e: org.nlogo.window.Events.ZoomedEvent) {
+    super.handle(e)
+    if(originalFontSize == -1)
+      originalFontSize = textArea.getFont.getSize
+    textArea.setFont(textArea.getFont.deriveFont(StrictMath.ceil(originalFontSize * zoomFactor).toFloat))
+    editorPaneFontSize = StrictMath.ceil(InfoFormatter.defaultFontSize * zoomFactor).toInt
+    updateEditorPane()
   }
 
   // the textArea will give us an outlandlishly large preferred size unless we restrain it

src/main/org/nlogo/app/InterfacePanel.java
@@ -272,16 +272,16 @@ strictfp class InterfacePanel
         parent.setSize(viewWidget.asWidget().getSize());
         enforceMinimumAndMaximumWidgetSizes(viewWidget.asWidget());
         parent.setLocation(x, y);
-        zoomer.zoomWidgetLocation
+        zoomer().zoomWidgetLocation
             (getWrapper(viewWidget.asWidget()),
-                true, true, 1.0, zoomer.zoomFactor());
-        zoomer.zoomWidgetSize
+                true, true, 1.0, zoomer().zoomFactor());
+        zoomer().zoomWidgetSize
             (getWrapper(viewWidget.asWidget()),
-                true, true, 1.0, zoomer.zoomFactor());
-        zoomer.scaleComponentFont
+                true, true, 1.0, zoomer().zoomFactor());
+        zoomer().scaleComponentFont
             (((org.nlogo.window.ViewWidget) viewWidget)
                 .view,
-                zoomer.zoomFactor(), 1.0, false);
+               zoomFactor(), 1.0, false);
       }
       return viewWidget.asWidget();
     } else {

src/main/org/nlogo/app/ProceduresTab.scala
@@ -17,7 +17,7 @@ class ProceduresTab(val workspace: AbstractWorkspace) extends JPanel
   with ProceduresMenuTarget
   with Events.SwitchedTabsEvent.Handler
   with org.nlogo.window.Events.CompiledEvent.Handler
-  with org.nlogo.window.Events.ZoomedEvent.Handler
+  with org.nlogo.window.Zoomable
   with org.nlogo.swing.Printable {
 
   private val listener = new TextListener() {
@@ -28,6 +28,7 @@ class ProceduresTab(val workspace: AbstractWorkspace) extends JPanel
   }
   val text = new EditorFactory(workspace).newEditor(100, 100, true, listener, true)
   text.setBorder(BorderFactory.createEmptyBorder(4, 7, 4, 7))
+  override def zoomTarget = text
 
   val errorLabel = new EditorAreaErrorLabel(text)
   val toolBar = getToolBar
@@ -104,14 +105,12 @@ class ProceduresTab(val workspace: AbstractWorkspace) extends JPanel
   }
 
   private var originalFontSize = -1
-  private var zoomFactor = 1.0
-  def handle(e: org.nlogo.window.Events.ZoomedEvent) {
-    if(zoomFactor != e.zoomFactor) {
-      zoomFactor = e.zoomFactor
-      if(originalFontSize == -1) originalFontSize = text.getFont.getSize
-      text.setFont(text.getFont.deriveFont(StrictMath.ceil(originalFontSize * zoomFactor).toFloat))
-      errorLabel.zoom(zoomFactor)
-    }
+  override def handle(e: org.nlogo.window.Events.ZoomedEvent) {
+    super.handle(e)
+    if(originalFontSize == -1)
+      originalFontSize = text.getFont.getSize
+    text.setFont(text.getFont.deriveFont(StrictMath.ceil(originalFontSize * zoomFactor).toFloat))
+    errorLabel.zoom(zoomFactor)
   }
 
   // Error code

src/main/org/nlogo/app/WidgetPanel.java
@@ -27,8 +27,7 @@ public strictfp class WidgetPanel
     java.awt.event.FocusListener,
     org.nlogo.window.Events.WidgetEditedEvent.Handler,
     org.nlogo.window.Events.WidgetRemovedEvent.Handler,
-    org.nlogo.window.Events.LoadBeginEvent.Handler,
-    org.nlogo.window.Events.ZoomedEvent.Handler {
+    org.nlogo.window.Events.LoadBeginEvent.Handler {
   static final int GRID_SNAP = 5;  // set the size of the grid, in pixels
 
   protected java.awt.Rectangle selectionRect;
@@ -36,7 +35,6 @@ public strictfp class WidgetPanel
   protected WidgetWrapper newWidget;
   protected List<WidgetWrapper> widgetsBeingDragged;
   protected final GUIWorkspace workspace;
-  final Zoomer zoomer = new Zoomer(this);
   protected final javax.swing.JComponent glassPane =
       new javax.swing.JComponent() {
         @Override
@@ -559,7 +557,7 @@ public strictfp class WidgetPanel
     wrapper.validate();
     wrapper.setVisible(true);
 
-    zoomer.zoomWidget(wrapper, true, loadingWidget, 1.0, zoomer.zoomFactor());
+    zoomer().zoomWidget(wrapper, true, loadingWidget, 1.0, zoomFactor());
 
     if (select) {
       newWidget = wrapper;
@@ -642,8 +640,9 @@ public strictfp class WidgetPanel
   }
 
   public void handle(org.nlogo.window.Events.ZoomedEvent e) {
+    super.handle(e);
     unselectWidgets();
-    zoomer.zoomWidgets(e.zoomFactor);
+    zoomer().zoomWidgets(zoomFactor());
     revalidate();
   }
 
@@ -674,7 +673,7 @@ public strictfp class WidgetPanel
 
   public void handle(org.nlogo.window.Events.WidgetEditedEvent e) {
     new org.nlogo.window.Events.DirtyEvent().raise(this);
-    zoomer.updateZoomInfo(e.widget);
+    zoomer().updateZoomInfo(e.widget);
   }
 
   public void handle(org.nlogo.window.Events.WidgetRemovedEvent e) {
@@ -701,7 +700,7 @@ public strictfp class WidgetPanel
   public void handle(org.nlogo.window.Events.LoadBeginEvent e) {
     unselectWidgets();
     removeAllWidgets();
-    zoomer.forgetAllZoomInfo();
+    zoomer().forgetAllZoomInfo();
   }
 
   @Override
@@ -847,11 +846,11 @@ public strictfp class WidgetPanel
   }
 
   public java.awt.Rectangle getUnzoomedBounds(java.awt.Component component) {
-    return zoomer.getUnzoomedBounds(component);
+    return zoomer().getUnzoomedBounds(component);
   }
 
   public void resetZoomInfo(Widget widget) {
-    zoomer.updateZoomInfo(widget);
+    zoomer().updateZoomInfo(widget);
   }
 
   public void resetSizeInfo(Widget widget) {
@@ -859,7 +858,7 @@ public strictfp class WidgetPanel
   }
 
   public boolean isZoomed() {
-    return zoomer.zoomFactor() != 1.0;
+    return zoomer().zoomFactor() != 1.0;
   }
 
   public boolean canAddWidget(String widget) {

src/main/org/nlogo/app/WidgetWrapper.java
@@ -262,7 +262,7 @@ public strictfp class WidgetWrapper
 
     if (getParent() instanceof WidgetPanel && !widget.isNote()) {
       WidgetPanel iP = (WidgetPanel) getParent();
-      dim = iP.zoomer.zoomSize(dim);
+      dim = iP.zoomer().zoomSize(dim);
     }
     if (selected()) {
       dim.width += BORDER_E + BORDER_W;
@@ -596,7 +596,7 @@ public strictfp class WidgetWrapper
   void doDrop() {
     selected(true, true); // 2nd true = change was temporary
     new org.nlogo.window.Events.DirtyEvent().raise(this);
-    ((WidgetPanel) getParent()).zoomer.updateZoomInfo(widget);
+    ((WidgetPanel) getParent()).zoomer().updateZoomInfo(widget);
   }
 
   private void enforceMinimumSize(java.awt.Rectangle r) {
@@ -604,7 +604,7 @@ public strictfp class WidgetWrapper
       java.awt.Dimension minWidgetSize = widget().getMinimumSize();
       if (getParent() instanceof WidgetPanel) {
         WidgetPanel iP = (WidgetPanel) getParent();
-        minWidgetSize = iP.zoomer.zoomSize(minWidgetSize);
+        minWidgetSize = iP.zoomer().zoomSize(minWidgetSize);
       }
       if (minWidgetSize == null) {
         minWidgetSize = new java.awt.Dimension(MIN_WIDGET_WIDTH, MIN_WIDGET_HEIGHT);
@@ -695,7 +695,7 @@ public strictfp class WidgetWrapper
       }
       if (getParent() instanceof WidgetPanel) {
         WidgetPanel iP = (WidgetPanel) getParent();
-        maxWidgetSize = iP.zoomer.zoomSize(maxWidgetSize);
+        maxWidgetSize = iP.zoomer().zoomSize(maxWidgetSize);
       }
 
       switch (mouseMode()) {
@@ -833,7 +833,7 @@ public strictfp class WidgetWrapper
   // return what our location would be if we *weren't* selected... this
   // is needed for the zooming code in InterfacePanel
 
-  java.awt.Point getUnselectedLocation() {
+  public java.awt.Point getUnselectedLocation() {
     java.awt.Point result = getLocation();
     if (selected()) {
       result.x += BORDER_E;

commit 56b003abe9af1e0f6ff73eca4628570d06fe93f2

fix regression in zooming
caused by b55c765c0450607df21ff4e377544e42b427896b

the bug could be seen as follows: open Fire (or any model really);
zoom two steps larger; __reload; zoom two steps smaller; the font
size is now a little larger than it should be, as you'll see if
you do __reload again and the font snaps back to the right size

I think the old logic in storeComponentFont was actually wrong, and
before b55c765, conditions weren't right for the bug to show up, but
now they are

src/main/org/nlogo/window/Zoomer.java
@@ -141,9 +141,9 @@ public strictfp class Zoomer {
     // want to remember what the font size at the 100% zoom level is, because
     // font sizes are so small that rounding error will mess us up unless we
     // always scale from the normal, unzoomed size
-    if (!(component instanceof ViewWidgetInterface) ||
-        !newWidget ||
-        loadingWidget) {
+    if ((!(component instanceof ViewWidgetInterface) ||
+         !newWidget ||
+         loadingWidget) && fonts.get(component) == null) {
       fonts.put(component, component.getFont());
       fontZooms.put(component, Double.valueOf(oldZoom));
     }