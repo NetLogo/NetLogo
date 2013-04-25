a25212a0979fb15bf29e18616bd091521337d9e7
Seth Tisue <seth@tisue.net>
2013-04-25 15:15:30 -0500

    simplify TokenMapper now that it's 2D-only

--- src/main/org/nlogo/lex/TokenMapper.scala
+++ src/main/org/nlogo/lex/TokenMapper.scala
@@ -6,26 +6,21 @@ import org.nlogo.util.Utils
 import org.nlogo.api._
 
 object TokenMapper extends TokenMapperInterface {
-
-  val paths = Seq("/system/tokens.txt")
-
-  /// public stuff
+  val Path = "/system/tokens.txt"
   def isCommand(s: String) = commands.contains(s.toUpperCase)
   def isReporter(s: String) = reporters.contains(s.toUpperCase)
   // caller's responsibility to validate input for these two
   def getCommand(s: String) = instantiate[TokenHolder](commands(s.toUpperCase))
   def getReporter(s: String) = instantiate[TokenHolder](reporters(s.toUpperCase))
-  private def entries(entryType: String, path: String) =
+  private def entries(entryType: String): Iterator[(String, String)] =
     for {
-      line <- Utils.getResourceLines(path)
+      line <- Utils.getResourceLines(Path)
       if !line.startsWith("#")
       Array(tpe, primName, className) = line.split(" ")
       if tpe == entryType
     } yield primName.toUpperCase -> ("org.nlogo.prim." + className)
-  private val commands =
-    paths.foldLeft(Map[String, String]())(_ ++ entries("C", _))
-  private val reporters =
-    paths.foldLeft(Map[String, String]())(_ ++ entries("R", _))
+  private val commands = entries("C").toMap
+  private val reporters = entries("R").toMap
   /// private helper
   private def instantiate[T](name: String) =
     Class.forName(name).newInstance.asInstanceOf[T]
