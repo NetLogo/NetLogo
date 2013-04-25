9a591b0d2eea82ca7e3ff5cd7654f345c116b1ee
Seth Tisue <seth@tisue.net>
2013-04-25 14:37:34 -0500

    oops

--- bin/shell.sh
+++ bin/shell.sh
@@ -3,5 +3,5 @@
 rlwrap java -classpath \
 target/NetLogoHeadless.jar:\
 $HOME/.sbt/boot/scala-2.10.1/lib/scala-library.jar:\
-lib_managed/jars/asm/asm-all/asm-all-3.3.1.jar:\
+lib_managed/jars/asm/asm-all/asm-all-3.3.1.jar \
 org.nlogo.headless.Shell "$@"
