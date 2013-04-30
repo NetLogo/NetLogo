2da7e6ff5fced60b85ab020d06eb675a5774b621
Seth Tisue <seth@tisue.net>
2013-04-29 19:42:39 -0500

    make bin/shell.sh aware of JAVA_HOME
    this is useful e.g. on monk2 where Java 7 is available but isn't
    the default JVM.

--- bin/shell.sh
+++ bin/shell.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-rlwrap java -classpath \
+if [ -z "$JAVA_HOME" ]
+then
+  JAVA=java
+else
+  JAVA=$JAVA_HOME/bin/java
+fi
+
+rlwrap $JAVA -classpath \
 target/NetLogoHeadless.jar:\
 $HOME/.sbt/boot/scala-2.10.1/lib/scala-library.jar:\
 lib_managed/jars/asm/asm-all/asm-all-3.3.1.jar \
