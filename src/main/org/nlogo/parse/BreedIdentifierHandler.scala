62c365ccf924a3823f6877b4810130fe353ea532
Seth Tisue <seth@tisue.net>
2013-05-03 12:51:37 -0400

    cleanup in BreedIdentifierHandler

--- src/main/org/nlogo/parse/BreedIdentifierHandler.scala
+++ src/main/org/nlogo/parse/BreedIdentifierHandler.scala
@@ -2,83 +2,102 @@
 
 package org.nlogo.parse
 
-import org.nlogo.api.{ AgentSet, Breed, Program, Token, TokenType }
-import org.nlogo.nvm.Instruction
+import org.nlogo.{ api, nvm, prim },
+  api.{ AgentSet, Breed, Program, Token, TokenType },
+  TokenType.{ Command, Reporter },
+  prim._
 
-// The Helper class and some of the methods aren't private because we want to get at them from
-// TestBreedIdentifierHandler. - ST 12/22/08
-private object BreedIdentifierHandler {
-  import org.nlogo.prim._
-  import TokenType.Command
-  import TokenType.Reporter
+object BreedIdentifierHandler {
+
   def process(token: Token, program: Program): Option[Token] =
-    handlers.toStream.flatMap(_.process(token, program)).headOption
-  def turtle(patternString: String, tokenType: TokenType, singular: Boolean, primClass: Class[_ <: Instruction]) =
-    new Helper(patternString, tokenType, singular, primClass,
-               _.breeds, _ => true)
-  def directedLink(patternString: String, tokenType: TokenType, singular: Boolean, primClass: Class[_ <: Instruction]) =
-    new Helper(patternString, tokenType, singular, primClass,
-               _.linkBreeds, _.isDirected)
-  def undirectedLink(patternString: String, tokenType: TokenType, singular: Boolean, primClass: Class[_ <: Instruction]) =
-    new Helper(patternString, tokenType, singular, primClass,
-               _.linkBreeds, !_.isDirected)
-  private val handlers = List(
+    handlers.toStream.flatMap(_.process(token, program))
+      .headOption
+
+  // a bit nasty, but will go away when we cut the dependency on nvm package - ST 5/3/13
+  import scala.language.existentials
+  type PrimClass = Class[_ <: nvm.Instruction]
+
+  case class Spec(
+    patternString: String,
+    tokenType: TokenType,
+    singular: Boolean,
+    primClass: PrimClass)
+
+  def turtle(spec: Spec) =
+    new Helper(spec, _.breeds, _ => true)
+
+  def directedLink(spec: Spec) =
+    new Helper(spec, _.linkBreeds, _.isDirected)
+
+  def undirectedLink(spec: Spec) =
+    new Helper(spec, _.linkBreeds, !_.isDirected)
+
+  val handlers = List(
     // prims for turtle breeds
-    turtle("CREATE-*"         , Command, false, classOf[_createturtles]),
-    turtle("CREATE-ORDERED-*" , Command, false, classOf[_createorderedturtles]),
-    turtle("HATCH-*"          , Command, false, classOf[_hatch]),
-    turtle("SPROUT-*"         , Command, false, classOf[_sprout]),
-    turtle("IS-*?"            , Reporter, true, classOf[_isbreed]),
-    turtle("*-HERE"           , Reporter, false, classOf[_breedhere]),
-    turtle("*-ON"             , Reporter, false, classOf[_breedon]),
-    turtle("*"                , Reporter, false, classOf[_breed]),
-    turtle("*"                , Reporter, true, classOf[_breedsingular]),
-    turtle("*-AT"             , Reporter, false, classOf[_breedat]),
+    turtle(Spec("CREATE-ORDERED-*" , Command , false, classOf[_createorderedturtles ])),
+    turtle(Spec("CREATE-*"         , Command , false, classOf[_createturtles        ])),
+    turtle(Spec("HATCH-*"          , Command , false, classOf[_hatch                ])),
+    turtle(Spec("SPROUT-*"         , Command , false, classOf[_sprout               ])),
+    turtle(Spec("*"                , Reporter, false, classOf[_breed                ])),
+    turtle(Spec("*-AT"             , Reporter, false, classOf[_breedat              ])),
+    turtle(Spec("*-HERE"           , Reporter, false, classOf[_breedhere            ])),
+    turtle(Spec("*-ON"             , Reporter, false, classOf[_breedon              ])),
+    turtle(Spec("*"                , Reporter, true , classOf[_breedsingular        ])),
+    turtle(Spec("IS-*?"            , Reporter, true , classOf[_isbreed              ])),
     // prims for link breeds
-    directedLink("*"               , Reporter, true, classOf[_linkbreedsingular]),
-    undirectedLink("*"             , Reporter, true, classOf[_linkbreedsingular]),
-    directedLink("*"               , Reporter, false, classOf[_linkbreed]),
-    undirectedLink("*"             , Reporter, false, classOf[_linkbreed]),
-    directedLink("IS-*?"           , Reporter, true, classOf[_isbreed]),
-    undirectedLink("IS-*?"         , Reporter, true, classOf[_isbreed]),
-    directedLink("CREATE-*-FROM"   , Command, true, classOf[_createlinkfrom]),
-    directedLink("CREATE-*-FROM"   , Command, false, classOf[_createlinksfrom]),
-    directedLink("CREATE-*-TO"     , Command, true, classOf[_createlinkto]),
-    directedLink("CREATE-*-TO"     , Command, false, classOf[_createlinksto]),
-    undirectedLink("CREATE-*-WITH" , Command, true, classOf[_createlinkwith]),
-    undirectedLink("CREATE-*-WITH" , Command, false, classOf[_createlinkswith]),
-    directedLink("IN-*-NEIGHBOR?"  , Reporter, true, classOf[_inlinkneighbor]),
-    directedLink("OUT-*-NEIGHBOR?" , Reporter, true, classOf[_outlinkneighbor]),
-    directedLink("IN-*-FROM"       , Reporter, true, classOf[_inlinkfrom]),
-    directedLink("OUT-*-TO"        , Reporter, true, classOf[_outlinkto]),
-    directedLink("OUT-*-NEIGHBORS" , Reporter, true, classOf[_outlinkneighbors]),
-    directedLink("IN-*-NEIGHBORS"  , Reporter, true, classOf[_inlinkneighbors]),
-    directedLink("MY-IN-*"         , Reporter, false, classOf[_myinlinks]),
-    directedLink("MY-OUT-*"        , Reporter, false, classOf[_myoutlinks]),
-    undirectedLink("*-NEIGHBORS"   , Reporter, true, classOf[_linkneighbors]),
-    undirectedLink("MY-*"          , Reporter, false, classOf[_mylinks]),
-    undirectedLink("*-WITH"        , Reporter, true, classOf[_linkwith]),
-    undirectedLink("*-NEIGHBOR?"   , Reporter, true, classOf[_linkneighbor])
+    directedLink   ( Spec("CREATE-*-FROM"   , Command , true , classOf[_createlinkfrom    ])),
+    directedLink   ( Spec("CREATE-*-FROM"   , Command , false, classOf[_createlinksfrom   ])),
+    directedLink   ( Spec("CREATE-*-TO"     , Command , false, classOf[_createlinksto     ])),
+    undirectedLink ( Spec("CREATE-*-WITH"   , Command , false, classOf[_createlinkswith   ])),
+    directedLink   ( Spec("CREATE-*-TO"     , Command , true , classOf[_createlinkto      ])),
+    undirectedLink ( Spec("CREATE-*-WITH"   , Command , true , classOf[_createlinkwith    ])),
+    directedLink   ( Spec("IN-*-FROM"       , Reporter, true , classOf[_inlinkfrom        ])),
+    directedLink   ( Spec("IN-*-NEIGHBOR?"  , Reporter, true , classOf[_inlinkneighbor    ])),
+    directedLink   ( Spec("IN-*-NEIGHBORS"  , Reporter, true , classOf[_inlinkneighbors   ])),
+    directedLink   ( Spec("IS-*?"           , Reporter, true , classOf[_isbreed           ])),
+    undirectedLink ( Spec("IS-*?"           , Reporter, true , classOf[_isbreed           ])),
+    directedLink   ( Spec("*"               , Reporter, false, classOf[_linkbreed         ])),
+    undirectedLink ( Spec("*"               , Reporter, false, classOf[_linkbreed         ])),
+    directedLink   ( Spec("*"               , Reporter, true , classOf[_linkbreedsingular ])),
+    undirectedLink ( Spec("*"               , Reporter, true , classOf[_linkbreedsingular ])),
+    undirectedLink ( Spec("*-NEIGHBOR?"     , Reporter, true , classOf[_linkneighbor      ])),
+    undirectedLink ( Spec("*-NEIGHBORS"     , Reporter, true , classOf[_linkneighbors     ])),
+    undirectedLink ( Spec("*-WITH"          , Reporter, true , classOf[_linkwith          ])),
+    directedLink   ( Spec("MY-IN-*"         , Reporter, false, classOf[_myinlinks         ])),
+    undirectedLink ( Spec("MY-*"            , Reporter, false, classOf[_mylinks           ])),
+    directedLink   ( Spec("MY-OUT-*"        , Reporter, false, classOf[_myoutlinks        ])),
+    directedLink   ( Spec("OUT-*-NEIGHBOR?" , Reporter, true , classOf[_outlinkneighbor   ])),
+    directedLink   ( Spec("OUT-*-NEIGHBORS" , Reporter, true , classOf[_outlinkneighbors  ])),
+    directedLink   ( Spec("OUT-*-TO"        , Reporter, true , classOf[_outlinkto         ]))
   )
-  class Helper
-    (patternString: String, tokenType: TokenType, singular: Boolean, primClass: Class[_ <: Instruction],
-     breeds: (Program) => collection.Map[String, Breed], isValidBreed: (Breed) => Boolean)
-  {
-    import java.util.regex.Pattern
-    val pattern = Pattern.compile("\\A"+patternString.replaceAll("\\?", "\\\\?").replaceAll("\\*", "(.+)")+"\\Z")
+
+  import java.util.regex.Pattern
+
+  class Helper(spec: Spec, breeds: Program => collection.Map[String, Breed],
+    isValidBreed: Breed => Boolean) {
+    val pattern = Pattern.compile("\\A" +
+      spec.patternString.replaceAll("\\?", "\\\\?")
+      .replaceAll("\\*", "(.+)")+"\\Z")
     def process(tok: Token, program: Program): Option[Token] = {
       val matcher = pattern.matcher(tok.value.asInstanceOf[String])
-      if(!matcher.matches()) return None
-      val name = matcher.group(1)
-      val breed =
-        breeds(program).values
-          .find{breed => name == (if (singular) breed.singular else breed.name)}
-          .getOrElse(return None)
-      if (!isValidBreed(breed)) return None
-      val instr = Instantiator.newInstance[Instruction](primClass, breed.name)
-      val tok2 = new Token(tok.value.asInstanceOf[String], tokenType, instr)(tok.startPos, tok.endPos, tok.fileName)
-      instr.token(tok2)
-      Some(tok2)
+      if(!matcher.matches())
+        None
+      else {
+        val name = matcher.group(1)
+        val breed =
+          breeds(program).values
+            .find{breed => name ==
+              (if (spec.singular) breed.singular else breed.name)}
+            .getOrElse(return None)
+        if (!isValidBreed(breed)) return None
+        val instr = Instantiator.newInstance[nvm.Instruction](
+          spec.primClass, breed.name)
+        val tok2 = new Token(tok.value.asInstanceOf[String], spec.tokenType, instr)(
+          tok.startPos, tok.endPos, tok.fileName)
+        instr.token(tok2)
+        Some(tok2)
+      }
     }
   }
+
 }
