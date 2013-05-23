a8334c85bc0d32c1ce280ac786c768f9ae127091
Seth Tisue <seth@tisue.net>
2013-05-23 15:12:46 -0400

    oops

--- src/main/org/nlogo/compile/BackEnd.scala
+++ src/main/org/nlogo/compile/BackEnd.scala
@@ -16,7 +16,7 @@ private object BackEnd {
   // StructureParser found the top level Procedures for us.  ExpressionParser
   // finds command tasks and makes Procedures out of them, too.  the remaining
   // phases handle all ProcedureDefinitions from both sources. - ST 2/4/11
-  def backEnd(defs: Seq[ProcedureDefinition], program: api.Program, source: String,
+  def backEnd(defs: Seq[parse.ProcedureDefinition], program: api.Program, source: String,
       profilingEnabled: Boolean, flags: nvm.CompilerFlags): nvm.CompilerResults = {
     // each Int is the position of that variable in the procedure's args list
     val alteredLets =
