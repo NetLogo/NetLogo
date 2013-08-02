b6c8d25bf2ea1a1710bda53b5a22b33b3055ec93
Seth Tisue <seth@tisue.net>
2013-08-02 14:43:21 -0400

    store JFlex-generated sources in standard sbt location
    for a long time we've been getting away with having them mixed in
    with the rest of our sources, but I'm finding that with sbt 0.13
    and Scala 2.11 I get problems with sbt seeing imaginary multiple
    copies of the same files and reporting compile errors as a result.
    
    it's probably better to follow the standard sbt structure anyway,
    so I'm going ahead and comitting this on the headless branch even
    though we don't have either sbt 0.13 or Scala 2.11 there yet.

--- project/JFlexRunner.scala
+++ project/JFlexRunner.scala
@@ -5,13 +5,13 @@ import Keys._
 object JFlexRunner {
 
   val task =
-    (cacheDirectory, javaSource in Compile, baseDirectory, streams) map {
+    (cacheDirectory, sourceManaged in Compile, baseDirectory, streams) map {
       (cacheDir, src, base, s) =>
         val cache =
           FileFunction.cached(cacheDir / "lexers", inStyle = FilesInfo.hash, outStyle = FilesInfo.hash) {
             in: Set[File] =>
-              Set(flex(s.log.info(_), base, src, "agent", "ImportLexer"),
-                  flex(s.log.info(_), base, src, "lex", "TokenLexer"))
+              Set(flex(s.log.info(_), base, src, "ImportLexer"),
+                  flex(s.log.info(_), base, src, "TokenLexer"))
           }
         cache(Set(base / "project" / "warning.txt",
                   base / "project" / "ImportLexer.flex",
@@ -20,10 +20,9 @@ object JFlexRunner {
 
   // this used to be broken into two tasks, but jflex doesnt seem to be threadsafe
   // so we have to run them serially, which means we have to generate them both each time. -JC 6/8/10
-  def flex(log: String => Unit, base: File, dir: File, ppackage: String, kind: String): File = {
+  def flex(log: String => Unit, base: File, dir: File, kind: String): File = {
     val project = base / "project"
-    val nlogoPackage = dir / "org" / "nlogo"
-    val result = nlogoPackage / ppackage / (kind + ".java")
+    val result = dir / (kind + ".java")
     log("generating " + result)
     JFlex.Main.main(Array("--quiet", (project / (kind + ".flex")).asFile.toString))
     IO.write(result,
