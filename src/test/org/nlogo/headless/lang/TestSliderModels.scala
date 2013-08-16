ffab17c9c9265a7bfc5ba2d5f2d5e43de40d6a78
Seth Tisue <seth@tisue.net>
2013-08-15 21:45:05 -0400

    remove unused code

--- src/test/org/nlogo/headless/lang/TestSliderModels.scala
+++ src/test/org/nlogo/headless/lang/TestSliderModels.scala
@@ -11,25 +11,15 @@ import ModelCreator._
  */
 class TestSliderModels extends FixtureSuite {
 
-  val code =
-    """|globals [glob1]
-       |to-report square [x]
-       |  report x * x
-       |end""".stripMargin
   val model =
-    Model(code,
-      widgets = List(Slider(name="density", max="99", current="57.0")))
+    Model(widgets = List(Slider(name="density", max="99", current="57.0")))
 
   test("simple slider test") { implicit fixture =>
     import fixture._
     open(model)
-    testReporter("glob1", "0")
-    testCommand("set glob1 12345")
-    testReporter("glob1", "12345")
     testReporter("density", "57")
     testCommand("set density 63")
     testReporter("density", "63")
-    testReporter("square 5", "25")
   }
 
 }
