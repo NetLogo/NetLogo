5774f5486b8afa6235fb86deb482d6d4fd67fba5
Seth Tisue <seth@tisue.net>
2013-08-28 12:29:01 -0500

    fix tortoise.sh to go through sbt
    see 10050db5f83789029399c7373531486a25790af7 for a discussion
    of the same change when it was made to shell.sh

--- bin/tortoise.sh
+++ bin/tortoise.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-rlwrap java -classpath \
-target/NetLogoHeadless.jar:\
-$HOME/.sbt/boot/scala-2.10.1/lib/scala-library.jar:\
-lib_managed/jars/asm/asm-all/asm-all-3.3.1.jar \
-org.nlogo.tortoise.Shell "$@"
+rlwrap ./sbt \
+  -Djline.terminal=jline.UnsupportedTerminal \
+  'run-main org.nlogo.tortoise.Shell'
