18c14baddd0a2211455bcc01d592ded331892b58
TheBizzle <jason.bertsche@gmail.com>
2013-09-01 14:05:59 -0500

    Adding API wrappers for agents

---
+++ src/main/scalajs/org/nlogo/tortoise/api/wrapper/AgentWrapper.scala
@@ -0,0 +1,27 @@
+package org.nlogo.tortoise.api
+package wrapper
+
+import
+  scala.js.annotation.expose
+
+import
+  org.nlogo.tortoise.engine.{ Agent, AnyJS, JSW, Patch, Turtle }
+
+trait AgentWrapper extends Wrapper {
+  override type ValueType <: Agent
+  @expose override def toString: String = value.toString
+}
+
+class TurtleWrapper(override val value: Turtle) extends AgentWrapper {
+  override type ValueType = Turtle
+  @expose def getTurtleVariable(n: Int):         AnyJS = value.getTurtleVariable(n)
+  @expose def setTurtleVariable(n: Int, v: JSW): Unit  = value.setTurtleVariable(n, v)
+  @expose def getPatchVariable(n: Int):          AnyJS = value.getPatchVariable(n)
+  @expose def setPatchVariable(n: Int, v: JSW):  Unit  = value.setPatchVariable(n, v)
+}
+
+class PatchWrapper(override val value: Patch) extends AgentWrapper {
+  override type ValueType = Patch
+  @expose def getPatchVariable(n: Int):          AnyJS = value.getPatchVariable(n)
+  @expose def setPatchVariable(n: Int, v: JSW):  Unit  = value.setPatchVariable(n, v)
+}
