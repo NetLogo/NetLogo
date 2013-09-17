7f5b397c368dcc00bfdc6e57640bfa92608145bc
TheBizzle <jason.bertsche@gmail.com>
2013-09-17 15:32:14 -0500

    Removing the array from 'VarMap'
    JS objects are already essentially linked maps, so I don't need to manage that myself

--- src/main/scalajs/org/nlogo/tortoise/adt/VarMap.scala
+++ src/main/scalajs/org/nlogo/tortoise/adt/VarMap.scala
@@ -3,14 +3,16 @@ package org.nlogo.tortoise.adt
 import
   VarMap.{ Key, Value }
 
-// A fake-y linked dictionary
 // It's abhorrent but the best way to get these objects into the `Dictionary`
 // seems to be to wrap them in thunks and turn them into JS functions --JAB (9/14/13)
-class VarMap private (array: ArrayJS[(Key, Value)], dictionary: Dictionary) {
+class VarMap private (dictionary: Dictionary) {
 
   // Carn sarn this lossy dictionary! --JAB (9/13/13)
   def apply(key: Key): Value        = unravel(key)
-  def apply(n: Int):   (Key, Value) = array(n)
+  def apply(n: Int):   (Key, Value) = {
+    val key = this.keys(n)
+    (key, this(key))
+  }
 
   def +=(pair: (Key, Value)): Unit = {
     val (key, value) = pair
@@ -23,28 +25,28 @@ class VarMap private (array: ArrayJS[(Key, Value)], dictionary: Dictionary) {
 
   def -=(key: Key): Unit = this -= this.indexOfKey(key)
   def -=(n: Int):   Unit = {
-    val key = array(n)._1
-    array.splice(n, 1)
+    val key = this.keys(n)
     ravel(key -> ()) // It would be nice to have access to the `delete` keyword; also, this will probably bite me in the ass. --JAB (7/13/13)
   }
 
-  def pairs: ArrayJS[(Key, Value)] = array
-
-  def size: Int = NumberJS.toDouble(array.length).toInt
-
-  def update(key: Key, value: Value): Unit = {
-    if (unravel(key) != ((): JSW))
-      array.push(key -> value)
-    ravel(key -> value)
+  def keys: ArrayJS[Key] = {
+    import DynamicJS.{ global => g }
+    g.Object.keys(dictionary).asInstanceOf[ArrayJS[Key]]
   }
+
+  def pairs: ArrayJS[(Key, Value)] = this.keys.E map (key => (key, this(key)))
+
+  def size: Int = NumberJS.toDouble(this.keys.length).toInt
+
+  def update(key: Key, value: Value): Unit = ravel(key -> value)
 
   // Not great.  --JAB (9/13/13)
   def exists(key: Key): Boolean = this.indexOfKey(key) != -1
 
   private def indexOfKey(key: Key): Int = {
     var i = 0
-    array.E foreach {
-      case (k, _) =>
+    this.keys.E foreach {
+      k =>
         if (k == key)
           return i
         else
@@ -75,7 +77,7 @@ object VarMap {
 
   def apply(pairs: ArrayJS[(Key, Value)]): VarMap = {
     val dict = Dictionary(AnyJS.toArray(pairs.E map { case (x, y) => (x, () => y)}): _*)
-    new VarMap(pairs, dict)
+    new VarMap(dict)
   }
 
 }
