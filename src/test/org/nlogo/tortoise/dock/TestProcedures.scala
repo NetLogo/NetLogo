f5351ddceb8e4ee2557f4cc619be065fb9d2ae7e
Seth Tisue <seth@tisue.net>
2013-09-20 10:29:08 -0500

    Tortoise testing: redo TestProcedures without turtles
    so we don't get distracting test failures here when turtle stuff is
    broken

--- src/test/org/nlogo/tortoise/dock/TestProcedures.scala
+++ src/test/org/nlogo/tortoise/dock/TestProcedures.scala
@@ -5,30 +5,37 @@ package dock
 
 class TestProcedures extends DockingSuite {
 
-  test("procedure call") { implicit fixture => import fixture._
-    declare("to foo cro 1 end")
+  test("procedure call 1") { implicit fixture => import fixture._
+    declare("globals [g] to foo set g g + 2 end")
+    testCommand("foo")
+    testCommand("output-print g")
+  }
+
+  test("procedure call 2") { implicit fixture => import fixture._
+    declare("globals [g] to foo set g g + 1 end")
     testCommand("foo foo foo")
-    testCommand("output-print count turtles")
+    testCommand("output-print g")
   }
 
   test("procedure call with one input") { implicit fixture => import fixture._
-    declare("to foo [x] cro x end")
+    declare("globals [g] to foo [x] set g g + x end")
     testCommand("foo 1 foo 2 foo 3")
-    testCommand("output-print count turtles")
+    testCommand("output-print g")
   }
 
   test("procedure call with three inputs") { implicit fixture => import fixture._
-    declare("to foo [x y z] cro x + y cro z end")
+    declare("globals [g] to foo [x y z] set g g + x + y set g g + z end")
     testCommand("foo 1 2 3")
-    testCommand("output-print count turtles")
+    testCommand("output-print g")
   }
 
   test("multiple procedures") { implicit fixture => import fixture._
-    declare("""|to foo [x y z] cro x + y cro z end
-                        |to goo [z] cro z * 10 end""".stripMargin)
+    declare("""|globals [g]
+               |to foo [x y z] set g g + x + y set g g + z end
+               |to goo [z] set g g + z * 10 end""".stripMargin)
     testCommand("foo 1 2 3")
     testCommand("goo 2")
-    testCommand("output-print count turtles")
+    testCommand("output-print g")
   }
 
   test("simple recursive call") { implicit fixture => import fixture._
