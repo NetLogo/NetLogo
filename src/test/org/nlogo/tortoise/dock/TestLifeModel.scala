ea2d892c2e6522b50df31acdfd52f32b160feb80
Seth Tisue <seth@tisue.net>
2013-10-03 11:46:09 -0400

    Tortoise: move Life Simple to models repo

--- src/test/org/nlogo/tortoise/dock/TestLifeModel.scala
+++ src/test/org/nlogo/tortoise/dock/TestLifeModel.scala
@@ -8,38 +8,9 @@ import org.nlogo.api.WorldDimensions
 class TestLifeModel extends DockingSuite {
 
   test("life") { implicit fixture => import fixture._
-    val lifeSrc =
-      """
-        |patches-own [ living? live-neighbors ]
-        |
-        |to setup
-        |  clear-all
-        |  ask patches [ cell-death ]
-        |  ask patch  0  0 [ cell-birth ]
-        |  ask patch -1  0 [ cell-birth ]
-        |  ask patch  0 -1 [ cell-birth ]
-        |  ask patch  0  1 [ cell-birth ]
-        |  ask patch  1  1 [ cell-birth ]
-        |  reset-ticks
-        |end
-        |
-        |to cell-birth set living? true  set pcolor white end
-        |to cell-death set living? false set pcolor black end
-        |
-        |to go
-        |  ask patches [
-        |    set live-neighbors count neighbors with [living?] ]
-        |  ask patches [
-        |    ifelse live-neighbors = 3
-        |      [ cell-birth ]
-        |      [ if live-neighbors != 2
-        |        [ cell-death ] ] ]
-        |  tick
-        |end
-      """.stripMargin
-    declare(lifeSrc, WorldDimensions.square(5))
+    open("models/test/tortoise/Life Simple.nlogo")
     testCommand("setup")
-    for (_ <- 1 to 5)
+    for (_ <- 1 to 30)
       testCommand("go")
     testCommand("""ask patches [output-print (word self " -> " living?) ]""")
   }
