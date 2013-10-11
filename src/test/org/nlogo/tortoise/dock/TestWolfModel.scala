3d91644084a95e463b30f5814515dd5412a9f178
Frank Duncan <frank@kank.net>
2013-10-11 14:27:13 -0500

    Remove wonky wolfsheep!

--- src/test/org/nlogo/tortoise/dock/TestWolfModel.scala
+++ src/test/org/nlogo/tortoise/dock/TestWolfModel.scala
@@ -6,15 +6,7 @@ package dock
 import org.nlogo.api
 
 class TestWolfModel extends DockingSuite {
-
   test("wolf") { implicit fixture => import fixture._
-    open("models/test/tortoise/Wolf Sheep Predation.nlogo")
-    testCommand("setup")
-    testCommand("repeat 20 [ go ]")
-    testCommand("output-print count wolves output-print count sheep output-print grass")
-  }
-
-  test("wolf real") { implicit fixture => import fixture._
     open("models/Sample Models/Biology/Wolf Sheep Predation.nlogo")
     testCommand("setup")
     testCommand("repeat 30 [ go ]")
