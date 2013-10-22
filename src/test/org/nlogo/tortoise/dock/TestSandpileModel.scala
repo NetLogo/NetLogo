4dead0feefafbc2f2e5743f86f2dbf14fc219ebb
Seth Tisue <seth@tisue.net>
2013-10-22 18:58:17 -0400

    Tortoise: simplify TestSandpileModel
    taking advantage of the fact that WidgetParser supports choosers now

--- src/test/org/nlogo/tortoise/dock/TestSandpileModel.scala
+++ src/test/org/nlogo/tortoise/dock/TestSandpileModel.scala
@@ -9,12 +9,7 @@ class TestSandpileModel extends DockingSuite with SlowTest {
 
   test("sandpile") { implicit fixture => import fixture._
     open("models/test/tortoise/Sandpile.nlogo")
-    // TODO next line shouldn't be necessary, but WidgetParser mysteriously lacks
-    // chooser support; it's NetLogo/NetLogo#456 - ST 10/11/13
-    testCommand("""set drop-location "center"""")
     testCommand("setup-random")
-    compare("drop-location")
-    compare("(word drop-patch)")
     // this takes a while on Rhino, but it probably isn't a good test unless we run the
     // model for long enough for good avalanches to get going - ST 10/10/13
     for (_ <- 1 to 100)
