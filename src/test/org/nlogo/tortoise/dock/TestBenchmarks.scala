45b7abca5bac0c3b2612ddaa43c3415a1092765b
Frank Duncan <frank@kank.net>
2013-10-24 20:41:27 -0500

    Add the int prim!  Make BZ Benchmark work!

--- src/test/org/nlogo/tortoise/dock/TestBenchmarks.scala
+++ src/test/org/nlogo/tortoise/dock/TestBenchmarks.scala
@@ -10,14 +10,21 @@ class TestBenchmarks extends DockingSuite with SlowTest {
   test("ants") { implicit fixture => import fixture._
     open("models/test/benchmarks/Ants Benchmark.nlogo")
     testCommand("setup")
-    testCommand("repeat 20 [ go ]")
+    testCommand("repeat 10 [ go ]")
     testCommand("ask turtle 0 [ move-to one-of patches with [shade-of? pcolor blue] ]")
-    testCommand("repeat 20 [ go ]")
+    testCommand("repeat 10 [ go ]")
   }
 
   test("bureaucrats") { implicit fixture => import fixture._
     open("models/test/benchmarks/Bureaucrats Benchmark.nlogo")
     testCommand("setup")
     testCommand("repeat 50 [ go ]")
+  }
+
+  test("BZ") { implicit fixture => import fixture._
+    open("models/test/benchmarks/BZ Benchmark.nlogo")
+    testCommand("resize-world -20 20 -20 20")
+    testCommand("setup")
+    testCommand("repeat 5 [ go ]")
   }
 }
