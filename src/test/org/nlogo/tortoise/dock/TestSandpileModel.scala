9239a11b80ac1da0c2b4f5cc3fcbc36ba6eea22f
Seth Tisue <seth@tisue.net>
2013-10-29 20:12:37 -0400

    Tortoise: also test uniform setup in Sandpile

--- src/test/org/nlogo/tortoise/dock/TestSandpileModel.scala
+++ src/test/org/nlogo/tortoise/dock/TestSandpileModel.scala
@@ -7,12 +7,22 @@ import org.nlogo.util.SlowTest
 
 class TestSandpileModel extends DockingSuite with SlowTest {
 
-  test("sandpile") { implicit fixture => import fixture._
+  test("sandpile random") { implicit fixture => import fixture._
     open("models/test/tortoise/Sandpile.nlogo")
     testCommand("setup-random")
     // this takes a while on Rhino, but it probably isn't a good test unless we run the
     // model for long enough for good avalanches to get going - ST 10/10/13
     for (_ <- 1 to 100)
+      testCommand("go")
+    compare("total")
+    compare("sizes")
+    compare("lifetimes")
+  }
+
+  test("sandpile uniform") { implicit fixture => import fixture._
+    open("models/test/tortoise/Sandpile.nlogo")
+    testCommand("setup-uniform 0")
+    for (_ <- 1 to 10)
       testCommand("go")
     compare("total")
     compare("sizes")
