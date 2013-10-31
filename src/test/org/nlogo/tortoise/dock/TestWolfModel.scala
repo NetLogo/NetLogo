634685798f9d20c4a0a9864a7be3bbdbad6861c3
Seth Tisue <seth@tisue.net>
2013-10-31 18:08:04 -0400

    Tortoise: beef up TestWolfModel

--- src/test/org/nlogo/tortoise/dock/TestWolfModel.scala
+++ src/test/org/nlogo/tortoise/dock/TestWolfModel.scala
@@ -6,10 +6,24 @@ package dock
 import org.nlogo.util.SlowTest
 
 class TestWolfModel extends DockingSuite with SlowTest {
-  test("wolf") { implicit fixture => import fixture._
+
+  // Fails if you run it longer (e.g. 40 ticks instead of 30).  It's probably because we
+  // aren't matching turtles-here ordering (which is NetLogo/Tortoise#31) - ST 10/31/13
+
+  test("wolf no grass") { implicit fixture => import fixture._
     open("models/Sample Models/Biology/Wolf Sheep Predation.nlogo")
+    testCommand("set grass? false")
     testCommand("setup")
     testCommand("repeat 30 [ go ]")
     testCommand("output-print count wolves output-print count sheep output-print grass")
   }
+
+  test("wolf grass") { implicit fixture => import fixture._
+    open("models/Sample Models/Biology/Wolf Sheep Predation.nlogo")
+    testCommand("set grass? true")
+    testCommand("setup")
+    testCommand("repeat 10 [ go ]")
+    testCommand("output-print count wolves output-print count sheep output-print grass")
+  }
+
 }
