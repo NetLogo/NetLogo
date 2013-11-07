406d976485a028d2a395802aa00a9b2bf45667f0
nicolaspayette <nicolaspayette@gmail.com>
2013-11-07 15:27:59 -0500

    review tab: properly style ViewWidgetPanel

--- src/main/org/nlogo/review/ViewPanel.scala
+++ src/main/org/nlogo/review/ViewPanel.scala
@@ -4,16 +4,24 @@ package org.nlogo.review
 
 import java.awt.Graphics
 import java.awt.Graphics2D
-import org.nlogo.mirror.FakeWorld
-import org.nlogo.mirror.ModelRun
-import javax.swing.JPanel
-import org.nlogo.mirror.FixedViewSettings
 
-/** This class is just a stub for now: it should eventually draw 
- *  a proper ViewWidgetPanel */
+import org.nlogo.mirror.FakeWorld
+import org.nlogo.mirror.FixedViewSettings
+import org.nlogo.mirror.ModelRun
+import org.nlogo.swing.Utils.createWidgetBorder
+import org.nlogo.window.InterfaceColors.GRAPHICS_BACKGROUND
+
+import javax.swing.BorderFactory
+import javax.swing.JPanel
+
 class ViewWidgetPanel(bounds: java.awt.Rectangle) extends JPanel {
   setLayout(null)
   setBounds(bounds)
+  setBackground(GRAPHICS_BACKGROUND)
+  locally {
+    val matteBorder = BorderFactory.createMatteBorder(1, 3, 4, 2, GRAPHICS_BACKGROUND)
+    setBorder(BorderFactory.createCompoundBorder(createWidgetBorder, matteBorder))
+  }
 }
 
 class ViewPanel(
