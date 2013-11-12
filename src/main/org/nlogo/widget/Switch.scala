37bebabaffc07956bc17708b0f30d0e5e44c999d
nicolaspayette <nicolaspayette@gmail.com>
2013-11-11 19:54:39 -0500

    extract Switch painting code in PaintableSwitch trait

--- src/main/org/nlogo/widget/Switch.scala
+++ src/main/org/nlogo/widget/Switch.scala
@@ -2,10 +2,14 @@
 
 package org.nlogo.widget
 
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.awt.event.MouseWheelEvent
+import java.awt.event.MouseWheelListener
+
 import org.nlogo.agent.BooleanConstraint
-import org.nlogo.window.{Events, InterfaceColors, MultiErrorWidget}
-import java.awt._
-import event.{MouseWheelEvent, MouseEvent, MouseAdapter, MouseWheelListener}
+import org.nlogo.window.Events
+import org.nlogo.window.MultiErrorWidget
 
 object Switch {
   val BORDERX: Int = 3
@@ -17,9 +21,8 @@ object Switch {
 }
 
 abstract class Switch extends MultiErrorWidget with MouseWheelListener
-  with org.nlogo.window.Events.AfterLoadEventHandler {
-
-  import Switch._
+  with org.nlogo.window.Events.AfterLoadEventHandler
+  with PaintableSwitch {
 
   protected var constraint = new BooleanConstraint
   protected val channel = new Channel
@@ -27,12 +30,9 @@ abstract class Switch extends MultiErrorWidget with MouseWheelListener
   protected var nameChanged = false
   protected var _name = ""
 
-  setBackground(InterfaceColors.SWITCH_BACKGROUND)
-  setBorder(widgetBorder)
-  setOpaque(true)
-  org.nlogo.awt.Fonts.adjustDefaultFont(this)
   add(dragger)
   add(channel)
+
   addMouseWheelListener(this)
   addMouseListener(new MouseAdapter {
     override def mousePressed(e: MouseEvent) {
@@ -61,78 +61,25 @@ abstract class Switch extends MultiErrorWidget with MouseWheelListener
     if (_name.length > 0) { new Events.AddBooleanConstraintEvent(_name, isOn).raise(this) }
   }
 
-  override def getPreferredSize(font: Font): Dimension = {
-    val fontMetrics: FontMetrics = getFontMetrics(font)
-    val height: Int = (fontMetrics.getMaxDescent + fontMetrics.getMaxAscent) + 2 * BORDERY
-    val width: Int = 6 * BORDERX + channel.getWidth + fontMetrics.stringWidth(displayName) + fontMetrics.stringWidth("Off")
-    new Dimension(StrictMath.max(MINWIDTH, width), StrictMath.max(MINHEIGHT, height))
-  }
+  def mouseWheelMoved(e: MouseWheelEvent) { isOn = !(e.getWheelRotation >= 1) }
 
-  override def getMinimumSize = new Dimension(MINWIDTH, MINHEIGHT)
-  override def getMaximumSize = new Dimension(10000, MINHEIGHT)
-
-  override def doLayout() {
-    super.doLayout()
-    val scaleFactor: Float = getHeight.asInstanceOf[Float] / MINHEIGHT.asInstanceOf[Float]
-    channel.setSize((CHANNEL_WIDTH * scaleFactor).asInstanceOf[Int], (CHANNEL_HEIGHT * scaleFactor).asInstanceOf[Int])
-    channel.setLocation(BORDERX, (getHeight - channel.getHeight) / 2)
-    dragger.setSize((channel.getWidth * 0.9).asInstanceOf[Int], (channel.getHeight * 0.35).asInstanceOf[Int])
-    dragger.setLocation(BORDERX + (channel.getWidth - dragger.getWidth) / 2, channel.getY + (if (isOn) (0.1 * channel.getHeight).asInstanceOf[Int] else (channel.getHeight - dragger.getHeight - (0.1 * channel.getHeight).asInstanceOf[Int])))
-  }
-
-  override def paintComponent(g: Graphics) {
-    super.paintComponent(g)
-    val fontMetrics: FontMetrics = g.getFontMetrics
-    val stringAscent: Int = fontMetrics.getMaxAscent
-    val controlRect: Rectangle = channel.getBounds
-    g.setColor(getForeground)
-    g.drawString("On", controlRect.width + BORDERX,
-      (getHeight - (2 * stringAscent) - (2 * BORDERY)) / 2 + stringAscent + 1)
-    g.drawString("Off", controlRect.width + BORDERX,
-      (getHeight - (2 * stringAscent) - (2 * BORDERY)) / 2 + 2 * stringAscent + 1)
-    val controlLabelWidth: Int =
-      StrictMath.max(fontMetrics.stringWidth("On"), fontMetrics.stringWidth("Off")) + controlRect.width + 2 * BORDERX
-    g.setColor(getForeground)
-    g.drawString(
-      org.nlogo.awt.Fonts.shortenStringToFit(displayName, getWidth - 3 * BORDERX - controlLabelWidth, fontMetrics),
-      controlLabelWidth + 2 * BORDERX,
-      (getHeight - fontMetrics.getHeight - (2 * BORDERY)) / 2 + stringAscent + 1)
-  }
-
-  def mouseWheelMoved(e: MouseWheelEvent) { isOn = ! (e.getWheelRotation >= 1) }
-
-  protected class Dragger extends javax.swing.JPanel {
-    setBackground(InterfaceColors.SWITCH_HANDLE)
-    setBorder(org.nlogo.swing.Utils.createWidgetBorder)
-    setOpaque(true)
+  protected class Dragger extends PaintableSwitchDragger {
     addMouseListener(new MouseAdapter {
       override def mousePressed(e: MouseEvent) {
         new Events.InputBoxLoseFocusEvent().raise(Switch.this)
-        isOn = ! isOn
+        isOn = !isOn
       }
     })
   }
 
-  protected class Channel extends javax.swing.JComponent {
-    setOpaque(false)
-    setBackground(org.nlogo.awt.Colors.mixColors(InterfaceColors.SWITCH_BACKGROUND, java.awt.Color.BLACK, 0.5))
+  protected class Channel extends PaintableSwitchChannel {
     addMouseListener(new MouseAdapter {
       override def mousePressed(e: MouseEvent) {
         new Events.InputBoxLoseFocusEvent().raise(Channel.this)
         if (org.nlogo.awt.Mouse.hasButton1(e)) {
-          isOn = ! isOn
+          isOn = !isOn
         }
       }
     })
-
-    override def paintComponent(g: Graphics) {
-      val x: Int = (getWidth * 0.2).toInt
-      val y: Int = (getHeight * 0.1).toInt
-      val width: Int = (getWidth * 0.6).toInt
-      val height: Int = (getHeight * 0.8).toInt
-      g.setColor(getBackground)
-      g.fillRect(x, y, width, height)
-      org.nlogo.swing.Utils.createWidgetBorder.paintBorder(this, g, x, y, width, height)
-    }
   }
 }
