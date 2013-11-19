fd8548c67b3e5af0c6e82d3225c726658b8bf9f1
Seth Tisue <seth@tisue.net>
2013-11-19 09:29:42 -0500

    add 'ensime generate' to nightly.sh

--- nightly.sh
+++ nightly.sh
@@ -16,7 +16,7 @@ mkdir -p tmp/nightly
 # maybe there's an easier way, than I've done it below, I don't know.
 # I suck at shell scripting - ST 2/15/11
 
-./sbt test:compile 2>&1 | tee tmp/nightly/compile.txt
+./sbt test:compile "ensime generate" 2>&1 | tee tmp/nightly/compile.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: test:compile"; exit 1; fi
 echo "*** done: test:compile"
 
