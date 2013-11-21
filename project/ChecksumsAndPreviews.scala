ed042f0207bb18517a078f8ef7d32847045b2536
Seth Tisue <seth@tisue.net>
2013-11-21 10:04:20 -0500

    simplify checksum/preview stuff using fullRunInputTask
    (I don't think fullRunInputTask existed back when I first wrote this
    stuff)

--- project/ChecksumsAndPreviews.scala
+++ project/ChecksumsAndPreviews.scala
@@ -5,25 +5,19 @@ object ChecksumsAndPreviews {
 
   // sbt already has a command called "checksums", so we prepend "all-" - ST 6/23/12
 
-  val checksum = inputKey[Option[String]]("update one model checksum")
-  val allChecksums = inputKey[Option[String]]("update all model checksums")
-  val preview = inputKey[Option[String]]("update one model preview image")
-  val allPreviews = inputKey[Option[String]]("update all model preview images")
+  val checksum = inputKey[Unit]("update one model checksum")
+  val allChecksums = inputKey[Unit]("update all model checksums")
+  val preview = inputKey[Unit]("update one model preview image")
+  val allPreviews = inputKey[Unit]("update all model preview images")
 
-  val settings: Seq[Setting[InputTask[Option[String]]]] =
+  private val main = "org.nlogo.headless.ChecksumsAndPreviews"
+
+  val settings: Seq[Setting[InputTask[Unit]]] =
     Seq(
-      checksum := makeTask("--checksum").evaluated,
-      allChecksums := makeTask("--checksums").evaluated,
-      preview := makeTask("--preview").evaluated,
-      allPreviews := makeTask("--previews").evaluated
+      fullRunInputTask(checksum,     Compile, main, "--checksum"  ),
+      fullRunInputTask(allChecksums, Compile, main, "--checksums" ),
+      fullRunInputTask(preview,      Compile, main, "--preview"   ),
+      fullRunInputTask(allPreviews,  Compile, main, "--previews"  )
     )
-
-  private def makeTask(flag: String): Def.Initialize[InputTask[Option[String]]] =
-    Def.inputTask {
-      val args: Seq[String] = Def.spaceDelimited("<args>").parsed
-      val cp = (fullClasspath in Compile).value
-      Run.run("org.nlogo.headless.ChecksumsAndPreviews",
-        cp.map(_.data), flag +: args, streams.value.log)(runner.value)
-    }
 
 }
