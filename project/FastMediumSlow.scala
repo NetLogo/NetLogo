0a0a94229b919ca0492a849cc2613587fc51ccb4
Seth Tisue <seth@tisue.net>
2013-11-23 16:09:50 -0500

    DRY up project/FastMediumSlow.scala

--- project/FastMediumSlow.scala
+++ project/FastMediumSlow.scala
@@ -3,30 +3,29 @@ import Keys._
 
 object FastMediumSlow {
 
-  val FastTest = config("fast") extend(Test)
-  val MediumTest = config("medium") extend(Test)
-  val SlowTest = config("slow") extend(Test)
+  val configs =
+    Seq("fast", "medium", "slow")
+      .map(config(_).extend(Test))
 
-  val configs = Seq(FastTest, MediumTest, SlowTest)
+  private val filters =
+    Seq(fastFilter _, mediumFilter _, slowFilter _)
 
-  lazy val settings =
-    inConfig(FastTest)(Defaults.testTasks) ++
-    inConfig(MediumTest)(Defaults.testTasks) ++
-    inConfig(SlowTest)(Defaults.testTasks) ++
-    Seq(
-      testOptions in FastTest :=
-        Seq(Tests.Filter(fastFilter((fullClasspath in Test).value, _))),
-      testOptions in MediumTest :=
-        Seq(Tests.Filter(mediumFilter((fullClasspath in Test).value, _))),
-      testOptions in SlowTest :=
-        Seq(Tests.Filter(slowFilter((fullClasspath in Test).value, _)))
-    )
+  val settings =
+    (configs zip filters).flatMap{
+      case (config, filter) =>
+        inConfig(config)(Defaults.testTasks) :+
+        (testOptions in config :=
+          Seq(Tests.Filter(filter((fullClasspath in Test).value, _))))
+    }
 
-  private def fastFilter(path: Classpath, name: String): Boolean = !slowFilter(path, name)
+  private def fastFilter(path: Classpath, name: String): Boolean =
+    !slowFilter(path, name)
+
   private def mediumFilter(path: Classpath, name: String): Boolean =
     fastFilter(path, name) ||
     name == "org.nlogo.headless.lang.TestReporters" ||
     name == "org.nlogo.headless.lang.TestCommands"
+
   private def slowFilter(path: Classpath, name: String): Boolean = {
     val jars = path.files.map(_.asURL).toArray[java.net.URL]
     val loader = new java.net.URLClassLoader(jars, getClass.getClassLoader)
