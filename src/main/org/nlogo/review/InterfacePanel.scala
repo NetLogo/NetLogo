745497b25bb774b31460f1c7ac2bf5119faf1e6a
nicolaspayette <nicolaspayette@gmail.com>
2013-12-04 16:53:11 -0500

    review tab: override getPreferredSize of interface panel
    for scrollbars to appear when needed

--- src/main/org/nlogo/review/InterfacePanel.scala
+++ src/main/org/nlogo/review/InterfacePanel.scala
@@ -10,7 +10,7 @@ import scala.Option.option2Iterable
 import javax.swing.JPanel
 
 class InterfacePanel(val reviewTab: ReviewTab) extends JPanel {
-  
+
   override def isOptimizedDrawingEnabled = false
 
   setOpaque(true)
@@ -34,4 +34,11 @@ class InterfacePanel(val reviewTab: ReviewTab) extends JPanel {
     setBackground(if (reviewTab.state.currentRun.isDefined) WHITE else GRAY)
     super.paintComponent(g)
   }
+
+  override def getPreferredSize: java.awt.Dimension =
+    if (_widgetPanels.nonEmpty) {
+      val maxX = _widgetPanels.map(p => p.getLocation().x + p.getSize().width).max
+      val maxY = _widgetPanels.map(p => p.getLocation().y + p.getSize().height).max
+      new java.awt.Dimension(maxX, maxY)
+    } else new java.awt.Dimension(0, 0)
 }
