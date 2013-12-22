cc5522f788962e92e429841080754315a48b4681
nicolaspayette <nicolaspayette@gmail.com>
2013-12-22 10:24:52 -0500

    model runs: mirror observer perspective

--- headless/src/main/org/nlogo/mirror/Mirrorables.scala
+++ headless/src/main/org/nlogo/mirror/Mirrorables.scala
@@ -4,7 +4,6 @@ package org.nlogo.mirror
 
 import scala.collection.JavaConverters.iterableAsScalaIterableConverter
 import scala.language.implicitConversions
-
 import org.nlogo.api
 import org.nlogo.api.AgentVariableNumbers._
 import org.nlogo.api.AgentVariables.getImplicitLinkVariables
@@ -36,6 +35,7 @@ object Mirrorables {
   case object Observer extends Kind {
     object Variables extends Enumeration {
       val TargetAgent = Value("targetAgent")
+      val Perspective = Value("perspective")
     }
   }
   case object World extends Kind {
@@ -111,7 +111,8 @@ object Mirrorables {
         .map(getAgentKey)
         .map(key => (Serializer.agentKindToInt(key.kind), key.id))
     override val variables = Map(
-      TargetAgent.id -> targetAgent)
+      TargetAgent.id -> targetAgent,
+      Perspective.id -> Int.box(observer.perspective.export))
   }
 
   class MirrorableWorld(world: api.World) extends Mirrorable {
