6a3f061ee144e28a816d4896b24626b7f4791cf0
Seth Tisue <seth@tisue.net>
2014-04-03 23:45:14 -0500

    eliminate DefaultCommand, DefaultReporter
    at some point, not exactly sure when, these became unneeded

--- src/main/api/Primitive.scala
+++ src/main/api/Primitive.scala
@@ -15,21 +15,6 @@ import org.nlogo.core
 trait Primitive {
 
   /**
-   * Returns a String which specifies which agents can run this primitive.  To specify
-   * observer use "O", to specify Turtle use "T", to specify Patch use "P", to specify
-   * link use "L".  To use combinations, put them togther.
-   *
-   * Example of a primitive allowed for all agents:
-   * <code>String getAgentClassString() { return "OTPL"; }</code>
-   *
-   * Example of a primitive allowed only for turtles:
-   * <code> String getAgentClassString() { return "T"; }</code>
-   *
-   * @return a String specifying the acceptable agent types.
-   */
-  def getAgentClassString: String
-
-  /**
    * Returns Syntax which specifies the syntax that is acceptable for this primitive.  Used by the
    * compiler for type-checking.
    *
