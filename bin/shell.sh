21a2ce680ff13242eaddc42fc4c0e417781f3c9a
Seth Tisue <seth@tisue.net>
2014-04-16 10:30:52 -0500

    fix regression in bin/shell.sh
    it needs to still work when no model path is supplied, too

--- bin/shell.sh
+++ bin/shell.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+if [ -n "$1" ]; then
+   ARG=" \"$1\""
+fi
+
 rlwrap ./sbt \
   -Djline.terminal=jline.UnsupportedTerminal \
   --warn \
-  "run-main org.nlogo.headless.Shell \"$1\""
+  "run-main org.nlogo.headless.Shell $ARG"
