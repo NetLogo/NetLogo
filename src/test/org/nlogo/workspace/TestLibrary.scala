e003d4712073a8afc9b80157dbc2508f84afd9d3
nicolaspayette <nicolaspayette@gmail.com>
2015-03-27 18:46:09 -0400

    add test for empty leafs folder in model library dialog
    for https://github.com/NetLogo/models/issues/33

--- src/test/org/nlogo/workspace/TestLibrary.scala
+++ src/test/org/nlogo/workspace/TestLibrary.scala
@@ -4,6 +4,7 @@ package org.nlogo.workspace
 
 import org.scalatest.FunSuite
 import ModelsLibrary._
+import scala.collection.JavaConverters._
 
 class TestLibrary extends FunSuite {
   test("every model has a unique name") {
@@ -13,5 +14,12 @@ class TestLibrary extends FunSuite {
       assert(!names.contains(name.toUpperCase), name)
       names += name.toUpperCase
     }
+  }
+  test("there are no empty leaf folders") {
+    val emptyLeafFolders = scanForModelsAtRoot("models", false)
+      .breadthFirstEnumeration.asScala
+      .collect { case node: ModelsLibrary.Node => node }
+      .filter { node => node.isFolder && !node.children.hasMoreElements }
+    assert(emptyLeafFolders.isEmpty, emptyLeafFolders.map(_.getPath.mkString("/")).toList)
   }
 }
