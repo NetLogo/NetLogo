568e8b14e1f3c1f31808995716f9371af4778d58
nicolaspayette <nicolaspayette@gmail.com>
2015-04-20 21:19:35 -0400

    remove outdated exclusion

--- src/test/org/nlogo/headless/TestCompileAll.scala
+++ src/test/org/nlogo/headless/TestCompileAll.scala
@@ -16,17 +16,6 @@ class TestCompileAll extends FunSuite with SlowTest{
   }
 
   def compile(path: String) {
-    import java.io.File.separatorChar
-    def pathMatches(bad: String) =
-      path.toUpperCase.containsSlice(separatorChar + bad + separatorChar)
-    if (pathMatches("DOESN'T COMPILE") ||
-        // letting the textbook team deal with these should help ensure
-        // the book gets updated too - ST 4/22/10
-        pathMatches("TEXTBOOK MODELS") ||
-        !Version.is3D && path.endsWith(".nlogo3d") ||
-        // in 3D skip models that aren't in the 3D directory.
-        Version.is3D && !pathMatches("3D"))
-      return
     val workspace = HeadlessWorkspace.newInstance
     // this keeps patches from being created, which we don't need,
     // and which was slowing things down - ST 1/13/05
