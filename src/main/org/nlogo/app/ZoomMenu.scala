140cd003690a4ad32f88fc8bb9337f25864aa8f6
Robert Grider <robert.grider@northwestern.edu>
2015-05-18 08:58:26 -0500

    Open the possibility for a zoomable InterfacePanelLite
    * Make ZoomMenu public
    * Decouple AppletPanel constructor to allow alternate InterfacePanelLite
      to be used

--- src/main/org/nlogo/app/ZoomMenu.scala
+++ src/main/org/nlogo/app/ZoomMenu.scala
@@ -5,7 +5,7 @@ package org.nlogo.app
 import org.nlogo.api.I18N
 // note that multiple instances of this class may exist as there are now multiple frames that each
 // have their own menu bar and menus  ev 8/25/05
-private class ZoomMenu extends org.nlogo.swing.Menu(I18N.gui.get("menu.zoom")) {
+class ZoomMenu extends org.nlogo.swing.Menu(I18N.gui.get("menu.zoom")) {
 
   implicit val i18nName = I18N.Prefix("menu.zoom")
 
