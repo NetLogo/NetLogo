6ffb84e75f68a51d7b375ce70290c37fb86a25f1
nicolaspayette <nicolaspayette@gmail.com>
2015-06-04 21:21:04 -0400

    exclude some folders from "non empty leafs folder" test
    this test would be better belong in the models repo but:
    
    - it relies on org.nlogo.workspace.ModelsLibrary.scanForModelsAtRoot
    - I'm hoping it won't be necessary anymore once #754 is fixed

--- src/test/org/nlogo/workspace/TestLibrary.scala
+++ src/test/org/nlogo/workspace/TestLibrary.scala
@@ -8,9 +8,12 @@ import scala.collection.JavaConverters._
 
 class TestLibrary extends FunSuite {
   test("there are no empty leaf folders") {
+    val exclusions = Set("project", "src", "target")
+      .map("/models/" + _ + "/")
     val emptyLeafFolders = scanForModelsAtRoot("models", false)
       .breadthFirstEnumeration.asScala
       .collect { case node: ModelsLibrary.Node => node }
+      .filterNot { node => exclusions.exists(node.getFilePath.contains) }
       .filter { node => node.isFolder && !node.children.hasMoreElements }
     assert(emptyLeafFolders.isEmpty, emptyLeafFolders.map(_.getPath.mkString("/")).toList)
   }
