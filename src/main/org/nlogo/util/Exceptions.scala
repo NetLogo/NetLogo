19e77f015abc4c7a69cf077e168f483c03823480
Idloj <Idloj@users.noreply.github.com>
2015-06-16 18:53:28 +0300

    Remove procedure syntax from `util` package

--- src/main/org/nlogo/util/Exceptions.scala
+++ src/main/org/nlogo/util/Exceptions.scala
@@ -8,22 +8,18 @@ object Exceptions
     def handle(t: Throwable)
   }
   val defaultHandler = new Handler() {
-    def handle(t: Throwable) {
-      t.printStackTrace(System.err)
-    }
+    def handle(t: Throwable) = t.printStackTrace(System.err)
   }
   private var handler: Handler = defaultHandler
-  def setHandler(handler: Handler) {
+  def setHandler(handler: Handler) = {
     assert(handler != null)
     this.handler = handler
   }
-  def handle(t: Throwable) {
-    handler.handle(t)
-  }
-  def ignore(t: Throwable) {
+  def handle(t: Throwable) = handler.handle(t)
+  def ignore(t: Throwable) = {
     // do nothing, but you could put debugging output here, or a debugger breakpoint - ST 5/14/03
   }
-  def warn(t: Throwable) {
+  def warn(t: Throwable) = {
     System.err.println("Warning -- Ignoring exception: " + t)
     t.printStackTrace(System.err)
   }
@@ -34,14 +30,11 @@ object Exceptions
   // very late in the game for NetLogo 5.0, so it seems safer not to rock the boat on this
   // at the moment. - ST 8/5/11
 
-  def ignoring(cs: Class[_]*)(body: =>Unit) {
+  def ignoring(cs: Class[_]*)(body: =>Unit) =
     util.control.Exception.catchingPromiscuously(cs: _*).withApply(ignore) { body }
-  }
-  def handling(cs: Class[_]*)(body: =>Unit) {
+  def handling(cs: Class[_]*)(body: =>Unit) =
     util.control.Exception.catchingPromiscuously(cs: _*).withApply(handle) { body }
-  }
-  def warning(cs: Class[_]*)(body: =>Unit) {
+  def warning(cs: Class[_]*)(body: =>Unit) =
     util.control.Exception.catchingPromiscuously(cs: _*).withApply(warn) { body }
-  }
 
 }
--- src/main/org/nlogo/util/Pico.scala
+++ src/main/org/nlogo/util/Pico.scala
@@ -7,19 +7,9 @@ import org.picocontainer.behaviors.Caching
 
 class Pico extends DefaultPicoContainer(new Caching)
 {
-  def add(name:String) {
-    addComponent(Class.forName(name))
-  }
-  def add(key: Class[_], name: String, parameters: Array[Parameter]) {
-    addComponent(key, Class.forName(name), parameters:_*)
-  }
-  def add(key: Class[_], name: String, parameters: Parameter*) {
-    addComponent(key, Class.forName(name), parameters:_*)
-  }
-  def add(key: Class[_], name: String) {
-    addComponent(key, Class.forName(name))
-  }
-  def addScalaObject(name:String) {
-    addComponent(Class.forName(name + "$").getField("MODULE$").get(null))
-  }
+  def add(name:String) = addComponent(Class.forName(name))
+  def add(key: Class[_], name: String, parameters: Array[Parameter]) = addComponent(key, Class.forName(name), parameters:_*)
+  def add(key: Class[_], name: String, parameters: Parameter*) = addComponent(key, Class.forName(name), parameters:_*)
+  def add(key: Class[_], name: String) = addComponent(key, Class.forName(name))
+  def addScalaObject(name:String) = addComponent(Class.forName(name + "$").getField("MODULE$").get(null))
 }
