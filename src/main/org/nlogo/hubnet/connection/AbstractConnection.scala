aeefccc7e5ca6ca76bd767bcd0daceb60c8b39a5
Idloj <Idloj@users.noreply.github.com>
2015-06-16 13:30:46 +0300

    Remove procedure syntax from `hubnet.connection` package

--- src/main/org/nlogo/hubnet/connection/AbstractConnection.scala
+++ src/main/org/nlogo/hubnet/connection/AbstractConnection.scala
@@ -21,7 +21,7 @@ abstract class AbstractConnection(name: String, connectionStreams: Streamable) e
   private var keepListening = true
   @volatile private var keepWriting = true
 
-  override def run() {
+  override def run() = {
     writingThread.start()
     while (keepListening) {
       try receiveData(input.readObject())
@@ -41,29 +41,27 @@ abstract class AbstractConnection(name: String, connectionStreams: Streamable) e
   // this should only be called if you need to wait for the send to
   // be completed it bypasses the writeQueue CB 09/28/2004
   @throws(classOf[java.io.IOException])
-  def waitForSendData(a: Any) {send(a)}
+  def waitForSendData(a: Any) = send(a)
 
   // use this method if you don't need to wait for the send to complete
   // this should only be used before stopWriting() is called.
-  def sendData(a: Any) {
+  def sendData(a: Any) =
     // Note that the writingThread may be dead already at this point,
     // but it doesn't matter... the caller is *never* guaranteed that
     // messages are going to get through, since the client could vanish
     // at any time. - ST 11/22/04
     writeQueue.add(a)
-  }
 
   // after a call to this, you should only call waitForSendData()
   // and not sendData() since the writingThread will be dead
-  def stopWriting() { keepWriting = false }
+  def stopWriting() = keepWriting = false
 
-  def disconnect(reason:String) {
-    if (keepListening) {
+  def disconnect(reason:String) = if (keepListening) {
       keepListening = false
       stopWriting()
       new Thread("SocketListener disconnect") {
-        override def run() {
-          def warn(body: => Unit) {
+        override def run() = {
+          def warn(body: => Unit) = {
             Exceptions.warning(classOf[java.io.IOException]) { body }
           }
           Exceptions.handling(classOf[RuntimeException]) {
@@ -74,13 +72,11 @@ abstract class AbstractConnection(name: String, connectionStreams: Streamable) e
         }
       }.start()
     }
-  }
 
   def getSendQueueSize = writeQueue.size
 
   @throws(classOf[java.io.IOException])
-  private def send(a: Any) {
-    output.synchronized {
+  private def send(a: Any) = output.synchronized {
       output.writeObject(a)
       // we're not sure if this call to flush is absolutely necessary.
       // it can take up to .02 seconds.  however, since it is in a
@@ -99,10 +95,10 @@ abstract class AbstractConnection(name: String, connectionStreams: Streamable) e
       count += 1
       if (count % RESET_DELAY == 0) output.reset()
     }
-  }
+
 
   private class WritingThread(name: String) extends Thread("WritingThread:" + name) {
-    override def run() {
+    override def run() = {
       while (keepWriting) {
         try
           try {
