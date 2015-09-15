43ffbbb5520a9c120fc696a4e75a9b0a92cd9d9f
Robert Grider <robert.grider@northwestern.edu>
2015-09-15 11:50:08 -0500

    Build extensions without starting new processes
    Conflicts:
    	project/Extensions.scala

--- project/Extensions.scala
+++ project/Extensions.scala
@@ -4,40 +4,50 @@ import Keys._
 
 object Extensions {
 
-  val extensions = TaskKey[Seq[File]](
-    "extensions", "builds extensions")
+  val extensions = TaskKey[Seq[File]]("extensions", "builds extensions")
 
-  val extensionsTask =
-    extensions <<= (baseDirectory, scalaInstance, streams) map {
-      (base, scala, s) =>
-        "git submodule --quiet update --init" ! s.log
-        val isDirectory = new java.io.FileFilter {
-          override def accept(f: File) = f.isDirectory
-        }
-        val dirs = IO.listFiles(isDirectory)(base / "extensions").toSeq
-        val caches = dirs.map{dir =>
-          FileFunction.cached(s.cacheDirectory / "extensions" / dir.getName,
-                              inStyle = FilesInfo.hash, outStyle = FilesInfo.hash) {
-            in =>
-              Set(buildExtension(dir, scala.libraryJar, s.log))
-          }}
-        caches.flatMap{cache => cache(Set(base / "NetLogo.jar", base / "NetLogoLite.jar"))}
-    } dependsOn(packageBin in Compile)
+  val extensionsTask = Seq(
+    extensions := {
+      val base                   = baseDirectory.value
+      val scala                  = scalaInstance.value
+      val s                      = streams.value
+      val packagedNetLogoJar     = (packageBin in Compile).value
+      val packagedNetLogoTestJar = (packageBin in Test).value
+      "git submodule --quiet update --init" ! s.log
+      val isDirectory = new java.io.FileFilter {
+        override def accept(f: File) = f.isDirectory
+      }
+      val dirs = IO.listFiles(isDirectory)(base / "extensions").toSeq
+      val caches = dirs.map{dir =>
+        FileFunction.cached(s.cacheDirectory / "extensions" / dir.getName,
+                            inStyle = FilesInfo.hash, outStyle = FilesInfo.hash) {
+          in =>
+            Set(buildExtension(dir, scala.libraryJar, packagedNetLogoJar, s.log, state.value))
+        }}
+      caches.flatMap{cache => cache(Set(base / "NetLogo.jar", base / "NetLogoLite.jar"))}
+    }
+  )
 
-  private def buildExtension(dir: File, scalaLibrary: File, log: Logger): File = {
+  class NestedConfiguration(val config: xsbti.AppConfiguration, baseDir: File, args: Array[String]) extends xsbti.AppConfiguration {
+    override val arguments = args
+    override val provider = config.provider
+    override val baseDirectory = baseDir
+  }
+
+  val runner = new xMain()
+
+  def config(state: State, dir: File, command: String) =
+    new NestedConfiguration(state.configuration, dir, Array(command))
+
+  private def buildExtension(dir: File, scalaLibrary: File, netLogoJar: File, log: Logger, state: State): File = {
     log.info("building extension: " + dir.getName)
+    System.setProperty("netlogo.jar.url", netLogoJar.toURI.toString)
+    val buildConfig  = config(state, dir, "package")
     val jar = dir / (dir.getName + ".jar")
-    val exitCode =
-      if((dir / "build.sbt").exists && (dir / "bin" / "sbt").exists)
-        Process(Seq("bin/sbt", "package"), dir,
-                "SCALA_JAR" -> scalaLibrary.getPath) ! log
-      else if((dir / "build.sbt").exists)
-        Process(Seq("./sbt", "package"), dir,
-                "SCALA_JAR" -> scalaLibrary.getPath) ! log
-      else
-        Process(Seq("make", "-s", jar.getName), dir,
-                "SCALA_JAR" -> scalaLibrary.getPath) ! log
-    assert(exitCode == 0, "extension build failed, exitCode = " + exitCode)
+    runner.run(buildConfig) match {
+      case e: xsbti.Exit   => assert(e.code == 0, "extension build failed, exitCode = " + e.code)
+      case r: xsbti.Reboot => assert(true == false, "expected application to build, instead rebooted")
+    }
     jar
   }
 
