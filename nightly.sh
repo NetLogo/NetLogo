aa0d02b7c40113437b9a36e871d95194007d99ad
Robert Grider <robert.grider@northwestern.edu>
2015-10-06 10:52:32 -0500

    Nightly uses renamed test commands

--- nightly.sh
+++ nightly.sh
@@ -19,25 +19,25 @@ mkdir -p tmp/nightly
 # here we're using pipes so "-e" isn't enough to stop when something fails.
 # maybe there's an easier way, than I've done it below, I don't know.
 # I suck at shell scripting - ST 2/15/11
-./sbt fast:test 2>&1 | tee tmp/nightly/0-fast-test.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: fast:test"; exit 1; fi
-echo "*** done: fast:test"
-./sbt nogen fast:test 2>&1 | tee tmp/nightly/1-nogen-fast-test.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen fast:test"; exit 1; fi
-echo "*** done: nogen fast:test"
-./sbt threed fast:test 2>&1 | tee tmp/nightly/2-threed-fast-test.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: threed fast:test"; exit 1; fi
-echo "*** done: threed fast:test"
+./sbt test:fast 2>&1 | tee tmp/nightly/0-fast-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: test:fast"; exit 1; fi
+echo "*** done: test:fast"
+./sbt nogen test:fast 2>&1 | tee tmp/nightly/1-nogen-fast-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen test:fast"; exit 1; fi
+echo "*** done: nogen test:fast"
+./sbt threed test:fast 2>&1 | tee tmp/nightly/2-threed-fast-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: threed test:fast"; exit 1; fi
+echo "*** done: threed test:fast"
 
-./sbt slow:test 2>&1 | tee tmp/nightly/3-slow-test.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: slow:test"; exit 1; fi
-echo "*** done: slow:test"
-./sbt nogen slow:test 2>&1 | tee tmp/nightly/4-nogen-slow-test.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen slow:test"; exit 1; fi
-echo "*** done: nogen slow:test"
-./sbt threed slow:test 2>&1 | tee tmp/nightly/5-threed-slow-test.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: threed slow:test"; exit 1; fi
-echo "*** done: threed slow:test"
+./sbt test:slow 2>&1 | tee tmp/nightly/3-slow-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: test:slow"; exit 1; fi
+echo "*** done: test:slow"
+./sbt nogen test:slow 2>&1 | tee tmp/nightly/4-nogen-slow-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen test:slow"; exit 1; fi
+echo "*** done: nogen test:slow"
+./sbt threed test:slow 2>&1 | tee tmp/nightly/5-threed-slow-test.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: threed test:slow"; exit 1; fi
+echo "*** done: threed test:slow"
 
 ./sbt depend 2>&1 | tee tmp/nightly/6-depend.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: depend"; exit 1; fi
