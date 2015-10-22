6305c70f6b3ce34e653952d3aaeef540484c37c8
Robert Grider <robert.grider@northwestern.edu>
2015-10-22 12:19:45 -0500

    ProceduresToHtml just calls into org.nlogo.parse.Colorizer

--- netlogo-gui/src/main/app/ProceduresToHtml.scala
+++ netlogo-gui/src/main/app/ProceduresToHtml.scala
@@ -13,50 +13,11 @@ object ProceduresToHtml {
   def newInstance = {
     val pico = new org.nlogo.util.Pico
     pico.addComponent(classOf[ProceduresToHtml])
-    pico.addComponent(classOf[org.nlogo.nvm.DefaultCompilerServices])
-    pico.addScalaObject("org.nlogo.compiler.Compiler")
     pico.getComponent(classOf[ProceduresToHtml])
   }
 }
-class ProceduresToHtml(compiler:CompilerServices) extends ProceduresToHtmlInterface {
+class ProceduresToHtml extends ProceduresToHtmlInterface {
   def convert(source:String):String = {
-    // getCharacterColors gives us a color for every character, but we want to wait until
-    // the color changes before we start a new font tag.  So we group the colors into
-    // sublists of equal colors.
-    val colorGroups = group(new EditorColorizer(compiler).getCharacterColors(source).toList)
-    // use a mutable StringBuilder and tail recursion so we don't blow the stack - ST 6/30/09
-    val result = new StringBuilder("<pre>")
-    def loop(source:String,colorGroups:List[List[Color]]) {
-      if(!colorGroups.isEmpty) {
-        val group = colorGroups.head
-        result ++= encode(source.take(group.size),group.head)
-        loop(source.drop(group.size),colorGroups.tail)
-      }
-    }
-    loop(source,colorGroups)
-    result ++= "\n</pre>\n"
-    result.toString
-  }
-  private def encode(source:Seq[Char],color:Color) = {
-    val hexColor = toHex(color.getRed) + toHex(color.getGreen) + toHex(color.getBlue)
-    "<font color=\"#" + hexColor + "\">" + source.map(escape).mkString + "</font>"
-  }
-  private def toHex(i:Int) = (if(i < 16) "0" else "") + i.toHexString
-  private def escape(c:Char) = escapeMap.getOrElse(c,c)
-  private val escapeMap = Map('&' -> "&amp;",
-                              '<' -> "&lt;",
-                              '>' -> "&gt;",
-                              '"' -> "&quot;")
-  // like group in Haskell. e.g. group(List(1,1,2,3,3,3)) = List(List(1,1),List(2),List(3,3,3)).
-  // imperative using ListBuffer so we don't blow the stack on long inputs. - ST 6/30/09
-  private def group[T](xs:List[T]):List[List[T]] = {
-    val result = new collection.mutable.ListBuffer[List[T]]
-    var rest = xs
-    while(!rest.isEmpty) {
-      val (firstGroup, more) = rest.span(_ == rest.head)
-      result += firstGroup
-      rest = more
-    }
-    result.toList
+    s"<pre>${org.nlogo.parse.Colorizer.toHtml(source)}\n</pre>\n"
   }
 }
--- netlogo-gui/src/main/app/ProceduresToHtmlInterface.scala
+++ netlogo-gui/src/main/app/ProceduresToHtmlInterface.scala
@@ -1,6 +1,7 @@
 // (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
 
 package org.nlogo.app
+
 trait ProceduresToHtmlInterface {
   def convert(code:String):String
 }
