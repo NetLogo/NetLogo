f1b2529dbecb6c2beb175e32b176f4530dd3292b
Robert Grider <robert.grider@northwestern.edu>
2015-11-02 09:41:45 -0600

    Pass AppHandler to App, removes MacHandlers
    - Fetch artifacts from AWS instead of ccl.northwestern.edu

--- project/Compiling.scala
+++ project/Compiling.scala
@@ -9,7 +9,7 @@ object Compiling {
     java5 <<= (baseDirectory, streams) map { (base, s) =>
       val classes = base / "dist" / "java5" / "classes.jar"
       if(!classes.exists) {
-        val url = new java.net.URL("http://ccl.northwestern.edu/devel/java5-classes.jar")
+        val url = new java.net.URL("http://ccl-artifacts.s3.amazonaws.com/java5-classes.jar")
         s.log.info("downloading " + url)
         IO.download(url, classes)
       }
