6fa2ebaf8d2bcbceff796dff2d02d637971802f2
Robert Grider <robert.grider@northwestern.edu>
2015-11-17 14:01:48 -0600

    Make AggregateLinuxBuild produce a tar.gz

--- dist/project/AggregateLinuxBuild.scala
+++ dist/project/AggregateLinuxBuild.scala
@@ -15,24 +15,36 @@ object AggregateLinuxBuild {
   //
   //  The desire is to merge all of the app folders, copy the libpackager and runtime, and be done
   def apply(aggregateTarget: File, buildsMap: Map[SubApplication, File]): File = {
+    val version = "5.2.2-RC1"
     val aggregateLinuxDir = aggregateTarget / "linux-full"
+    // we build the image in this directory, and we later use tar.gz to package it
+    val imageDir = aggregateLinuxDir / s"netlogo-$version"
+    val archiveName = s"netlogo-$version.tar.gz"
     IO.delete(aggregateLinuxDir)
-    IO.createDirectory(aggregateLinuxDir)
+    IO.createDirectory(imageDir)
     val baseImage = buildsMap.head._2
-    IO.copyDirectory(baseImage / "runtime", aggregateLinuxDir / "runtime")
-    IO.copyFile(baseImage / "libpackager.so", aggregateLinuxDir / "libpackager.so")
-    IO.createDirectory(aggregateLinuxDir / "app")
+    IO.copyDirectory(baseImage / "runtime", imageDir / "runtime")
+    IO.copyFile(baseImage / "libpackager.so", imageDir / "libpackager.so")
+    IO.createDirectory(imageDir / "app")
+
     buildsMap.foreach {
       case (app, image) =>
         val name = app.name.replaceAllLiterally(" ", "")
-        IO.copyFile(image / name, aggregateLinuxDir / name)
+        IO.copyFile(image / name, imageDir / name)
         val copies = Path.allSubpaths(image / "app").map {
-          case (f, relPath) => (f, aggregateLinuxDir / "app" / relPath)
+          case (f, relPath) => (f, imageDir / "app" / relPath)
         }
         IO.copy(copies)
     }
-    // TODO: Tag with version
-    IO.zip(Path.allSubpaths(aggregateLinuxDir), aggregateTarget / "netlogo-linux.zip")
-    aggregateTarget / "netlogo-linux.zip"
+
+    buildsMap.map(_._1.name.replaceAllLiterally(" ", "")).foreach { executableName =>
+      (imageDir / executableName).setExecutable(true)
+    }
+
+    val res = Process(Seq("tar", "-zcf", archiveName, imageDir.getName), aggregateLinuxDir).!
+    if (res != 0)
+      sys.error("failed to build tarball")
+
+    aggregateLinuxDir / archiveName
   }
 }
