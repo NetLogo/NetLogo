325754c79a923d45624ed74dba8f030431c4542f
Idloj <Idloj@users.noreply.github.com>
2015-11-28 22:39:49 +0200

    Don't scroll when the text is edited

--- src/main/org/nlogo/editor/LineNumbersBar.scala
+++ src/main/org/nlogo/editor/LineNumbersBar.scala
@@ -2,14 +2,16 @@
 
 package org.nlogo.editor
 
-import java.awt.{ Color, Dimension }
+import java.awt.{ Color, Dimension, Font }
 import javax.swing.JTextPane
 import javax.swing.event.{ DocumentEvent, DocumentListener }
+import javax.swing.text.DefaultCaret
 
 class LineNumbersBar(editor: EditorArea[_]) extends JTextPane with DocumentListener {
   updateNumbers()
   setEnabled(false)
   setBackground(new Color(240, 240, 240))
+  getCaret.asInstanceOf[DefaultCaret].setUpdatePolicy(DefaultCaret.NEVER_UPDATE)
   editor.getDocument.addDocumentListener(this)
 
   def updateNumbers() = {
@@ -19,6 +21,7 @@ class LineNumbersBar(editor: EditorArea[_]) extends JTextPane with DocumentListe
 
   override def getPreferredSize = new Dimension(super.getPreferredSize.width, editor.getPreferredSize.height)
   override def getFont = editor.getFont
+  override def getFontMetrics(f: Font) = editor.getFontMetrics(f)
 
   def insertUpdate(e: DocumentEvent) = updateNumbers()
   def removeUpdate(e: DocumentEvent) = updateNumbers()
