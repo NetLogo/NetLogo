960903c8db94637f38b9560fd57f6714dcd2d86d
Idloj <Idloj@users.noreply.github.com>
2015-12-06 15:56:44 +0200

    Align line numbers with text Don't call setText if there is no need

--- src/main/org/nlogo/editor/LineNumbersBar.scala
+++ src/main/org/nlogo/editor/LineNumbersBar.scala
@@ -8,20 +8,25 @@ import javax.swing.event.{ DocumentEvent, DocumentListener }
 import javax.swing.text.DefaultCaret
 
 class LineNumbersBar(editor: EditorArea[_]) extends JTextPane with DocumentListener {
+  var previousLastLineNumber = -1
+
   updateNumbers()
   setEnabled(false)
   setBackground(new Color(240, 240, 240))
+  setBorder(editor.getBorder)
   getCaret.asInstanceOf[DefaultCaret].setUpdatePolicy(DefaultCaret.NEVER_UPDATE)
   editor.getDocument.addDocumentListener(this)
 
   def updateNumbers() = {
-    val lastLineNumber = editor.getText().filter(_=='\n').length + 1
-    setText(1 to lastLineNumber mkString("\n"))
+    val lastLineNumber = editor.getText().count(_=='\n') + 1
+    if(lastLineNumber != previousLastLineNumber) {
+      setText(1 to lastLineNumber mkString("\n"))
+      previousLastLineNumber = lastLineNumber
+    }
   }
 
   override def getPreferredSize = new Dimension(super.getPreferredSize.width, editor.getPreferredSize.height)
   override def getFont = editor.getFont
-  override def getFontMetrics(f: Font) = editor.getFontMetrics(f)
 
   def insertUpdate(e: DocumentEvent) = updateNumbers()
   def removeUpdate(e: DocumentEvent) = updateNumbers()
