93b00a400f06a80e5d3a1a86814d431bb5c91669
Nicolas Payette <nicolaspayette@gmail.com>
2015-12-28 14:31:39 -0500

    use imports in GraphicsPreview

--- src/main/org/nlogo/app/GraphicsPreview.scala
+++ src/main/org/nlogo/app/GraphicsPreview.scala
@@ -1,39 +1,51 @@
 package org.nlogo.app
 
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.Image
+import java.awt.RenderingHints
+
+import org.nlogo.awt.Images.loadImageFile
+import org.nlogo.window.GraphicsPreviewInterface
+
+import javax.swing.JPanel
+
 class GraphicsPreview
-  extends javax.swing.JPanel
+  extends JPanel
   // not JComponent otherwise super.paintComponent() doesn't paint the
   // background color for reasons I can't fathom - ST 8/3/03
-  with org.nlogo.window.GraphicsPreviewInterface {
+  with GraphicsPreviewInterface {
 
-  private var image: java.awt.Image = null
+  private var image: Image = null
 
-  setBackground(java.awt.Color.BLACK)
+  setBackground(Color.BLACK)
   setOpaque(true)
-  setPreferredSize(new java.awt.Dimension(400, 400))
+  setPreferredSize(new Dimension(400, 400))
   setMinimumSize(getPreferredSize)
   setMaximumSize(getPreferredSize)
 
   def setImage(imagePath: String) {
     image = null
     if (imagePath != null) {
-      image = org.nlogo.awt.Images.loadImageFile(imagePath, false)
+      image = loadImageFile(imagePath, false)
     }
     repaint()
   }
 
-  def setImage(newImage: java.awt.Image) {
+  def setImage(newImage: Image) {
     image = newImage
     repaint()
   }
 
-  override def paintComponent(g: java.awt.Graphics) {
+  override def paintComponent(g: Graphics) {
     if (image == null) {
       super.paintComponent(g)
     } else {
-      g.asInstanceOf[java.awt.Graphics2D].setRenderingHint(
-        java.awt.RenderingHints.KEY_RENDERING,
-        java.awt.RenderingHints.VALUE_RENDER_QUALITY)
+      g.asInstanceOf[Graphics2D].setRenderingHint(
+        RenderingHints.KEY_RENDERING,
+        RenderingHints.VALUE_RENDER_QUALITY)
       val width = image.getWidth(null)
       val height = image.getHeight(null)
       if (width == height) {
