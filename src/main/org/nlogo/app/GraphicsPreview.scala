48959de7b79e70ec050661f8a7c937aeb003f2ab
Nicolas Payette <nicolaspayette@gmail.com>
2015-12-28 17:15:28 -0500

    shrink instead of cropping in GraphicsPreview
    close https://github.com/NetLogo/models/issues/48

--- src/main/org/nlogo/app/GraphicsPreview.scala
+++ src/main/org/nlogo/app/GraphicsPreview.scala
@@ -18,7 +18,7 @@ class GraphicsPreview
   // background color for reasons I can't fathom - ST 8/3/03
   with GraphicsPreviewInterface {
 
-  private var image: Image = null
+  private var image: Option[Image] = None
 
   setBackground(Color.BLACK)
   setOpaque(true)
@@ -26,35 +26,34 @@ class GraphicsPreview
   setMinimumSize(getPreferredSize)
   setMaximumSize(getPreferredSize)
 
-  def setImage(imagePath: String) {
-    image = null
-    if (imagePath != null) {
-      image = loadImageFile(imagePath, false)
-    }
-    repaint()
-  }
+  def setImage(imagePath: String): Unit =
+    setImage(Option(imagePath).map(loadImageFile(_, false)).orNull)
 
-  def setImage(newImage: Image) {
-    image = newImage
+  def setImage(newImage: Image): Unit = {
+    image = Option(newImage)
     repaint()
   }
 
   override def paintComponent(g: Graphics) {
-    if (image == null) {
-      super.paintComponent(g)
-    } else {
-      g.asInstanceOf[Graphics2D].setRenderingHint(
-        RenderingHints.KEY_RENDERING,
-        RenderingHints.VALUE_RENDER_QUALITY)
-      val width = image.getWidth(null)
-      val height = image.getHeight(null)
-      if (width == height) {
-        g.drawImage(image, 0, 0, 400, 400, this)
-      } else if (width > height) {
-        g.drawImage(image, 0, 0, (width * (400.0 / height)).toInt, 400, this)
-      } else {
-        g.drawImage(image, 0, 0, 400, (height * (400.0 / width)).toInt, this)
-      }
+    image match {
+      case None => super.paintComponent(g)
+      case Some(image) =>
+        g.asInstanceOf[Graphics2D].setRenderingHint(
+          RenderingHints.KEY_RENDERING,
+          RenderingHints.VALUE_RENDER_QUALITY)
+        val ratio = image.getWidth(null) / image.getHeight(null).toDouble
+        val (w, h) = ratio match {
+          case 1.0          => (400, 400)
+          case r if r < 1.0 => ((400 * r).toInt, 400)
+          case r if r > 1.0 => (400, (400 * (1 / r)).toInt)
+        }
+        val x = (400 - w) / 2
+        val y = (400 - h) / 2
+        if (ratio != 1.0) {
+          g.setColor(Color.black)
+          g.fillRect(0, 0, 400, 400)
+        }
+        g.drawImage(image, x, y, w, h, this)
     }
   }
 }
