f95a4418459fb95be0fd4dc906550c322ea6a202
Robert Grider <robert.grider@northwestern.edu>
2016-03-16 15:01:58 -0500

    Build jars under the appropriate name

--- project/Packaging.scala
+++ project/Packaging.scala
@@ -6,8 +6,6 @@ object Packaging {
 
   val settings = Seq(
     publishArtifact in Test := true,
-    artifactName in Compile := { (_, _, _) => "NetLogo.jar" },
-    artifactName in Test := { (_, _, _) => "NetLogo-tests.jar" },
     packageOptions <+= dependencyClasspath in Runtime map {
       classpath =>
         Package.ManifestAttributes((
