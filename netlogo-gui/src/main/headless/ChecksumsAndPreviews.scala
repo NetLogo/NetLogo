ad09104c82a63e60d5ac136284b8863b5ed70f64
Robert Grider <robert.grider@northwestern.edu>
2016-03-18 12:12:39 -0500

    Troubleshoot error with Checksums and Previews

--- netlogo-gui/src/main/headless/ChecksumsAndPreviews.scala
+++ netlogo-gui/src/main/headless/ChecksumsAndPreviews.scala
@@ -154,14 +154,13 @@ object ChecksumsAndPreviews {
     }
     def getRevisionNumber(modelPath: String): String = {
       val cmds = Array("git", "log", "--pretty=format:%h",
-                       new java.io.File(modelPath).getAbsolutePath)
-      val stdInput = new java.io.BufferedReader(
-        new java.io.InputStreamReader(
-          Runtime.getRuntime().exec(cmds,
-                                    Array[String](),
-                                    new java.io.File(ModelsLibrary.modelsRoot))
-          .getInputStream))
-      stdInput.readLine().trim
+        new java.io.File(modelPath).getAbsolutePath)
+      val proc =
+        Runtime.getRuntime().exec(cmds, Array[String](), new java.io.File(ModelsLibrary.modelsRoot))
+      val stdInput = new java.io.BufferedReader(new java.io.InputStreamReader(proc.getInputStream))
+      val stdError = scala.io.Source.fromInputStream(proc.getErrorStream)
+      Option(stdInput.readLine()).map(_.trim).getOrElse(
+        throw new Exception("Error fetching SHA1 of model: " + stdError.mkString))
     }
   }
 }
