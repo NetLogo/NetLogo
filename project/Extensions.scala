838b917aa6c3a387224713fe5fcb9a9da6c86bd2
Robert Grider <robert.grider@northwestern.edu>
2016-03-18 13:25:05 -0500

    Update extension plugin to version 3
    - adds forExtension, forAllExtension tasks
    - Removes unneccessary APIVersion (now in netlogo-core)

--- project/Extensions.scala
+++ project/Extensions.scala
@@ -12,6 +12,8 @@ object Extensions {
   val excludedExtensions = SettingKey[Seq[String]]("extensions excluded for this configuration")
   val extensions = TaskKey[Seq[File]]("extensions", "builds extensions")
   val extension = InputKey[Seq[File]]("extension", "build a single extension")
+  val forExtension = InputKey[Unit]("forExtension", "run an sbt command in a single extension")
+  val forAllExtensions = InputKey[Unit]("forAllExtensions", "run an sbt command in all extensions")
 
   val isDirectory = new java.io.FileFilter {
     override def accept(f: File) = f.isDirectory
@@ -27,30 +29,57 @@ object Extensions {
     }
   }
 
+  val sbtCommandParser: Parser[String] = {
+    import Parser._
+    (Space ~> StringBasic)
+  }
+
+  val initSbtCommandParser: Initialize[Parser[String]] =
+    Def.setting(sbtCommandParser)
+
+  val extensionAndCommandParser: Initialize[Parser[(File, String)]] = {
+    import Parser._
+    Def.bind(extensionParser)(e => Def.setting { e ~ sbtCommandParser })
+  }
+
   lazy val settings = Seq(
     extensionDeps := {
       val packagedNetLogoJar     = (packageBin in Compile).value
       val packagedNetLogoTestJar = (packageBin in Test).value
       (packagedNetLogoJar, packagedNetLogoTestJar)
     },
+    forExtension := {
+      val (extensionDir, command) = extensionAndCommandParser.parsed
+      val (packagedNetLogoJar, packagedNetLogoTestJar) = extensionDeps.value
+
+      runExtensionCommand(extensionDir, state.value, packagedNetLogoJar, command)
+    },
+    forAllExtensions := {
+      val command = initSbtCommandParser.parsed
+      val (packagedNetLogoJar, packagedNetLogoTestJar) = extensionDeps.value
+      val dirs = extensionDirs(extensionRoot.value)
+
+      dirs.foreach { dir =>
+        runExtensionCommand(dir, state.value, packagedNetLogoJar, command)
+      }
+    },
     extension  := {
       val extensionDir = extensionParser.parsed
       val (packagedNetLogoJar, packagedNetLogoTestJar) = extensionDeps.value
-      val s = streams.value
-      val scala = scalaInstance.value
 
-      buildExtension(extensionDir, scala.libraryJar, packagedNetLogoJar, s.log, state.value)(Set()).toSeq
+      streams.value.log.info("building extension: " + extensionDir.getName)
+      buildExtension(extensionDir, packagedNetLogoJar, state.value)(Set()).toSeq
     },
     extensions := {
       val base                   = baseDirectory.value
       val (packagedNetLogoJar, packagedNetLogoTestJar) = extensionDeps.value
       val s = streams.value
-      val scala = scalaInstance.value
       ("git -C " + base + " submodule --quiet update --init") ! s.log
       val dirs = extensionDirs(extensionRoot.value)
       dirs.filterNot(f => excludedExtensions.value.contains(f.getName)).flatMap{ dir =>
-        cacheBuild(s.cacheDirectory, dir, Set(base / "NetLogo.jar", base / "NetLogoLite.jar"))(
-          buildExtension(dir, scala.libraryJar, packagedNetLogoJar, s.log, state.value))
+        cacheBuild(s.cacheDirectory, dir, Set(base / "NetLogo.jar", base / "NetLogoLite.jar")) {
+          s.log.info("building extension: " + dir.getName)
+          buildExtension(dir, packagedNetLogoJar, state.value) }
       }
     },
     excludedExtensions := Seq(),
@@ -77,16 +106,19 @@ object Extensions {
     buildCached(otherDeps).toSeq
   }
 
-  private def buildExtension(dir: File, scalaLibrary: File, netLogoJar: File, log: Logger, state: State): Set[File] => Set[File] = {
-    log.info("building extension: " + dir.getName)
-    System.setProperty("netlogo.jar.url", netLogoJar.toURI.toString)
-    val buildConfig  = config(state, dir, "package")
-    val jar = dir / (dir.getName + ".jar")
+  private def buildExtension(dir: File, netLogoJar: File, state: State): Set[File] => Set[File] = {
+    runExtensionCommand(dir, state, netLogoJar, "package")
+
+    { files => Set(dir / (dir.getName + ".jar")) }
+  }
+
+  private def runExtensionCommand(dir: File, state: State, netLogoJar: File, command: String): Unit = {
+    System.setProperty("netlogo.jar.file", netLogoJar.getAbsolutePath)
+    val buildConfig  = config(state, dir, command)
     runner.run(buildConfig) match {
-      case e: xsbti.Exit   => assert(e.code == 0, "extension build failed, exitCode = " + e.code)
+      case e: xsbti.Exit   => assert(e.code == 0, "failed to build " + dir.getName + ", exitCode = " + e.code)
       case r: xsbti.Reboot => assert(true == false, "expected application to build, instead rebooted")
     }
-    { files => Set(jar) }
   }
 
 }
