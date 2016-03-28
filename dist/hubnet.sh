710b2960a535eb46c9aa3f1d0cf8be1b9820d674
Robert Grider <robert.grider@northwestern.edu>
2016-03-28 10:49:18 -0500

    Remove unused files

--- dist/hubnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd "`dirname "$0"`"             # the copious quoting is for handling paths with spaces
-# -Xmx1024m                     use up to 1GB RAM (edit to increase)
-# -classpath NetLogo.jar        specify main jar (HubNet.jar is only for the client applet)
-# -Dfile.encoding=UTF-8         ensure Unicode characters in model files are compatible cross-platform
-# org.nlogo.hubnet.client.App   specify the client
-# "$@"                          pass along any command line arguments
-java -Xmx1024m -classpath NetLogo.jar -Dfile.encoding=UTF-8 org.nlogo.hubnet.client.App "$@"
