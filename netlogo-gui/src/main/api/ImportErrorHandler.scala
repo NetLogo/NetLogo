1adac4ed2692290008d139c38e369447c63ea5c1
Robert Grider <robert.grider@northwestern.edu>
2016-05-26 14:16:17 -0500

    Remove duplicate classes from api package

--- netlogo-gui/src/main/api/ImportErrorHandler.scala
+++
@@ -1,7 +0,0 @@
-// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
-
-package org.nlogo.api
-
-trait ImportErrorHandler {
-  def showError(title: String, message: String, defaultAction: String)
-}
--- netlogo-gui/src/main/api/RandomServices.scala
+++
@@ -1,8 +0,0 @@
-// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
-
-package org.nlogo.api
-
-trait RandomServices {
-  def mainRNG: MersenneTwisterFast
-  def auxRNG: MersenneTwisterFast
-}
