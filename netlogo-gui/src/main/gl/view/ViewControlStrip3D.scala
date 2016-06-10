75f934c690bf939e61c6f056e93ee2e694675dc1
Robert Grider <robert.grider@northwestern.edu>
2016-06-10 12:13:11 -0500

    Make tick counter under speed slider in 3D as well as in 2D

--- netlogo-gui/src/main/gl/view/ViewControlStrip3D.scala
+++ netlogo-gui/src/main/gl/view/ViewControlStrip3D.scala
@@ -14,14 +14,12 @@ class ViewControlStrip3D(workspace: GUIWorkspace, val tickCounter: TickCounterLa
   val controls = new ViewUpdatePanel(workspace, displaySwitch, true, tickCounter)
   org.nlogo.awt.Fonts.adjustDefaultFont(tickCounter)
   setLayout(new java.awt.BorderLayout)
-  add(tickCounter, BorderLayout.WEST)
-  controls.setOpaque(false)
   add(controls, BorderLayout.CENTER)
 
   def updateTicks() {
     val width = tickCounter.getMinimumSize.width
     // don't think this should be necessary but I couldn't get it to work otherwise ev 8/28/07
-    if(width != getMinimumSize.width)
+    if (width != getMinimumSize.width)
       doLayout()
   }
 }
