977c943543b6f35ac363f3e13a32b45dd15dede8
Robert Grider <robert.grider@northwestern.edu>
2016-11-17 21:15:15 -0600

    Add qualifier to TestModelNetLogoVersions to prevent duplicate test names

--- netlogo-gui/src/test/workspace/TestModelNetLogoVersions.scala
+++ netlogo-gui/src/test/workspace/TestModelNetLogoVersions.scala
@@ -16,7 +16,7 @@ class TestModelNetLogoVersions extends FunSuite with SlowTest {
   val dummyWorkspace = new DummyWorkspace()
   val extensionManager = new ExtensionManager(dummyWorkspace, new JarLoader(dummyWorkspace))
   for(path <- paths)
-    test(path, SlowTest.Tag) {
+    test("model version: " + path, SlowTest.Tag) {
       // if this test suddenly starts failing, make sure all models are the most current version
       val loader =
         fileformat.basicLoader
