b7d6339c47f3b362a48084877c0e99ca54827a76
brotatotes <hao.awesome@gmail.com>
2017-02-22 09:21:12 -0600

    preliminary other and with transformers

--- netlogo-core/src/main/compile/middle/AgentsetLazinessTransformer.scala
+++ netlogo-core/src/main/compile/middle/AgentsetLazinessTransformer.scala
@@ -3,21 +3,34 @@
 package org.nlogo.compile
 package middle
 
+import org.nlogo.compile.api.{AstTransformer, Expression, ReporterApp, Statement}
 import org.nlogo.prim._
-import org.nlogo.compile.api.{ AstTransformer, Expression, ReporterApp, Statement }
+import org.nlogo.prim.etc._member
+import org.nlogo.prim._count
+
+import scala.collection.mutable.Buffer
 
 class AgentsetLazinessTransformer extends AstTransformer {
   override def visitReporterApp(app: ReporterApp): ReporterApp = {
     val newApp = super.visitReporterApp(app)
     newApp.reporter match {
-      case e: _other =>
+      case _: _other | _: _with =>
         val forceArgs =
           newApp.args.head match {
-            case ReporterApp(_, f: _force, innerArgs, loc) =>
-              Seq(newApp.copy(args = innerArgs))
-            case _ => Seq(newApp)
+            case ReporterApp(_, _: _force, innerArgs, _) =>
+              Buffer[Expression](newApp.copy(args = innerArgs))
+            case _ => Buffer[Expression](newApp)
           }
         new ReporterApp(_force.coreprim(), new _force(), forceArgs, newApp.sourceLocation)
+
+      case _: _count | _: _any | _: _member =>
+        val forceArgs = newApp.args.head match {
+          case ReporterApp(_, _: _force, innerArgs, _) =>
+            innerArgs.toBuffer
+          case _ => newApp.args.toBuffer
+        }
+        new ReporterApp(app.coreReporter, app.reporter, forceArgs, newApp.sourceLocation)
+
       case _ => newApp
     }
   }
--- netlogo-core/src/test/compile/middle/AgentsetLazinessTransformerTests.scala
+++ netlogo-core/src/test/compile/middle/AgentsetLazinessTransformerTests.scala
@@ -35,15 +35,51 @@ class AgentsetLazinessTransformerTests extends FunSuite {
     assertResult("_let[_force[_other[_other[_turtles[]]]]]")(compileCommands("let foo other other turtles"))
   } }
 
-  test("let count other turtles does not forces evaluation") { new Helper {
+  test("let with turtles forces evaluation") { new Helper {
+    assertResult("_let[_force[_with[_turtles[], [_constboolean:true[]]]]]")(compileCommands("let foo turtles with [ true ]"))
+  } }
+
+  test("let with with turtles forces evaluation once") { new Helper {
+    assertResult("_let[_force[_with[_with[_turtles[], [_constboolean:true[]]]]]]")(compileCommands("let foo turtles with [ true ] with [ true ]"))
+  } }
+
+  test("let count turtles does not force evaluation") { new Helper {
+    assertResult("_let[_count[_turtles[]]]")(compileCommands("let foo count turtles"))
+  } }
+
+  test("let count other turtles does not force evaluation") { new Helper {
     assertResult("_let[_count[_other[_turtles[]]]]")(compileCommands("let foo count other turtles"))
   } }
 
-  test("let with turtles forces evaluation") { new Helper {
-    assertResult("_let[_force[_with[_turtles[], _constboolean:true[]]]]")(compileCommands("let foo turtles with [ true ]"))
+  test("let count with turtles does not force evaluation") { new Helper {
+    assertResult("_let[_count[_with[_turtles[], [_constboolean:true[]]]]]")(compileCommands("let foo count turtles with [ true ]"))
   } }
 
-  test("list other turtles forces evalation") { new Helper {
+  test("list other turtles forces evaluation") { new Helper {
     assertResult("_let[_list[_force[_other[_turtles[]]]]]")(compileCommands("let foo (list other turtles)"))
   } }
+
+  test("list other with turtles forces evaluation once") { new Helper {
+    assertResult("_let[_list[_force[_other[_with[_turtles[], [_constboolean:true[]]]]]]]")(compileCommands("let foo (list other turtles with [ true ])"))
+  } }
+
+  test("any? turtles does not force evaluation") { new Helper {
+    assertResult("_let[_any[_turtles[]]]")(compileCommands("let foo any? turtles"))
+  } }
+
+  test("any? other turtles does not force evaluation") { new Helper {
+    assertResult("_let[_any[_other[_turtles[]]]]")(compileCommands("let foo any? other turtles"))
+  } }
+
+  test("any? other other turtles does not force evaluation") { new Helper {
+    assertResult("_let[_any[_other[_other[_turtles[]]]]]")(compileCommands("let foo any? other other turtles"))
+  } }
+
+  test("any? other with turtles does not force evaluation") { new Helper {
+    assertResult("_let[_any[_other[_with[_turtles[], [_constboolean:true[]]]]]]")(compileCommands("let foo any? other turtles with [ true ]"))
+  } }
+
+//  test("member other does not force evaluation") { new Helper {
+//    assertResult()
+//  } }
 }
