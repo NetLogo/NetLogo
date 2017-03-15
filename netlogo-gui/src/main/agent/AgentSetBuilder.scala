5731d300db3dd10807c770174bfd8b7ee69b3791
Robert Grider <robert.grider@northwestern.edu>
2017-03-15 11:14:21 -0500

    Back AgentSetBuilder with ArrayList

--- netlogo-gui/src/main/agent/AgentSetBuilder.scala
+++ netlogo-gui/src/main/agent/AgentSetBuilder.scala
@@ -4,19 +4,23 @@ package org.nlogo.agent
 
 import org.nlogo.core
 
+import java.util.ArrayList
+
 class AgentSetBuilder(kind: core.AgentKind, capacity: Int) {
   // for convenience from Java
   def this(kind: core.AgentKind) =
     this(kind, 1)
   private[this] var buf =
-    new collection.mutable.ArrayBuffer[Agent](capacity)
+    new ArrayList[Agent](capacity)
   def add(agent: Agent) {
-    buf += agent
+    buf.add(agent)
   }
   def contains(agent: Agent) =
     buf.contains(agent)
   def build(): IndexedAgentSet = {
-    val result = AgentSet.fromArray(kind, buf.toArray)
+    val agentArray = new Array[Agent](buf.size)
+    buf.toArray(agentArray)
+    val result = AgentSet.fromArray(kind, agentArray)
     buf = null // prevent repeated builds
     result
   }
