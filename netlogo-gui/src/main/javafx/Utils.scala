8b99272b0eb22c1f2ffcb0585930064ea924cfb7
Robert Grider <robert.grider@northwestern.edu>
2017-04-18 16:51:46 -0500

    Add single-argument changeListener to javafx.Utils

--- netlogo-gui/src/main/javafx/Utils.scala
+++ netlogo-gui/src/main/javafx/Utils.scala
@@ -4,6 +4,9 @@ package org.nlogo.javafx
 
 import javafx.event.{ Event, EventHandler }
 import javafx.beans.value.{ ChangeListener, ObservableValue }
+import javafx.beans.property.{ DoubleProperty, SimpleDoubleProperty }
+
+import org.nlogo.internalapi.Monitorable
 
 object Utils {
   def handler[T <: Event](f: T => Unit): EventHandler[T] = {
@@ -20,5 +23,22 @@ object Utils {
         f(o, oldValue, newValue)
       }
     }
+  }
+
+  def changeListener[T](f: T => Unit): ChangeListener[T] = {
+    new ChangeListener[T]() {
+      def changed(o: ObservableValue[_ <: T], oldValue: T, newValue: T): Unit = {
+        f(newValue)
+      }
+    }
+  }
+
+  implicit class RichDoubleMonitorable(m: Monitorable[Double]) {
+    def valueProperty: DoubleProperty = {
+      val p = new SimpleDoubleProperty(m.defaultValue)
+      m.onUpdate({ updated => p.setValue(updated) })
+      p
+    }
+
   }
 }
