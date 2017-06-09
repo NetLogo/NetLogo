ee55bc2b727e16b9fa0021f696ea35f145b5ef52
Robert Grider <robert.grider@northwestern.edu>
2017-06-09 15:52:36 -0500

    Consolidate org.nlogo.lab package

--- netlogo-headless/src/main/headless/HeadlessWorkspace.scala
+++ netlogo-headless/src/main/headless/HeadlessWorkspace.scala
@@ -47,10 +47,7 @@ object HeadlessWorkspace {
   def newLab: LabInterface = {
     val utilities: CompilerUtilitiesInterface =
       Femto.scalaSingleton("org.nlogo.parse.CompilerUtilities")
-    // kludgy, use AnyRef here because ProtocolLoader doesn't implement an interface - ST 4/25/13
-    val protocolLoader: AnyRef =
-      Femto.get("org.nlogo.lab.ProtocolLoader", utilities)
-    Femto.get("org.nlogo.lab.Lab", protocolLoader)
+    Femto.get("org.nlogo.lab.Lab")
   }
 
 }
