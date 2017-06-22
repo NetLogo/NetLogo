d42ffad9678be692465d258bb4d3a6fdd19e9a02
Robert Grider <robert.grider@northwestern.edu>
2017-06-22 11:47:52 -0500

    Split FrontEndTests out into five different parts

---
+++ parser-core/src/test/parse/BaseParserTest.scala
@@ -0,0 +1,43 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.parse
+
+import
+  org.nlogo.core.{ CompilerException, Statements, TestUtils },
+    TestUtils.cleanJsNumbers
+
+import org.scalatest.FunSuite
+
+trait BaseParserTest { this: FunSuite =>
+  val PREAMBLE = "to __test "
+  val POSTAMBLE = "\nend"
+
+  /// helpers
+  def compile(source: String, preamble: String = PREAMBLE, postamble: String = POSTAMBLE): Seq[Statements] =
+    FrontEnd.frontEnd(preamble + source + postamble) match {
+      case (procs, _) =>
+        procs.map(_.statements)
+    }
+  def testParse(input: String, result: String, preamble: String = PREAMBLE) {
+    assertResult(cleanJsNumbers(result))(cleanJsNumbers(compile(input, preamble).mkString))
+  }
+  def runFailure(input: String, message: String, start: Int, end: Int, preamble: String = PREAMBLE) {
+    doFailure(input, message, start, end, preamble)
+  }
+  def doFailure(input: String, message: String, start: Int, end: Int, preamble: String = PREAMBLE) {
+    val e = intercept[CompilerException] { compile(input, preamble = preamble) }
+    assertResult(message)(e.getMessage)
+    val programText = preamble + input + POSTAMBLE
+    def programTextWithContext(s: Int, e: Int) = {
+      val beforeContext = programText.slice((s - 5) max 0, s)
+      val t = programText.slice(s, e)
+      val afterContext = programText.slice(e, (e + 5) min (programText.length - 1))
+      s"... $beforeContext|$t|$afterContext ..."
+    }
+    val expectedErrorSource = programTextWithContext(start + preamble.length, end + preamble.length)
+    val actualErrorSource = programTextWithContext(e.start, e.end)
+    assert(start == e.start - preamble.length, s"incorrect start, expected: $expectedErrorSource but was actually $actualErrorSource\n")
+    assert(end   == e.end   - preamble.length, s"incorrect end, expected: $expectedErrorSource but was actually $actualErrorSource")
+  }
+
+}
---
+++ parser-core/src/test/parse/SourcePositionTests.scala
@@ -0,0 +1,126 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.parse
+
+import org.nlogo.core
+
+import org.scalatest.FunSuite
+
+class SourcePositionTests extends FunSuite with BaseParserTest {
+  /// tests using testStartAndEnd
+  test("StartAndEndPositions0") {
+    testStartAndEnd("ca",
+      "Statements 'ca' " +
+      "Statement 'ca' ")
+  }
+  test("StartAndEndPositions1") {
+    testStartAndEnd("__ignore 5",
+      "Statements '__ignore 5' " +
+      "Statement '__ignore 5' " +
+      "ReporterApp '5' ")
+  }
+  test("StartAndEndPositions2") {
+    testStartAndEnd("__ignore n-values 5 [[] -> world-width]",
+      "Statements '__ignore n-values 5 [[] -> world-width]' " +
+      "Statement '__ignore n-values 5 [[] -> world-width]' " +
+      "ReporterApp 'n-values 5 [[] -> world-width]' " +
+      "ReporterApp '5' " +
+      "ReporterApp '[[] -> world-width]' " +
+      "ReporterApp 'world-width' ")
+  }
+  test("StartAndEndPositions8") {
+    testStartAndEnd("crt 1",
+      "Statements 'crt 1' " +
+      "Statement 'crt 1' " +
+      "ReporterApp '1' " +
+      "CommandBlock '' " +
+      "Statements '' ")
+  }
+  test("StartAndEndPositions9") {
+    testStartAndEnd("crt 1 [ ]",
+      "Statements 'crt 1 [ ]' " +
+      "Statement 'crt 1 [ ]' " +
+      "ReporterApp '1' " +
+      "CommandBlock '[ ]' " +
+      "Statements '' ")
+  }
+
+  test("StartAndEndPositions10") {
+    testStartAndEnd("ask turtles with [color = red ] [ fd 1 ]",
+      "Statements 'ask turtles with [color = red ] [ fd 1 ]' " +
+      "Statement 'ask turtles with [color = red ] [ fd 1 ]' " +
+      "ReporterApp 'turtles with [color = red ]' " +
+      "ReporterApp 'turtles' " +
+      "ReporterBlock '[color = red ]' " +
+      "ReporterApp 'color = red' " +
+      "ReporterApp 'color' " +
+      "ReporterApp 'red' " +
+      "CommandBlock '[ fd 1 ]' " +
+      "Statements 'fd 1' " +
+      "Statement 'fd 1' " +
+      "ReporterApp '1' ")
+  }
+
+  test("While") {
+    testStartAndEnd("while [count turtles < 10] [ crt 1 ]",
+      "Statements 'while [count turtles < 10] [ crt 1 ]' " +
+      "Statement 'while [count turtles < 10] [ crt 1 ]' " +
+      "ReporterBlock '[count turtles < 10]' " +
+      "ReporterApp 'count turtles < 10' " +
+      "ReporterApp 'count turtles' " +
+      "ReporterApp 'turtles' " +
+      "ReporterApp '10' " +
+      "CommandBlock '[ crt 1 ]' " +
+      "Statements 'crt 1' " +
+      "Statement 'crt 1' " +
+      "ReporterApp '1' " +
+      "CommandBlock '' " +
+      "Statements '' ")
+  }
+
+  // issue #417 (source positions for literal lists)
+  test("literal list") {
+    testStartAndEnd("print [1 2 3]",
+      "Statements 'print [1 2 3]' " +
+      "Statement 'print [1 2 3]' " +
+      "ReporterApp '[1 2 3]' ")
+  }
+
+  /**
+   * utility method useful for testing that start()
+   * and end() return right answers everywhere
+   */
+  def statementsToString(ss: Seq[core.Statements], source: String) =
+    (for (stmts <- ss) yield {
+      val visitor = new PositionsCheckVisitor(source)
+      visitor.visitStatements(stmts)
+      visitor.buf.toString
+    }).mkString
+  /// helper
+  def testStartAndEnd(source: String, preorderDump: String) {
+    assertResult(preorderDump)(statementsToString(compile(source), source))
+  }
+  // preorder traversal
+  class PositionsCheckVisitor(source: String) extends core.AstVisitor {
+    val buf = new StringBuilder()
+    override def visitCommandBlock(node: core.CommandBlock) { visit(node); super.visitCommandBlock(node) }
+    override def visitReporterApp(node: core.ReporterApp) { visit(node); super.visitReporterApp(node) }
+    override def visitReporterBlock(node: core.ReporterBlock) { visit(node); super.visitReporterBlock(node) }
+    override def visitStatement(node: core.Statement) { visit(node); super.visitStatement(node) }
+    override def visitStatements(node: core.Statements) {
+      if (node.stmts.isEmpty)
+        buf.append(node.getClass.getSimpleName + " '' ")
+      else visit(node)
+      super.visitStatements(node)
+    }
+    def visit(node: core.AstNode) {
+      val start = node.start - PREAMBLE.length
+      val end = node.end - PREAMBLE.length
+      val text =
+        try "'" + source.substring(start, end) + "'"
+        catch { case _: StringIndexOutOfBoundsException =>
+          "out of bounds: " + ((start, end)) }
+      buf.append(node.getClass.getSimpleName + " " + text + " ")
+    }
+  }
+}
