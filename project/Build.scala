a2435e0e9e3559c74ff222cc1031ccb907648650
Robert Grider <robert.grider@northwestern.edu>
2017-08-04 12:50:49 -0500

    Bump version to 6.0.2

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object NetLogoBuild {
   lazy val numericMarketingVersion = settingKey[String]("Numeric-only version attached to the build for end-user identification")
 
   val settings = Seq(
-    marketingVersion        := "6.0.2-RC2",
+    marketingVersion        := "6.0.2",
     numericMarketingVersion := "6.0.2",
     buildDate := {
       val dateFormat =
