6d123284cc22d4fb47a8648f25fa10ab444d3ae5
LaCuneta <jeremy.baker@northwestern.edu>
2018-10-03 13:52:37 -0500

    UI: Add hotkey Command-U for "Show Usage"
    Fixes #1399

--- netlogo-gui/src/main/ide/ShowUsageBoxAction.scala
+++ netlogo-gui/src/main/ide/ShowUsageBoxAction.scala
@@ -3,15 +3,30 @@
 package org.nlogo.ide
 
 import java.awt.event.ActionEvent
-import javax.swing.{ AbstractAction, Action }
+import javax.swing.text.{ Document, JTextComponent }
 
-import org.nlogo.editor.EditorAwareAction
+import org.nlogo.editor.{ AbstractEditorArea, DocumentAction }
+import org.nlogo.swing.UserAction,
+  UserAction.{ EditCategory, EditFormatGroup, KeyBindings, MenuAction },
+    KeyBindings.keystroke
 
-class ShowUsageBoxAction(showUsageBox: ShowUsageBox) extends AbstractAction with EditorAwareAction {
-  putValue(Action.NAME, "Show Usage")
+class ShowUsageBoxAction(showUsageBox: ShowUsageBox) extends DocumentAction("Show Usage")
+  with FocusedOnlyAction with MenuAction {
+    accelerator = keystroke('U', withMenu = true)
+    group       = EditFormatGroup
+    category    = EditCategory
 
-  def actionPerformed(e: ActionEvent): Unit = {
-    showUsageBox.init(editor)
-    showUsageBox.showBox(location, documentOffset)
-  }
+    def perform(component: JTextComponent, document: Document, e: ActionEvent): Unit = {
+      if (component.isInstanceOf[AbstractEditorArea]) {
+        val editor = component.asInstanceOf[AbstractEditorArea]
+        showUsageBox.init(editor)
+      }
+
+      val caretScreenPosition = component.getCaret.getMagicCaretPosition
+      val screenPosition = component.getLocationOnScreen
+      screenPosition.x += caretScreenPosition.x;
+      screenPosition.y += caretScreenPosition.y;
+      showUsageBox.showBox(screenPosition, component.getCaretPosition)
+    }
+
 }
