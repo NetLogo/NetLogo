8ba19d3d10814a53cc6eba5a61f05e497c977cfb
LaCuneta <jeremy.baker@northwestern.edu>
2018-10-03 12:22:09 -0500

    UI: Add hotkey Command-E for "Jump to Declaration"

--- netlogo-gui/src/main/ide/JumpToDeclarationAction.scala
+++ netlogo-gui/src/main/ide/JumpToDeclarationAction.scala
@@ -3,14 +3,22 @@
 package org.nlogo.ide
 
 import java.awt.event.ActionEvent
-import javax.swing.{AbstractAction, Action}
+import javax.swing.text.{ Document, JTextComponent }
 
 import org.nlogo.core.I18N
-import org.nlogo.editor.EditorAwareAction
+import org.nlogo.editor.{ DocumentAction }
+import org.nlogo.swing.UserAction,
+  UserAction.{ EditCategory, EditFormatGroup, KeyBindings, MenuAction },
+    KeyBindings.keystroke
 
-class JumpToDeclarationAction() extends AbstractAction with EditorAwareAction {
-  putValue(Action.NAME, I18N.gui.get("tabs.code.rightclick.jumptodeclaration"))
-  def actionPerformed(e: ActionEvent): Unit = {
-    JumpToDeclaration.jumpToDeclaration(documentOffset, editor)
-  }
+class JumpToDeclarationAction() extends DocumentAction(I18N.gui.get("tabs.code.rightclick.jumptodeclaration"))
+  with FocusedOnlyAction with MenuAction {
+    accelerator = keystroke('E', withMenu = true)
+    group       = EditFormatGroup
+    category    = EditCategory
+
+    def perform(component: JTextComponent, document: Document, e: ActionEvent): Unit = {
+      JumpToDeclaration.jumpToDeclaration(component.getCaretPosition, component)
+    }
+
 }
