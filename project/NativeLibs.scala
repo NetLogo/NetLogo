aaa4eab1ef7037c956e5d379b2cc60d207f30b52
LaCuneta <jeremy.baker@northwestern.edu>
2020-07-02 11:02:03 -0500

    Infrastructure: Start using JOGL 2.4.0 pre-release
    Necessary to support Liberica OpenJDK 8, and it should be pretty stable
    as its nearing release.

--- project/NativeLibs.scala
+++ project/NativeLibs.scala
@@ -23,8 +23,8 @@ object NativeLibs {
     nativeLibs := {
         val baseURL = "https://s3.amazonaws.com/ccl-artifacts/"
         val joglNatives = baseDirectory.value / "natives"
-        val joglTmp = baseDirectory.value / "jogl-2.3.2.zip"
-        val joglUrl = new URL(baseURL + "jogl-2.3.2.zip")
+        val joglTmp = baseDirectory.value / "jogl-2.4.0-natives-rc-20200307.zip"
+        val joglUrl = new URL(baseURL + "jogl-2.4.0-natives-rc-20200307.zip")
         IO.createDirectory(joglNatives)
         FileActions.download(joglUrl, joglTmp)
         IO.unzip(joglTmp, joglNatives)
