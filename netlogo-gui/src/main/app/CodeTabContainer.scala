58cdff96e34da438dc8e22c1677e4eefc9119f5b
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2020-10-26 11:00:46 -0400

    remove extra spaces in CodeTabContainer

--- netlogo-gui/src/main/app/CodeTabContainer.scala
+++ netlogo-gui/src/main/app/CodeTabContainer.scala
@@ -10,7 +10,7 @@ import org.nlogo.window.Event.LinkChild
 // It contains the CodeTabsPanel which owns and manages the CodeTabs.
 // It is created and destroyed 'on demand' as needed. AAB 10/2020
 class CodeTabContainer(owner:          Frame,
-                       codeTabbedPane: JTabbedPane) extends JDialog(owner) with LinkChild    {
+                       codeTabbedPane: JTabbedPane) extends JDialog(owner) with LinkChild {
 
   this.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE)
   this.add(codeTabbedPane, BorderLayout.CENTER)
