30800b69700e4c115f9c56a9bd4688ca1b2bdc6e
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2020-11-20 15:36:55 -0600

    Infrastructure/Build: Add a vid library for signing for notarization

--- project/AggregateMacBuild.scala
+++ project/AggregateMacBuild.scala
@@ -221,6 +221,7 @@ object PackageMacAggregate {
     val jarLibsToSign = Map(
       ("extensions/.bundled/gogo/hid4java.jar", Seq("darwin/libhidapi.dylib")),
       ("extensions/.bundled/nw/gephi-toolkit-0.8.2-all.jar", Seq("native/Mac/i386/libsqlitejdbc.jnilib", "native/Mac/x86_64/libsqlitejdbc.jnilib")),
+      ("extensions/.bundled/vid/core-video-capture-1.3.10.jar", Seq("org/openimaj/video/capture/nativelib/darwin_universal/libOpenIMAJGrabber.dylib")),
       ("Java/java-objc-bridge-1.0.0.jar", Seq("libjcocoa.dylib"))
     )
     jarLibsToSign.foreach { case (jarPath: String, libsToSign: Seq[String]) => signJarLibs(aggregateMacDir / jarPath, appSigningOptions, libsToSign) }
