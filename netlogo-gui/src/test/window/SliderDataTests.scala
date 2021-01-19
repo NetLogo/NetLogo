af21831bd3d4d1501026bae7f0422a1658b216f4
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2021-01-19 10:14:18 -0600

    Tests: Fix yet more slider data property check failures

--- netlogo-gui/src/test/window/SliderDataTests.scala
+++ netlogo-gui/src/test/window/SliderDataTests.scala
@@ -21,35 +21,57 @@ class SliderDataTests extends PropSpec with PropertyChecks {
     inc <- Gen.oneOf(0.0, 0.1, 0.01, 1.0, 2.0)
   } yield new SliderData(dummyErrorHandler, min, max, inc)
 
+  // We use these generators to avoid the `whenever` checks in the tests from failing more than 10 times
+  // in a row, which fails the test. This happens quite often with a few different tests relying on
+  // tossing out the "bad" data as opposed to generating "good" data directly.
+
+  // Note that even with these generators guaranteeing our values for the tests, we have to leave the
+  // `whenever` checks there.  If a test actually fails the checker will try to shrink the values for a
+  // "minimal" test result which ignores the generators, and likely will pop an incorrect error message.
+
+  // -Jeremy B January 2021
+
+  val genSmallInt:  Gen[Int] = for (n <- Gen.choose(0, 200)) yield n
+  val genBigInt:    Gen[Int] = for (n <- Gen.choose(201, Int.MaxValue - 1)) yield n
   val genPosOddInt: Gen[Int] = for (n <- Gen.choose(0, Int.MaxValue - 1)) yield if (n % 2 == 1) n else n + 1
+  val genBigDouble: Gen[Double] = for (n <- Gen.choose(0.0, Double.MaxValue / 2.0)) yield n
 
   // set value tests
   property("after setting constraints, value is never higher than max(min,max)") {
-    forAll((s: SliderData, d: Double) =>
-      whenever(d > 0) {
+    forAll(genSlider, genBigDouble) { (s, d) =>
+      whenever (d > 0) {
         s.value = s.maximum + d
         // some value on the constraint must change, or the value isnt updated.
         // im not actually sure if this is a good rule or not.
         s.setSliderConstraint(new ConstantSliderConstraint(s.minimum - 1, s.maximum, s.increment))
-        assert(s.value <= math.max(s.minimum, s.maximum))})}
+        assert(s.value <= math.max(s.minimum, s.maximum))
+      }
+    }
+  }
 
   //
   // precision tests.
   //
   property("double precision") {
     forAll(genPosOddInt) { (r) =>
-      // use 1.r
-      val min      = (1 + "." + r).toDouble
-      val result   = new SliderData(dummyErrorHandler, minimum = min).precision
-      val expected = r.toString.length
-      assertResult(expected)(result)
+      whenever (r >= 0 && (r % 2 == 1)) {
+        // use 1.r
+        val min      = (1 + "." + r).toDouble
+        val result   = new SliderData(dummyErrorHandler, minimum = min).precision
+        val expected = r.toString.length
+        assertResult(expected)(result)
+      }
     }
   }
 
   property("precision for ints should always be zero") {
-    forAll((i: Int) =>
-      whenever(!i.toDouble.toString.contains("E")) {
-        assertResult(0)(new SliderData(dummyErrorHandler, minimum = i).precision)})}
+    forAll(genSmallInt) { (i) =>
+      whenever (i >= 0) {
+        val result = new SliderData(dummyErrorHandler, minimum = i).precision
+        assertResult(0)(result)
+      }
+    }
+  }
 
   property("sanity precision tests") {
     def testPrecision(d: Double, expected: Int) {
@@ -102,10 +124,13 @@ class SliderDataTests extends PropSpec with PropertyChecks {
   }
 
   property("when min > max, effective max is always min") {
-    forAll((min: Double, max: Double) =>
+    forAll(genBigInt, genSmallInt) { (min, max) =>
       whenever(min > max) {
-        assertResult(min)(
-          new SliderData(dummyErrorHandler, minimum = min, maximum = max).effectiveMaximum)})}
+        val result = new SliderData(dummyErrorHandler, minimum = min, maximum = max).effectiveMaximum
+        assertResult(min)(result)
+      }
+    }
+  }
 
   property("when inc == 0, effective max is always min") {
     forAll((min: Double) =>
@@ -115,7 +140,9 @@ class SliderDataTests extends PropSpec with PropertyChecks {
   property("effective maximum is never greater than max") {
     forAll((s: SliderData) =>
       whenever(s.minimum <= s.maximum) {
-        assert(s.effectiveMaximum <= s.maximum)})}
+        assert(s.effectiveMaximum <= s.maximum)
+      })
+  }
 
   implicit def arbDynConstraint: Arbitrary[DynamicSliderConstraint] = Arbitrary(dynConstraint)
   val dynConstraint: Gen[DynamicSliderConstraint] = {
