4ca8a2a66da7b6fd768cb723dd2cd2cb967ce974
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2021-06-29 13:10:41 -0400

    use invokeLater to force separated code tab to the front

--- netlogo-gui/src/main/app/CodeTabsPanel.scala
+++ netlogo-gui/src/main/app/CodeTabsPanel.scala
@@ -131,4 +131,11 @@ class CodeTabsPanel(workspace:            GUIWorkspace,
       new AppEvents.SwitchedTabsEvent(previousTab, currentTab).raise(this)
     }
   }
+  java.awt.EventQueue.invokeLater(new Runnable() {
+
+    override def run(): Unit = {
+        codeTabContainer.toFront()
+        codeTabContainer.repaint()
+    }
+})
 }
