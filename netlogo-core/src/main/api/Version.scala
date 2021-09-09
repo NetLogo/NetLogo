3a40601f79e7a4d5bb23c0701cc287532ca04600
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2021-09-09 13:11:06 -0400

    remove extra comma from Version.scala

--- netlogo-core/src/main/api/Version.scala
+++ netlogo-core/src/main/api/Version.scala
@@ -124,7 +124,7 @@ trait Version {
 
   private def compatibleOverrides = Map(
     "6.1" -> Seq("6.0"),
-    "6.2" -> Seq("6.1", "6.0"),
+    "6.2" -> Seq("6.1", "6.0")
   )
 
   private def versionNumber(v: String) =
