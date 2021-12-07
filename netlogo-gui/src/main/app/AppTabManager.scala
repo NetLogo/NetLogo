2f7e1da60da07d1f9206d7319eced84cd5427d9d
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2021-12-07 16:32:28 -0500

    add hotkeys for Comment / Uncomment, Shift Left, Shift Right, Jump To Declaration for separate code tab

--- netlogo-gui/src/main/app/AppTabManager.scala
+++ netlogo-gui/src/main/app/AppTabManager.scala
@@ -7,6 +7,8 @@ import java.awt.event.{ ActionEvent, KeyEvent }
 import javax.swing.{ Action, AbstractAction, ActionMap, InputMap, JComponent, JMenu, JMenuItem, JTabbedPane, KeyStroke }
 
 import org.nlogo.app.codetab.{ CodeTab }
+
+import org.nlogo.ide.{ JumpToDeclarationAction, ShiftActions, ToggleComments }
 import org.nlogo.swing.UserAction
 import org.nlogo.window.Event
 
@@ -426,6 +428,20 @@ class AppTabManager(val appTabsPanel:          Tabs,
   }
 
   def setSeparateCodeTabBindings(): Unit = {
+    // These code tab specific Edit menu hotkeys are needed
+    // Comment / Uncomment: meta pressed SEMICOLON
+    // Shift Left: meta pressed OPEN_BRACKET
+    // Shift Right: meta pressed CLOSE_BRACKET
+    // Show Usage: meta pressed U
+    // Jump To Declaration: meta pressed E
+    println(getMainCodeTab.editorFactory)
+    addCodeTabContainerKeyStroke(intKeyToMenuKeystroke(KeyEvent.VK_SEMICOLON), new ToggleComments(), "CommentUncomment")
+    addCodeTabContainerKeyStroke(intKeyToMenuKeystroke(KeyEvent.VK_OPEN_BRACKET), new ShiftActions.Left(), "ShiftLeft")
+    addCodeTabContainerKeyStroke(intKeyToMenuKeystroke(KeyEvent.VK_CLOSE_BRACKET), new ShiftActions.Right(), "ShiftRight")
+    //addCodeTabContainerKeyStroke(intKeyToMenuKeystroke(KeyEvent.VK_U), new ShowUsageBoxAction(showUsageBox()), "ShowUsage")
+    addCodeTabContainerKeyStroke(intKeyToMenuKeystroke(KeyEvent.VK_E), new JumpToDeclarationAction(), "JumpToDeclaration")
+
+    // Close the separate code tab: meta pressed W
     addCodeTabContainerKeyStroke(intKeyToMenuKeystroke(KeyEvent.VK_W), RejoinCodeTabsAction, "popInCodeTab")
   }
 
