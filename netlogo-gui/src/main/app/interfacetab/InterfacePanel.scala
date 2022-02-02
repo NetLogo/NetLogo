028bb54783e425d846d2bb7c62cc3e8ec6ca67d4
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2022-02-02 14:33:31 -0500

    for Java 11 - use getMenuShortcutKeyMaskEx

--- netlogo-gui/src/main/app/interfacetab/InterfacePanel.scala
+++ netlogo-gui/src/main/app/interfacetab/InterfacePanel.scala
@@ -290,7 +290,7 @@ class InterfacePanel(val viewWidget: ViewWidgetInterface, workspace: GUIWorkspac
   def keyTyped(e: KeyEvent): Unit = {
     if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED &&
       !e.isActionKey &&
-    (e.getModifiersEx & getToolkit.getMenuShortcutKeyMask) == 0) {
+    (e.getModifiersEx & getToolkit.getMenuShortcutKeyMaskEx) == 0) {
       Option(findActionButton(e.getKeyChar)).foreach { button =>
         button.keyTriggered()
       }
