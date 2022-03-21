e82d9a5c89f8efb46acc26faa54c2f6c5bd0fb91
ocaml-n00b <vipersoftceo@gmail.com>
2022-03-21 02:25:43 -0400

    Simplified SDM-gui interface by means of adding flows with right mouse button

--- netlogo-gui/src/main/sdm/gui/InspectionTool.java
+++ netlogo-gui/src/main/sdm/gui/InspectionTool.java
@@ -1,15 +1,19 @@
 // (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
 
 package org.nlogo.sdm.gui;
+import org.nlogo.sdm.Model;
 
 strictfp class InspectionTool
     extends org.jhotdraw.standard.SelectionTool {
 
   private final AggregateModelEditor editor;
+  private Model model;
+  public RateConnectionTool rct;
 
-  InspectionTool(AggregateModelEditor editor) {
+  InspectionTool(AggregateModelEditor editor, Model model) {
     super(editor);
     this.editor = editor;
+    this.model = model;
   }
 
   protected void inspectFigure(org.jhotdraw.framework.Figure f) {
@@ -19,15 +23,44 @@ strictfp class InspectionTool
   @Override
   public void mouseDown(java.awt.event.MouseEvent e, int x, int y) {
     setView((org.jhotdraw.framework.DrawingView) e.getSource());
-    if (e.getClickCount() == 2) {
-      org.jhotdraw.framework.Figure figure =
-          drawing().findFigure(e.getX(), e.getY());
-      if (figure != null) {
-        inspectFigure(figure);
-        return;
+
+    if (! e.isMetaDown()){
+      if (e.getClickCount() == 2) {
+        org.jhotdraw.framework.Figure figure =
+            drawing().findFigure(e.getX(), e.getY());
+        if (figure != null) {
+          inspectFigure(figure);
+          return;
+        }
       }
-    }
     super.mouseDown(e, x, y);
+    } else {
+      // TODO: add check to choose between flow and link
+      // new AggregateConnectionTool(model, editor, new BindingConnection())
+      rct = new RateConnectionTool(model, editor, new RateConnection());
+      rct.mouseDown(e, x, y);
+    }
+  }
+
+  @Override
+  public void mouseUp(java.awt.event.MouseEvent e, int x, int y) {
+    if (! e.isMetaDown()){
+      super.mouseUp(e,x,y);
+    }
+    else {
+      rct.mouseUp(e, x, y);
+    }
+  }
+
+  // TODO: add mouse move event call
+  @Override
+  public void mouseDrag(java.awt.event.MouseEvent e, int x, int y) {
+    if (! e.isMetaDown()){
+      super.mouseDrag(e,x,y);
+    } 
+    else {
+      rct.mouseDrag(e, x, y);
+    }
   }
 
   @Override
