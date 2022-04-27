62a09e74076fc3a2a702c50432ce8afa985d1810
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2022-04-27 10:48:36 -0500

    Feature: `__mkdir` path is relative to current model

--- netlogo-gui/src/main/prim/etc/_mkdir.scala
+++ netlogo-gui/src/main/prim/etc/_mkdir.scala
@@ -5,11 +5,17 @@ package org.nlogo.prim.etc
 import org.nlogo.nvm.{ Command, Context }
 
 class _mkdir extends Command {
-
   override def perform(c: Context) {
-    // leaving this double-underscored for now sine it isn't relative to the model's location, like
+    // leaving this double-underscored for now since it isn't relative to the model's location, like
     // it ought to be - ST 2/7/11
-    new java.io.File(argEvalString(c, 0)).mkdir()
+    // I made this relative to the model's location, like it ought to have been.  I did not make
+    // it a "regular" prim because 1) that's too much work for why I'm doing it (language tests in
+    // extensions) and 2) I'm not sure file-based prims like this one should live in the core language
+    // (as opposed to extensions).  -Jeremy B April 2022
+    val filePath     = argEvalString(c, 0)
+    val absolutePath = workspace.fileManager.attachPrefix(filePath)
+    val directory    = new java.io.File(absolutePath)
+    directory.mkdir()
     c.ip = next
   }
 }
