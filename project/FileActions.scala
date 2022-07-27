4223ec43d2df1dc1b772bd6eb084bd33748f7f45
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2022-07-27 16:48:31 -0400

    Add documentation to project/FileActions.scala

--- project/FileActions.scala
+++ project/FileActions.scala
@@ -69,9 +69,17 @@ object FileActions {
     })
   }
 
+  // Creates a new directory specified as a file
+  // The createDirectories method should be used if it is necessaryto create
+  // all nonexistent parent directories first.
+  // No error if the directory already exists
   def createDirectory(f: File): Unit =
     createDirectory(f.toPath)
 
+  // Creates a new directory specified as a path
+  // The createDirectories method should be used if it is necessary to create
+  // all nonexistent parent directories first.
+  // No error if the directory already exists
   def createDirectory(p: Path): Unit = {
     try {
       Files.createDirectory(p)
@@ -80,8 +88,12 @@ object FileActions {
     }
   }
 
+  // Creates a directory by creating all nonexistent parent directories first.
+  // No error if the directory already exists
   def createDirectories(f: File): Unit = createDirectories(f.toPath)
 
+  // Creates a directory by creating all nonexistent parent directories first.
+  // No error if the directory already exists
   def createDirectories(p: Path): Unit = { Files.createDirectories(p) }
 
   def copyAllPaths(copies: Traversable[(Path, Path)]): Unit = {
@@ -94,35 +106,52 @@ object FileActions {
     }
   }
 
+  // Copies a file or directory to a destination
+  // If directory copies all contents
+  // overwrites any target files that exist
   def copyAny(src: File, dest: File): Unit =
     copyAny(src.toPath, dest.toPath)
 
+  // Copies a file or directory (as path) to a destination (as path).
+  // If directory copies all contents.
+  // Overwrites any target files that exist.
   def copyAny(src: Path, dest: Path): Unit =
     src match {
       case f if Files.isDirectory(f) => copyDirectory(src, dest)
       case f => copyFile(src, dest)
     }
 
+  // Copies a file to a destination, overwriting the target file if it exists.
   def copyFile(src: File, dest: File): Unit = {
     copyFile(src.toPath, dest.toPath)
   }
 
+  // Copies a path to a destination, overwriting the target path if it exists
+  // If file is a directory, does not copy contents.
   def copyFile(src: Path, dest: Path): Unit = {
     Files.copy(src, dest, StandardCopyOption.COPY_ATTRIBUTES, StandardCopyOption.REPLACE_EXISTING)
   }
 
+  // Copies a directory (as file) and its contents to a destination,
+  // overwriting any target files that exist
   def copyDirectory(src: File, dest: File): Unit = {
     copyDirectory(src.toPath, dest.toPath)
   }
 
+  // Copies a directory (as path) and its contents to a destination,
+  // overwriting any target files that exists
   def copyDirectory(src: Path, dest: Path): Unit = {
     Files.walkFileTree(src, new java.util.HashSet(), Int.MaxValue, new CopyVisitor(src, dest))
   }
 
+  // Copies a directory (as path) and its contents that satisfy a filter
+  // to a destination, overwriting any target files that exists
   def copyDirectory(src: Path, dest: Path, filter: Path => Boolean): Unit = {
     Files.walkFileTree(src, new java.util.HashSet(), Int.MaxValue, new CopyFilterVisitor(src, dest, filter))
   }
 
+  // Moves the file to the target file, failing if the target file exists
+  // Moving a directory will fail if it requires moving sub files.
   def moveFile(src: File, dest: File): Unit = {
     Files.move(src.toPath, dest.toPath)
   }
@@ -133,10 +162,15 @@ object FileActions {
     listVisitor.pathsFound
   }
 
+  // Copies bytes from the URL source to a file destination.
+  // The directories up to destination will be created if they don't already exist.
+  // The destination will be overwritten if it already exists.
   def download(url: URL, file: File): Unit = {
     FileUtils.copyURLToFile(url, file)
   }
 
+  // Deletes file, recursively if it is a directory.
+  // No error if file does not exist
   def remove(f: File): Unit = {
     IO.delete(f)
   }
