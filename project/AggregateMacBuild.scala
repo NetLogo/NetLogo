030fcfb4559ce1b232d58363b5611500f343d875
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2022-08-08 13:18:21 -0400

    gephi no longer uses libsqlitejdbc.jnilib files that need signing

--- project/AggregateMacBuild.scala
+++ project/AggregateMacBuild.scala
@@ -217,7 +217,6 @@ object PackageMacAggregate {
     // -Jeremy B July 2020
     val jarLibsToSign = Map(
       ("extensions/.bundled/gogo/hid4java-0.7.0.jar", Seq("darwin/libhidapi.dylib")),
-      ("extensions/.bundled/nw/gephi-toolkit-0.9.7.jar", Seq("org/sqlite/native/Mac/aarch64/libsqlitejdbc.jnilib", "org/sqlite/native/Mac/x86_64/libsqlitejdbc.jnilib")),
       ("extensions/.bundled/vid/core-video-capture-1.4-20220209.101851-153.jar", Seq("org/openimaj/video/capture/nativelib/darwin_universal/libOpenIMAJGrabber.dylib")),
       ("Java/java-objc-bridge-1.0.0.jar", Seq("libjcocoa.dylib"))
     )
