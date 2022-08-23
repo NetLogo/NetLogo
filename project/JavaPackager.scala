73389f7fb93159724fa4fa9e9f01bbf81ed1647b
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2022-08-23 13:42:12 -0500

    Meta: Add commentary about new jpackage setup

--- project/JavaPackager.scala
+++ project/JavaPackager.scala
@@ -10,6 +10,62 @@ import java.io.File
 import java.util.jar.Attributes.Name._
 import scala.sys.process.Process
 
+/*
+
+Oh hello.  We use `jpackage` to create "app images" on macOS, Windows, and Linux.  These
+are the base binaries/executables/directories that we take to finalize a distribution
+package.
+
+Netlogo bundles 4 apps, NetLogo, NetLogo 3D, HubNet Client, and Behaviorsearch.
+
+For Linux and Windows we use the `--add-launcher` flag to specify the settings for the
+extra 3 launchers beyond NetLogo.
+
+Linux has to create some sym links so the binaries and the NetLogo-specific resource dirs
+(models, extensions, natives, etc) are easily accessible.  Otherwise, it just makes its
+TGZ file and is good to go.
+
+Windows has to do some work to get the icons in the right place to be used, then also to
+handle its UUIDs and WiX setup to generate the MSI file.
+
+For macOS it's not as simple as using `--add-launcher`, as it puts the executables inside
+the main `NetLogo.app` package where there is no easy way for a user to find and execute
+them.  So for macOS we instead do this:
+
+1. Generate each `${launcher.name}.app` separately as a single `generateAppImage()` call.
+2. Copy out the Java runtime and app library directory to a new bundle directory.
+3. Remove the runtime and app library directories from each app.  Do some work on the
+   `${launcher.name}.cfg` file to get it working with the updated paths, and include some
+   macOS-specific flags.
+4. Code sign the bundled dir files as needed.
+5. Build the DMG.
+6. Sign the DMG.
+
+macOS also has a little more work to do to get its icon (icns) files in the right places.
+A lot (but not all) of the weirdness for macOS is because it uses the `MacApplication`
+wrapper to handle actually running the 4 apps, so extra deps need to be copied over and
+extra config mashed in to make sure it runs correctly.
+
+The final result of `packageMacAggregate`, `packageLinuxAggregate`, or
+`packageWinAggregate` should be a new archive file (dmg, msi, or tgz) in the
+`dist/target/downloadPages` directory if everything goes correctly..
+
+Here is the generate workflow for `JavaPackager`.
+
+- Call `setupAppImageInput()` to create the directory that `jpackage` will use as its
+  `--input`.
+- Call `generateAppImage()` to turn that input into a `--dest` directory.
+- Call `copyExtraFiles()` to get the NetLogo-specific resource directories and root files
+  needed by NetLogo put in place.
+- Call `createScripts()` to create the runner scripts for headless/gui.
+
+Before and after each of those calls any extra prep work can be done.  Most of the work is
+usually after the `createScripts()` to finalize the package.
+
+-Jeremy B August 2022
+
+*/
+
 object JavaPackager {
   def mainArtifactSettings: Seq[Setting[_]] =
     Seq(
