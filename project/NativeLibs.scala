6bb2fca01b7317946f5344d1e885848390989d56
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2023-03-07 15:20:07 -0600

    Infrastructure: Bump JOGL to 2.4.0 final release

--- project/NativeLibs.scala
+++ project/NativeLibs.scala
@@ -21,23 +21,24 @@ object NativeLibs {
 
   lazy val nativeLibsTask =
     nativeLibs := {
-        val baseURL = "https://s3.amazonaws.com/ccl-artifacts/"
-        val joglNatives = baseDirectory.value / "natives"
-        val joglTmp = baseDirectory.value / "jogl-2.4.0-natives-rc-20210111.zip"
-        val joglUrl = new URL(baseURL + "jogl-2.4.0-natives-rc-20210111.zip")
-        IO.createDirectory(joglNatives)
-        FileActions.download(joglUrl, joglTmp)
-        IO.unzip(joglTmp, joglNatives)
-        IO.delete(joglTmp)
+      val joglFile = "jogl-2.4.0-natives.zip"
+      val baseURL = "https://s3.amazonaws.com/ccl-artifacts/"
+      val joglNatives = baseDirectory.value / "natives"
+      val joglTmp = baseDirectory.value / joglFile
+      val joglUrl = new URL(baseURL + joglFile)
+      IO.createDirectory(joglNatives)
+      FileActions.download(joglUrl, joglTmp)
+      IO.unzip(joglTmp, joglNatives)
+      IO.delete(joglTmp)
 
-        // if we need any additional libraries
-        // excluded by this, make sure the copyright notice is updated
-        (joglNatives.allPaths).get.foreach { f =>
-          if (Seq("jocl", "mobile", "newt", "openal", "oal", "jogl_cg", "joal")
-            .exists(notwanted => f.getName.contains(notwanted)))
-          IO.delete(f)
-        }
-        (joglNatives.allPaths).get
+      // if we need any additional libraries
+      // excluded by this, make sure the copyright notice is updated
+      (joglNatives.allPaths).get.foreach { f =>
+        if (Seq("jocl", "mobile", "newt", "openal", "oal", "jogl_cg", "joal")
+          .exists(notwanted => f.getName.contains(notwanted)))
+        IO.delete(f)
+      }
+      (joglNatives.allPaths).get
     }
 
   lazy val cocoaLibsTask =
