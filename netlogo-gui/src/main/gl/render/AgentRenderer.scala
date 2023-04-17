4d13810842bbb144acff08a855c5779183a6095e
Bryan Head <bryan.head@gmail.com>
2023-04-17 10:42:24 -0700

    Add newline support to labels in netlogo 3d

--- netlogo-gui/src/main/gl/render/AgentRenderer.scala
+++ netlogo-gui/src/main/gl/render/AgentRenderer.scala
@@ -17,15 +17,21 @@ private[render] object AgentRenderer {
   private val FontScale = 6500f
 
   def renderString(gl: GL2, world: World, str: String, color: AnyRef, fontSize: Int, patchSize: Double) = {
+    val lines = str.split("\n")
     val rgb: Array[Float] =
       org.nlogo.api.Color.getColor(color).getRGBColorComponents(null)
     val scale: Float = (fontSize * 12 / patchSize.toFloat) / FontScale
     gl.glColor3fv(java.nio.FloatBuffer.wrap(rgb))
     gl.glDisable(GLL.GL_LIGHTING)
     gl.glScalef(scale, scale, 3.0f)
-    val strwidth: Float = glut.glutStrokeLength(GLUT.STROKE_ROMAN, str)
+    val strwidth: Float = glut.glutStrokeLength(GLUT.STROKE_ROMAN, lines(0))
     gl.glTranslatef(-strwidth / -2.0f, 0.0f, 0.0f)
-    glut.glutStrokeString(GLUT.STROKE_ROMAN, str)
+    lines.foreach { l =>
+      glut.glutStrokeString(GLUT.STROKE_ROMAN, l)
+      // Why -120.0f and not, like, 1.0f? TBH I don't know. The glScalef takes care of factoring out font and patch
+      // size, so it makes sense this would be a constant. But beyond that, this just seemed to work...
+      gl.glTranslatef(-glut.glutStrokeLength(GLUT.STROKE_ROMAN, l), -120.0f, 0.0f)
+    }
     gl.glEnable(GLL.GL_LIGHTING)
   }
 
