b4e4086daf4a290729cf5b4d7308c8ff0203f77a
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2023-05-02 13:34:17 -0500

    Optimization: Link neighbor speedups for headless
    Oops, forgot these in the initial commit.

--- netlogo-headless/src/main/prim/etc/_outlinkneighbor.scala
+++ netlogo-headless/src/main/prim/etc/_outlinkneighbor.scala
@@ -15,6 +15,6 @@ class _outlinkneighbor(private[this] val breedName: String) extends Reporter {
   def report_1(context: Context, target: Turtle): Boolean = {
     val parent: Turtle = context.agent.asInstanceOf[Turtle]
     val breed: AgentSet = if (breedName == null) world.links else world.getLinkBreed(breedName)
-    world.linkManager.linksTo(parent, target, breed).length > 0
+    world.linkManager.isLinkedTo(parent, target, breed)
   }
 }
