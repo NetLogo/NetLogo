1d53a601cc8ada3bfe44abc9532089c32c3b7a20
Isaac Brown <isaacbrown5763@gmail.com>
2023-07-19 17:14:03 -0400

    Started GUI improvements for run metrics

---
+++ netlogo-gui/src/main/properties/MetricsBooleanEditor.scala.scala
@@ -0,0 +1,23 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.properties
+
+import scala.collection.mutable.ArrayBuffer
+
+class MetricsBooleanEditor(accessor: PropertyAccessor[Boolean], properties: ArrayBuffer[PropertyEditor[_]]) extends BooleanEditor(accessor)
+{
+    override def changed(): Unit =
+    {
+        properties.find(_.accessor.accessString == "runMetricsConditions") match
+        {
+            case Some(p) => p.setEnabled(get.getOrElse(true))
+            case None =>
+        }
+
+        properties.find(_.accessor.accessString == "runMetricsCombine") match
+        {
+            case Some(p) => p.setEnabled(get.getOrElse(true))
+            case None =>
+        }
+    }
+}
