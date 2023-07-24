cab34c95cbd437a687374480bc2f93328039fbd1
Ryan <rypoon18@gmail.com>
2023-07-23 23:11:25 -0400

    clean up recursive disabling

--- netlogo-gui/src/main/properties/MetricsBooleanEditor.scala
+++ netlogo-gui/src/main/properties/MetricsBooleanEditor.scala
@@ -1,17 +1,9 @@
 package org.nlogo.properties
 
 import scala.collection.mutable.ArrayBuffer
-import java.awt.Container
 
 class MetricsBooleanEditor(accessor: PropertyAccessor[Boolean], properties: ArrayBuffer[PropertyEditor[_]]) extends BooleanEditor(accessor)
 {
-    private def setEnabledRecursive(component: Container, state: Boolean): Unit = {
-        component.getComponents().foreach(c => {
-            setEnabledRecursive(c.asInstanceOf[Container], state)
-            c.setEnabled(state)
-        })
-    }
-
     override def changed(): Unit =
     {
         properties.find(_.accessor.accessString == "runMetricsCombine") match
@@ -22,7 +14,7 @@ class MetricsBooleanEditor(accessor: PropertyAccessor[Boolean], properties: Arra
 
         properties.find(_.accessor.accessString == "runMetricsConditions") match
         {
-            case Some(p) => setEnabledRecursive(p, !get.getOrElse(true))
+            case Some(p) => p.setEnabled(!get.getOrElse(true))
             case None =>
         }
     }
