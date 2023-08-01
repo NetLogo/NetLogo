fe10bc10f0e5cbf5a93e2a7b8c33ba8267854d76
Isaac Brown <isaacbrown5763@gmail.com>
2023-08-01 13:04:52 -0400

    Added tuple syntax for varying parameters

--- netlogo-gui/src/main/lab/gui/ManagerDialog.scala
+++ netlogo-gui/src/main/lab/gui/ManagerDialog.scala
@@ -3,7 +3,7 @@
 package org.nlogo.lab.gui
 
 import org.nlogo.api.LabProtocol
-import org.nlogo.api.{ RefEnumeratedValueSet, LabProtocol }
+import org.nlogo.api.{ RefEnumeratedValueSet, LabProtocol, ValueList }
 import org.nlogo.window.{ EditDialogFactoryInterface, MenuBarFactory }
 import java.awt.{ Component, Dimension }
 import javax.swing.{ JButton, JDialog, JLabel, JList, JMenuBar, JOptionPane, JPanel, JScrollPane, ListCellRenderer }
@@ -117,11 +117,12 @@ private class ManagerDialog(manager:       LabManager,
     editProtocol(
       new LabProtocol(
         "experiment", "setup", "go", "", 1, true, true, "", 0, "", List("count turtles"),
-        manager.workspace.world.synchronized {
-          manager.workspace.world.program.interfaceGlobals.toList
-          .map{case variableName: String =>
-            new RefEnumeratedValueSet(
-              variableName, List(manager.workspace.world.getObserverVariableByName(variableName)))}}),
+        ValueList(
+          manager.workspace.world.synchronized {
+            manager.workspace.world.program.interfaceGlobals.toList
+            .map{case variableName: String =>
+              new RefEnumeratedValueSet(
+                variableName, List(manager.workspace.world.getObserverVariableByName(variableName)))}})),
       true)
   }
   private def duplicate() { editProtocol(selectedProtocol, true) }
