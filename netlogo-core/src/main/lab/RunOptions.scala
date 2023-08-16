80ebaa29fba43f54d365fbbb3e82482f9ebd7e0d
Isaac Brown <isaacbrown5763@gmail.com>
2023-08-16 17:09:14 -0400

    Moved file to hopefully fix error

--- netlogo-core/src/main/lab/RunOptions.scala
+++
@@ -1,6 +0,0 @@
-// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
-
-package org.nlogo.lab
-
-// moved out of netlogo-gui into netlogo-core to fix headless compiler error - IOB 8/16/23
-case class RunOptions(threadCount: Int, table: String, spreadsheet: String, updateView: Boolean, updatePlotsAndMonitors: Boolean)
