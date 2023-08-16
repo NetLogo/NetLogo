06818befa8208ecf5eec6a552140540e86f07f3b
Isaac Brown <isaacbrown5763@gmail.com>
2023-08-16 16:39:17 -0400

    Fixed headless compiler error

--- netlogo-gui/src/main/lab/gui/RunOptionsDialog.scala
+++ netlogo-gui/src/main/lab/gui/RunOptionsDialog.scala
@@ -5,6 +5,7 @@ package org.nlogo.lab.gui
 import org.nlogo.api.{Editable, Property, RunOptions}
 import org.nlogo.awt.UserCancelException
 import org.nlogo.core.{ I18N }
+import org.nlogo.lab.RunOptions
 import org.nlogo.window.EditDialogFactoryInterface
 
 import java.io.File
