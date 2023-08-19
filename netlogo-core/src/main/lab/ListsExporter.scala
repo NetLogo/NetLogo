7d9bfd46197729564e76d68dafddfa1eb0a9985d
Isaac Brown <isaacbrown5763@gmail.com>
2023-08-19 01:54:57 -0400

    Improved algorithm for table format

--- netlogo-core/src/main/lab/ListsExporter.scala
+++ netlogo-core/src/main/lab/ListsExporter.scala
@@ -56,42 +56,39 @@ class ListsExporter(modelFileName: String,
       case ListsExporter.TableFormat(fileName) => {
         val lines = scala.io.Source.fromFile(fileName).getLines.drop(6)
         val header = lines.next.split(",")
-        val runIndex = header.indexWhere(_.contains("[run number]"))
         val stepIndex = header.indexWhere(_.contains("[step]"))
-        val parameterIndices = runIndex + 1 until stepIndex
+        val parameterIndices = 1 until stepIndex
+        // (reporter, run, parameters, step, data)
         var sortedLines = Seq[(String, Int, String, Int, String)]()
         for (line <- lines) {
           val els = line.split(",")
-          for (i <- 0 until els.length) {
+          for (i <- stepIndex + 1 until els.length) {
+            // only add line if reporter returned a list
             if (els(i).contains("[")) {
               sortedLines = sortedLines :+ ((header(i),
-                                             els(runIndex).replaceAll("\\D", "").toInt,
+                                             els(0).replaceAll("\\D", "").toInt,
                                              parameterIndices.map(els(_)).mkString(","),
                                              els(stepIndex).replaceAll("\\D", "").toInt,
                                              els(i).replaceAll("[\"\\[\\]]", "").replace(" ", ",")))
             }
           }
         }
+        // sort lines by run, then by reporter, then by step
         sortedLines = sortedLines.sortWith((a, b) =>
           if (a._2 == b._2) {
             if (a._1 == b._1) {
               a._4 < b._4
-            } else header.indexWhere(_ == a._1) < header.indexWhere(_ == b._1)
+            } else header.indexOf(a._1) < header.indexOf(b._1)
           } else a._2 < b._2
         )
         out.print("[reporter],[run number]")
-        for (i <- parameterIndices)
-          out.print("," + header(i))
+        parameterIndices.foreach(i => out.print("," + header(i)))
         out.print(",[step]")
         for (i <- 0 until sortedLines.map(_._5.split(",").length).max) {
           out.print(s",[$i]")
         }
         out.println()
-        for (line <- sortedLines)
-        {
-          out.print(line._1 + "," + line._2 + "," + line._3 + "," + line._4 + "," + line._5)
-          out.println()
-        }
+        sortedLines.foreach(line => out.println(line.productIterator.mkString(",")))
       }
     }
     out.close()
