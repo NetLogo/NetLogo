c2744ca387007b35e80ae1f2c2b410f2f3b569ad
Isaac Brown <isaacbrown5763@gmail.com>
2023-09-01 22:16:17 -0400

    Fixed bugs in tests

--- netlogo-core/src/main/fileformat/package.scala
+++ netlogo-core/src/main/fileformat/package.scala
@@ -45,15 +45,15 @@ package object fileformat {
       .addFormat[Array[String], NLogoFormat](new NLogoFormat)
       .addSerializer[Array[String], NLogoFormat](NLogoModelSettings)
 
-  def standardLoader(literalParser: LiteralParser) = {
+  def standardLoader(literalParser: LiteralParser, editNames: Boolean = false) = {
     new ConfigurableModelLoader()
       .addFormat[Array[String], NLogoFormat](new NLogoFormat)
       .addSerializer[Array[String], NLogoFormat](NLogoModelSettings)
       .addSerializer[Array[String], NLogoFormat](new NLogoHubNetFormat(literalParser))
       .addSerializer[Array[String], NLogoFormat](new NLogoPreviewCommandsFormat())
-      .addSerializer[Array[String], NLogoFormat](new NLogoLabFormat(literalParser))
+      .addSerializer[Array[String], NLogoFormat](new NLogoLabFormat(literalParser, editNames))
       .addFormat[Array[String], NLogoThreeDFormat](new NLogoThreeDFormat)
-      .addSerializer[Array[String], NLogoThreeDFormat](new NLogoThreeDLabFormat(literalParser))
+      .addSerializer[Array[String], NLogoThreeDFormat](new NLogoThreeDLabFormat(literalParser, editNames))
       .addSerializer[Array[String], NLogoThreeDFormat](NLogoThreeDModelSettings)
       .addSerializer[Array[String], NLogoThreeDFormat](NLogoThreeDPreviewCommandsFormat)
   }
