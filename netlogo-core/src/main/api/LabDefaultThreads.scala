c917a82358984ab760239f5b4ffe0f179c83b923
Aaron Brandes <aaron.brandes@northwestern.edu>
2023-09-14 12:17:59 -0400

    Minor LabDefaultThreads formatting change

--- netlogo-core/src/main/api/LabDefaultThreads.scala
+++ netlogo-core/src/main/api/LabDefaultThreads.scala
@@ -4,7 +4,7 @@ package org.nlogo.api
 import scala.math.floor
 
 object LabDefaultThreads {
- val RATIO = 0.75
- // Determines the number of threads in BehaviorSpace if the user has not specified a value
- def getLabDefaultThreads: Int= { floor(Runtime.getRuntime.availableProcessors * RATIO).toInt }
+  val RATIO = 0.75
+  // Determines the number of threads in BehaviorSpace if the user has not specified a value
+  def getLabDefaultThreads: Int = { floor(Runtime.getRuntime.availableProcessors * RATIO).toInt }
 }
