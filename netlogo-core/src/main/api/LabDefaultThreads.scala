42894bcaf30913459db3150cacdd8a1834d09123
Aaron Brandes <aaron.brandes@northwestern.edu>
2023-09-14 11:44:50 -0400

    BehaviorSpace - minor changes for parallel run default

--- netlogo-core/src/main/api/LabDefaultThreads.scala
+++ netlogo-core/src/main/api/LabDefaultThreads.scala
@@ -6,5 +6,5 @@ import scala.math.floor
 object LabDefaultThreads {
  val RATIO = 0.75
  // Determines the number of threads in BehaviorSpace if the user has not specified a value
- def getLabDefaultThreads :Int  = { floor(Runtime.getRuntime.availableProcessors * RATIO).toInt }
+ def getLabDefaultThreads: Int= { floor(Runtime.getRuntime.availableProcessors * RATIO).toInt }
 }
