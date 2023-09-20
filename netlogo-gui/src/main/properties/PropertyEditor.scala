4e2c091d943ff67566f5eb4d1a9d6d81bd20d7ee
Isaac Brown <isaacbrown5763@gmail.com>
2023-09-20 11:11:42 -0500

    Relocate for merge

--- netlogo-gui/src/main/properties/PropertyEditor.scala
+++ netlogo-gui/src/main/properties/PropertyEditor.scala
@@ -9,7 +9,6 @@ abstract class PropertyEditor[T](val accessor: PropertyAccessor[T],
                                  val handlesOwnErrors: Boolean = false)
          extends PropertyPanel
 {
-
   def changed() // abstract
 
   private val originalValue: T = accessor.get
@@ -20,10 +19,4 @@ abstract class PropertyEditor[T](val accessor: PropertyAccessor[T],
   def get: Option[T]
   def set(value: T): Unit
   def apply() { get.foreach(accessor.set) }
-
-  def setTooltip(text: String) = setToolTipText(text)
-  def tooltipFont(component: java.awt.Component) =
-    if (useTooltip)
-      component.setFont(component.getFont.deriveFont(
-                        java.util.Collections.singletonMap(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON)))
 }
--- netlogo-gui/src/main/properties/PropertyPanel.scala
+++ netlogo-gui/src/main/properties/PropertyPanel.scala
@@ -12,4 +12,10 @@ abstract class PropertyPanel extends javax.swing.JPanel
     c.insets = new java.awt.Insets(3, 3, 3, 3)
     c
   }
+
+  def setTooltip(text: String) = setToolTipText(text)
+  def tooltipFont(component: java.awt.Component) =
+    if (useTooltip)
+      component.setFont(component.getFont.deriveFont(
+                        java.util.Collections.singletonMap(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON)))
 }
