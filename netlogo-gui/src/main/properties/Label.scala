cbfad64039bd377bc39b3b1312e77aa48ec5618c
Isaac Brown <isaacbrown5763@gmail.com>
2023-09-19 19:03:39 -0500

    Added note about underlines

---
+++ netlogo-gui/src/main/properties/Label.scala
@@ -0,0 +1,14 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.properties
+
+abstract class Label(accessor: PropertyAccessor[String], useTooltip: Boolean)
+  extends PropertyEditor(accessor, useTooltip)
+{
+  setLayout(new java.awt.BorderLayout(BORDER_PADDING, 0))
+  val label = new javax.swing.JLabel(accessor.displayName)
+  tooltipFont(label)
+  label.setFont(label.getFont.deriveFont(java.awt.Font.BOLD))
+  add(label, java.awt.BorderLayout.CENTER)
+  override def set(value: String) { }
+}
