ad4880a5f08f9126a7b940b005a2ccf4e3825d17
Isaac Brown <isaacbrown5763@gmail.com>
2024-03-24 18:12:53 -0500

    Initialize

--- netlogo-gui/src/main/swing/Utils.scala
+++ netlogo-gui/src/main/swing/Utils.scala
@@ -1,7 +1,7 @@
 // (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
 
 package org.nlogo.swing
-
+import com.formdev.flatlaf.FlatDarkLaf  //FlatDarkLaf
 import java.awt.{ Color, Frame, Insets }
 import java.awt.event.KeyEvent
 
@@ -32,7 +32,7 @@ final object Utils {
       // this slider thing is a workaround for Java bug parade bug #6465237 - ST 1/20/09
       UIManager.put("Slider.paintValue", false)
       if (System.getProperty("os.name").startsWith("Mac")) {
-        val lookAndFeel = System.getProperty("netlogo.swing.laf", UIManager.getSystemLookAndFeelClassName)
+        //val lookAndFeel = System.getProperty("netlogo.swing.laf", UIManager.getSystemLookAndFeelClassName)
         UIManager.getDefaults.put("TabbedPane.foreground", new Color(0, 0, 0))
         UIManager.getDefaults.put("TabbedPane.selectedTabPadInsets", new Insets(0,0,-2,0))
         UIManager.getDefaults.put("TabbedPane.contentBorderInsets", new Insets(0,-10,-13,-9))
@@ -40,7 +40,11 @@ final object Utils {
         // which was distractingly bad for the name field of the "PlotPen" JTable when
         // that field was clicked, moved off and then released - RG 7/1/16
         UIManager.getDefaults.put("Table.focusCellBackground", new Color(202, 202, 202))
-        UIManager.setLookAndFeel(lookAndFeel)
+        //UIManager.setLookAndFeel(lookAndFeel)
+
+       // UIManager.setLookAndFeel(new FlatDarkLaf())
+       FlatDarkLaf.setup();
+
       } else if (System.getProperty("os.name").startsWith("Windows")) {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
       } else if (System.getProperty("swing.defaultlaf") == null) {
@@ -49,7 +53,7 @@ final object Utils {
           // mode is enabled, NetLogo looks very wonky as it's not setup to properly
           // handle it with all its custom UI elements.  So we need to fall back to good
           // ol' Nimbus.  -Jeremy B September 2022 SEO: dark mode, dark theme
-          UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel")
+          UIManager.setLookAndFeel(new FlatDarkLaf())
         } catch {
           case _: Exception => UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
         }
