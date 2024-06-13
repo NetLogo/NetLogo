c3b0b408bc7325782ad32c5e7c71418a3ac1aaca
ERSUCC <isaacbrown5763@gmail.com>
2024-06-13 14:24:24 -0500

    Included text highlighting in preference

--- netlogo-gui/src/main/window/EditorAreaErrorLabel.scala
+++ netlogo-gui/src/main/window/EditorAreaErrorLabel.scala
@@ -2,16 +2,19 @@
 
 package org.nlogo.window
 
+import java.util.prefs.Preferences
+
 import org.nlogo.core.CompilerException
 import org.nlogo.editor.AbstractEditorArea
 
 class EditorAreaErrorLabel(val editorArea: AbstractEditorArea) extends ErrorLabel {
+  val prefs = Preferences.userRoot.node("/org/nlogo/NetLogo")
 
   override def setError(compilerError: Exception, offset: Int): Unit = {
     super.setError(compilerError, offset)
 
     compilerError match {
-      case ex: CompilerException =>
+      case ex: CompilerException if prefs.getBoolean("focusOnError", true) =>
         editorArea.select(ex.start - offset, ex.end - offset)
         editorArea.setSelection(false)
         editorArea.requestFocus()
