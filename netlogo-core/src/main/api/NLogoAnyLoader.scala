d3cd33b54d4d0c8c77266abce1c4db18d9f3a5a6
ERSUCC <isaacbrown5763@gmail.com>
2024-07-01 21:23:08 -0500

    Fixed any loader

--- netlogo-core/src/main/api/NLogoAnyLoader.scala
+++ netlogo-core/src/main/api/NLogoAnyLoader.scala
@@ -9,8 +9,8 @@ import org.nlogo.core.Model
 import scala.reflect.ClassTag
 import scala.util.{ Failure, Try }
 
-class NLogoAnyLoader(loaders: GenericModelLoader*) extends GenericModelLoader {
-  def readModel(uri: URI): Try[Model] = {
+class NLogoAnyLoader(loaders: List[GenericModelLoader]) extends ConfigurableModelLoader {
+  override def readModel(uri: URI): Try[Model] = {
     for (loader <- loaders) {
       Try {
         return loader.readModel(uri)
@@ -21,7 +21,7 @@ class NLogoAnyLoader(loaders: GenericModelLoader*) extends GenericModelLoader {
                           GenericModelLoader.getURIExtension(uri).getOrElse("") + "\"."))
   }
 
-  def readModel(source: String, extension: String): Try[Model] = {
+  override def readModel(source: String, extension: String): Try[Model] = {
     for (loader <- loaders) {
       Try {
         return loader.readModel(source, extension)
@@ -31,12 +31,12 @@ class NLogoAnyLoader(loaders: GenericModelLoader*) extends GenericModelLoader {
     Failure(new Exception("Unable to read model with format \"" + extension + "\"."))
   }
 
-  def save(model: Model, uri: URI): Try[URI] = {
+  override def save(model: Model, uri: URI): Try[URI] = {
     Failure(new Exception("Unable to save model with format \"" +
                           GenericModelLoader.getURIExtension(uri).getOrElse("") + "\"."))
   }
 
-  def sourceString(model: Model, extension: String): Try[String] = {
+  override def sourceString(model: Model, extension: String): Try[String] = {
     for (loader <- loaders) {
       Try {
         return loader.sourceString(model, extension)
@@ -46,7 +46,7 @@ class NLogoAnyLoader(loaders: GenericModelLoader*) extends GenericModelLoader {
     Failure(new Exception("Unable to create source string for model with format \"" + extension + "\"."))
   }
 
-  def emptyModel(extension: String): Model = {
+  override def emptyModel(extension: String): Model = {
     for (loader <- loaders) {
       Try {
         return loader.emptyModel(extension)
@@ -56,33 +56,29 @@ class NLogoAnyLoader(loaders: GenericModelLoader*) extends GenericModelLoader {
     throw new Exception("Unable to create empty model for format \"" + extension + "\".")
   }
 
-  def addSerializers[A, B <: ModelFormat[A, B]](ss: Seq[ComponentSerialization[A, B]])
+  override def addSerializers[A, B <: ModelFormat[A, B]](ss: Seq[ComponentSerialization[A, B]])
     (implicit aTag: ClassTag[A], bTag: ClassTag[B], matchingFormat: ClassTag[FormatterPair[A, B]]): NLogoAnyLoader = {
     
-    loaders.foreach(
-      _ match {
+    new NLogoAnyLoader(loaders.map(loader => {
+      loader match {
         case loader: ConfigurableModelLoader =>
           loader.addSerializers[A, B](ss)(aTag, bTag, matchingFormat)
-        
-        case _ =>
-      }
-    )
 
-    this
+        case _ => loader
+      }
+    }))
   }
 
-  def addSerializer[A, B <: ModelFormat[A, B]](s: ComponentSerialization[A, B])
+  override def addSerializer[A, B <: ModelFormat[A, B]](s: ComponentSerialization[A, B])
     (implicit aTag: ClassTag[A], bTag: ClassTag[B], matchingFormat: ClassTag[FormatterPair[A, B]]): NLogoAnyLoader = {
 
-    loaders.foreach(
-      _ match {
+    new NLogoAnyLoader(loaders.map(loader => {
+      loader match {
         case loader: ConfigurableModelLoader =>
           loader.addSerializer[A, B](s)(aTag, bTag, matchingFormat)
 
-        case _ =>
+        case _ => loader
       }
-    )
-
-    this
+    }))
   }
 }
