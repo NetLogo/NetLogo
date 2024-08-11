809fe6e4cdf32b3e4b25bb2d8cfbc319dcc912f1
ERSUCC <isaacbrown5763@gmail.com>
2024-08-11 15:39:44 -0500

    Fixed vertical slider widget

--- netlogo-gui/src/main/window/Widget.scala
+++ netlogo-gui/src/main/window/Widget.scala
@@ -53,7 +53,7 @@ abstract class Widget extends JPanel {
   def load(widget: WidgetModel): Object
   def sourceOffset = 0
   def hasContextMenuInApplet = false
-  def getUnzoomedPreferredSize: Dimension = getPreferredSize(originalFont)
+  def getUnzoomedPreferredSize: Dimension = getPreferredSize
   def needsPreferredWidthFudgeFactor = true
   def isButton = false
   def isTurtleForeverButton = false
