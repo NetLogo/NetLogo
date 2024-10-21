aa0ad0617269fe2f8dc6108da9a5961813eab0ac
ERSUCC <isaacbrown5763@gmail.com>
2024-10-21 16:33:53 -0500

    Improved logging path checking

--- netlogo-gui/src/main/app/tools/PreferencesDialog.scala
+++ netlogo-gui/src/main/app/tools/PreferencesDialog.scala
@@ -59,6 +59,15 @@ extends ToolDialog(parent, "preferences") {
         return false
       }
     }
+    try {
+      preferences.find(_.i18nKey == "uiScale").map(_.asInstanceOf[Preferences.StringPreference].component.getText.toDouble)
+    } catch {
+      case e: NumberFormatException =>
+        new OptionPane(this, I18N.gui.get("common.messages.error"), I18N.gui.get("tools.preferences.scaleError"),
+                       OptionPane.Options.Ok, OptionPane.Icons.Error)
+        return false
+      }
+    }
     true
   }
 
