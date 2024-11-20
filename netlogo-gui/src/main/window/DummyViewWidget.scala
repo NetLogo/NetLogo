705fbfbbe94361cd9b3452ca0ef4698d0220d470
ERSUCC <isaacbrown5763@gmail.com>
2024-11-19 18:51:00 -0600

    Fixed some color sync bugs in hubnet

--- netlogo-gui/src/main/window/DummyViewWidget.scala
+++ netlogo-gui/src/main/window/DummyViewWidget.scala
@@ -2,13 +2,14 @@
 
 package org.nlogo.window
 
+import java.awt.{ Color, Dimension, Rectangle }
+import java.util.{ List => JList }
+import javax.swing.border.LineBorder
+
 import org.nlogo.agent.World
 import org.nlogo.api.{ Editable, Property }
 import org.nlogo.core.{ I18N, View => CoreView }
 import org.nlogo.theme.InterfaceColors
-
-import java.awt.{ Dimension, Rectangle }
-import java.util.{ List => JList }
 
 class DummyViewWidget(val world: World)
     extends SingleErrorWidget
@@ -16,7 +17,7 @@ class DummyViewWidget(val world: World)
 
   type WidgetModel = CoreView
 
-  setBackgroundColor(InterfaceColors.GRAPHICS_BACKGROUND)
+  setBackgroundColor(Color.BLACK)
 
   private var newWidth = StrictMath.round(world.worldWidth * world.patchSize).toInt
   private var newHeight = StrictMath.round(world.worldHeight * world.patchSize).toInt
@@ -65,6 +66,10 @@ class DummyViewWidget(val world: World)
   }
 
   override def hasContextMenu: Boolean = false
+
+  override def syncTheme() {
+    setBorder(new LineBorder(InterfaceColors.VIEW_BORDER, 2))
+  }
 
   /// load & save
   override def model: WidgetModel = {
