efa0dab3f67d778854681975e93862477d14b7c1
ERSUCC <isaacbrown5763@gmail.com>
2024-12-12 16:40:24 -0600

    Removed unnecessary equivalence code

--- parser-core/src/main/core/Shape.scala
+++ parser-core/src/main/core/Shape.scala
@@ -19,16 +19,6 @@ object Shape {
     def x: Int
     def y: Int
     def diameter: Int
-
-    override def equals(other: Any) = {
-      other match {
-        case circle: Circle =>
-          color == circle.color && filled == circle.filled && marked == circle.marked && x == circle.x &&
-            y == circle.y && diameter == circle.diameter
-
-        case _ => false
-      }
-    }
   }
 
   trait Line extends Element {
@@ -37,16 +27,6 @@ object Shape {
     def startPoint: (Int, Int)
     def endPoint: (Int, Int)
     def filled: Boolean
-
-    override def equals(other: Any) = {
-      other match {
-        case line: Line =>
-          color == line.color && marked == line.marked && startPoint == line.startPoint && endPoint == line.endPoint &&
-            filled == line.filled
-
-        case _ => false
-      }
-    }
   }
 
   trait Rectangle extends Element {
@@ -59,16 +39,6 @@ object Shape {
     def getY: Int = upperLeftCorner._2
     def getWidth: Int = lowerRightCorner._1 - upperLeftCorner._1
     def getHeight: Int = lowerRightCorner._2 - upperLeftCorner._2
-
-    override def equals(other: Any) = {
-      other match {
-        case rectangle: Rectangle =>
-          color == rectangle.color && filled == rectangle.filled && marked == rectangle.marked &&
-            upperLeftCorner == rectangle.upperLeftCorner && lowerRightCorner == rectangle.lowerRightCorner
-
-        case _ => false
-      }
-    }
   }
 
   trait Polygon extends Element {
@@ -78,30 +48,12 @@ object Shape {
     def points: Seq[(Int, Int)]
     def xCoords: Seq[Int] = points.map(_._1)
     def yCoords: Seq[Int] = points.map(_._2)
-
-    override def equals(other: Any) = {
-      other match {
-        case polygon: Polygon =>
-          color == polygon.color && filled == polygon.filled && marked == polygon.marked && points == polygon.points
-
-        case _ => false
-      }
-    }
   }
 
   trait LinkLine {
     def xcor: Double
     def isVisible: Boolean
     def dashChoices: Seq[Float]
-
-    override def equals(other: Any) = {
-      other match {
-        case line: LinkLine =>
-          xcor == line.xcor && isVisible == line.isVisible && dashChoices == line.dashChoices
-
-        case _ => false
-      }
-    }
   }
 
   object LinkLine {
@@ -119,16 +71,6 @@ object Shape {
     def rotatable: Boolean
     def editableColorIndex: Int
     def elements: Seq[Element]
-
-    override def equals(other: Any) = {
-      other match {
-        case shape: VectorShape =>
-          name == shape.name && rotatable == shape.rotatable && editableColorIndex == shape.editableColorIndex &&
-            elements == shape.elements
-
-        case _ => false
-      }
-    }
   }
 
   trait LinkShape extends Shape {
@@ -136,16 +78,6 @@ object Shape {
     def curviness: Double
     def linkLines: Seq[LinkLine]
     def indicator: VectorShape
-
-    override def equals(other: Any) = {
-      other match {
-        case shape: LinkShape =>
-          name == shape.name && curviness == shape.curviness && linkLines == shape.linkLines &&
-            indicator == shape.indicator
-
-        case _ => false
-      }
-    }
   }
 }
 
