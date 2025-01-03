1038fe363bf41ce26f1ce15e17c5e59188dac15f
ERSUCC <isaacbrown5763@gmail.com>
2025-01-03 12:55:38 -0600

    Added check for missing brackets

--- netlogo-core/src/main/api/LabVariableParser.scala
+++ netlogo-core/src/main/api/LabVariableParser.scala
@@ -21,6 +21,9 @@ object LabVariableParser
     var constants = List[RefValueSet]()
     var subExperiments = List[List[RefValueSet]]()
     for (o <- list.toList) {
+      if (!o.isInstanceOf[LogoList])
+        return (None, I18N.gui.get("edit.behaviorSpace.expectedList"))
+
       o.asInstanceOf[LogoList].toList match {
         case List() => return (None, I18N.gui.getN("edit.behaviorSpace.list.field"))
         case List(variableName: String, more: LogoList) =>
@@ -64,44 +67,49 @@ object LabVariableParser
           constants = constants :+ constant
         case List(first: LogoList, more@_*) =>
           var subExperiment = List[RefValueSet]()
-          (List(first) ++ more).foreach(_.asInstanceOf[LogoList].toList match {
-            case List() => return (None, I18N.gui.getN("edit.behaviorSpace.list.field"))
-            case List(variableName: String, more: LogoList) =>
-              more.toList match {
-                case List(first: java.lang.Double,
-                          step: java.lang.Double,
-                          last: java.lang.Double) =>
-                  if (((last > first && step > 0) || (last < first && step < 0)) &&
-                        Int.MaxValue / totalCombinations > ((last - first) / step + 1)) {
-                    val multiplier: Int = ((last - first) / step + 1).toInt
-                    totalCombinations = totalCombinations * (if (multiplier == 0) 1 else multiplier)
-                  } else
-                    return (None, I18N.gui.getN("edit.behaviorSpace.list.increment",
-                                                s"[ ${'"' + variableName + '"'} [ $first $step $last ] ]"))
-                  val exp = new SteppedValueSet(variableName,
-                                                BigDecimal(Dump.number(first)),
-                                                BigDecimal(Dump.number(step)),
-                                                BigDecimal(Dump.number(last)))
-                  if (subExperiment.exists(_.variableName == variableName)) {
-                    return (None, I18N.gui.getN("edit.behaviorSpace.variableDefinedTwiceSubexperiment",
-                      variableName))
-                  }
-                  subExperiment = subExperiment :+ exp
-                case _ =>
-                  return (None, I18N.gui.getN("edit.behaviorSpace.list.incrementinvalid", Dump.list(more)))
-              }
-            case List(variableName: String, more@_*) =>
-              if (more.isEmpty) { return (None, I18N.gui.getN("edit.behaviorSpace.expectedValue", variableName)) }
-              if (Int.MaxValue / totalCombinations > more.toList.size)
-                totalCombinations = totalCombinations * more.toList.size
-              else return (None, I18N.gui.getN("edit.behaviorSpace.list.variablelist", variableName))
-              val exp = new RefEnumeratedValueSet(variableName, more.toList)
-              if (subExperiment.exists(_.variableName == variableName)) {
-                return (None, I18N.gui.getN("edit.behaviorSpace.variableDefinedTwiceSubexperiment", variableName))
-              }
-              subExperiment = subExperiment :+ exp
-            case _ =>
-              return (None, I18N.gui.get("edit.behaviorSpace.invalidFormat"))
+          (List(first) ++ more).foreach(l => {
+            if (!l.isInstanceOf[LogoList])
+              return (None, I18N.gui.get("edit.behaviorSpace.expectedList"))
+
+            l.asInstanceOf[LogoList].toList match {
+              case List() => return (None, I18N.gui.getN("edit.behaviorSpace.list.field"))
+              case List(variableName: String, more: LogoList) =>
+                more.toList match {
+                  case List(first: java.lang.Double,
+                            step: java.lang.Double,
+                            last: java.lang.Double) =>
+                    if (((last > first && step > 0) || (last < first && step < 0)) &&
+                          Int.MaxValue / totalCombinations > ((last - first) / step + 1)) {
+                      val multiplier: Int = ((last - first) / step + 1).toInt
+                      totalCombinations = totalCombinations * (if (multiplier == 0) 1 else multiplier)
+                    } else
+                      return (None, I18N.gui.getN("edit.behaviorSpace.list.increment",
+                                                  s"[ ${'"' + variableName + '"'} [ $first $step $last ] ]"))
+                    val exp = new SteppedValueSet(variableName,
+                                                  BigDecimal(Dump.number(first)),
+                                                  BigDecimal(Dump.number(step)),
+                                                  BigDecimal(Dump.number(last)))
+                    if (subExperiment.exists(_.variableName == variableName)) {
+                      return (None, I18N.gui.getN("edit.behaviorSpace.variableDefinedTwiceSubexperiment",
+                        variableName))
+                    }
+                    subExperiment = subExperiment :+ exp
+                  case _ =>
+                    return (None, I18N.gui.getN("edit.behaviorSpace.list.incrementinvalid", Dump.list(more)))
+                }
+              case List(variableName: String, more@_*) =>
+                if (more.isEmpty) { return (None, I18N.gui.getN("edit.behaviorSpace.expectedValue", variableName)) }
+                if (Int.MaxValue / totalCombinations > more.toList.size)
+                  totalCombinations = totalCombinations * more.toList.size
+                else return (None, I18N.gui.getN("edit.behaviorSpace.list.variablelist", variableName))
+                val exp = new RefEnumeratedValueSet(variableName, more.toList)
+                if (subExperiment.exists(_.variableName == variableName)) {
+                  return (None, I18N.gui.getN("edit.behaviorSpace.variableDefinedTwiceSubexperiment", variableName))
+                }
+                subExperiment = subExperiment :+ exp
+              case _ =>
+                return (None, I18N.gui.get("edit.behaviorSpace.invalidFormat"))
+            }
           })
           subExperiments = subExperiments :+ subExperiment
         case _ =>
