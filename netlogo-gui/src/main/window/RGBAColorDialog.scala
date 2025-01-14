70d344085d6e527ec9dceb5647509ae18d66877f
ERSUCC <isaacbrown5763@gmail.com>
2025-01-14 10:26:01 -0600

    Commented out code that freezes on Mac

--- netlogo-gui/src/main/window/RGBAColorDialog.scala
+++ netlogo-gui/src/main/window/RGBAColorDialog.scala
@@ -5,26 +5,26 @@ package org.nlogo.window
 import java.awt.Frame
 import javax.swing.{ JDialog, WindowConstants }
 
-import javafx.application.Platform
-import javafx.beans.value.ObservableValue
-import javafx.concurrent.Worker.State
+// import javafx.application.Platform
+// import javafx.beans.value.ObservableValue
+// import javafx.concurrent.Worker.State
 import javafx.embed.swing.JFXPanel
-import javafx.scene.Scene
-import javafx.scene.layout.VBox
-import javafx.scene.web.WebView
+// import javafx.scene.Scene
+// import javafx.scene.layout.VBox
+// import javafx.scene.web.WebView
 
-import netscape.javascript.JSObject
+// import netscape.javascript.JSObject
 
 import org.nlogo.theme.ThemeSync
 
 class RGBAColorDialog(parent: Frame, returnColor: Boolean) extends JDialog(parent) with ThemeSync {
-  private class Bridge {
-    def selectColor(color: AnyRef) {
-      println(color)
-    }
-  }
+  // private class Bridge {
+  //   def selectColor(color: AnyRef) {
+  //     println(color)
+  //   }
+  // }
 
-  private val bridge = new Bridge
+  // private val bridge = new Bridge
 
   setResizable(false)
   setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE)
@@ -34,25 +34,27 @@ class RGBAColorDialog(parent: Frame, returnColor: Boolean) extends JDialog(paren
 
     add(panel)
 
-    Platform.runLater(() => {
-      val webView = new WebView
+    // this results in an infinite freeze on Mac, if this file is kept it will need to be fixed (Isaac B 1/14/25)
 
-      webView.getEngine.load(getClass.getResource("/web/color-picker/index.html").toString)
+    // Platform.runLater(() => {
+    //   val webView = new WebView
 
-      webView.getEngine.getLoadWorker.stateProperty.addListener(
-        (value: ObservableValue[_ <: State], oldState: State, newState: State) => {
-          webView.getEngine.executeScript("window").asInstanceOf[JSObject].setMember("bridge", bridge)
-          webView.getEngine.executeScript("initWithMode(\"RGBA\")")
-          
-          val width = webView.getEngine.executeScript("document.body.clientWidth").asInstanceOf[Number].intValue
-          val height = webView.getEngine.executeScript("document.body.clientHeight").asInstanceOf[Number].intValue
-          
-          setSize(width, height)
-        }
-      )
+    //   webView.getEngine.load(getClass.getResource("/web/color-picker/index.html").toString)
 
-      panel.setScene(new Scene(new VBox(webView)))
-    })
+    //   webView.getEngine.getLoadWorker.stateProperty.addListener(
+    //     (value: ObservableValue[_ <: State], oldState: State, newState: State) => {
+    //       webView.getEngine.executeScript("window").asInstanceOf[JSObject].setMember("bridge", bridge)
+    //       webView.getEngine.executeScript("initWithMode(\"RGBA\")")
+
+    //       val width = webView.getEngine.executeScript("document.body.clientWidth").asInstanceOf[Number].intValue
+    //       val height = webView.getEngine.executeScript("document.body.clientHeight").asInstanceOf[Number].intValue
+
+    //       setSize(width, height)
+    //     }
+    //   )
+
+    //   panel.setScene(new Scene(new VBox(webView)))
+    // })
   }
 
   def syncTheme() {
