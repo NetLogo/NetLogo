12b22122461e86919ece2b652bbd78bb6e3aa82b
ERSUCC <isaacbrown5763@gmail.com>
2025-01-27 20:37:47 -0600

    Improved root theme sync call

--- netlogo-gui/src/main/app/AppFrame.scala
+++ netlogo-gui/src/main/app/AppFrame.scala
@@ -9,12 +9,11 @@ import javax.swing.{ JFrame, WindowConstants }
 import org.nlogo.api.Exceptions
 import org.nlogo.awt.UserCancelException
 import org.nlogo.swing.NetLogoIcon
-import org.nlogo.theme.ThemeSync
 import org.nlogo.window.LinkRoot
 import org.nlogo.window.Event.LinkParent
 import org.nlogo.window.Events.IconifiedEvent
 
-class AppFrame extends JFrame with LinkParent with LinkRoot with ThemeSync with NetLogoIcon {
+class AppFrame extends JFrame with LinkParent with LinkRoot with NetLogoIcon {
   setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
   setMinimumSize(new Dimension(800, 300))
 
@@ -32,8 +31,4 @@ class AppFrame extends JFrame with LinkParent with LinkRoot with ThemeSync with 
       new IconifiedEvent(AppFrame.this, false).raise(App.app)
     }
   })
-
-  def syncTheme() {
-    App.app.syncWindowThemes()
-  }
 }
