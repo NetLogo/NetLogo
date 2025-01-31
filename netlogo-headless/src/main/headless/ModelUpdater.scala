2f2de8c43af5e7da192cbb6118ac0665010adb3d
Isaac Brown <isaacbrown5763@gmail.com>
2025-01-30 18:36:56 -0600

    Improved model updater

--- netlogo-headless/src/main/headless/ModelUpdater.scala
+++ netlogo-headless/src/main/headless/ModelUpdater.scala
@@ -10,26 +10,30 @@ import org.nlogo.workspace.ModelsLibrary
 import scala.collection.JavaConverters._
 
 // updates models in the old .nlogo format to the new .nlogox format
+// also resaves models that are already in the .nlogox format
 object ModelUpdater {
   private val loader = FileFormat.standardAnyLoader(
     Femto.scalaSingleton[LiteralParser]("org.nlogo.parse.CompilerUtilities"))
 
-  private def resaveModel(path: String) {
-    val newPath = "(.nlogo3d|.nlogo)$".r.replaceFirstIn(path, ".nlogox")
-
-    loader.readModel(Paths.get(path).toUri).foreach(model => {
-      println("Resaving \"" + path + "\" as \"" + newPath + "\"")
+  private def resaveModel(oldPath: String, newPath: String) {
+    loader.readModel(Paths.get(oldPath).toUri).foreach(model => {
+      println("Resaving \"" + oldPath + "\" as \"" + newPath + "\"")
 
       loader.save(model.copy(version = Version.version), Paths.get(newPath).toUri)
 
-      Files.delete(Paths.get(path))
+      if (oldPath != newPath)
+        Files.delete(Paths.get(oldPath))
     })
   }
 
   def run() {
     Files.walk(Paths.get(ModelsLibrary.modelsRoot)).iterator.asScala.toArray.foreach(path => {
-      if (Files.isRegularFile(path) && (path.toString.endsWith(".nlogo") || path.toString.endsWith(".nlogo3d")))
-        resaveModel(path.toString)
+      if (Files.isRegularFile(path)) {
+        if (path.toString.endsWith(".nlogo") || path.toString.endsWith(".nlogo3d"))
+          resaveModel(path.toString, "(.nlogo3d|.nlogo)$".r.replaceFirstIn(path.toString, ".nlogox"))
+        else if (path.toString.endsWith(".nlogox"))
+          resaveModel(path.toString, path.toString)
+      }
     })
   }
 }
