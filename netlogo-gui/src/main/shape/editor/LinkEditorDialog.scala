fc691e0a41ac3df27c4184ac427250213dc60d6b
ERSUCC <isaacbrown5763@gmail.com>
2025-02-08 12:06:17 -0600

    Fixed LinkEditorDialog

--- netlogo-gui/src/main/shape/editor/LinkEditorDialog.scala
+++ netlogo-gui/src/main/shape/editor/LinkEditorDialog.scala
@@ -15,7 +15,7 @@ import scala.util.{ Failure, Success, Try }
 
 class LinkEditorDialog(parent: JDialog, list: DrawableList[LinkShape], shape: LinkShape)
   extends JDialog(parent, I18N.gui.get("tools.linkEditor"), true) with EditorDialog.VectorShapeContainer {
-  
+
   private implicit val i18nPrefix = I18N.Prefix("tools.linkEditor")
 
   private val name = new TextField(shape.name, 10)
@@ -30,101 +30,103 @@ class LinkEditorDialog(parent: JDialog, list: DrawableList[LinkShape], shape: Li
 
   private val originalShape = shape
 
-  def update(originalShape: Shape, newShape: Shape) {
+  construct()
+
+  private def construct(): Unit = {
+    setResizable(false)
+    setLocation(parent.getX + 10, parent.getY + 10)
+    setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
+
+    addWindowListener(new WindowAdapter {
+      override def windowClosing(e: WindowEvent) {
+        saveShape()
+        dispose()
+        setVisible(false)
+      }
+    })
+
+    Utils.addEscKeyAction(this, new AbstractAction {
+      def actionPerformed(e: ActionEvent) {
+        if (originalShape.toString != getCurrentShape.toString ||
+            new OptionPane(LinkEditorDialog.this, I18N.gui.get("tools.shapesEditor.confirmCancel"),
+                          I18N.gui.get("tools.shapesEditor.confirmCancel.message"), OptionPane.Options.YES_NO,
+                          OptionPane.Icons.QUESTION).getSelectedIndex != 0)
+          return
+
+        dispose()
+      }
+    })
+
+    setLayout(new GridBagLayout)
+
+    getContentPane.setBackground(InterfaceColors.DIALOG_BACKGROUND)
+
+    locally {
+      val c = new GridBagConstraints
+
+      c.gridx = 0
+      c.fill = GridBagConstraints.HORIZONTAL
+      c.insets = new Insets(6, 6, 6, 6)
+
+      add(new LabeledComponent(I18N.gui("name"), name) {
+        setForeground(InterfaceColors.DIALOG_TEXT)
+      }, c)
+
+      c.insets = new Insets(0, 6, 6, 6)
+
+      add(new LabeledComponent(I18N.gui("direction"), new Button(I18N.gui("edit"), () => {
+        new EditorDialog(LinkEditorDialog.this, LinkEditorDialog.this, shape.directionIndicator, false)
+      })) {
+        setForeground(InterfaceColors.DIALOG_TEXT)
+      }, c)
+
+      add(new LabeledComponent(I18N.gui("curviness"), curviness) {
+        setForeground(InterfaceColors.DIALOG_TEXT)
+      }, c)
+
+      add(new LabeledComponent(I18N.gui("leftLine"), dashes(2)) {
+        setForeground(InterfaceColors.DIALOG_TEXT)
+      }, c)
+
+      add(new LabeledComponent(I18N.gui("middleLine"), dashes(1)) {
+        setForeground(InterfaceColors.DIALOG_TEXT)
+      }, c)
+
+      add(new LabeledComponent(I18N.gui("rightLine"), dashes(0)) {
+        setForeground(InterfaceColors.DIALOG_TEXT)
+      }, c)
+
+      val cancel = new Button(I18N.gui.get("common.buttons.cancel"), dispose)
+      val done = new Button(I18N.gui.get("common.buttons.ok"), saveShape)
+
+      add(new ButtonPanel(Array(done, cancel)), c)
+
+      getRootPane.setDefaultButton(done)
+    }
+
+    list.update()
+
+    pack()
+
+    // when name is not enabled focus goes to the curviness
+    // field instead ev 2/18/08
+    if (ShapeList.isDefaultShapeName(shape.name)) {
+      name.setEnabled(false)
+      curviness.requestFocus()
+    } else {
+      name.setEnabled(true)
+      name.requestFocus()
+    }
+
+    setVisible(true)
+  }
+
+  def update(originalShape: Shape, newShape: Shape): Unit = {
     shape.directionIndicator = newShape.asInstanceOf[VectorShape]
   }
 
   def exists(name: String): Boolean =
     false
-
-  setResizable(false)
-  setLocation(parent.getX + 10, parent.getY + 10)
-  setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
-
-  addWindowListener(new WindowAdapter {
-    override def windowClosing(e: WindowEvent) {
-      saveShape()
-      dispose()
-      setVisible(false)
-    }
-  })
-
-  Utils.addEscKeyAction(this, new AbstractAction {
-    def actionPerformed(e: ActionEvent) {
-      if (originalShape.toString != getCurrentShape.toString ||
-          new OptionPane(LinkEditorDialog.this, I18N.gui.get("tools.shapesEditor.confirmCancel"),
-                         I18N.gui.get("tools.shapesEditor.confirmCancel.message"), OptionPane.Options.YES_NO,
-                         OptionPane.Icons.QUESTION).getSelectedIndex != 0)
-        return
-
-      dispose()
-    }
-  })
-
-  setLayout(new GridBagLayout)
-
-  getContentPane.setBackground(InterfaceColors.DIALOG_BACKGROUND)
-
-  locally {
-    val c = new GridBagConstraints
-
-    c.gridx = 0
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.insets = new Insets(6, 6, 6, 6)
-
-    add(new LabeledComponent(I18N.gui("name"), name) {
-      setForeground(InterfaceColors.DIALOG_TEXT)
-    }, c)
-
-    c.insets = new Insets(0, 6, 6, 6)
-
-    add(new LabeledComponent(I18N.gui("direction"), new Button(I18N.gui("edit"), () => {
-      new EditorDialog(LinkEditorDialog.this, LinkEditorDialog.this, shape.directionIndicator, false)
-    })) {
-      setForeground(InterfaceColors.DIALOG_TEXT)
-    }, c)
-
-    add(new LabeledComponent(I18N.gui("curviness"), curviness) {
-      setForeground(InterfaceColors.DIALOG_TEXT)
-    }, c)
-
-    add(new LabeledComponent(I18N.gui("leftLine"), dashes(2)) {
-      setForeground(InterfaceColors.DIALOG_TEXT)
-    }, c)
-
-    add(new LabeledComponent(I18N.gui("middleLine"), dashes(1)) {
-      setForeground(InterfaceColors.DIALOG_TEXT)
-    }, c)
-
-    add(new LabeledComponent(I18N.gui("rightLine"), dashes(0)) {
-      setForeground(InterfaceColors.DIALOG_TEXT)
-    }, c)
-
-    val cancel = new Button(I18N.gui.get("common.buttons.cancel"), dispose)
-    val done = new Button(I18N.gui.get("common.buttons.ok"), saveShape)
-
-    add(new ButtonPanel(Array(done, cancel)), c)
-
-    getRootPane.setDefaultButton(done)
-  }
-
-  list.update()
-
-  pack()
-  
-  // when name is not enabled focus goes to the curviness
-  // field instead ev 2/18/08
-  if (ShapeList.isDefaultShapeName(shape.name)) {
-    name.setEnabled(false)
-    curviness.requestFocus()
-  }
-
-  else {
-    name.setEnabled(true)
-    name.requestFocus()
-  }
-
-  setVisible(true)
 
   private def saveShape() {
     val nameStr = name.getText.trim.toLowerCase
@@ -141,7 +143,7 @@ class LinkEditorDialog(parent: JDialog, list: DrawableList[LinkShape], shape: Li
         new OptionPane(this, I18N.gui("confirmOverwrite"), I18N.gui("nameConflict"), OptionPane.Options.YES_NO,
                        OptionPane.Icons.QUESTION).getSelectedIndex != 0)
       return
-    
+
     shape.name = nameStr
 
     Try(curviness.getText.toDouble) match {
@@ -149,7 +151,7 @@ class LinkEditorDialog(parent: JDialog, list: DrawableList[LinkShape], shape: Li
       case Failure(_) =>
         new OptionPane(this, I18N.gui("invalid"), I18N.gui("invalidCurviness"), OptionPane.Options.OK,
                        OptionPane.Icons.ERROR)
-        
+
         return
     }
 
