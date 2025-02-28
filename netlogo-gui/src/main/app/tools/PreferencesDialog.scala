bad3368ed3cd3577cd6d53bea96bb574b7db8d93
ERSUCC <isaacbrown5763@gmail.com>
2025-02-28 13:19:27 -0600

    Improved logging path checking

--- netlogo-gui/src/main/app/tools/PreferencesDialog.scala
+++ netlogo-gui/src/main/app/tools/PreferencesDialog.scala
@@ -4,6 +4,7 @@ package org.nlogo.app.tools
 
 import java.awt.{ BorderLayout, Frame }
 import java.io.File
+import java.nio.file.Files
 import java.util.prefs.{ Preferences => JavaPreferences }
 import javax.swing.{ BorderFactory, SwingConstants }
 import javax.swing.border.EmptyBorder
@@ -45,12 +46,22 @@ class PreferencesDialog(parent: Frame, preferences: Seq[Preference])
       val path = preferences.find(x => x.i18nKey == "logDirectory").get.
                  asInstanceOf[Preferences.LogDirectory].textField.getText
       val file = new File(path)
-      if (path.nonEmpty && !file.exists) {
+      if (path.isEmpty) {
+        new OptionPane(this, I18N.gui.get("common.messages.error"), I18N.gui.get("tools.preferences.emptyDirectory"),
+                       OptionPane.Options.Ok, OptionPane.Icons.Error)
+        return false
+      }
+      if (!file.exists) {
         if (new OptionPane(this, I18N.gui.get("common.messages.warning"),
                            I18N.gui.get("tools.preferences.missingDirectory"), OptionPane.Options.YesNo,
                            OptionPane.Icons.Warning).getSelectedIndex != 0)
           return false
         file.mkdirs
+      }
+      if (!Files.isWritable(file.toPath)) {
+        new OptionPane(this, I18N.gui.get("common.messages.error"), I18N.gui.get("tools.preferences.badPermissions"),
+                       OptionPane.Options.Ok, OptionPane.Icons.Error)
+        return false
       }
     }
     try {
