NetLogo/NetLogo

commit 43d6cfab5d5f28f991b98ce44302403793e391cc

Feature/Major: Add announcement banner

netlogo-core/src/main/api/InfoDownloader.scala
@@ -1,0 +1,59 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.api
+
+import java.io.{ File, IOException }
+import java.lang.Boolean
+import java.net.{ HttpURLConnection, URL }
+import java.nio.file.{ Files, Paths, StandardCopyOption }
+import java.security.{ DigestInputStream, MessageDigest }
+import java.util.Arrays
+import java.util.prefs.{ Preferences => JPreferences }
+
+trait InfoDownloader {
+
+  private val prefs = JPreferences.userNodeForPackage(getClass)
+
+  def prefsKey: String
+
+  def enabled: Boolean =
+    !Boolean.getBoolean(s"netlogo.$prefsKey.disabled")
+
+  /** Downloads the URL and update the GUI if the hash is different */
+  def apply(url: URL, callback: File => Unit = ((_: File) => ())): Unit = {
+    if (enabled) {
+      Exceptions.ignoring(classOf[IOException]) {
+
+        val md   = MessageDigest.getInstance("MD5")
+        val conn = url.openConnection.asInstanceOf[HttpURLConnection]
+
+        if (conn.getResponseCode == 200) {
+          val response = new DigestInputStream(conn.getInputStream, md)
+          Files.copy(response, Paths.get(FileIO.perUserFile(urlToHash(url))), StandardCopyOption.REPLACE_EXISTING)
+        }
+
+        val localHash = prefs.getByteArray(urlToFullHash(url), null)
+        val newHash   = md.digest
+
+        if (true || !Arrays.equals(localHash, newHash)) {
+          prefs.putByteArray(urlToFullHash(url), newHash)
+          callback(new File(FileIO.perUserFile(urlToHash(url))))
+        }
+
+      }
+    }
+  }
+
+  /** Ensures the next reload updates the GUI */
+  def invalidateCache(url: URL): Unit =
+    prefs.put(urlToFullHash(url), "")
+
+  def urlToHash(url: URL): String = {
+    val noTrailingSlash = url.toString.stripSuffix("/")
+    noTrailingSlash.substring(noTrailingSlash.lastIndexOf('/') + 1)
+  }
+
+  private def urlToFullHash(url: URL): String =
+    s"${urlToHash(url)}-md5"
+
+}

netlogo-core/src/main/api/AnnouncementsInfoDownloader.scala
@@ -1,0 +1,81 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.api
+
+import java.io.File
+import java.net.URL
+import java.time.LocalDate
+
+import scala.io.Source
+
+import org.json.simple.{ JSONObject, JSONArray }
+import org.json.simple.parser.JSONParser
+import org.json.simple.parser.ParseException
+
+object AnnouncementsInfoDownloader extends InfoDownloader {
+
+  override val prefsKey = "announcements"
+
+  val defaultURL = new URL("https://ccl.northwestern.edu/netlogo/announce.json")
+
+  def fetchAndThen(f: (Seq[Announcement]) => Unit): Unit = {
+    this.apply(defaultURL, parse _ andThen f)
+  }
+
+  def parse(file: File): Seq[Announcement] = {
+
+    import scala.collection.JavaConverters.asScalaIteratorConverter
+
+    val contents = Source.fromFile(file).mkString
+
+    val json = new JSONParser()
+
+    try {
+
+      val arr  = json.parse(contents).asInstanceOf[JSONArray]
+      val iter = arr.iterator()
+
+      val announcements =
+        iter.asScala.toVector.map {
+          case obj: JSONObject =>
+
+            val id       = obj.get(      "id").asInstanceOf[Number].intValue
+            val title    = obj.get(   "title").asInstanceOf[String]
+            val dateStr  = obj.get(    "date").asInstanceOf[String]
+            val lifespan = obj.get("lifespan").asInstanceOf[Number].longValue
+            val typeStr  = obj.get(    "type").asInstanceOf[String]
+            val summary  = obj.get( "summary").asInstanceOf[String]
+            val desc     = obj.get(    "desc").asInstanceOf[String]
+
+            val Array(months, days, years) = dateStr.split("/")
+            val date                       = LocalDate.of(2000 + years.toInt, months.toInt, days.toInt)
+
+            val annType =
+              typeStr match {
+                case "release"  => Release
+                case "event"    => Event
+                case "advisory" => Advisory
+                case _          => throw new Exception(s"Unknown announcement type: ${typeStr}")
+              }
+
+            val endDate =
+              if (lifespan == 0)
+                None
+              else
+                Option(date.plusDays(lifespan))
+
+            Announcement(id, title, date, endDate, annType, summary, desc)
+
+        }
+
+        announcements.filter((x) => x.endDate.map(_.isAfter(LocalDate.now())).getOrElse(true))
+
+    } catch {
+      case ex: ParseException =>
+        println(ex)
+        Seq.empty
+    }
+
+  }
+
+}

netlogo-core/src/main/api/LibraryInfoDownloader.scala
@@ -2,55 +2,6 @@
 
 package org.nlogo.api
 
-import java.io.{ File, IOException }
-import java.lang.Boolean
-import java.net.{ HttpURLConnection, URL }
-import java.nio.file.{ Files, Paths, StandardCopyOption }
-import java.security.{ DigestInputStream, MessageDigest }
-import java.util.Arrays
-import java.util.prefs.{ Preferences => JPreferences }
-
-object LibraryInfoDownloader {
-
-  private val prefs = JPreferences.userNodeForPackage(getClass)
-
-  def enabled: Boolean =
-    !Boolean.getBoolean("netlogo.libraries.disabled")
-
-  /** Downloads the URL and update the GUI if the hash is different */
-  def apply(url: URL, callback: File => Unit = ((_: File) => ())): Unit =
-    if (enabled) {
-      Exceptions.ignoring(classOf[IOException]) {
-
-        val md   = MessageDigest.getInstance("MD5")
-        val conn = url.openConnection.asInstanceOf[HttpURLConnection]
-
-        if (conn.getResponseCode == 200) {
-          val response = new DigestInputStream(conn.getInputStream, md)
-          Files.copy(response, Paths.get(FileIO.perUserFile(urlToHash(url))), StandardCopyOption.REPLACE_EXISTING)
-        }
-
-        val localHash = prefs.getByteArray(urlToFullHash(url), null)
-        val newHash   = md.digest
-
-        if (!Arrays.equals(localHash, newHash)) {
-          prefs.putByteArray(urlToFullHash(url), newHash)
-          callback(new File(FileIO.perUserFile(urlToHash(url))))
-        }
-
-      }
-    }
-
-  /** Ensures the next reload updates the GUI */
-  def invalidateCache(url: URL): Unit =
-    prefs.put(urlToFullHash(url), "")
-
-  def urlToHash(url: URL): String = {
-    val noTrailingSlash = url.toString.stripSuffix("/")
-    noTrailingSlash.substring(noTrailingSlash.lastIndexOf('/') + 1)
-  }
-
-  private def urlToFullHash(url: URL): String =
-    s"${urlToHash(url)}-md5"
-
+object LibraryInfoDownloader extends InfoDownloader {
+  override val prefsKey = "libraries"
 }

netlogo-gui/resources/i18n/GUI_Strings_en.properties
@@ -142,6 +142,7 @@ menu.help = Help
 menu.help.lookUpInDictionary = Look Up In Dictionary
 menu.help.netLogoUserManual = NetLogo User Manual
 menu.help.netLogoDictionary = NetLogo Dictionary
+menu.help.news = Latest NetLogo News
 menu.help.bind = Beginner's Interactive NetLogo Dictionary
 menu.help.netLogoUsersGroup = NetLogo Users Group
 menu.help.netLogoStackOverflow = NetLogo Q & A - Stack Overflow

netlogo-gui/src/main/api/Announcements.scala
@@ -1,0 +1,12 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.api
+
+import java.time.LocalDate
+
+sealed trait AnnouncementType
+case object Release  extends AnnouncementType
+case object Advisory extends AnnouncementType
+case object Event    extends AnnouncementType
+
+case class Announcement(id: Int, title: String, date: LocalDate, endDate: Option[LocalDate], annType: AnnouncementType, summary: String, desc: String)

netlogo-gui/src/main/app/App.scala
@@ -548,7 +548,10 @@ class App extends
         _tabManager.switchWindow(true)
       }
 
+      AnnouncementsInfoDownloader.fetchAndThen(_tabManager.interfaceTab.appendAnnouncements)
+
       syncWindowThemes()
+
     }
     catch {
       case ex: java.lang.Throwable =>

netlogo-gui/src/main/app/HelpActions.scala
@@ -52,6 +52,8 @@ object HelpActions {
       docPath("index.html")),
     new LocalBrowseAction(I18N.gui.get("menu.help.netLogoDictionary"),
       docPath("index2.html")),
+    new RemoteBrowseAction(I18N.gui.get("menu.help.news"),
+      new URI("https://ccl.northwestern.edu/netlogo/announce-test.shtml")),
     new RemoteBrowseAction(I18N.gui.get("menu.help.bind"),
       new URI("https://ccl.northwestern.edu/netlogo/bind")),
     new RemoteBrowseAction(I18N.gui.get("menu.help.netLogoUsersGroup"),

commit 686d6f1b1ed8387781c4ac349e1d4accc9f25c1b

Bugfix: Avoid unnecessary rewrites of metadata files
This fixes a bug that I accidentally introduced in commit
43d6cfab5d5f28f991b98ce44302403793e391cc .  I accidentally checked in
some debugging/prototyping code for the announcement banner.

My apologies to all of my fans who were disappointed by this mistake.

netlogo-core/src/main/api/InfoDownloader.scala
@@ -35,7 +35,7 @@ trait InfoDownloader {
         val localHash = prefs.getByteArray(urlToFullHash(url), null)
         val newHash   = md.digest
 
-        if (true || !Arrays.equals(localHash, newHash)) {
+        if (!Arrays.equals(localHash, newHash)) {
           prefs.putByteArray(urlToFullHash(url), newHash)
           callback(new File(FileIO.perUserFile(urlToHash(url))))
         }