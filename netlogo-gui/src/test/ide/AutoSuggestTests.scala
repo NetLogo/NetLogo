59abed085239453b9e756a5e2aac5e9a134429c1
ERSUCC <isaacbrown5763@gmail.com>
2025-05-01 19:07:32 -0500

    Added more AutoSuggest tests

--- netlogo-gui/src/test/ide/AutoSuggestTests.scala
+++ netlogo-gui/src/test/ide/AutoSuggestTests.scala
@@ -2,12 +2,37 @@
 
 package org.nlogo.ide
 
+import org.nlogo.core.NetLogoCore
+
 import org.scalatest.funsuite.AnyFunSuite
 
 class AutoSuggestTests extends AnyFunSuite {
-  val autoSuggest = new AutoSuggest(Set.empty[String], () => Set.empty[String])
-  test("empty"){
-    val testList = Seq()
-    assertResult(testList)(autoSuggest.getSuggestions("zzzzz"))
+  private val autoSuggest = AutoSuggest(NetLogoCore, None)
+
+  test("invalid string") {
+    assertResult(Seq())(autoSuggest.getSuggestions("zzzzz"))
+  }
+
+  test("partially completed unique command") {
+    assertResult(Seq("print"))(autoSuggest.getSuggestions("pri"))
+  }
+
+  test("completed unique command") {
+    assertResult(Seq("print"))(autoSuggest.getSuggestions("print"))
+  }
+
+  test("completed prefix command") {
+    assertResult(Seq("ask", "ask-concurrent"))(autoSuggest.getSuggestions("ask"))
+  }
+
+  test("valid suffix") {
+    assertResult(Seq())(autoSuggest.getSuggestions("concurrent"))
+  }
+
+  test("empty string") {
+    val allTokens = (NetLogoCore.tokenMapper.allCommandNames ++ NetLogoCore.tokenMapper.allReporterNames)
+                      .toSeq.map(_.toLowerCase).sorted
+
+    assertResult(allTokens)(autoSuggest.getSuggestions(""))
   }
 }
