5bca3e22aa879b4111620ead488d62d0149eb993
Kritphong Mongkhonvanit <kritphong@u.northwestern.edu>
2025-05-02 00:20:38 -0500

    Extend the parser to support the define-library syntax
    This doesn't actually do anything useful yet.

--- parser-core/src/main/core/StructureDeclarations.scala
+++ parser-core/src/main/core/StructureDeclarations.scala
@@ -31,6 +31,12 @@ object StructureDeclarations {
 
   case class Identifier(name: String, token: Token)
 
+  case class DefineLibrary(name: String, version: String, exportSpecs: Seq[ExportSpec], token: Token)
+      extends Declaration
+  sealed trait ExportSpec
+  case class SimpleExport(name: String)
+      extends ExportSpec
+
   case class Library(name: String, options: Seq[LibraryOption], token: Token)
       extends Declaration {
 
