2a4c73f45443498509eb03dbd8b82af571f582f8
Kritphong Mongkhonvanit <kritphong@u.northwestern.edu>
2025-05-23 00:50:34 -0500

    Rename the "library" syntax to "import"
    Also exclude globals, breeds, and breed variables from being imported.

--- parser-core/src/main/core/Keywords.scala
+++ parser-core/src/main/core/Keywords.scala
@@ -11,7 +11,7 @@ object Keywords {
     "TO", "TO-REPORT", "END",
     "GLOBALS", "TURTLES-OWN", "LINKS-OWN", "PATCHES-OWN",
     "DIRECTED-LINK-BREED", "UNDIRECTED-LINK-BREED",
-    "EXTENSIONS", "DEFINE-LIBRARY", "LIBRARY", "__INCLUDES")
+    "EXTENSIONS", "DEFINE-LIBRARY", "IMPORT", "__INCLUDES")
   def isKeyword(s: String) =
     keywords.contains(s.toUpperCase(Locale.ENGLISH)) || s.toUpperCase(Locale.ENGLISH).endsWith("-OWN")
 }
