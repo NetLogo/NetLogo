c9ced4f838fefff5558abc3042f1c33274473fd0
ERSUCC <isaacbrown5763@gmail.com>
2025-06-29 00:28:35 -0500

    Switch to unified LabProtocol

--- netlogo-core/src/main/api/LabDefaultValues.scala
+++ netlogo-core/src/main/api/LabDefaultValues.scala
@@ -1,7 +1,6 @@
 // (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
 
 package org.nlogo.api
-import scala.math.floor
 
 object LabDefaultValues {
   def getDefaultName: String = "experiment"
@@ -19,9 +18,8 @@ object LabDefaultValues {
   def getDefaultMetrics: List[String] = List("count turtles")
   def getDefaultConstants: List[RefValueSet] = Nil
   def getDefaultSubExperiments: List[List[RefValueSet]] = Nil
-  val RATIO = 0.75
   // Determines the number of threads in BehaviorSpace if the user has not specified a value
-  def getDefaultThreads: Int = { floor(Runtime.getRuntime.availableProcessors * RATIO).toInt }
+  def getDefaultThreads: Int = (Runtime.getRuntime.availableProcessors * 0.75).floor.toInt
   def getRecommendedMaxThreads: Int = Runtime.getRuntime.availableProcessors
   def getDefaultTable: String = ""
   def getDefaultSpreadsheet: String = ""
--- netlogo-core/src/main/api/LabProtocol.scala
+++ netlogo-core/src/main/api/LabProtocol.scala
@@ -2,24 +2,74 @@
 
 package org.nlogo.api
 
-case class LabProtocol(name: String = LabDefaultValues.getDefaultName,
-                    preExperimentCommands: String = LabDefaultValues.getDefaultPreExperimentCommands,
-                    setupCommands: String = LabDefaultValues.getDefaultSetupCommands,
-                    goCommands: String = LabDefaultValues.getDefaultGoCommands,
-                    postRunCommands: String = LabDefaultValues.getDefaultPostRunCommands,
-                    postExperimentCommands: String = LabDefaultValues.getDefaultPostExperimentCommands,
-                    repetitions: Int = LabDefaultValues.getDefaultRepetitions,
-                    sequentialRunOrder: Boolean = LabDefaultValues.getDefaultSequentialRunOrder,
-                    runMetricsEveryStep: Boolean = LabDefaultValues.getDefaultRunMetricsEveryStep,
-                    runMetricsCondition: String = LabDefaultValues.getDefaultRunMetricsCondition,
-                    timeLimit: Int = LabDefaultValues.getDefaultTimeLimit,
-                    exitCondition: String = LabDefaultValues.getDefaultExitCondition,
-                    metrics: List[String] = LabDefaultValues.getDefaultMetrics,
-                    constants: List[RefValueSet] = LabDefaultValues.getDefaultConstants,
-                    subExperiments: List[List[RefValueSet]] = LabDefaultValues.getDefaultSubExperiments,
-                    runsCompleted: Int = 0,
-                    runOptions: LabRunOptions = new LabRunOptions)
-{
+object LabProtocol {
+  def defaultGUIProtocol: LabProtocol = {
+    new LabProtocol(
+      LabDefaultValues.getDefaultName,
+      LabDefaultValues.getDefaultPreExperimentCommands,
+      LabDefaultValues.getDefaultSetupCommands,
+      LabDefaultValues.getDefaultGoCommands,
+      LabDefaultValues.getDefaultPostRunCommands,
+      LabDefaultValues.getDefaultPostExperimentCommands,
+      LabDefaultValues.getDefaultRepetitions,
+      LabDefaultValues.getDefaultSequentialRunOrder,
+      LabDefaultValues.getDefaultRunMetricsEveryStep,
+      LabDefaultValues.getDefaultRunMetricsCondition,
+      LabDefaultValues.getDefaultTimeLimit,
+      LabDefaultValues.getDefaultExitCondition,
+      LabDefaultValues.getDefaultMetrics,
+      LabDefaultValues.getDefaultConstants,
+      LabDefaultValues.getDefaultSubExperiments
+    )
+  }
+
+  def defaultCodeProtocol(name: String): LabProtocol = {
+    new LabProtocol(
+      name,
+      "",
+      "",
+      "",
+      "",
+      "",
+      LabDefaultValues.getDefaultRepetitions,
+      LabDefaultValues.getDefaultSequentialRunOrder,
+      LabDefaultValues.getDefaultRunMetricsEveryStep,
+      "",
+      LabDefaultValues.getDefaultTimeLimit,
+      "",
+      Nil,
+      Nil,
+      Nil
+    )
+  }
+}
+
+class LabProtocol(
+  var name: String,
+  var preExperimentCommands: String,
+  var setupCommands: String,
+  var goCommands: String,
+  var postRunCommands: String,
+  var postExperimentCommands: String,
+  var repetitions: Int,
+  var sequentialRunOrder: Boolean,
+  var runMetricsEveryStep: Boolean,
+  var runMetricsCondition: String,
+  var timeLimit: Int,
+  var exitCondition: String,
+  var metrics: List[String],
+  var constants: List[RefValueSet],
+  var subExperiments: List[List[RefValueSet]],
+  var threadCount: Int = LabDefaultValues.getDefaultThreads,
+  var table: String = LabDefaultValues.getDefaultTable,
+  var spreadsheet: String = LabDefaultValues.getDefaultSpreadsheet,
+  var stats: String = LabDefaultValues.getDefaultStats,
+  var lists: String = LabDefaultValues.getDefaultLists,
+  var updateView: Boolean = LabDefaultValues.getDefaultUpdateView,
+  var updatePlotsAndMonitors: Boolean = LabDefaultValues.getDefaultUpdatePlotsAndMonitors,
+  var mirrorHeadlessOutput: Boolean = LabDefaultValues.getDefaultMirrorHeadlessOutput,
+  var runsCompleted: Int = 0
+) {
   val valueSets =
     if (subExperiments.isEmpty)
       List(constants)
@@ -66,5 +116,59 @@ case class LabProtocol(name: String = LabDefaultValues.getDefaultName,
     else {
       Iterator.fill(repetitions)(valueSets.map(x => combinations(x.reverse)).flatten).flatten
     }
+  }
+
+  def copy(
+    name: String = name,
+    preExperimentCommands: String = preExperimentCommands,
+    setupCommands: String = setupCommands,
+    goCommands: String = goCommands,
+    postRunCommands: String = postRunCommands,
+    postExperimentCommands: String = postExperimentCommands,
+    repetitions: Int = repetitions,
+    sequentialRunOrder: Boolean = sequentialRunOrder,
+    runMetricsEveryStep: Boolean = runMetricsEveryStep,
+    runMetricsCondition: String = runMetricsCondition,
+    timeLimit: Int = timeLimit,
+    exitCondition: String = exitCondition,
+    metrics: List[String] = metrics,
+    constants: List[RefValueSet] = constants,
+    subExperiments: List[List[RefValueSet]] = subExperiments,
+    threadCount: Int = threadCount,
+    table: String = table,
+    spreadsheet: String = spreadsheet,
+    stats: String = stats,
+    lists: String = lists,
+    updateView: Boolean = updateView,
+    updatePlotsAndMonitors: Boolean = updatePlotsAndMonitors,
+    mirrorHeadlessOutput: Boolean = mirrorHeadlessOutput,
+    runsCompleted: Int = runsCompleted
+  ): LabProtocol = {
+    new LabProtocol(
+      name,
+      preExperimentCommands,
+      setupCommands,
+      goCommands,
+      postRunCommands,
+      postExperimentCommands,
+      repetitions,
+      sequentialRunOrder,
+      runMetricsEveryStep,
+      runMetricsCondition,
+      timeLimit,
+      exitCondition,
+      metrics,
+      constants,
+      subExperiments,
+      threadCount,
+      table,
+      spreadsheet,
+      stats,
+      lists,
+      updateView,
+      updatePlotsAndMonitors,
+      mirrorHeadlessOutput,
+      runsCompleted
+    )
   }
 }
