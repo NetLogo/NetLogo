67fc269d8434695a46341960b84f762b1a21f80f
ERSUCC <isaacbrown5763@gmail.com>
2025-07-02 01:51:40 -0500

    Add debug wrapper method for analytics

--- netlogo-gui/src/main/analytics/Analytics.scala
+++ netlogo-gui/src/main/analytics/Analytics.scala
@@ -6,14 +6,25 @@ import java.net.URI
 
 import org.matomo.java.tracking.{ MatomoRequest, MatomoTracker, TrackerConfiguration }
 
+import org.nlogo.core.NetLogoPreferences
+
 object Analytics {
-  private val config = TrackerConfiguration.builder.apiEndpoint(URI.create("http://129.105.119.160:8080/matomo.php"))
+  private val config = TrackerConfiguration.builder.apiEndpoint(URI.create("https://ccl.northwestern.edu:8080/matomo.php"))
                                                    .defaultSiteId(1)
                                                    .build()
 
   private val tracker = new MatomoTracker(config)
 
+  private def wrapRequest(request: MatomoRequest): Unit = {
+    if (NetLogoPreferences.getBoolean("sendAnalytics", true)) {
+      tracker.sendBulkRequestAsync(request).handle((_, error) => {
+        if (error != null)
+          println(error)
+      })
+    }
+  }
+
   def appStart(): Unit = {
-    tracker.sendBulkRequestAsync(MatomoRequest.request.actionName("App Start").build())
+    wrapRequest(MatomoRequest.request.actionName("App Start").build())
   }
 }
