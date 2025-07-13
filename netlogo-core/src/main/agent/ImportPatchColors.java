801ce01046a2fc5f576e54ce547b3082473356f3
ERSUCC <isaacbrown5763@gmail.com>
2025-07-13 14:12:01 -0500

    Close underlying input stream after reading patch colors from file
    Fixes #1454

--- netlogo-core/src/main/agent/ImportPatchColors.java
+++ netlogo-core/src/main/agent/ImportPatchColors.java
@@ -13,6 +13,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.DataBuffer;
 import java.awt.image.SampleModel;
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +23,9 @@ public class ImportPatchColors {
       throws java.io.IOException {
 
     String filename = imageFile.getAbsolutePath();
-    BufferedImage image = ImageIO.read(imageFile.getInputStream());
+    FileInputStream stream = imageFile.getInputStream();
+    BufferedImage image = ImageIO.read(stream);
+    stream.close();
     // sometime we have to throw the exception ourselves,
     // because we just get back null. Booo.  -CLB
     if (image == null) {
