e15fd61ef33fbd4336563305e33975f9a9c592a8
ERSUCC <isaacbrown5763@gmail.com>
2025-07-13 21:09:29 -0500

    Exclude Bug Hunt Camouflage from checksum tests

--- netlogo-gui/src/main/headless/ChecksumsAndPreviewsSettings.scala
+++ netlogo-gui/src/main/headless/ChecksumsAndPreviewsSettings.scala
@@ -64,7 +64,8 @@ object ChecksumsAndPreviewsSettings {
     , Some("it uses the import-a extension") -> List(
       "/models/Code Examples/Image Resource Example.nlogox",
       "/models/Sample Models/Biology/Evolution/Mammoths.nlogox",
-      "/models/Sample Models/Computer Science/Robotic Factory.nlogox"
+      "/models/Sample Models/Computer Science/Robotic Factory.nlogox",
+      "/models/Sample Models/Biology/Evolution/Bug Hunt Camouflage.nlogox"
     )
 
   )
