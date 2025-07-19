ee65ad4fc22f255890b6d88745b89fbc8c88a29d
ERSUCC <isaacbrown5763@gmail.com>
2025-07-19 03:22:40 -0500

    Fix annoying annotation about plot 2

--- netlogo-core/src/main/api/ImporterUser.scala
+++ netlogo-core/src/main/api/ImporterUser.scala
@@ -7,7 +7,7 @@ import java.util.{ List => JList }
 trait ImporterUser extends WorldResizer {
   def setOutputAreaContents(text: String): Unit
   def currentPlot(plot: String): Unit  // for importing plots
-  def getPlot(plot: String): PlotInterface
+  def maybeGetPlot(plot: String): Option[PlotInterface]
   def isExtensionName(name: String): Boolean  // for importing extensions
   @throws(classOf[ExtensionException])
   def importExtensionData(name: String, data: JList[Array[String]], handler: ImportErrorHandler): Unit
