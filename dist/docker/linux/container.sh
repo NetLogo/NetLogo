deef86d08867476b6e88a0fa45b763b2911e372d
ERSUCC <isaacbrown5763@gmail.com>
2025-07-26 13:03:57 -0500

    Get Docker packaging for Linux working on Windows

--- dist/docker/linux/container.sh
+++ dist/docker/linux/container.sh
@@ -7,7 +7,7 @@ cd /root/
 # 32-bit `jpackage` support
 dpkg --add-architecture i386
 apt update
-apt install -y libc6:i386 libncurses5:i386 libstdc++6:i386 zlib1g-dev:i386
+apt install -y libc6:i386 libncurses6:i386 libstdc++6:i386 zlib1g-dev:i386
 
 # Install general tools
 apt install -y curl git wget
@@ -23,63 +23,48 @@ apt install -y sbt
 apt install -y aspell
 
 # Add Python and numpy for LevelSpace extension
-apt install -y python3 python3-pip
-pip3 install numpy
+apt install -y python3 python3-numpy
 
 # Patched `wkhtmltopdf` for NL release docs
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-apt install -y /root/wkhtmlto*
-rm /root/wkhtmlto*
+apt install -y wkhtmltopdf
+rm wkhtmltox_0.12.6-1.focal_amd64.deb
 
 # NPM installation for NL color picker
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 . /root/.nvm/nvm.sh
 nvm install 20.16
 
-mkdir /root/jvm-store/
+mkdir jvm-store
 
 # Liberica 32-bit JDK for 32-bit release
 wget https://download.bell-sw.com/java/17.0.15+10/bellsoft-jdk17.0.15+10-linux-i586.tar.gz
 gunzip bellsoft*
 tar -xf bellsoft*
 rm bellsoft*
-mv jdk-17.0.15 /root/jvm-store/liberica-jdk-17.0.15/
+mv jdk-17.0.15 jvm-store/liberica-jdk-17.0.15
 
 # OpenJDK 64-bit JDK for primary JDK and 64-bit release
 wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
 gunzip openjdk*
 tar -xf openjdk*
 rm openjdk*
-mv jdk-17.0.2 /root/jvm-store/openjdk-17.0.2/
+mv jdk-17.0.2 jvm-store/openjdk-17.0.2
 
 # Configure Java for general NL build
 export JAVA_HOME=/root/jvm-store/openjdk-17.0.2/
+export PATH=$PATH:/root/jvm-store/openjdk-17.0.2/bin/
 
 # Initialize NetLogo repo
-git clone https://github.com/NetLogo/NetLogo
+git clone https://github.com/NetLogo/NetLogo --recurse-submodules
 cd NetLogo
-git submodule update --init --recursive
 
 # Set up Mathematica-Link
-mv /root/JLink.jar /root/NetLogo/Mathematica-Link/
+mv /JLink.jar Mathematica-Link/
 
-# Create JDK conf file for NL release
-cat > /root/NetLogo/.jdks.yaml << EOF
-- vendor:       "Liberica"
-  version:      "17.0.15"
-  architecture: "32"
-  path:         "/root/jvm-store/liberica-jdk-17.0.15/"
+# Set up JDK locations
+mv /.jdks.yaml .
 
-- vendor:       "OpenJDK"
-  version:      "17.0.2"
-  architecture: "64"
-  path:         "/root/jvm-store/openjdk-17.0.2/"
-EOF
-
-/root/NetLogo/sbt dist/buildNetLogo
-
-/root/NetLogo/sbt "dist/packageLinuxAggregate OpenJDK_17.0.2_64"
-mv /root/NetLogo/dist/target/downloadPages/*.tgz /root/NL-Linux-64.tgz
-
-/root/NetLogo/sbt "dist/packageLinuxAggregate Liberica_17.0.15_32"
-mv /root/NetLogo/dist/target/downloadPages/*.tgz /root/NL-Linux-32.tgz
+sbt dist/buildNetLogo
+sbt "dist/packageLinuxAggregate OpenJDK_17.0.2_64"
+sbt "dist/packageLinuxAggregate Liberica_17.0.15_32"
