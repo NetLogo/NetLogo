52d75d13a4fdd64014f9b0ea26329d50eab10fda
ERSUCC <isaacbrown5763@gmail.com>
2025-08-24 04:12:00 -0500

    Get rid of PicoContainer
    It overcomplicates app initialization, leads to confusing runtime errors, and prevents multiple models from being open at the same time

--- netlogo-gui/src/main/hubnet/client/App.scala
+++ netlogo-gui/src/main/hubnet/client/App.scala
@@ -4,7 +4,7 @@ package org.nlogo.hubnet.client
 
 import org.nlogo.core.Femto
 import org.nlogo.api.{ DummyExtensionManager, NetLogoThreeDDialect, NetLogoLegacyDialect, Version }
-import org.nlogo.util.NullAppHandler
+import org.nlogo.util.AppHandler
 import org.nlogo.nvm.{ DefaultCompilerServices, PresentationCompilerInterface }
 import org.nlogo.workspace.AbstractWorkspace
 import org.nlogo.window.VMCheck
@@ -14,12 +14,12 @@ import org.nlogo.window.VMCheck
  */
 object App {
   def main(args: Array[String]): Unit = {
-    mainWithAppHandler(args, NullAppHandler)
+    mainWithAppHandler(args, new AppHandler)
   }
 
-  def mainWithAppHandler(args: Array[String], handler: Object): Unit = {
-    handler.getClass.getDeclaredMethod("init").invoke(handler)
-    handler.getClass.getDeclaredMethod("ready", classOf[AnyRef]).invoke(handler, this)
+  def mainWithAppHandler(args: Array[String], handler: AppHandler): Unit = {
+    handler.init()
+    handler.ready(this)
     AbstractWorkspace.isApp(true)
     VMCheck.detectBadJVMs()
     val compiler = new DefaultCompilerServices(
--- netlogo-gui/src/main/util/NullAppHandler.scala
+++ netlogo-gui/src/main/util/NullAppHandler.scala
@@ -5,7 +5,7 @@ package org.nlogo.util
 // this is a dummy object used in App and hubnet client App as
 // a stub alternative to the OS-Specific application handler
 // passed in by the mac launcher
-object NullAppHandler {
+class AppHandler {
   def init(): Unit = {}
-  def ready(app: Object): Unit = {}
+  def ready(app: AnyRef): Unit = {}
 }
