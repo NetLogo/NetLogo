5828f5092cef7a1ea7f1df5c0924bb0cd10b259a
ERSUCC <isaacbrown5763@gmail.com>
2025-09-09 17:45:34 -0500

    Update code and GitHub actions workflows for Java 21

--- netlogo-gui/src/main/gl/render/LinkRenderer3D.scala
+++ netlogo-gui/src/main/gl/render/LinkRenderer3D.scala
@@ -4,9 +4,7 @@ package org.nlogo.gl.render
 
 import org.nlogo.api.{ Agent, Link, Link3D, World }
 
-private class LinkRenderer3D(world: World, shapeRenderer: ShapeRenderer)
-extends LinkRenderer(world, shapeRenderer) {
-
+class LinkRenderer3D(world: World, shapeRenderer: ShapeRenderer) extends LinkRenderer(world, shapeRenderer) {
   override def getOrientation(agent: Agent): Array[Double] = {
     val link = agent.asInstanceOf[Link3D]
     Array(link.heading, link.pitch, 0d)
