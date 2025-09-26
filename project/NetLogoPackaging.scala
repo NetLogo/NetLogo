3b27cead91b05eca7bc778db3e07ac4f58653a75
ERSUCC <isaacbrown5763@gmail.com>
2025-09-26 17:03:14 -0500

    Unify Mac build process in SBT and packaged app
    Also fixes a bug where Cmd+Q bypassed NetLogo's quit method

--- project/NetLogoPackaging.scala
+++ project/NetLogoPackaging.scala
@@ -64,7 +64,7 @@ object NetLogoPackaging {
     ).map(p => (" " ~> p))
     .getOrElse(Parser.failure("Add at least one JDK to '.jdks.yaml' at the root of the NetLogo repository."))
 
-  def settings(netlogo: Project, macApp: Project, behaviorsearchProject: Project): Seq[Setting[?]] = Seq(
+  def settings(netlogo: Project, behaviorsearchProject: Project): Seq[Setting[?]] = Seq(
     netLogoRoot     := (netlogo / baseDirectory).value,
     mathematicaRoot := netLogoRoot.value.getParentFile / "Mathematica-Link",
     configRoot      := baseDirectory.value / "configuration",
@@ -420,9 +420,6 @@ object NetLogoPackaging {
           , "-Dorg.nlogo.mac.appClassName=org.nlogo.app.App$"
           , "-Dapple.awt.application.appearance=system"
           )
-        , Seq()
-        , Some("netlogo-mac-app.jar")
-        , Some("org.nlogo.app.MacApplication")
         ) {
           override def name = s"NetLogo ${this.version}"
         }
@@ -434,9 +431,6 @@ object NetLogoPackaging {
           , "-Dorg.nlogo.mac.appClassName=org.nlogo.app.App$"
           , "-Dapple.awt.application.appearance=system"
           )
-        , Seq()
-        , Some("netlogo-mac-app.jar")
-        , Some("org.nlogo.app.MacApplication")
         ) {
           override def name = s"NetLogo 3D ${this.version}"
         }
@@ -447,10 +441,7 @@ object NetLogoPackaging {
             "-Xdock:name=HubNet"
           , "-Dorg.nlogo.mac.appClassName=org.nlogo.hubnet.client.App$"
           , "-Dapple.laf.useScreenMenuBar=true"
-         )
-        , Seq()
-        , Some("netlogo-mac-app.jar")
-        , Some("org.nlogo.app.MacApplication")
+          )
         ) {
           override def name = s"HubNet Client ${this.version}"
         }
@@ -469,12 +460,6 @@ object NetLogoPackaging {
       )
 
       val inputDir = JavaPackager.setupAppImageInput(log, version, buildJDK, buildDir, netLogoJar, dependencies)
-
-      val macAppMainJar = (macApp / Compile / packageBin).value
-      val macAppDeps = removeJdkLibraries((macApp / Runtime / dependencyClasspath).value).files
-      (macAppDeps :+ macAppMainJar).foreach( (dep) => {
-        FileActions.copyFile(dep, inputDir / dep.getName)
-      })
 
       val destDir = buildDir / s"${platform}-dest-${buildJDK.version}-${buildJDK.architecture}"
       FileActions.remove(destDir)
