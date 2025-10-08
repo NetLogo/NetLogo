141df642906be0384be0c2d76e216ab93b49ad68
TheBizzle <jason.bertsche@gmail.com>
2025-10-08 12:58:22 -0500

    Bugfix: Handle async code better in ExtMan
    We were using `Future` to get concurrency, but not ever really waiting
    for the `Future` to resolve.  Now, we thread it more fully through the
    call stack.  It's a bit messy, but closer to correct.
    
    Fixes #2567

--- netlogo-core/src/main/api/LibraryManager.scala
+++ netlogo-core/src/main/api/LibraryManager.scala
@@ -6,6 +6,9 @@ import java.io.File
 import java.net.URL
 import java.nio.file.{ Files, FileAlreadyExistsException, Path, Paths }
 
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
+
 import com.typesafe.config.{ Config, ConfigException, ConfigFactory, ConfigRenderOptions, ConfigValueFactory }
 
 import org.nlogo.core.LibraryInfo
@@ -19,53 +22,49 @@ object LibraryManager {
   private val libsLocation     = "libraries-location.conf"
   private val allLibsName      = "libraries.conf"
   private val bundledsConfig   = ConfigFactory.parseResources("system/bundled-libraries.conf")
-  private val metadataURL      = getMetadataURL()
 
-  private lazy val branchURL: URL = {
+  private val branchURLFuture = {
 
-    val baseURL =
-      if (LibraryInfoDownloader.enabled) {
+    val baseURLOptF =
+      if (LibraryInfoDownloader.enabled)
+        LibraryInfoDownloader(new URL(s"$libsLocationSite/$libsLocation")).map(
+          _.flatMap {
+            case (file, _) =>
+              Try(ConfigFactory.parseFile(file).getString("location")).toOption
+          }
+        )
+      else
+        Future(None)
 
-        val locationURL    = new URL(s"$libsLocationSite/$libsLocation")
-        LibraryInfoDownloader(locationURL)
-        val locationPath   = FileIO.perUserExtensionFile(libsLocation).toString
-        val locationConfig = ConfigFactory.parseFile(new File(locationPath))
-
-        try {
-          locationConfig.getString("location")
-        } catch {
-          case ex: ConfigException => bundledsConfig.getString("fallback-libraries-location")
-        }
-
-      } else {
-        bundledsConfig.getString("fallback-libraries-location")
-      }
-
-    new URL(s"$baseURL/refs/heads/${APIVersion.version}")
+    baseURLOptF.map {
+      opt =>
+        val baseURL = opt.getOrElse(bundledsConfig.getString("fallback-libraries-location"))
+        val url = new URL(s"$baseURL/refs/heads/${APIVersion.version}")
+        url
+    }
 
   }
 
-  private def getMetadataURL(): URL = {
-    new URL(s"${branchURL}/$allLibsName")
-  }
+  private val metadataURLFuture: Future[URL] = branchURLFuture.map(url => new URL(s"$url/$allLibsName"))
 
   private var loadedOnce = false
 
-  private def reloadMetadata(isFirstLoad: Boolean, complete: () => Unit): Unit = {
+  private def reloadMetadata(isFirstLoad: Boolean, complete: () => Unit): Future[Unit] = {
     // If not first load (user clicked a button) or metadata not loaded once, load it!
     // This is an attempt to avoid multiple redundant remote fetches during test runs.
     // -JeremyB April 2019
     if (!isFirstLoad || !loadedOnce) {
-      LibraryInfoDownloader.invalidateCache(metadataURL)
-      LibraryInfoDownloader(metadataURL).foreach {
-        case Some(file, rewrite) if rewrite =>
-          complete()
-
-        case _ =>
+      metadataURLFuture.map {
+        metadataURL =>
+          LibraryInfoDownloader.invalidateCache(metadataURL)
+          LibraryInfoDownloader(metadataURL).foreach {
+            case Some(file, rewrite) if rewrite => complete()
+            case _                              => ()
+          }
+          loadedOnce = true
       }
-      loadedOnce = true
     } else {
-      complete()
+      Future(complete())
     }
   }
 }
@@ -83,7 +82,6 @@ class LibraryManager(userExtPath: Path, unloadExtensions: () => Unit) extends Co
   private var infoChangeCallbacks = Seq[InfoChangeCallback]()
 
   val allLibsPath = FileIO.perUserExtensionFile(allLibsName).toString
-  val metadataURL = LibraryManager.metadataURL
 
   if (!Files.exists(Paths.get(userInstalledsPath)))
     // Concurrency during testing can cause the above check to be true but the exception to still be thrown,
@@ -125,7 +123,19 @@ class LibraryManager(userExtPath: Path, unloadExtensions: () => Unit) extends Co
     infoChangeCallbacks = infoChangeCallbacks :+ callback
   }
 
-  def updateLists(configFile: File, isFirstLoad: Boolean = false, useBundled: Boolean = true): Unit = {
+  def updateMetadata(): Future[Unit] = {
+    LibraryManager.metadataURLFuture.map {
+      metadataURL =>
+        LibraryInfoDownloader(metadataURL).flatMap {
+          _.flatMap {
+            case (file, true) => Option(updateLists(file))
+            case _            => None
+          }.getOrElse(Future.unit)
+        }
+    }
+  }
+
+  def updateLists(configFile: File, isFirstLoad: Boolean = false, useBundled: Boolean = true): Future[Unit] = {
 
     try {
 
@@ -135,48 +145,53 @@ class LibraryManager(userExtPath: Path, unloadExtensions: () => Unit) extends Co
       else
         ConfigFactory.parseFile(new File(userInstalledsPath))
 
-      updateList(config, installedLibsConf, "extensions", useBundled)
+      Future(updateList(config, installedLibsConf, "extensions", useBundled))
 
     } catch {
       case ex: ConfigException =>
         if (isFirstLoad)
           // In case only the local copy got messed up somehow -- EL 2018-06-02
-          LibraryInfoDownloader.invalidateCache(metadataURL)
+          LibraryManager.metadataURLFuture.map(url => LibraryInfoDownloader.invalidateCache(url))
         else
           throw new MetadataLoadingException(ex)
     }
 
   }
 
-  private def updateList(config: Config, installedLibsConf: Config, category: String, useBundled: Boolean) = {
+  private def updateList(config: Config, installedLibsConf: Config, category: String, useBundled: Boolean): Future[Unit] = {
 
     import scala.jdk.CollectionConverters.ListHasAsScala
 
     def getStringOption(c: Config, path: String, default: Option[String] = None): Option[String] =
       if (c.hasPath(path)) Option(c.getString(path)) else default
 
-    libraries =
-      config.getConfigList(category).asScala.map {
-        c =>
+    LibraryManager.branchURLFuture.map {
+      branchURL =>
 
-          val name        = c.getString("name")
-          val codeName    = c.getString("codeName")
-          val shortDesc   = c.getString("shortDescription")
-          val longDesc    = c.getString("longDescription")
-          val version     = c.getString("version")
-          val homepage    = new URL(c.getString("homepage"))
+        libraries =
+          config.getConfigList(category).asScala.map {
+            c =>
 
-          val installedVersionPath = s"""$category."$codeName".installedVersion"""
-          val installedVersion     = getStringOption(installedLibsConf, installedVersionPath)
-          val bundled              = useBundled && bundledsConfig.hasPath(installedVersionPath) && installedVersion.isEmpty
-          val minNetLogoVersion    = getStringOption(c, "minNetLogoVersion")
+              val name        = c.getString("name")
+              val codeName    = c.getString("codeName")
+              val shortDesc   = c.getString("shortDescription")
+              val longDesc    = c.getString("longDescription")
+              val version     = c.getString("version")
+              val homepage    = new URL(c.getString("homepage"))
 
-          LibraryInfo(name, codeName, shortDesc, longDesc, version, homepage, bundled, installedVersion,
-                      minNetLogoVersion, LibraryManager.branchURL)
+              val installedVersionPath = s"""$category."$codeName".installedVersion"""
+              val installedVersion     = getStringOption(installedLibsConf, installedVersionPath)
+              val bundled              = useBundled && bundledsConfig.hasPath(installedVersionPath) && installedVersion.isEmpty
+              val minNetLogoVersion    = getStringOption(c, "minNetLogoVersion")
 
-      }.toSeq
+              LibraryInfo(name, codeName, shortDesc, longDesc, version, homepage, bundled, installedVersion,
+                          minNetLogoVersion, branchURL)
 
-    infoChangeCallbacks.foreach(_.apply(libraries))
+          }.toSeq
+
+        infoChangeCallbacks.foreach(_.apply(libraries))
+
+    }
 
   }
 
--- netlogo-gui/src/main/app/tools/LibrariesDialog.scala
+++ netlogo-gui/src/main/app/tools/LibrariesDialog.scala
@@ -9,7 +9,7 @@ import javax.swing.border.EmptyBorder
 
 import scala.concurrent.ExecutionContext
 
-import org.nlogo.api.{ LibraryInfoDownloader, LibraryManager }
+import org.nlogo.api.LibraryManager
 import org.nlogo.core.{ I18N, Token }
 import org.nlogo.swing.{ CustomOptionPane, DialogButton, OptionPane, ProgressListener, ScrollPane, TextArea, Transparent }
 import org.nlogo.theme.{ InterfaceColors, ThemeSync }
@@ -73,15 +73,8 @@ class LibrariesDialog( parent:          Frame
       listener.start()
 
       import ExecutionContext.Implicits.global
-      LibraryInfoDownloader(manager.metadataURL).foreach {
-        pairOpt =>
-          pairOpt.foreach {
-            case (file, didRewrite) =>
-              if (didRewrite) {
-                manager.updateLists(file)
-              }
-          }
-          listener.finish()
+      manager.updateMetadata().foreach {
+        _ => listener.finish()
       }
 
       buttonPanel.getComponents.foreach(c => buttonPanel.remove(c))
