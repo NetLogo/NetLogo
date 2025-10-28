8c0139bba83a7e035791218151bad46de8a2bdb6
ERSUCC <isaacbrown5763@gmail.com>
2025-10-28 12:00:31 -0500

    Prevent None.get in user-yes-or-no? dialog
    Fixes #2577

--- netlogo-gui/src/main/prim/gui/_useryesorno.scala
+++ netlogo-gui/src/main/prim/gui/_useryesorno.scala
@@ -15,8 +15,8 @@ class _useryesorno extends Reporter {
       case gw: GUIWorkspace =>
         val yesNoMessage = args(0).report(context)
         gw.updateUI()
-        val result: java.lang.Boolean = workspace.waitForResult(
-          new ReporterRunnable[java.lang.Boolean] {
+        val result: Option[java.lang.Boolean] = workspace.waitForResult(
+          new ReporterRunnable[Option[java.lang.Boolean]] {
             override def run() = {
               gw.view.mouseDown(false)
               val response = new OptionPane(gw.getFrame, I18N.gui.get("dialog.userYesOrNo"),
@@ -26,11 +26,11 @@ class _useryesorno extends Reporter {
                                                 I18N.gui.get("common.buttons.halt")),
                                             OptionPane.Icons.Question).getSelectedIndex
               response match {
-                case 0 => java.lang.Boolean.TRUE
-                case 1 => java.lang.Boolean.FALSE
-                case _ => null
+                case 0 => Option(java.lang.Boolean.TRUE)
+                case 1 => Option(java.lang.Boolean.FALSE)
+                case _ => None
               }}})
-        Option(result).getOrElse(throw new HaltException(true))
+        result.getOrElse(throw new HaltException(true))
       case _ =>
         throw new RuntimePrimitiveException(
           context, this, "You can't get user input headless.")
