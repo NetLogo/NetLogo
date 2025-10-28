bde2dfbab03883caf9b316e897fdccd8bf14cf57
ERSUCC <isaacbrown5763@gmail.com>
2025-10-28 00:34:05 -0500

    Add round-trip GUI tests

---
+++ netlogo-core/src/test/util/GuiTest.scala
@@ -0,0 +1,9 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.util
+
+import org.scalatest.{ Tag => STTag }
+
+object GuiTest {
+  object Tag extends STTag("org.nlogo.util.GuiTestTag")
+}
