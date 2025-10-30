6d2258797887cad7df91038b03bdbcf565a2ebd6
ERSUCC <isaacbrown5763@gmail.com>
2025-10-30 01:38:54 -0500

    Add GUI menu action tests

--- netlogo-gui/src/main/window/JFXColorPicker.scala
+++ netlogo-gui/src/main/window/JFXColorPicker.scala
@@ -24,12 +24,12 @@ import org.nlogo.analytics.Analytics
 import org.nlogo.api.{ Color => NLColor }
 import org.nlogo.awt.EventQueue
 import org.nlogo.core.{ Color => CoreColor, I18N, LogoList }
-import org.nlogo.swing.Positioning
+import org.nlogo.swing.{ AutomateWindow, Positioning }
 import org.nlogo.theme.{ InterfaceColors, ThemeSync }
 
 class JFXColorPicker( frame: Frame, modal: Boolean, config: JFXCPConfig, initialValue: Option[NLColorValue] = None
                     , pickCallback: (String) => Unit = (_ => {}), cancelCallback: () => Unit = (() => {}))
-  extends JDialog(frame, I18N.gui.get("tools.colorpicker"), modal) with ThemeSync {
+  extends JDialog(frame, I18N.gui.get("tools.colorpicker"), modal) with ThemeSync with AutomateWindow {
 
   private val nlBabyMonitor = new Bridge
   private val panel         = new JFXPanel
