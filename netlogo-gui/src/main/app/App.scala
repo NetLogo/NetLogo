d9e3f3d4de2411d0ca1a9c0d6fe440760a4b7bdf
TheBizzle <jason.bertsche@gmail.com>
2025-11-05 11:18:07 -0600

    Whitespace: Align syntax

--- netlogo-gui/src/main/app/App.scala
+++ netlogo-gui/src/main/app/App.scala
@@ -459,7 +459,7 @@ class App extends org.nlogo.window.Event.LinkChild
       new ExtensionAssistant(
         frame
       , libMan.getExtensionInfos.map(_.codeName).toSet
-      , (name) => libMan.lookupExtension(name, "").fold("N/A")(_.version)
+      , (name)            => libMan.lookupExtension(name, "").fold("N/A")(_.version)
       , { (name, version) => libMan.lookupExtension(name, version).foreach(libMan.installExtension); compileLater() }
       )
     })
