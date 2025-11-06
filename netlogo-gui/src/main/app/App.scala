4cb9d423334c4025f6313d3080dde50277091daa
ERSUCC <isaacbrown5763@gmail.com>
2025-11-05 18:30:28 -0600

    Ensure the GUI tests work properly in sequence

--- netlogo-gui/src/main/app/App.scala
+++ netlogo-gui/src/main/app/App.scala
@@ -153,12 +153,10 @@ object App {
 
       val cmdArgs = processCommandLineArguments(args)
 
-      if (cmdArgs.automated) {
-        WindowAutomator.enableAutomation()
-        FileDialog.enableAutomation()
-        PrinterManager.enableAutomation()
-        BrowserLauncher.enableAutomation()
-      }
+      WindowAutomator.setAutomated(cmdArgs.automated)
+      FileDialog.setAutomated(cmdArgs.automated)
+      PrinterManager.setAutomated(cmdArgs.automated)
+      BrowserLauncher.setAutomated(cmdArgs.automated)
 
       SetSystemLookAndFeel.setSystemLookAndFeel()
 
@@ -188,6 +186,15 @@ object App {
     } catch {
       case ex: Throwable =>
         StartupError.report(ex)
+    }
+  }
+
+  // used by GUI tests to restore the default behavior in between suites (Isaac B 11/5/25)
+  def reset(): Unit = {
+    if (app != null) {
+      WindowAutomator.resetWindows()
+
+      app.frame.dispose()
     }
   }
 
@@ -416,7 +423,7 @@ class App(args: App.CommandLineArgs) extends LinkChild with Exceptions.Handler w
   }
 
   val fileManager = new FileManager(workspace, modelLoader, converter, dirtyMonitor, modelSaver, mainMenuBar,
-                                    frame, tabManager, workspaceFactory)
+                                    frame, tabManager, workspaceFactory, args.testing)
 
   private val recentFilesMenu = new RecentFilesMenu(frame, fileManager)
 
