39176b9f324d2cc79576289d9667e0ec7717c88e
ERSUCC <isaacbrown5763@gmail.com>
2025-11-05 18:30:28 -0600

    Ensure the GUI tests work properly in sequence

--- netlogo-gui/src/main/swing/WindowAutomator.scala
+++ netlogo-gui/src/main/swing/WindowAutomator.scala
@@ -10,8 +10,8 @@ object WindowAutomator {
   private var isAutomating: Boolean     = false
   private var windows:      Seq[Window] = Seq()
 
-  def enableAutomation(): Unit = {
-    isAutomating = true
+  def setAutomated(automated: Boolean): Unit = {
+    isAutomating = automated
   }
 
   // helper for GUI testing, prevents windows from blocking regardless of modality (Isaac B 10/29/25)
@@ -30,5 +30,11 @@ object WindowAutomator {
 
   def getVisibleWindows: Seq[Window] =
     windows.filter(_.isVisible)
+
+  def resetWindows(): Unit = {
+    windows.foreach(_.dispose())
+
+    windows = Seq()
+  }
 
 }
