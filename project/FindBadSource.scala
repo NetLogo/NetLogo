e65eaa440b92edcecf874bcc4080eeb47fb9e3b1
TheBizzle <jason.bertsche@gmail.com>
2025-11-13 12:50:44 -0600

    Infrastructure/Minor: Improve linter tool
    Co-authored-by: ERSUCC <isaacbrown5763@gmail.com>

--- project/FindBadSource.scala
+++ project/FindBadSource.scala
@@ -1,68 +1,49 @@
 // (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
 
 import java.io.File
+import java.nio.charset.MalformedInputException
 
 import sbt.TaskKey
 
 import scala.io.Source
+import scala.sys.process.Process
 
 object FindBadSource {
-  private val findBadSource = TaskKey[Unit]("findBadSource", "Search text files in the repo for general formatting issues")
+  private val findBadSource =
+    TaskKey[Unit]("findBadSource", "Search text files in the repo for general formatting issues")
 
   lazy val settings = {
     findBadSource := {
-      // probably there are a lot more that could be here
-      val extensions =
-        List("java", "scala", "py", "txt", "sh", "nlogo", "nlogo3d", "nlogox", "nlogox3d", "html", "css", "properties",
-             "md", "csv", "asc", "prj", "xml")
+      val rules = Seq("*.scala", "*.java", "*.html", "*.md", "*.mustache", "*.css", "*.js", ":!:*.min.js", "*.nlogox*")
 
-      def ignore(path: String): Boolean = {
-        path.contains("/extensions/gis/") ||
-        path.contains("/src_managed/") ||
-        path.contains("/tmp/") ||
-        path.endsWith("Lexer.java") ||
-        path.endsWith("net-logo-web.html") ||
-        path.startsWith("./.idea/") ||
-        path.startsWith("./docs/scaladoc/") ||
-        path.startsWith("./dist/i18n/") ||
-        !extensions.exists(path.endsWith)
-      }
+      Process(Seq("git", "ls-files", "--recurse-submodules", "--") ++ rules).!!
+        .split("\n").foreach { path =>
 
-      val directories =
-        List("extensions", "netlogo-gui", "netlogo-core", "netlogo-headless", "parser-core", "parser-js", "parser-jvm",
-             "test")
+        val source = Source.fromFile(path)
+        val contents = source.mkString
 
-      def recurseDirectories(dir: File): Unit = {
-        dir.listFiles.foreach { path =>
-          if (path.isDirectory) {
-            recurseDirectories(path)
-          } else if (!ignore(path.getAbsolutePath.replace("\\", "/"))) {
-            val source = Source.fromFile(path)
-            val contents = source.mkString
+        source.close()
 
-            source.close()
+        var problems = Seq[String]()
 
-            var problems = Seq[String]()
+        if (contents.nonEmpty && contents.last != '\n')
+          problems = problems :+ "Missing newline at eof"
 
-            if (contents.nonEmpty && contents.last != '\n')
-              problems = problems :+ "Missing newline at eof"
+        if (contents.contains('\r'))
+          problems = problems :+ "Carriage return character(s) found"
 
-            if (contents.contains('\r'))
-              problems = problems :+ "Carriage return character(s) found"
+        if (contents.contains('\t'))
+          problems = problems :+ "Tab character(s) found"
 
-            if (contents.contains('\t'))
-              problems = problems :+ "Tab character(s) found"
+        if (contents.split("\n").exists("\\s+$".r.findFirstIn(_).isDefined))
+          problems = problems :+ "Trailing whitespace found"
 
-            if (problems.nonEmpty) {
-              println(path)
+        if (problems.nonEmpty) {
+          println(path)
 
-              problems.foreach(p => println(s"  $p"))
-            }
-          }
+          problems.foreach(p => println(s"  $p"))
         }
       }
-
-      directories.foreach(dir => recurseDirectories(new File(dir)))
     }
   }
 }
