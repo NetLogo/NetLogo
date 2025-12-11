a1bb0ce607795e80da9470ffec2829d69ce45c1e
ERSUCC <isaacbrown5763@gmail.com>
2025-12-10 21:47:29 -0600

    Speed up shuffle primitive in headless
    Reduced runtime of RPS preview commands in headless from 100s to 1s

--- netlogo-headless/src/main/prim/etc/_shuffle.scala
+++ netlogo-headless/src/main/prim/etc/_shuffle.scala
@@ -2,23 +2,20 @@
 
 package org.nlogo.prim.etc
 
-import java.util.{ Collections, List => JList }
+import java.util.{ ArrayList, Collections }
 
 import org.nlogo.core.LogoList
 import org.nlogo.nvm.{ Context, Reporter }
 
-import scala.collection.mutable.ListBuffer
-import scala.jdk.CollectionConverters.BufferHasAsJava
-
 class _shuffle extends Reporter {
   def report(context: Context): AnyRef = {
-    val result: JList[AnyRef] = ListBuffer(argEvalList(context, 0).toVector*).asJava
+    val result = new ArrayList[AnyRef](argEvalList(context, 0).toJava)
     Collections.shuffle(result, context.job.random)
     LogoList.fromJava(result)
   }
 
   def report_1(context: Context, l0: LogoList): LogoList = {
-    val result: JList[AnyRef] = ListBuffer(l0.toVector*).asJava
+    val result = new ArrayList[AnyRef](l0.toJava)
     Collections.shuffle(result, context.job.random)
     LogoList.fromJava(result)
   }
