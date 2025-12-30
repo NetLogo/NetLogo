00e9f64feee74a05f93b1c646a566c6eb760a403
ERSUCC <isaacbrown5763@gmail.com>
2025-12-30 14:16:55 -0600

    Prevent NumberFormatException when attempting to approximate Infinity or NaN

--- netlogo-core/src/main/api/Approximate.scala
+++ netlogo-core/src/main/api/Approximate.scala
@@ -4,16 +4,28 @@ package org.nlogo.api
 
 object Approximate {
   def approximate(n: Double, places: Int): Double = {
-    if (n >= 0) {
+    if (!n.isFinite) {
+      n
+    } else if (n >= 0) {
       BigDecimal(n).setScale(places, BigDecimal.RoundingMode.HALF_UP).toDouble
     } else {
       BigDecimal(n).setScale(places, BigDecimal.RoundingMode.HALF_DOWN).toDouble
     }
   }
 
-  def approximateCeiling(n: Double, places: Int): Double =
-    BigDecimal(n).setScale(places, BigDecimal.RoundingMode.CEILING).toDouble
+  def approximateCeiling(n: Double, places: Int): Double = {
+    if (!n.isFinite) {
+      n
+    } else {
+      BigDecimal(n).setScale(places, BigDecimal.RoundingMode.CEILING).toDouble
+    }
+  }
 
-  def approximateFloor(n: Double, places: Int) =
-    BigDecimal(n).setScale(places, BigDecimal.RoundingMode.FLOOR).toDouble
+  def approximateFloor(n: Double, places: Int) = {
+    if (!n.isFinite) {
+      n
+    } else {
+      BigDecimal(n).setScale(places, BigDecimal.RoundingMode.FLOOR).toDouble
+    }
+  }
 }
