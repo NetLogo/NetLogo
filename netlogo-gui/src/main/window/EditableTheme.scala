4978565f83778d5e32556f334c10e91ffc415d15
ERSUCC <isaacbrown5763@gmail.com>
2026-02-02 13:24:13 -0600

    Refactor: Add color group names to I18N

--- netlogo-gui/src/main/window/EditableTheme.scala
+++ netlogo-gui/src/main/window/EditableTheme.scala
@@ -13,102 +13,104 @@ class EditableColor(val key: String, var value: Color) {
 }
 
 trait EditableTheme(base: ColorTheme) {
+  private implicit val i18nPrefix: I18N.Prefix = I18N.Prefix("menu.tools.themeEditor")
+
   protected var name: String = base.name
   protected var isDark: Boolean = base.isDark
 
   protected val colorGroups: Seq[(String, Seq[EditableColor])] = Seq(
-    "Menu" -> Seq(
+    I18N.gui("menuGroup") -> Seq(
       "menuBarBorder", "menuBackground", "menuBackgroundHover", "menuTextHover", "menuTextDisabled", "menuBorder"
     ),
-    "Dialog" -> Seq(
+    I18N.gui("dialogGroup") -> Seq(
       "dialogBackground", "dialogBackgroundSelected", "dialogText", "dialogTextSelected", "primaryButtonBackground",
       "primaryButtonBackgroundHover", "primaryButtonBackgroundPressed", "primaryButtonText", "primaryButtonBorder",
       "secondaryButtonBackground", "secondaryButtonBackgroundHover", "secondaryButtonBackgroundPressed",
       "secondaryButtonText", "secondaryButtonBorder", "infoIcon", "warningIcon", "errorIcon", "updateIcon"
     ),
-    "Tabs" -> Seq(
+    I18N.gui("tabsGroup") -> Seq(
       "tabBackground", "tabBackgroundHover", "tabBackgroundSelected", "tabBackgroundError", "tabText",
       "tabTextSelected", "tabTextError", "tabBorder", "tabSeparator", "tabCloseButtonBackgroundHover"
     ),
-    "Toolbar" -> Seq(
+    I18N.gui("toolbarGroup") -> Seq(
       "toolbarBackground", "toolbarText", "toolbarTextSelected", "toolbarControlBackground",
       "toolbarControlBackgroundHover", "toolbarControlBackgroundPressed", "toolbarControlBorder",
       "toolbarControlBorderSelected", "toolbarControlFocus", "toolbarToolSelected", "toolbarImage",
       "toolbarImageSelected", "toolbarImageDisabled", "toolbarSeparator"
     ),
-    "Scroll Bar" -> Seq(
+    I18N.gui("scrollBarGroup") -> Seq(
       "scrollBarBackground", "scrollBarForeground", "scrollBarForegroundHover"
     ),
-    "Checkbox" -> Seq(
+    I18N.gui("checkboxGroup") -> Seq(
       "checkboxBackgroundUnselected", "checkboxBackgroundUnselectedHover", "checkboxBackgroundSelected",
       "checkboxBackgroundSelectedHover", "checkboxBackgroundDisabled", "checkboxBorder", "checkboxCheck"
     ),
-    "Radio Button" -> Seq(
+    I18N.gui("radioButtonGroup") -> Seq(
       "radioButtonBackground", "radioButtonBackgroundHover", "radioButtonSelected", "radioButtonSelectedHover",
       "radioButtonBorder"
     ),
-    "Text Area" -> Seq(
+    I18N.gui("textAreaGroup") -> Seq(
       "textAreaBackground", "textAreaText", "textAreaBorderEditable", "textAreaBorderNoneditable"
     ),
-    "Command Center" -> Seq(
+    I18N.gui("commandCenterGroup") -> Seq(
       "commandCenterBackground", "commandCenterText", "locationToggleImage", "commandOutputBackground"
     ),
-    "Widgets" -> Seq(
+    I18N.gui("widgetsGroup") -> Seq(
       "widgetText", "widgetTextError", "widgetHoverShadow", "widgetPreviewCover", "widgetPreviewCoverNote",
       "widgetHandle", "displayAreaBackground", "displayAreaText"
     ),
-    "Button Widget" -> Seq(
+    I18N.gui("buttonWidgetGroup") -> Seq(
       "buttonBackground", "buttonBackgroundHover", "buttonBackgroundPressed", "buttonBackgroundPressedHover",
       "buttonBackgroundDisabled", "buttonText", "buttonTextPressed", "buttonTextDisabled"
     ),
-    "Slider Widget" -> Seq(
+    I18N.gui("sliderWidgetGroup") -> Seq(
       "sliderBackground", "sliderBarBackground", "sliderBarBackgroundFilled", "sliderThumbBackground",
       "sliderThumbBackgroundPressed", "sliderThumbBorder"
     ),
-    "Switch Widget" -> Seq(
+    I18N.gui("switchWidgetGroup") -> Seq(
       "switchBackground", "switchToggle", "switchToggleBackgroundOn", "switchToggleBackgroundOff"
     ),
-    "Chooser Widget" -> Seq(
+    I18N.gui("chooserWidgetGroup") -> Seq(
       "chooserBackground", "chooserBorder"
     ),
-    "Input Widget" -> Seq(
+    I18N.gui("inputWidgetGroup") -> Seq(
       "inputBackground", "inputBorder"
     ),
-    "View Widget" -> Seq(
+    I18N.gui("viewWidgetGroup") -> Seq(
       "viewBackground", "viewBorder"
     ),
-    "Monitor Widget" -> Seq(
+    I18N.gui("monitorWidgetGroup") -> Seq(
       "monitorBackground", "monitorBorder"
     ),
-    "Plot Widget" -> Seq(
+    I18N.gui("plotWidgetGroup") -> Seq(
       "plotBackground", "plotBorder", "plotMouseBackground", "plotMouseText"
     ),
-    "Output Widget" -> Seq(
+    I18N.gui("outputWidgetGroup") -> Seq(
       "outputBackground", "outputBorder"
     ),
-    "Interface Tab" -> Seq(
+    I18N.gui("interfaceTabGroup") -> Seq(
       "interfaceBackground", "splitPaneDividerBackground", "speedSliderBarBackground",
       "speedSliderBarBackgroundFilled", "speedSliderThumb", "speedSliderThumbDisabled"
     ),
-    "Info Tab" -> Seq(
+    I18N.gui("infoTabGroup") -> Seq(
       "infoBackground", "infoH1Background", "infoH1Color", "infoH2Background", "infoH2Color", "infoH3Color",
       "infoH4Color", "infoPColor", "infoCodeBackground", "infoCodeText", "infoBlockBar", "infoLink"
     ),
-    "Code Tab" -> Seq(
+    I18N.gui("codeTabGroup") -> Seq(
       "checkFilled", "errorLabelText", "errorLabelBackground", "warningLabelText", "warningLabelBackground",
       "errorHighlight", "codeBackground", "codeLineHighlight", "codeBracketHighlight", "codeSelection",
       "codeSeparator", "literalToken", "commandToken", "reporterToken", "keywordToken", "commentToken", "defaultToken"
     ),
-    "System Dynamics" -> Seq(
+    I18N.gui("systemDynamicsGroup") -> Seq(
       "stockBackground", "converterBackground"
     ),
-    "Announcement Banner" -> Seq(
+    I18N.gui("announcementBannerGroup") -> Seq(
       "announceX", "announceXHovered", "announceXPressed", "announceRelease", "announceAdvisory", "announceEvent"
     ),
-    "Color Picker" -> Seq(
+    I18N.gui("colorPickerGroup") -> Seq(
       "colorPickerOutputBackground", "colorPickerCheckmark", "colorPickerCopyHover"
     ),
-    "Agent Monitors" -> Seq(
+    I18N.gui("agentMonitorGroup") -> Seq(
       "agentMonitorSeparator"
     )
   ).map((name, values) => (name, values.map(key => new EditableColor(key, base.colors(key)))))
