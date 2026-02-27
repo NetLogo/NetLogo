a952a8658b0b8123a2d25b977ec10fdd81eb3f5b
ERSUCC <isaacbrown5763@gmail.com>
2026-02-26 22:48:43 -0600

    Bugfix: Build manual PDF correctly in Windows packaging workflow

--- project/Docs.scala
+++ project/Docs.scala
@@ -26,9 +26,7 @@ object Docs {
       (Compile / doc).value
     },
     manualPDF := {
-      Process(Seq("yarn", "run", "init"), helioRoot.value).!
-      Process(Seq("yarn", "run", "docs:build"), docsSource.value, "HELIO_HEADLESS" -> "1").!
-      Process(Seq("yarn", "run", "docs:generate-manual"), docsSource.value).!
+      Process(Seq("bash", "scripts/release.sh"), helioRoot.value, "HELIO_HEADLESS" -> "1").!
 
       val manualSource = (docsSource.value / ".build" / "NetLogo_User_Manual.pdf").toPath
       val manualDest = (baseDirectory.value / "NetLogo_User_Manual.pdf").toPath
