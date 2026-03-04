bfd5da7e2f4cef84833e26831d330440b36a2a7b
ERSUCC <isaacbrown5763@gmail.com>
2026-03-03 19:59:26 -0600

    Enhancement: Match command history button with rest of GUI
    Also add some space on either side of the Command Center output box

--- netlogo-gui/src/main/app/common/HistoryPrompt.scala
+++ netlogo-gui/src/main/app/common/HistoryPrompt.scala
@@ -4,68 +4,55 @@ package org.nlogo.app.common
 
 // pulled this out of CommandLine.java so I could translate it separately to Scala - ST 8/19/10
 
-import java.awt.Insets
-import java.awt.event.{ ActionEvent, MouseAdapter, MouseEvent }
-import javax.swing.{ AbstractAction, JButton }
+import java.awt.Dimension
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
 
-import org.nlogo.awt.Mouse
-import org.nlogo.core.{ AgentKind, I18N }
-import org.nlogo.swing.{ MenuItem, PopupMenu, Utils }
+import org.nlogo.core.I18N
+import org.nlogo.swing.{ Button, CollapsibleArrow, MenuItem, PopupMenu }
 
-class HistoryPrompt(commandLine: CommandLine) extends JButton {
+class HistoryPrompt(commandLine: CommandLine) extends Button(null) {
+  setAction(new AbstractAction {
+    override def actionPerformed(e: ActionEvent): Unit = {
+      doPopupMenu()
+    }
+  })
 
-  locally {
-    val arrowIcon = Utils.icon("/images/popup.gif")
-    setIcon(arrowIcon)
-    setDisabledIcon(arrowIcon)
+  setIcon(new CollapsibleArrow(true))
+
+  override def getPreferredSize: Dimension = {
+    val height = commandLine.getPreferredSize.height
+
+    new Dimension(height + 3, height)
   }
-  setEnabled(false)
-  setOpaque(true) // needed as of quaqua 3.4.1 - ST 10/4/05
-  setFont(getFont.deriveFont(9f)) // play nice with zoomer
-  addMouseListener(
-    new MouseAdapter {
-      override def mousePressed(e: MouseEvent): Unit = {
-        if(!e.isPopupTrigger && Mouse.hasButton1(e))
-          doPopupMenu()}})
-  lazy val isMac = System.getProperty("os.name").startsWith("Mac")
-
-  override def getInsets = new Insets(4, 4, 4, 4)  // ad hoc - ST 11/24/04
 
   private def doPopupMenu(): Unit = {
-    val popMenu = new PopupMenu(I18N.gui.get("tabs.run.commandcenter.history"))
+    val menu = new PopupMenu(I18N.gui.get("tabs.run.commandcenter.history"))
 
-    for(ex <- commandLine.getExecutionList) {
-      val str =
-        if(commandLine.agent != null)          // if we're in an agent monitor
-          CommandLine.PROMPT + " " + ex.string
-        else {
-          val prompt = ex.agentClass match {
-            case AgentKind.Observer => CommandLine.OBSERVER_PROMPT
-            case AgentKind.Turtle   => CommandLine.TURTLE_PROMPT
-            case AgentKind.Patch    => CommandLine.PATCH_PROMPT
-            case AgentKind.Link     => CommandLine.LINK_PROMPT
+    if (commandLine.getExecutionList.isEmpty) {
+      menu.add(new MenuItem(I18N.gui.get("tabs.run.commandcenter.nohistory"))).setEnabled(false)
+    } else {
+      commandLine.getExecutionList.foreach { command =>
+        val prompt = {
+          if (commandLine.agent != null) { // if we're in an agent monitor
+            CommandLine.PROMPT
+          } else {
+            command.getPrompt
           }
-          prompt + " " + ex.string
         }
-      popMenu.add(new MenuItem(new AbstractAction(str) {
-        def actionPerformed(e: ActionEvent): Unit = {
-          commandLine.setExecutionString(ex)
+
+        menu.add(new MenuItem(s"$prompt ${command.string}", () => {
+          commandLine.setExecutionString(command)
           commandLine.requestFocus()
-        }
-      }))
+        }))
+      }
+
+      menu.addSeparator()
+      menu.add(new MenuItem(I18N.gui.get("tabs.run.commandcenter.useArrowKeys"))).setEnabled(false)
+      menu.addSeparator()
+      menu.add(new MenuItem(I18N.gui.get("tabs.run.commandcenter.clearHistory"), () => commandLine.clearList()))
     }
-    if (commandLine.getExecutionList.isEmpty)
-      popMenu.add(new MenuItem(I18N.gui.get("tabs.run.commandcenter.nohistory"))).setEnabled(false)
-    else {
-      popMenu.addSeparator()
-      popMenu.add(new MenuItem(I18N.gui.get("tabs.run.commandcenter.useArrowKeys"))).setEnabled(false)
-      popMenu.addSeparator()
-      popMenu.add(new MenuItem(new AbstractAction(I18N.gui.get("tabs.run.commandcenter.clearHistory")) {
-        def actionPerformed(e: ActionEvent): Unit = {
-          commandLine.clearList()
-        }
-      }))
-    }
-    popMenu.show(this, getWidth / 2, getHeight / 2)
+
+    menu.show(this, 0, getHeight)
   }
 }
