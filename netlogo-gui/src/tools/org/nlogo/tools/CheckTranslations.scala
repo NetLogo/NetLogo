1281225db88aebef39640a3cfc678304aedd35c5
ERSUCC <isaacbrown5763@gmail.com>
2026-06-16 12:39:00 -0500

    Infrastructure/Bugfix: Include shared strings and error strings in checkTranslations

--- netlogo-gui/src/tools/org/nlogo/tools/CheckTranslations.scala
+++ netlogo-gui/src/tools/org/nlogo/tools/CheckTranslations.scala
@@ -16,13 +16,31 @@ object CheckTranslations {
       return
     }
 
-    val englishKeys: Set[String] = I18N.gui.getBundle(Locale.US).getKeys.asScala.toSet
+    val englishGUI: Set[String] = keySet(I18N.gui, Locale.US)
+    val englishShared: Set[String] = keySet(I18N.shared, Locale.US)
+    val englishErrors: Set[String] = keySet(I18N.errors, Locale.US)
 
     args.foreach { lang =>
-      val missingKeys = englishKeys &~ I18N.gui.getBundle(new Locale(lang)).getKeys.asScala.toSet
+      val locale = new Locale(lang)
 
-      if (missingKeys.nonEmpty)
-        println(s"Language $lang is missing translations for the following keys:\n  ${missingKeys.toSeq.sorted.mkString("\n  ")}")
+      val missingGUI: Set[String] = englishGUI &~ keySet(I18N.gui, locale)
+      val missingShared: Set[String] = englishShared &~ keySet(I18N.shared, locale)
+      val missingErrors: Set[String] = englishErrors &~ keySet(I18N.errors, locale)
+
+      if (missingGUI.nonEmpty || missingShared.nonEmpty || missingErrors.nonEmpty) {
+        println(s"Language $lang is missing translations for the following keys:")
+        printKeys(missingGUI, "(GUI) ")
+        printKeys(missingShared, "(Shared) ")
+        printKeys(missingErrors, "(Errors) ")
+      }
     }
+  }
+
+  private def keySet(bundle: I18N.BundleKind, locale: Locale): Set[String] =
+    bundle.getBundle(locale).getKeys.asScala.toSet
+
+  private def printKeys(keys: Set[String], prefix: String): Unit = {
+    if (keys.nonEmpty)
+      println(keys.mkString(s"  $prefix", s"\n  $prefix", ""))
   }
 }
