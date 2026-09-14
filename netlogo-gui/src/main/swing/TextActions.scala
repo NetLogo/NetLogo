dc6ab454cf72396c14de61f738b79ee7ff06da9f
ERSUCC <isaacbrown5763@gmail.com>
2026-09-14 17:43:01 -0500

    Bugfix: Standardize behavior of prev/next word shortcuts

--- netlogo-gui/src/main/swing/TextActions.scala
+++ netlogo-gui/src/main/swing/TextActions.scala
@@ -25,11 +25,67 @@ object TextActions {
     c.isLetterOrDigit || "_.?=*!<>:#+/%$^'&-".contains(c)
 
   abstract class ExtendedTextAction(name: String, comp: JTextComponent, select: Boolean) extends TextAction(name) {
-    protected def setCaret(pos: Int): Unit = {
-      if (select) {
-        comp.moveCaretPosition(pos)
+    protected var text: String = ""
+
+    // getText makes a copy of the entire text, so each text action caches it before doing all of its computation to
+    // prevent unnecessary repeated copying of potentially large strings. (Isaac B 9/14/26)
+    override def actionPerformed(e: ActionEvent): Unit = {
+      text = comp.getText
+
+      performAction()
+    }
+
+    protected def prevChar: Option[Char] = {
+      val caret: Int = comp.getCaretPosition
+
+      if (caret > 0) {
+        Option(text(caret - 1))
       } else {
-        comp.setCaretPosition(pos)
+        None
+      }
+    }
+
+    protected def nextChar: Option[Char] = {
+      val caret: Int = comp.getCaretPosition
+
+      if (caret < text.size) {
+        Option(text(caret))
+      } else {
+        None
+      }
+    }
+
+    protected def prevCharType: Option[CharType] =
+      prevChar.map(CharType(_))
+
+    protected def nextCharType: Option[CharType] =
+      nextChar.map(CharType(_))
+
+    protected def moveCaret(offset: Int): Unit = {
+      if (select) {
+        comp.moveCaretPosition(comp.getCaretPosition + offset)
+      } else {
+        comp.setCaretPosition(comp.getCaretPosition + offset)
+      }
+    }
+
+    protected def performAction(): Unit
+
+    protected sealed abstract trait CharType
+
+    protected object CharType {
+      case object Ident extends CharType
+      case object Plain extends CharType
+      case object Space extends CharType
+
+      def apply(char: Char): CharType = {
+        if (char.isWhitespace) {
+          CharType.Space
+        } else if (isIdentChar(char)) {
+          CharType.Ident
+        } else {
+          CharType.Plain
+        }
       }
     }
   }
@@ -37,15 +93,13 @@ object TextActions {
   class CorrectPreviousWordAction(comp: JTextComponent, select: Boolean)
     extends ExtendedTextAction("previous word", comp, select) {
 
-    def actionPerformed(e: ActionEvent): Unit = {
-      if (comp.getCaretPosition > 0) {
-        val text = comp.getText
+    override def performAction(): Unit = {
+      while (prevChar.exists(_.isWhitespace))
+        moveCaret(-1)
 
-        while (comp.getCaretPosition > 0 && !isIdentChar(text(comp.getCaretPosition - 1)))
-          setCaret(comp.getCaretPosition - 1)
-
-        while (comp.getCaretPosition > 0 && isIdentChar(text(comp.getCaretPosition - 1)))
-          setCaret(comp.getCaretPosition - 1)
+      prevCharType.foreach { tpe =>
+        while (prevCharType.contains(tpe))
+          moveCaret(-1)
       }
     }
   }
@@ -53,65 +107,71 @@ object TextActions {
   class CorrectNextWordAction(comp: JTextComponent, select: Boolean)
     extends ExtendedTextAction("next word", comp, select) {
 
-    def actionPerformed(e: ActionEvent): Unit = {
-      val text = comp.getText
+    override def performAction(): Unit = {
+      while (nextChar.exists(_.isWhitespace))
+        moveCaret(1)
 
-      if (comp.getCaretPosition < text.size) {
-        while (comp.getCaretPosition < text.size && !isIdentChar(text(comp.getCaretPosition)))
-          setCaret(comp.getCaretPosition + 1)
-
-        while (comp.getCaretPosition < text.size && isIdentChar(text(comp.getCaretPosition)))
-          setCaret(comp.getCaretPosition + 1)
+      nextCharType.foreach { tpe =>
+        while (nextCharType.contains(tpe))
+          moveCaret(1)
       }
     }
   }
 
-  class CorrectSelectWordAction(comp: JTextComponent) extends TextAction("select word") {
-    def actionPerformed(e: ActionEvent): Unit = {
-      val text = comp.getText
+  class CorrectSelectWordAction(comp: JTextComponent) extends ExtendedTextAction("select word", comp, true) {
+    override def performAction(): Unit = {
+      val prev: Option[Char] = prevChar
+      val next: Option[Char] = nextChar
 
-      if (text.nonEmpty) {
-        val start = isIdentChar(text(comp.getCaretPosition.min(text.size - 1))) ||
-                    isIdentChar(text((comp.getCaretPosition - 1).max(0)))
+      // this defines a hierarchy of selection choices that attempts to find the thing that the user most likely wants
+      // to select, which is especially important when the action is activated with the caret on a boundary between
+      // character types. if there's a word character on either side of the caret, select the word it belongs to. if
+      // neither side has a word character but there is another non-space character on either side, select the sequence
+      // of non-word, non-space characters it belongs to. otherwise, select the sequence of whitespace characters
+      // surrounding the caret. (Isaac B 9/14/26)
+      val selectType: CharType = prev.filter(isIdentChar).orElse(next.filter(isIdentChar))
+                                     .orElse(prev.filterNot(_.isWhitespace)).orElse(next.filterNot(_.isWhitespace))
+                                     .fold(CharType.Space)(CharType(_))
 
-        while (comp.getCaretPosition > 0 && isIdentChar(text(comp.getCaretPosition - 1)) == start)
-          comp.setCaretPosition(comp.getCaretPosition - 1)
+      while (prevCharType.contains(selectType))
+        comp.setCaretPosition(comp.getCaretPosition - 1)
 
-        while (comp.getCaretPosition < text.size && isIdentChar(text(comp.getCaretPosition)) == start)
-          comp.moveCaretPosition(comp.getCaretPosition + 1)
-      }
+      while (nextCharType.contains(selectType))
+        moveCaret(1)
     }
   }
 
-  class CorrectDeletePrevWordAction(comp: JTextComponent) extends TextAction("delete previous word") {
-    def actionPerformed(e: ActionEvent): Unit = {
-      if (comp.getCaretPosition > 0) {
-        val text = comp.getText
-        val start = isIdentChar(text(comp.getCaretPosition - 1))
+  class CorrectDeletePrevWordAction(comp: JTextComponent)
+    extends ExtendedTextAction("delete previous word", comp, true) {
 
-        while (comp.getCaretPosition > 0 && isIdentChar(text(comp.getCaretPosition - 1)) == start)
-          comp.moveCaretPosition(comp.getCaretPosition - 1)
+    override def performAction(): Unit = {
+      if (prevChar.exists(_.isWhitespace))
+        moveCaret(-1)
 
-        comp.replaceSelection(null)
-        comp.setCaretPosition(comp.getCaretPosition)
+      prevCharType.foreach { tpe =>
+        while (prevCharType.contains(tpe))
+          moveCaret(-1)
       }
+
+      comp.replaceSelection(null)
+      comp.setCaretPosition(comp.getCaretPosition)
     }
   }
 
-  class CorrectDeleteNextWordAction(comp: JTextComponent) extends TextAction("delete next word") {
-    def actionPerformed(e: ActionEvent): Unit = {
-      val text = comp.getText
+  class CorrectDeleteNextWordAction(comp: JTextComponent) extends ExtendedTextAction("delete next word", comp, true) {
+    override def performAction(): Unit = {
+      val start: Int = comp.getCaretPosition
 
-      if (comp.getCaretPosition < text.size) {
-        val startPos = comp.getCaretPosition
-        val start = isIdentChar(text(startPos))
+      if (nextChar.exists(_.isWhitespace))
+        moveCaret(1)
 
-        while (comp.getCaretPosition < text.size && isIdentChar(text(comp.getCaretPosition)) == start)
-          comp.moveCaretPosition(comp.getCaretPosition + 1)
-
-        comp.replaceSelection(null)
-        comp.setCaretPosition(startPos)
+      nextCharType.foreach { tpe =>
+        while (nextCharType.contains(tpe))
+          moveCaret(1)
       }
+
+      comp.replaceSelection(null)
+      comp.setCaretPosition(start)
     }
   }
 
